6ee87c474d28d90c908c5533fe1e289e0b11e76d
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2012-11-08 09:52:24 +0100

    Moved things around according to new folder structure. Of course, nothing works now, we have to readapt all imports.

--- manage.py
+++ manage.py
@@ -1,10 +1,1 @@
-#!/usr/bin/env python
-import os
-import sys
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aidasrv.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+aida/djsite/manage.py

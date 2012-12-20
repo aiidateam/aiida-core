0be5ffdc03c683476ebee2000d29805408df7a94
Boris Kozinsky <Boris.Kozinsky@bosch.com>
2012-12-20 01:45:48 -0500

    Adjusted the models2 file again.

--- aida/djsite/main/models2.py
+++ aida/djsite/main/models2.py
@@ -11,7 +11,7 @@ from aida.djsite.settings.settings import LOCAL_REPOSITORY
 
 class User(AuthUser):
     '''
-    Need to extend the User class with uuid, and add user-computer-username field
+    Need to extend the User class with uuid
     '''
     uuid = UUIDField(auto=True)
         
@@ -28,19 +28,19 @@ class NameClass(BaseClass):
     def __unicode__(self):
         return self.title
     
-qualitychoice = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))     
+quality_choice = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))     
 
  
 class EntityClass(BaseClass):
     crtime = m.DateTimeField(auto_now_add=True, editable=False)
     modtime = m.DateTimeField(auto_now=True)
     user = m.ForeignKey(User)
-    quality = m.IntegerField(choices=qualitychoice, null=True, blank=True)  
+    quality = m.IntegerField(choices=quality_choice, null=True, blank=True)  
     metadata = m.TextField(blank=True)
     class Meta:
         abstract = True
                 
-        
+
 class CommentClass(EntityClass):
     parent = m.ForeignKey('self', blank=True, null=True)
     class Meta:
@@ -49,107 +49,94 @@ class CommentClass(EntityClass):
 #-------------------------- Primary Classes ---------------------------------
 
 #################################################################
-class Calculation(EntityClass):
+calcstate_choice = (('prepared', 'prepared'),
+                    ('submitted', 'submitted'), 
+                    ('ququed', 'queued'),
+                    ('running', 'runnning'),
+                    ('failed', 'failed'),
+                    ('finished', 'finished'),
+                    ('completed', 'completed'))
+
+class Calc(EntityClass):
     code = m.ForeignKey('Code')
-    project = m.ForeignKey('Project')
-    state = m.ForeignKey('CalculationState')
-    type = m.ForeignKey('CalculationType')
+    state = m.CharField(max_length=255, choices=calcstate_choice, db_index=True)
+    type = m.ForeignKey('CalcType')
     dataout = m.ManyToManyField('Data', blank=True)   
-    attr = m.ManyToManyField('CalculationAttr', through = 'CalculationAttrVal')
-    group = m.ManyToManyField('CalculationGroup', blank=True)
+    attr = m.ManyToManyField('CalcAttr', through = 'CalcAttrVal')
+    group = m.ManyToManyField('CalcGroup', blank=True)
 
-
-class CalculationGroup(BaseClass):  
-    type = m.ForeignKey('CalculationGroupType')
-
-
-class CalculationGroupType(NameClass):
+class CalcType(NameClass):   
     '''
-    Groups of calculations can be: projects, collections, or workflows
-    ''' 
-    pass
-    
-
-class CalculationState(NameClass):     
-    '''
-    Calculation state can be: prepared, submitted, queued, running, failed, finished, completed.
-    '''
-    pass
-
-
-class CalculationType(NameClass):   
-    '''
-    Calculation type may be: spectrum, energy etc or just a blank link.
+    Calc type may be: spectrum, energy, md, etc or just a blank link.
     This is for user convenience.
     '''
     pass 
 
+calcgrouptype_choice = (('project', 'project'), ('collection', 'collection'), ('workflow', 'workflow'))
 
-class CalculationComment(CommentClass):
+class CalcGroup(BaseClass):  
+    type =  m.CharField(max_length=255, choices=calcgrouptype_choice, db_index=True)
+    
+
+class CalcComment(CommentClass):
     '''
     Convenience feature
     '''
     pass
 
 
-class CalculationAttr(NameClass):
+class CalcAttr(NameClass):
     '''
-    Attributes are convenience ONLY for storing metadata and tagging.
+    Attributes are annotations ONLY for storing metadata and tagging. This is only for querying convenience.
     Actual input and output data should never go here.
     '''
-    pass 
+    isunique = m.BooleanField()
 
 
-class CalculationAttrVal(m.Model):
-    item = m.ForeignKey('Calculation')
-    attr = m.ForeignKey('CalculationAttr')
+class CalcAttrVal(EntityClass):
+    item = m.ForeignKey('Calc')
+    attr = m.ForeignKey('CalcAttr')
     txtval = m.TextField()
     numval = m.FloatField()
-    time = m.DateTimeField(auto_now=True)
-    class Meta:
-        unique_together=('item','attr')
 
 ##############################################################
 
 class Data(EntityClass):
-    calculation = m.ForeignKey('Calculation', related_name='datain')      #unique parent calculation for each data
+    calc = m.ForeignKey('Calc', related_name='datain')      #unique parent Calc for each data
     type = m.ForeignKey('DataType')
-    element = m.ManyToManyField('Element')
     numval = m.FloatField()
     txtval = m.TextField()
-    path = m.TextField(null=True, blank=True)
+    isfile = m.BooleanField()
+    isdone = m.BooleanField()
+    group = m.ManyToManyField('DataGroup', blank=True)
+    linkdata = m.ManyToManyField('self')
     attr = m.ManyToManyField('DataAttr', through = 'DataAttrVal')
 
-        
-class DataGroup(BaseClass):  
-    type = m.ForeignKey('DataGroupType')
+#datagrouptype_choice = (('collection', 'collection'), ('relation', 'relation'))        
 
-class DataGroupType(NameClass):
-    '''
-    Data group types can be: collection or relation (data objects linked logically)
-    '''
+class DataGroup(BaseClass):
+    #type = m.CharField(max_length=255, choices=datagrouptype_choice)
     pass
 
-class DataType(NameClass):
+class DataType(EntityClass):
     '''
-    Data types can be: energy, spectrucm, density, structure, potential, etc
+    Data types can be: energy, spectrucm, density, structure, potential, file directory, etc
     '''
-    format = m.TextField() 
+
 
 class DataComment(CommentClass):
     pass
 
-class DataAttr(EntityClass):   
-    pass
 
-class DataAttrVal(m.Model):
+class DataAttr(NameClass):   
+    isunique = m.BooleanField()
+
+
+class DataAttrVal(EntityClass):
     item = m.ForeignKey('Data')
     attr = m.ForeignKey('DataAttr')
     txtval = m.TextField()
     numval = m.FloatField()
-    time = m.DateTimeField(auto_now=True)
-    class Meta:
-        unique_together=('item','attr')
 
 #############################################################
 
@@ -169,30 +156,31 @@ class Computer(NameClass):
     hostname = m.CharField(max_length=255, unique=True)
     workdir = m.CharField(max_length=255)
     
-class ComputerUsername(m.Model):
-    """Association of aida users with given remote usernames on a computer.
-
-    There is an unique_together constraint to be sure that each aida user
-    has no more than one remote username on a given computer.
-
-    Attributes:
-        computer: the remote computer
-        aidauser: the aida user
-        remoteusername: the username of the aida user on the remote computer
-    NOTE: this table can be eliminated in favor of a text field in each computer containing a dict.
-    """
-    computer = m.ForeignKey('Computer')
-    aidauser = m.ForeignKey(User)
-    remoteusername = m.CharField(max_length=255)
-
-    class Meta:
-        unique_together = (("aidauser", "computer"),)
-
-    def __unicode__(self):
-        return self.aidauser.username + " => " + \
-            self.remoteusername + "@" + self.computer.hostname
-
-################################################
+    
+#class ComputerUsername(m.Model):
+#    """Association of aida users with given remote usernames on a computer.
+#
+#    There is an unique_together constraint to be sure that each aida user
+#    has no more than one remote username on a given computer.
+#
+#    Attributes:
+#        computer: the remote computer
+#        aidauser: the aida user
+#        remoteusername: the username of the aida user on the remote computer
+#    NOTE: this table can be eliminated in favor of a text field in each computer containing a dict.
+#    """
+#    computer = m.ForeignKey('Computer')
+#    aidauser = m.ForeignKey(User)
+#    remoteusername = m.CharField(max_length=255)
+#
+#    class Meta:
+#        unique_together = (("aidauser", "computer"),)
+#
+#    def __unicode__(self):
+#        return self.aidauser.username + " => " + \
+#            self.remoteusername + "@" + self.computer.hostname
+#
+#################################################
 #
 #class Job(DataClass):
 #    computer = m.ForeignKey('Computer') #computer to which to submit
@@ -201,12 +189,11 @@ class ComputerUsername(m.Model):
 
 class Code(EntityClass):
     type = m.ForeignKey('CodeType') #to identify which parser/plugin
-    state = m.ForeignKey('CodeState') #need to define
     computer = m.ForeignKey('Computer', blank=True) #for checking computer compatibility, empty means all. 
-    attr = m.ManyToManyField('CodeAttr', through='CodeAttrVal')
     group = m.ManyToManyField('CodeGroup', blank=True)
+    attr = m.ManyToManyField('CodeAttr', through='CodeAttrVal')
     def __unicode__(self):
-        return self.title
+        return self.name
 
 class CodeGroup(NameClass):  
     pass    
@@ -214,12 +201,9 @@ class CodeGroup(NameClass):
 class CodeComment(CommentClass): 
     pass
 
-class CodeState(NameClass):   
-    pass
-
 class CodeType(NameClass):   
     """
-    This class defines the code type. Note that the code title should follow
+    This class defines the code type (family). Note that the code title should follow
     a specific syntax since from the title AIDA retrieves which input (and
     output) plugins to use.
     The conversion is described in the functions of the 
@@ -227,58 +211,15 @@ class CodeType(NameClass):
     """
     pass
 
-class CodeAttr(NameClass):  
-    pass
+class CodeAttr(NameClass):
+    isunique = m.BooleanField()
 
 
-class CodeAttrVal(m.Model):
+class CodeAttrVal(EntityClass):
     item = m.ForeignKey('Code')
     attr = m.ForeignKey('CodeAttr')
     txtval = m.TextField()
     numval = m.FloatField()
-    time = m.DateTimeField(auto_now=True)
-    class Meta:
-        unique_together=('item','attr')
-
-###############################################
-
-class Element(BaseClass):
-    """
-    This table contains the atomic elements, from hydrogen (Z=1) to
-    Lawrencium (Z=103).
-
-    The element symbol is stored in the 'title' field. 
-    The element name is stored in the 'description' field.
-    
-    The atomic mass is stored under the 'mass' key in the json-dumped
-    data field. The mass is taken from ASE and, where not available,
-    it has been substituted by a nearest integer number.
-    
-    This default value has not been set as an attribute because the
-    attribute require an owner, and this hinders the usage of fixtures
-    to fill default data in the database since no default user exists.
-    """
-    Z = m.IntegerField(unique=True)
-    attr = m.ManyToManyField('ElementAttr', through = 'ElementAttrVal')
-    group = m.ManyToManyField('ElementGroup', blank=True)
-        
-class ElementGroup(NameClass):  
-    pass
-
-class ElementAttr(NameClass):   
-    pass
-
-class ElementAttrVal(m.Model):
-    item = m.ForeignKey('Element')
-    attr = m.ForeignKey('ElementAttr')
-    txtval = m.TextField()
-    numval = m.FloatField()
-    time = m.DateTimeField(auto_now=True)
-    class Meta:
-        unique_together=('item','attr')
-
-######################################################
-
 
 
 

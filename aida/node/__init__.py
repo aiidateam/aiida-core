ed85dd82db376e5bb1c2a60655efcd81a649cbfc
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2013-04-25 10:17:18 -0400

    Fix to the models, a few syntax errors

--- aida/node/__init__.py
+++ aida/node/__init__.py
@@ -1,15 +1,35 @@
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
 from aida.djsite.main.models import Node as DjangoNode
-from aida.exceptions import NotExistent
+from aida.common.exceptions import NotExistent
+
 
 class Node(object):
     """
     Base class to map a node in the DB + its permanent repository counterpart.
     """
     def __init__(self,uuid=None):
-        if uuid:
-            
-        self._djtable = DjangoNode()
+        if uuid is not None:
+            try:
+                self._tablerow = DjangoNode.objects.get(uuid=uuid)
+                # I do not check for multiple entries found
+            except DoesNotExist:
+                raise NotExistent("No entry with the UUID {} found".format(
+                    uuid))
+        else:
+            self._tablerow = DjangoNode.objects.create()
+            # Check if here we already have a UUID
 
+        
+        
     @property
-    def djtable(self):
-        return self._djtable
+    def uuid(self):
+        return self.tablerow.uuid
+        
+    @property
+    def tablerow(self):
+        return self._tablerow
+
+if __name__ == '__main__':
+    a = Node()
+    print a.uuid

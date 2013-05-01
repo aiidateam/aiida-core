656bcf9697205c8d9d66471e27c6a6eeaa7a6f26
Boris Kozinsky <Boris.Kozinsky@bosch.com>
2013-05-01 12:36:28 -0400

    some more thoughts in the dbgraph

--- aida/common/dbgraph.py
+++ aida/common/dbgraph.py
@@ -1,4 +1,4 @@
-import aida.orm
+import aida.node
 
 ''' 
 Graph Rules:
@@ -8,7 +8,16 @@ Graph Rules:
 3. Each calculation must be linked to one and only one code
 4. Each calculation must be linked to one and only one computer
 
-5. A data node may contain multiple data nodes. This is nested hierarchy.
+5. A data node may contain multiple data nodes. This is nested hierarchy. 
+If there is a link between a data and a caclulation, there must be a link 
+between at least one sub-data and one sub-calculation.
+
+Alternatively, all subdata must be linked to at least one sub-calculation.
+
+This is a mess, delay this for now.
+
+
+
 6. A calculation node may contain multiple calculation nodes. This is a workflow.
 
 '''
@@ -18,4 +27,12 @@ def GraphCheck():
         raise GraphError('Node {0} violates connection rules')
    
    
- 
+def SuperNodes():
+    
+    
+    
+    
+    
+    
+    
+    

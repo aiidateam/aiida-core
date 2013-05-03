6458612bd21507d9d4a55ca8aa87a2b89f549bb0
Boris Kozinsky <Boris.Kozinsky@bosch.com>
2013-05-03 10:19:42 -0400

    more graph stuff

--- aida/common/dbgraph.py
+++ aida/common/dbgraph.py
@@ -105,8 +105,25 @@ def SuperNodes():
     pass
     
     
-    
-    
+def tc(g):
+    """ Given a graph @g, returns the transitive closure of @g """
+    ret = {}
+    for scc in GraphLoops(g):
+            ws = set()
+            ews = set()
+            for v in scc:
+                    ws.update(g[v])
+            for w in ws:
+                    assert w in ret or w in scc
+                    ews.add(w)
+                    ews.update(ret.get(w,()))
+            if len(scc) > 1:
+                    ews.update(scc)
+            ews = tuple(ews)
+            for v in scc:
+                    ret[v] = ews
+    return ret
+
     
     
     

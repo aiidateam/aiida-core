3224281711e9803f527eae1b7dad11d458ee979f
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2013-05-09 02:17:20 +0200

    Implemented bash completion.

--- aida/cmdline/baseclass.py
+++ aida/cmdline/baseclass.py
@@ -6,15 +6,20 @@ class VerdiCommand(object):
     def command_name(self):
         return self.__class__.__name__.lower()
     
-    def run(*args):
+    def run(self,*args):
         """
         Method executed when the command is called from the command line.
         """
         print >> sys.stderr, "This command has not been implemented yet"
 
-    def complete(*subargs):
+    def complete(self, subargs_idx, subargs):
         """
         Method called when the user asks for the bash completion.
-        Return a list of valid keywords.
+        Print a list of valid keywords.
+        Returning without printing will use standard bash completion.
+        
+        subargs_idx: the index of the subargs where the TAB key was pressed
+            (0 is the first element of subargs)
+        subargs: a list of subarguments to this command
         """
-        return []
+        return

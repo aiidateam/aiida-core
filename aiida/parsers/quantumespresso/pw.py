eb6aa841fb9c8778683e4427b1c005bf3073a9bc
Andrea Cepellotti <andrea.cepellotti@epfl.ch>
2013-06-15 18:44:46 +0200

    Finished the pw parser class. But still is completely untested.

--- aiida/parsers/quantumespresso/pw.py
+++ aiida/parsers/quantumespresso/pw.py
@@ -11,6 +11,7 @@ class PwscfParser(Parser):
     """
     This class is the implementation of the Parser class for PWscf.
     """
+
     def __init__(self,calc):
         # First I need to check if I receive a valid input
         if not isinstance(calc,PwCalculation):
@@ -32,23 +33,25 @@ class PwscfParser(Parser):
         import os
         import glob
         
-        calc_input_name = self.calc.get_linkname_parameters()
-        #calc_inputs = self.calc.query( DataFactory('parameter'), _with_link_to_calc_of_the_same_name_that_I_just_got_above ) # TODO: fix this
-        calc_input_parameterdata = self.calc.get_inputs() # retrieve the whole list of input links
+        input_param_name = self.calc.get_linkname_parameters()
+        #ParameterData = DataFactory('parameter')
+         # retrieve the whole list of input links
+        calc_input_parameterdata = self.calc.get_inputs(type=ParameterData,also_labels=True)
         
         # TODO count how many occurences of the same input params happen
-        # and check that they occur only once
-        if parser_opts occur_more_than_once:
-            raise QEOutputParsingError("Too many parser_opts attached are found: {}".format(the_num))
-        if input_params occur_not_once:
-            raise QEOutputParsingError("Too many input_params are found: {}".format(the_num))
+        params = [i[1] for i in calc_input_parameterdata if i[0]==input_param_name]
+        if len(params) != 1:
+            raise QEOutputParsingError("Too many input_params are found: {}".format(params))
+        calc_input = params[0]
         
-        for this_obj in calc_input_parameterdata:
-            if this_obj is parser_opts: # TODO fix it
-                parser_opts = copy.deepcopy(this_obj)
-            if this_obj is input_params: # TODO fix it
-                calc_input = copy.deepcopy(this_obj)
-
+        parser_opts_query = [i[1] for i in calc_input_parameterdata if i[0]=='parser_opts']
+        # TODO : there should be a function returning the name of parser_opts
+        if len(parser_opts_query)>1:
+            raise QEOutputParsingError("Too many parser_opts attached are found: {}"
+                                       .format(parser_opts_query))
+        if parser_opts_query:
+            parser_opts = parser_opts_query[0]
+        
         # TODO: pass this input_dict to the parser. It might need it.            
         input_dict = calc_input.get_dict()
         
@@ -58,11 +61,13 @@ class PwscfParser(Parser):
             raise NotImplementedError("The parser_options feature is not yet implemented")
         parser_opts = []
         
-        calc_outputs = self.calc.get_outputs()
-        # TODO : look for outputs of class SandboxFolder() with link to calc called "retrieved"
-        if len(calc_outputs)!=1:
-            raise QEOutputParsingError("Output folder should be found once, found it instead {} times".format(len(calc_outputs)) )
-        out_folder = calc_outputs[0]
+        calc_outputs = self.calc.get_outputs(type=SandboxFolder,also_labels=True)
+        retrieved_files = [i[1] for i in calc_outputs if i[0]=='retrieved']
+        # TODO : linkname 'retrieved' should be more given py PwCalculation
+        if len(retrieved_files)!=1:
+            raise QEOutputParsingError("Output folder should be found once, found it instead {} times"
+                                       .format(len(retrieved_files)) )
+        out_folder = calc_outputs[0][1]
         
         # check what is inside the folder
         list_of_files = out_folder.get_content_list()
@@ -92,8 +97,14 @@ class PwscfParser(Parser):
         
         # save it into db
         output_params.store()
-        self.calc.add_link_from(output_params, label='output_parameters') 
-        # TODO check name of the link
-        #TODO in this way, we should call the input one as 'input_parameters'
-
+        self.calc.add_link_from(output_params, label=self.get_linkname_outparams() )
+        # TODO check name of the link input_parameters to be consistent with this
+        # TODO : 'output_parameters' is the name of the link that the parser decides.
+        # How does the Calculation know it?
         
+        
+    def get_linkname_outparams(self):
+        """
+        The name of the link used for the output parameters
+        """
+        return 'output_parameters'

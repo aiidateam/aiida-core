3e3472d509958f4df13306a5b4f36767f32b3cd6
Andrea Cepellotti <andrea.cepellotti@epfl.ch>
2013-06-22 19:00:09 +0200

    Added parsing of the output structure of pw: it is now converted to a StructureData. Added a cell_converter qe->aiida. Added a script to test a case with an output structure. Added in structure two methods: reset_cell and reset_sites_positions

--- aiida/parsers/plugins/quantumespresso/__init__.py
+++ aiida/parsers/plugins/quantumespresso/__init__.py
@@ -8,3 +8,28 @@ class QEOutputParsingError(OutputParsingError):
     #     self.message = wrappedmessage
     #     self.module = "qe-pw"
 
+def convert_qe2aiida_structure(output_dict,input_structure=None):
+    """
+    Receives the dictionary cell parsed from quantum espresso
+    Convert it into an AiiDA structure object
+    """
+    from aiida.orm import DataFactory
+    StructureData = DataFactory('structure')
+
+    cell_dict = output_dict['cell']
+
+    # If I don't have any help, I will set up the cell as it is in QE
+    if not input_structure:
+        
+        s = StructureData(cell=cell_dict['lattice_parameters'])
+        for atom in cell_dict['atoms']:
+            s.append_atom( position=tuple(atom[1]),symbols=[ atom[0] ] )
+            
+    else:
+        
+        s = input_structure.copy()
+        s.reset_cell(cell_dict['lattice_vectors'])
+        new_pos = [ i[1] for i in cell_dict['atoms'] ]
+        s.reset_sites_positions( new_pos )
+    
+    return s

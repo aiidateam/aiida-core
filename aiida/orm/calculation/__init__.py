c174f68536c2b511497bdce1fb9c2e325fa28f59
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2013-07-24 10:05:09 +0200

    Bugfix, missing 'return self' at the end of the inherited store() method of calculations

--- aiida/orm/calculation/__init__.py
+++ aiida/orm/calculation/__init__.py
@@ -85,6 +85,8 @@ class Calculation(Node):
         if issubclass(caller_module_class, Workflow):
             #uuid = caller_frame.f_locals.get('self', None).uuid()
             caller_frame.f_locals.get('self', None).get_step(caller_funct).add_calculation(self)
+        
+        return self
             
     def validate(self):
         from aiida.common.exceptions import MissingPluginError, ValidationError

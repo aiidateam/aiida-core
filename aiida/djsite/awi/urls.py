8e8815f5c0ccb14078b07684ca008936f389e868
Valentin Bersier <bersier@theospc12.epfl.ch>
2014-03-11 13:21:25 +0100

    Finalized login/logout modules

--- aiida/djsite/awi/urls.py
+++ aiida/djsite/awi/urls.py
@@ -4,7 +4,8 @@ from awi import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='home'), # home page
-	url(r'^login', views.login, name='login'), #login page
+	url(r'^login', views.login_view, name='login'), #login page
+	url(r'^logout', views.logout_view, name='logout'), #logout page
 	url(r'^computers/rename/(?P<computer_id>\d+)', views.computers_rename, name='computers_rename'), # rename a computer (ajax)
 	url(r'^computers/detail/(?P<computer_id>\d+)/(?P<ajax>\w+)', views.computers_detail, name='computers_detail_ajax'), # details for a computer called in ajax
 	url(r'^computers/detail/(?P<computer_id>\d+)', views.computers_detail, name='computers_detail'), # details for a computer

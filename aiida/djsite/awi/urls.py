7c81bc90a2481d5e71f9e1c83affc4a7624d102b
Valentin Bersier <bersier@theospc12.epfl.ch>
2014-03-13 12:45:46 +0100

    Modifications on the AWI application, including working renaming function and better mockup for the filters

--- aiida/djsite/awi/urls.py
+++ aiida/djsite/awi/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
 	url(r'^$', views.index, name='home'), # home page
 	url(r'^login', views.login_view, name='login'), #login page
 	url(r'^logout', views.logout_view, name='logout'), #logout page
+	url(r'^filters/value/(?P<field>[a-z0-9\-_]+)', views.filters_value, name='filters_value'), # update a filter (ajax)
 	url(r'^computers/rename/(?P<computer_id>\d+)', views.computers_rename, name='computers_rename'), # rename a computer (ajax)
 	url(r'^computers/detail/(?P<computer_id>\d+)/(?P<ajax>\w+)', views.computers_detail, name='computers_detail_ajax'), # details for a computer called in ajax
 	url(r'^computers/detail/(?P<computer_id>\d+)', views.computers_detail, name='computers_detail'), # details for a computer

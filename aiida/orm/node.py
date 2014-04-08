46a034a54c5c338d3fbc4a87d51286862534bf0d
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2014-04-08 16:19:59 +0200

    Added the listing of existing calculation plugins with 'verdi calculation plugins'

--- aiida/orm/node.py
+++ aiida/orm/node.py
@@ -901,10 +901,10 @@ class Node(object):
 
         :todo: optimize! At the moment, the call is very slow because it is 
             also calling attr.getvalue() for each attribute, that has to
-             perform complicated queries to rebuild the object.
+            perform complicated queries to rebuild the object.
 
         :param bool also_updatable: if False, does not iterate over 
-                      attributes that are updatable
+            attributes that are updatable
         """
         from aiida.djsite.db.models import DbAttribute
 #        TODO: check what happens if someone stores the object while

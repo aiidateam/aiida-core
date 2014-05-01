826a4b9f7d3a427dfd162b95d9e952e965ecc7d5
Valentin Bersier <bersier@theospc12.epfl.ch>
2014-05-01 11:11:18 +0200

    Various changes in AWI, needed to pull the new branch

--- aiida/djsite/awi/static/awi/js/calculations_manager.js
+++ aiida/djsite/awi/static/awi/js/calculations_manager.js
@@ -2,7 +2,7 @@ function CalculationsManager(ApplicationManager) {
 	this.applicationManager = ApplicationManager;
 	this.module = 'calculations';
 	this.moduleS = 'calculation';
-	this.columns = 8; // number of columns in the listing table
+	this.columns = 6; // number of columns in the listing table
 	this.loadUrl = modules_urls[this.module].listing;
 	this.filterUrls = modules_urls.filters[this.module];
 	this.table = $('#' + this.module + '-list');
@@ -96,39 +96,45 @@ CalculationsManager.prototype.load = function (scroll) {
 			// for each calculation, we build the html of a table row
 			$.each(data.objects, function (k, o) {
 				rows.push(''); // reserve a spot in the output for this row
-				$.getJSON(o.user, function (subdata) {
-					var username = subdata.username;
-					$.getJSON(o.dbcomputer, function (subsubdata) {
-						var computername = subsubdata.name;
-						//var ctime = new Date((o.ctime || "").replace(/-/g,"/").replace(/[TZ]/g," "));
-						var ctime = new Date();
-						ctime.setISO8601(o.ctime);
-						// we update the corresponding line, this ensures that data is gonna be displayed in the right order at the end
-						rows[k] = '<tr id="row-' + o.id + '">' +
-							'<td>' + o.id + '</td>' +
-							'<td class="name"><a href="' + self.getUrl('detail') + o.id + '" class="show-detail" data-id="' + o.id +
-							'"><strong>' + o.label + '</strong>&nbsp;<span class="right-caret"></span></a></td>' +
-							'<td>' + o.description.trunc(15, true, true) + '</td>' + /* description is truncated via this custom function */
-							'<td><span title="' + o.type + '" data-toggle="tooltip">' + o.type.split('.').slice(-2)[0] + '</span></td>' +
-							'<td>' + computername + '</td>' +
-							'<td>' + username + '</td>' +
-							'<td>' + ctime.toLocaleString() + '</td>' +
-							'<td>' +
-								'<div class="btn-group">' + /* actions dropdown */
-									'<button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">' +
-										'Action <span class="caret"></span>' +
-									'</button>' +
-									'<ul class="dropdown-menu dropdown-menu-right" role="menu">' +
-										'<li><a href="' + self.getUrl('detail') + o.id + '" class="show-detail"' +
-											'data-id="' + o.id + '"><span class="glyphicon glyphicon-tasks"></span>&nbsp;&nbsp;Details</a></li>' +
-									'</ul>' +
-								'</div>' +
-							'</td>' +
-						'</tr>';
-						if (k == data.objects.length - 1) {
-							next();
-						}
-					});
+				
+				// http://localhost:8000/api/v1/attribute/?id__in=23&id__in=26&id__in=27&id__in=28&id__in=29&id__in=75&id__in=78&id__in=80&id__in=81&id__in=82&id__in=83&key=state
+				// http://localhost:8000/api/v1/attribute/?id__in=23,26,27,28,29,75,78,80,81,82,83&key=state
+				var stateUrl = self.getAPIUrl('attributes') + '?';
+				var attrIds = [];
+				for (var i = 0, len = o.attributes.length; i < len; i++) {
+					attrIds.push(o.attributes[i].substring(0, o.attributes[i].length - 1).split('/').pop());
+				}
+				stateUrl = stateUrl + attrIds.join(',');
+				stateUrl = stateUrl + '&key=state';
+				
+				$.getJSON(o.dbcomputer, function (subsubdata) {
+					var computername = subsubdata.name;
+					//var ctime = new Date((o.ctime || "").replace(/-/g,"/").replace(/[TZ]/g," "));
+					var ctime = new Date();
+					ctime.setISO8601(o.ctime);
+					// we update the corresponding line, this ensures that data is gonna be displayed in the right order at the end
+					rows[k] = '<tr id="row-' + o.id + '">' +
+						'<td>' + o.id + '</td>' +
+						'<td class="name"><a href="' + self.getUrl('detail') + o.id + '" class="show-detail" data-id="' + o.id +
+						'"><strong>' + o.label + '</strong>&nbsp;<span class="right-caret"></span></a></td>' +
+						'<td><span title="' + o.type + '" data-toggle="tooltip">' + o.type.split('.').slice(-2)[0] + '</span></td>' +
+						'<td>' + computername + '</td>' +
+						'<td>' + ctime.toLocaleString() + '</td>' +
+						'<td>' +
+							'<div class="btn-group">' + /* actions dropdown */
+								'<button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown">' +
+									'Action <span class="caret"></span>' +
+								'</button>' +
+								'<ul class="dropdown-menu dropdown-menu-right" role="menu">' +
+									'<li><a href="' + self.getUrl('detail') + o.id + '" class="show-detail"' +
+										'data-id="' + o.id + '"><span class="glyphicon glyphicon-tasks"></span>&nbsp;&nbsp;Details</a></li>' +
+								'</ul>' +
+							'</div>' +
+						'</td>' +
+					'</tr>';
+					if (k == data.objects.length - 1) {
+						next();
+					}
 				});
 			});
 			var next = function () {
@@ -196,98 +202,23 @@ CalculationsManager.prototype.loadDetail = function (url, id) {
 	var self = this;
 	
 	$.getJSON(url, function (data) {
-		var loader = $('#' + self.module + '-detail-' + id + ' ~ .dots');
-		var ajaxLoaded = 0;
-		var mtime = new Date();
-		mtime.setISO8601(data.mtime);
-		var rows = [
-			'<div class="ajax-hide">',
-			'<ul class="media-list">',
-			'<li class="media"><strong class="pull-left">Description</strong><div class="media-body">' + data.description + '</div></li>',
-			'<li class="media"><strong class="pull-left">Type</strong><div class="media-body">' + data.type + '</div></li>',
-			'<li class="media"><strong class="pull-left">Attributes</strong><div class="media-body"><ul class="media-list">'
-		];
 		
-		var next = function () {
-			loader.fadeTo('fast', 0.01, function () { /* we hide the loader and show the details html */
-				$('#' + self.module + '-detail-' + id).prepend(rows.join(''));
-				$('#' + self.module + '-detail-' + id + ' .ajax-hide').slideDown(function () {
-					loader.hide();
-					$('#' + self.module + '-detail-' + id + '>.detail-close').fadeIn();
-				});
-				$('#' + self.module + '-detail-' + id).find('span').tooltip();
-			});
-		};
+		$.getJSON(data.user, function (subdata) {
+			var loader = $('#' + self.module + '-detail-' + id + ' ~ .dots');
+			var ajaxLoaded = 0;
+			var mtime = new Date();
+			mtime.setISO8601(data.mtime);
+			var rows = [
+				'<div class="ajax-hide">',
+				'<ul class="media-list">',
+				'<li class="media"><strong class="pull-left">User</strong><div class="media-body">' + subdata.username + '</div></li>',
+				'<li class="media"><strong class="pull-left">Description</strong><div class="media-body">' + data.description + '</div></li>',
+				'<li class="media"><strong class="pull-left">Type</strong><div class="media-body">' + data.type + '</div></li>',
+				'<li class="media"><strong class="pull-left">Attributes</strong><div class="media-body"><ul class="media-list">'
+			];
 		
-		$.each(data.attributes, function (k, v) { /* we go over all attributes and display them in a nested way */
-			var rowstart = rows.length; // index of the attribute row
-			rows.push(''); // reserve a spot
-			// here do the ajax to get attribute infos, and then update the corresponding row
-			$.getJSON(v, function (subdata) {
-				if (subdata.value instanceof Array) {
-					subdata.value = subdata.value.join('<br>');
-				} else if (typeof subdata.value === 'number') {
-					subdata.value = String(subdata.value);
-				} else if (typeof subdata.value === 'object') {
-					var content = ['<dl class="dl-horizontal">'];
-					$.each(subdata.value, function (key, val) {
-						content.push('<dt>' + key.trunc(16, false, true) + '</dt>');
-						content.push('<dd>' + val + '</dd>');
-					});
-					content.push('</dl>');
-					subdata.value = content.join('');
-				} else {
-					subdata.value = subdata.value.trunc(100);
-				}
-				rows[rowstart] = '<li class="media"><strong class="pull-left">' + subdata.key.trunc(18, false, true) +
-					'</strong><div class="media-body" style="max-width: 320px;">' + subdata.value + '</div></li>';
-				ajaxLoaded++;
-				if (ajaxLoaded == data.attributes.length+data.inputs.length+data.outputs.length) {
-					next();
-				}
-			});
-		});
-		rows.push(
-			'</ul></div></li>',
-			'<li class="media"><strong class="pull-left">Inputs</strong><div class="media-body"><ul class="media-list">'
-		);
-		$.each(data.inputs, function (k, v) { /* we go over all inputs and display them in a nested way */
-			var rowstart = rows.length; // index of the input row
-			rows.push(''); // reserve a spot
-			$.getJSON(v, function (subdata) {
-				rows[rowstart] = subdata.type + '<br>';
-				ajaxLoaded++;
-				if (ajaxLoaded == data.attributes.length+data.inputs.length+data.outputs.length) {
-					next();
-				}
-			});
-		});
-		rows.push(
-			'</ul></div></li>',
-			'<li class="media"><strong class="pull-left">Outputs</strong><div class="media-body"><ul class="media-list">'
-		);
-		$.each(data.outputs, function (k, v) { /* we go over all outputs and display them in a nested way */
-			var rowstart = rows.length; // index of the output row
-			rows.push(''); // reserve a spot
-			$.getJSON(v, function (subdata) {
-				rows[rowstart] = subdata.type + '<br>';
-				ajaxLoaded++;
-				if (ajaxLoaded == data.attributes.length+data.inputs.length+data.outputs.length) {
-					next();
-				}
-			});
-		});
-		rows.push(
-			'</ul></div></li>',
-			'<li class="media"><strong class="pull-left">Modification</strong><div class="media-body">' + mtime.toLocaleString() + '</div></li>',
-			'<li class="media"><strong class="pull-left">Node version</strong><div class="media-body">' + data.nodeversion + '</div></li>',
-			'<li class="media"><strong class="pull-left">UUID</strong><div class="media-body">' + data.uuid + '</div></li>',
-			'</ul></div>'
-		);
-		/*
-		var timer = function () {
-			if (ajaxLoaded == data.attributes.length+data.inputs.length+data.outputs.length) {
-				loader.fadeTo('fast', 0.01, function () {
+			var next = function () {
+				loader.fadeTo('fast', 0.01, function () { /* we hide the loader and show the details html */
 					$('#' + self.module + '-detail-' + id).prepend(rows.join(''));
 					$('#' + self.module + '-detail-' + id + ' .ajax-hide').slideDown(function () {
 						loader.hide();
@@ -295,11 +226,88 @@ CalculationsManager.prototype.loadDetail = function (url, id) {
 					});
 					$('#' + self.module + '-detail-' + id).find('span').tooltip();
 				});
-			} else {
-				window.setTimeout(timer, 100);
-			}
-		};
-		timer();*/
+			};
+		
+			$.each(data.attributes, function (k, v) { /* we go over all attributes and display them in a nested way */
+				var rowstart = rows.length; // index of the attribute row
+				rows.push(''); // reserve a spot
+				// here do the ajax to get attribute infos, and then update the corresponding row
+				$.getJSON(v, function (subdata) {
+					if (subdata.value instanceof Array) {
+						subdata.value = subdata.value.join('<br>');
+					} else if (typeof subdata.value === 'number') {
+						subdata.value = String(subdata.value);
+					} else if (typeof subdata.value === 'object') {
+						var content = ['<dl class="dl-horizontal">'];
+						$.each(subdata.value, function (key, val) {
+							content.push('<dt>' + key.trunc(18, false, true) + '</dt>');
+							content.push('<dd>' + val + '</dd>');
+						});
+						content.push('</dl>');
+						subdata.value = content.join('');
+					} else if (subdata.key === 'scheduler_lastchecktime') {
+						var time = new Date();
+						time.setISO8601(subdata.value);
+						subdata.value = time.toLocaleString();
+					} else if (subdata.key === 'state') {
+						subdata.value = self.colorState(subdata.value);
+					} else if (subdata.key === 'scheduler_state') {
+						subdata.value = self.colorSchedulerState(subdata.value);
+					} else {
+						subdata.value = subdata.value.trunc(100);
+					}
+					rows[rowstart] = '<li class="media"><strong class="pull-left">' + subdata.key.trunc(18, false, true) +
+						'</strong><div class="media-body">' + subdata.value + '</div></li>';
+					ajaxLoaded++;
+					if (ajaxLoaded == data.attributes.length + data.inputs.length + data.outputs.length) {
+						next();
+					}
+				});
+			});
+			rows.push(
+				'</ul></div></li>',
+				'<li class="media"><strong class="pull-left">Inputs</strong><div class="media-body"><dl class="dl-horizontal">'
+			);
+			$.each(data.inputs, function (k, v) { /* we go over all inputs and display them in a nested way */
+				var rowstart = rows.length; // index of the input row
+				rows.push(''); // reserve a spot
+				$.getJSON(v, function (subdata) {
+					rows[rowstart] = '<dt style="width: 30px;">' + subdata.id + '</dt><dd style="margin-left: 50px;"><a href="' +
+					self.getUrl('apidetail') + subdata.id + '/' +
+					'" target="_blank" class="text-info"><span class="glyphicon glyphicon-new-window"></span></a>&nbsp;&nbsp;<a href="#">' +
+						subdata.type + '</a></dd>';
+					ajaxLoaded++;
+					if (ajaxLoaded == data.attributes.length + data.inputs.length + data.outputs.length) {
+						next();
+					}
+				});
+			});
+			rows.push(
+				'</dl></div></li>',
+				'<li class="media"><strong class="pull-left">Outputs</strong><div class="media-body"><dl class="dl-horizontal">'
+			);
+			$.each(data.outputs, function (k, v) { /* we go over all outputs and display them in a nested way */
+				var rowstart = rows.length; // index of the output row
+				rows.push(''); // reserve a spot
+				$.getJSON(v, function (subdata) {
+					rows[rowstart] = '<dt style="width: 30px;">' + subdata.id + '</dt><dd style="margin-left: 50px;"><a href="' +
+					self.getUrl('apidetail') + subdata.id + '/' +
+					'" target="_blank" class="text-info"><span class="glyphicon glyphicon-new-window"></span></a>&nbsp;&nbsp;<a href="#">' +
+						subdata.type + '</a></dd>';
+					ajaxLoaded++;
+					if (ajaxLoaded == data.attributes.length + data.inputs.length + data.outputs.length) {
+						next();
+					}
+				});
+			});
+			rows.push(
+				'</dl></div></li>',
+				'<li class="media"><strong class="pull-left">Modification</strong><div class="media-body">' + mtime.toLocaleString() + '</div></li>',
+				'<li class="media"><strong class="pull-left">Node version</strong><div class="media-body">' + data.nodeversion + '</div></li>',
+				'<li class="media"><strong class="pull-left">UUID</strong><div class="media-body">' + data.uuid + '</div></li>',
+				'</ul></div>'
+			);
+		});
 	});
 };
 
@@ -323,6 +331,45 @@ CalculationsManager.prototype.getAPIUrl = function (resource) {
 		return url.substring(0, url.length - 2);
 	} else {
 		return url;
+	}
+};
+
+// get colored state, warning is orange, info is light blue, danger is red, success is green, primary is blue, default is grey
+CalculationsManager.prototype.colorState = function (state) {
+	var states = {
+		'NEW'				: 'warning',
+		'TOSUBMIT'			: 'info',
+		'SUBMITTING'		: 'default',
+		'COMPUTED'			: 'default',
+		'RETRIEVING'		: 'default',
+		'PARSING'			: 'default',
+		'WITHSCHED'			: 'primary',
+		'FINISHED'			: 'success',
+		'SUBMISSIONFAILED'	: 'danger',
+		'RETRIEVALFAILED'	: 'danger',
+		'PARSINGFAILED'		: 'danger',
+		'FAILED'			: 'danger'
+	};
+	if (states[state] !== undefined) {
+		return '<span class="label label-' + states[state] + '">' + state + '</span>';
+	} else {
+		return state;
+	}
+};
+
+// get colored state, warning is orange, info is light blue, danger is red, success is green, primary is blue, default is grey
+CalculationsManager.prototype.colorSchedulerState = function (state) {
+	var states = {
+		'QUEUED'			: 'info',
+		'QUEUED_HELD'		: 'warning',
+		'RUNNING'			: 'primary',
+		'SUSPENDED'			: 'warning',
+		'DONE'				: 'success'
+	};
+	if (states[state] !== undefined) {
+		return '<span class="label label-' + states[state] + '">' + state + '</span>';
+	} else {
+		return state;
 	}
 };
 

465b6d7449c27b4e2b6e8d36f84db97e7664133e
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2014-07-06 11:26:46 +0200

    Removed old unused doc, added (optional) read the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -292,3 +292,19 @@ epub_copyright = u'2012-2014, Giovanni Pizzi, Andrea Cepellotti, Riccardo Sabati
 
 # Allow duplicate toc entries.
 #epub_tocdup = True
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed
+# from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    try:
+        import sphinx_rtd_theme
+        html_theme = 'sphinx_rtd_theme'
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    except ImportError:
+        # No sphinx_rtd_theme installed
+        pass
+
+# otherwise, readthedocs.org uses their theme by default, so no need
+# to specify it

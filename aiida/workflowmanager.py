1708214ededd36621619f33f31f669d98d4c1884
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2014-07-07 17:26:32 +0200

    Fixed unicode in copyright string

--- aiida/workflowmanager.py
+++ aiida/workflowmanager.py
@@ -8,7 +8,7 @@ from aiida.common import aiidalogger
 from aiida.common.datastructures import wf_states, wf_exit_call, wf_default_call
 
 __author__ = "Giovanni Pizzi, Andrea Cepellotti, Riccardo Sabatini, Nicola Marzari, and Boris Kozinsky"
-__copyright__ = "Copyright (c), 2012-2014, École Polytechnique Fédérale de Lausanne (EPFL), Laboratory of Theory and Simulation of Materials (THEOS), MXC - Station 12, 1015 Lausanne, Switzerland. All rights reserved."
+__copyright__ = u"Copyright (c), 2012-2014, École Polytechnique Fédérale de Lausanne (EPFL), Laboratory of Theory and Simulation of Materials (THEOS), MXC - Station 12, 1015 Lausanne, Switzerland. All rights reserved."
 __license__ = "MIT license, see LICENSE.txt file"
 __version__ = "0.2.0"
 

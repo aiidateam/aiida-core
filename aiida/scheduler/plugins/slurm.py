fe5e27cba55d7c31d2e50de997e2d6cc064e3053
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2014-07-07 17:26:32 +0200

    Fixed unicode in copyright string

--- aiida/scheduler/plugins/slurm.py
+++ aiida/scheduler/plugins/slurm.py
@@ -36,7 +36,7 @@ from aiida.scheduler.datastructures import (
 ## TO  TIMEOUT         Job terminated upon reaching its time limit.
 
 __author__ = "Giovanni Pizzi, Andrea Cepellotti, Riccardo Sabatini, Nicola Marzari, and Boris Kozinsky"
-__copyright__ = "Copyright (c), 2012-2014, École Polytechnique Fédérale de Lausanne (EPFL), Laboratory of Theory and Simulation of Materials (THEOS), MXC - Station 12, 1015 Lausanne, Switzerland. All rights reserved."
+__copyright__ = u"Copyright (c), 2012-2014, École Polytechnique Fédérale de Lausanne (EPFL), Laboratory of Theory and Simulation of Materials (THEOS), MXC - Station 12, 1015 Lausanne, Switzerland. All rights reserved."
 __license__ = "MIT license, see LICENSE.txt file"
 __version__ = "0.2.0"
 

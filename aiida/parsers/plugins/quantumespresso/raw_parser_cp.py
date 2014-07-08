4a70f61546effe92897d927a5d43f32e17a6f068
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2014-07-08 17:14:21 +0200

    There was a double import

--- aiida/parsers/plugins/quantumespresso/raw_parser_cp.py
+++ aiida/parsers/plugins/quantumespresso/raw_parser_cp.py
@@ -2,7 +2,6 @@
 from aiida.parsers.plugins.quantumespresso import QEOutputParsingError
 from xml.dom.minidom import parseString
 from aiida.parsers.plugins.quantumespresso.raw_parser_pw import *
-from aiida.parsers.plugins.quantumespresso.raw_parser_pw import *
 
 __author__ = "Giovanni Pizzi, Andrea Cepellotti, Riccardo Sabatini, Nicola Marzari, and Boris Kozinsky"
 __copyright__ = u"Copyright (c), 2012-2014, École Polytechnique Fédérale de Lausanne (EPFL), Laboratory of Theory and Simulation of Materials (THEOS), MXC - Station 12, 1015 Lausanne, Switzerland. All rights reserved."

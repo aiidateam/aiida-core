0078f9da01e5e5dc41fb79dce218ba65947566cc
Nicolas Mounet <nicolas.mounet@epfl.ch>
2014-08-12 17:31:16 +0200

    Bugs fixed in aiida/parsers/plugins/quantumespresso/raw_parser_pw.py, in the case with both bands and smearing

--- aiida/parsers/plugins/quantumespresso/raw_parser_pw.py
+++ aiida/parsers/plugins/quantumespresso/raw_parser_pw.py
@@ -974,11 +974,11 @@ def parse_pw_xml_output(data,dir_with_bands=None):
            parsed_data['number_of_electrons']/2. < parsed_data['number_of_bands']:
         # initialize lumo
             lumo = parsed_data['homo']+10000.0
-            for list_bands in parsed_data['bands']:
+            for list_bands in bands_dict['bands']:
                 for value in list_bands:
                     if (value > parsed_data['fermi_energy']) and (value<lumo):
                         lumo=value
-            if (lumo==parsed_data['homo']+10000.0) or lumo<=parsed_data['fermy_energy']:
+            if (lumo==parsed_data['homo']+10000.0) or lumo<=parsed_data['fermi_energy']:
                 #might be an error for bandgap larger than 10000 eV...
                 raise QEOutputParsingError('Error while searching for LUMO.')
             parsed_data['lumo']=lumo

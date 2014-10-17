09763adc05b48bb5ee908835c5a575ca034c4815
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2014-10-17 16:55:22 +0200

    Added the functions to check the schema version in the DbSetting table, and possibly stop if there is a version mismatch. Also moved load_django to load_dbenv(). Will rename everything in the next commit.

--- aiida/__init__.py
+++ aiida/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from aiida.djsite.utils import load_dbenv
 
 __author__ = "Giovanni Pizzi, Andrea Cepellotti, Riccardo Sabatini, Nicola Marzari, and Boris Kozinsky"
 __copyright__ = u"Copyright (c), 2012-2014, École Polytechnique Fédérale de Lausanne (EPFL), Laboratory of Theory and Simulation of Materials (THEOS), MXC - Station 12, 1015 Lausanne, Switzerland. All rights reserved."

ab0a5f1248342f662c638bd02fd2171aba814b91
Andrius Merkys <andrius.merkys@epfl.ch>
2014-10-20 12:03:26 +0200

    Implementing fetching of CIF file contents from the Web in dbentry.py.

--- aiida/tools/dbentry.py
+++ aiida/tools/dbentry.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import urllib2
+
 class DBEntry(object):
     """
     Represents the entry from the structure database (COD, ICSD, ...).
@@ -9,20 +11,21 @@ class DBEntry(object):
     source_db = "" # Identification of the source database
     db_id = ""     # Structure identifyer
 
+    cif = "" # Contents of raw CIF file
+
     def __init__(self, url, **kwargs):
         self.url = url
         if 'source_db' in kwargs.keys():
             self.source_db = kwargs['source_db']
         if 'db_id' in kwargs.keys():
             self.db_id = kwargs['db_id']
+        cif = urllib2.urlopen( self.url ).read()
 
     def get_raw_cif(self):
         """
-        Downloads CIF file from the database using the URL, specified in
-        the object's constructor. If the CIF file is already fetched and
-        stored, returns it's contents.
+        Returns raw contents of a CIF file as string.
         """
-        raise NotImplementedError()
+        return self.cif
 
     def get_cif_node(self):
         """

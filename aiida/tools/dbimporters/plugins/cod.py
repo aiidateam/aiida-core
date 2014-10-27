072b859894c70a6babf131a7e836765272876df0
Andrius Merkys <andrius.merkys@epfl.ch>
2014-10-27 13:39:33 +0100

    Implementing querying COD using cell measurement and diffraction pressure values.

--- aiida/tools/dbimporters/plugins/cod.py
+++ aiida/tools/dbimporters/plugins/cod.py
@@ -59,6 +59,7 @@ class CodDbImporter(aiida.tools.dbimporters.baseclasses.DbImporter):
     angle_precision       = 0.001
     volume_precision      = 0.001
     temperature_precision = 0.001
+    pressure_precision    = 1
 
     def length_clause(self, key, values):
         """
@@ -84,33 +85,43 @@ class CodDbImporter(aiida.tools.dbimporters.baseclasses.DbImporter):
         """
         return self.double_clause(key, values, self.temperature_precision)
 
-    keywords = { 'id'                : [ 'file',      int_clause ],
-                 'element'           : [ 'element',   composition_clause ],
-                 'number_of_elements': [ 'nel',       int_clause ],
-                 'mineral_name'      : [ 'mineral',   str_fuzzy_clause ],
-                 'chemical_name'     : [ 'chemname',  str_fuzzy_clause ],
-                 'formula'           : [ 'formula',   formula_clause ],
-                 'volume'            : [ 'vol',       volume_clause ],
-                 'spacegroup'        : [ 'sg',        str_exact_clause ],
-                 'spacegroup_hall'   : [ 'sgHall',    str_exact_clause ],
-                 'a'                 : [ 'a',         length_clause ],
-                 'b'                 : [ 'b',         length_clause ],
-                 'c'                 : [ 'c',         length_clause ],
-                 'alpha'             : [ 'alpha',     angle_clause ],
-                 'beta'              : [ 'beta',      angle_clause ],
-                 'gamma'             : [ 'gamma',     angle_clause ],
-                 'z'                 : [ 'Z',         int_clause ],
-                 'measurement_temp'  : [ 'celltemp',  temperature_clause ],
-                 'diffraction_temp'  : [ 'diffrtemp', temperature_clause ],
-                 'authors'           : [ 'authors',   str_fuzzy_clause ],
-                 'journal'           : [ 'journal',   str_fuzzy_clause ],
-                 'title'             : [ 'title',     str_fuzzy_clause ],
-                 'year'              : [ 'year',      int_clause ],
-                 'journal_volume'    : [ 'volume',    int_clause ],
-                 'journal_issue'     : [ 'issue',     str_exact_clause ],
-                 'first_page'        : [ 'firstpage', str_exact_clause ],
-                 'last_page'         : [ 'lastpage',  str_exact_clause ],
-                 'doi'               : [ 'doi',       str_exact_clause ] }
+    def pressure_clause(self, key, values):
+        """
+        Returns SQL query predicate for querying pressure.
+        """
+        return self.double_clause(key, values, self.pressure_precision)
+
+    keywords = { 'id'                : [ 'file',          int_clause ],
+                 'element'           : [ 'element',       composition_clause ],
+                 'number_of_elements': [ 'nel',           int_clause ],
+                 'mineral_name'      : [ 'mineral',       str_fuzzy_clause ],
+                 'chemical_name'     : [ 'chemname',      str_fuzzy_clause ],
+                 'formula'           : [ 'formula',       formula_clause ],
+                 'volume'            : [ 'vol',           volume_clause ],
+                 'spacegroup'        : [ 'sg',            str_exact_clause ],
+                 'spacegroup_hall'   : [ 'sgHall',        str_exact_clause ],
+                 'a'                 : [ 'a',             length_clause ],
+                 'b'                 : [ 'b',             length_clause ],
+                 'c'                 : [ 'c',             length_clause ],
+                 'alpha'             : [ 'alpha',         angle_clause ],
+                 'beta'              : [ 'beta',          angle_clause ],
+                 'gamma'             : [ 'gamma',         angle_clause ],
+                 'z'                 : [ 'Z',             int_clause ],
+                 'measurement_temp'  : [ 'celltemp',      temperature_clause ],
+                 'diffraction_temp'  : [ 'diffrtemp',     temperature_clause ],
+                 'measurement_pressure':
+                                       [ 'cellpressure',  pressure_clause ],
+                 'diffraction_pressure':
+                                       [ 'diffrpressure', pressure_clause ],
+                 'authors'           : [ 'authors',       str_fuzzy_clause ],
+                 'journal'           : [ 'journal',       str_fuzzy_clause ],
+                 'title'             : [ 'title',         str_fuzzy_clause ],
+                 'year'              : [ 'year',          int_clause ],
+                 'journal_volume'    : [ 'volume',        int_clause ],
+                 'journal_issue'     : [ 'issue',         str_exact_clause ],
+                 'first_page'        : [ 'firstpage',     str_exact_clause ],
+                 'last_page'         : [ 'lastpage',      str_exact_clause ],
+                 'doi'               : [ 'doi',           str_exact_clause ] }
 
     def __init__(self, **kwargs):
         self.db         = None

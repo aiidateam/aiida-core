07565893701e48bac8ed0480c267fad51fe279ae
Andrius Merkys <andrius.merkys@epfl.ch>
2014-10-30 14:25:46 +0100

    Adding one more test case for CodtoolsParser. Fixing a bug.

--- aiida/djsite/db/subtests/codtools.py
+++ aiida/djsite/db/subtests/codtools.py
@@ -91,5 +91,62 @@ class TestCodtools(AiidaTestCase):
         success, nodes = parser.parse_from_calc()
 
         self.assertEquals(success, True)
+        self.assertEquals(len(nodes), 1)
         self.assertEquals(nodes[0][0], 'cif')
         self.assertEquals(isinstance(nodes[0][1], CifData), True)
+        self.assertEquals(nodes[0][1].generate_md5(),
+                          'b5bb739a254514961a157503daf715eb')
+
+    def test_2(self):
+        import tempfile
+        from aiida.orm.data.cif import CifData
+        from aiida.orm.data.folder import FolderData
+        from aiida.orm.data.parameter import ParameterData
+        from aiida.common.exceptions import InputValidationError
+        from aiida.common.datastructures import calc_states
+
+        file_content = "data_test _cell_length_a 10(1)"
+        errors = "first line\nlast line"
+        with tempfile.NamedTemporaryFile() as f:
+            f.write(file_content)
+            f.flush()
+            cif = CifData(file=f.name)
+        c = CodtoolsCalculation(computer=self.computer,
+                                resources={
+                                    'num_machines': 1,
+                                    'num_mpiprocs_per_machine': 1}
+                                )
+        f = SandboxFolder()
+
+        c.use_cif(cif)
+
+        calc = c._prepare_for_submission(f, c.get_inputdata_dict())
+        c.store()
+        c._set_state(calc_states.PARSING)
+
+        fd = FolderData()
+        fd.store()
+
+        fd._add_link_from(c, label="retrieved")
+
+        with open("{}/{}".format(fd._get_folder_pathsubfolder.abspath,
+                                 calc['stdout_name']), 'w') as o:
+            o.write(file_content)
+            o.flush()
+
+        with open("{}/{}".format(fd._get_folder_pathsubfolder.abspath,
+                                 calc['stderr_name']), 'w') as o:
+            o.write(errors)
+            o.flush()
+
+        parser = CodtoolsParser(c)
+        success, nodes = parser.parse_from_calc()
+
+        self.assertEquals(success, True)
+        self.assertEquals(len(nodes), 2)
+        self.assertEquals(nodes[0][0], 'cif')
+        self.assertEquals(isinstance(nodes[0][1], CifData), True)
+        self.assertEquals(nodes[0][1].generate_md5(),
+                          'b5bb739a254514961a157503daf715eb')
+        self.assertEquals(nodes[1][0], 'messages')
+        self.assertEquals(isinstance(nodes[1][1], ParameterData), True)

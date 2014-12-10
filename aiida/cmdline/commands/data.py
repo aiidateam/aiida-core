b940a653fe13e3f7031b3d80191308f3670e6138
Andrea Cepellotti <andrea.cepellotti@epfl.ch>
2014-12-10 15:29:16 +0100

    Implemented a verdi data bands list. There is still space for performance improvements, at least for what concerns the filters on the structure elements. The BandsData prints also the formula of the closest structure found in input.

--- aiida/cmdline/commands/data.py
+++ aiida/cmdline/commands/data.py
@@ -25,6 +25,7 @@ class Data(VerdiCommandRouter):
         self.routed_subcommands = {
             'upf': _Upf,
             'structure': _Structure,
+            'bands': _Bands,
             'cif': _Cif,
             'trajectory': _Trajectory,
             'parameter': _Parameter,
@@ -159,7 +160,173 @@ class _Upf(VerdiCommandWithSubcommands):
         else:
             print "No valid UPF pseudopotential family found."
          
+         
+class _Bands(VerdiCommandWithSubcommands):
+    """
+    Manipulation on the bands
+    """
+    def __init__(self):
+        """
+        A dictionary with valid commands and functions to be called.
+        """
+        self.valid_subcommands = {
+            'list': (self.list, self.complete_none),
+            }
+        
+    def list(self, *args):
+        """
+        List all AiiDA BandsData
+        """
+        import argparse
+        parser = argparse.ArgumentParser(
+            prog=self.get_full_command_name(),
+            description='List AiiDA BandsDatas. Note: the formula is taken '
+                        "from the closest structure found in the database graph")
+        parser.add_argument('-e','--element',nargs='+', type=str, default=None,
+                            help="Print all bandsdatas from structures "
+                            "containing desired elements")
+        parser.add_argument('-eo','--element-only',nargs='+', type=str, default=None,
+                            help="Print all bandsdatas from structures "
+                            "containing only the selected elements")        
+        parser.add_argument('-f', '--formulamode', type=str, default='hill', 
+                            help="Formula printing mode (hill, reduce, allreduce"
+                            " or compact1) (if None, does not print the formula)",
+                            action='store')
+        parser.add_argument('-p', '--past-days', metavar='N', 
+                            help="Add a filter to show only bandsdatas created in the past N days",
+                            type=int, action='store')
+         
+        load_dbenv()        
+        import datetime
+        from collections import defaultdict
+        from aiida.orm import DataFactory
+        from django.db.models import Q
+        from django.utils import timezone
+        from aiida.djsite.utils import get_automatic_user
+        from aiida.common.utils import grouper
+        from aiida.orm.data.structure import (get_formula, get_symbols_string,
+                                              has_vacancies)
+        from aiida.djsite.db import models
+        from aiida.orm import Node
+        from aiida.djsite.db.models import DbPath
+
+        query_group_size = 100 # we group the attribute query in chunks of this size
+        
+        args = list(args)
+        parsed_args = parser.parse_args(args)
+        
+        StructureData = DataFactory('structure')
+        BandsData = DataFactory('array.bands')
+        now = timezone.now()
+        
+        # First, I run a query to get all BandsData of the past N days
+        q_object = Q(user=get_automatic_user())
+        if parsed_args.past_days is not None:
+            now = timezone.now()
+            n_days_ago = now - datetime.timedelta(days=parsed_args.past_days)
+            q_object.add(Q(ctime__gte=n_days_ago), Q.AND)
+        else:
+            bands_list = BandsData.query(q_object).distinct().order_by('ctime')
+        
+        bands_list_data = bands_list.values_list('pk', 'label', 'ctime')
+        
+        # split data in chunks
+        grouped_bands_list_data = grouper(query_group_size,
+                                          [(_[0],_[1],_[2]) for _ in bands_list_data])
+        
+        print "ID\tformula\tctime\tlabel"
+
+        for this_chunk in grouped_bands_list_data:
+            # gather all banddata pks
+            pks = [_[0] for _ in this_chunk]
             
+            # get all the StructureData that are parents of the selected bandsdatas
+            q_object = Q(child__in=pks)
+            q_object.add(Q(child__user=get_automatic_user()), Q.AND)
+            q_object.add(Q(parent__type='data.structure.StructureData.'), Q.AND)
+            structure_list = DbPath.objects.filter(q_object).distinct()
+            structure_list_data = structure_list.values_list('parent_id','child_id','depth')
+            
+            # select the pks of all structure involved
+            # the right structure is chosen as the closest structure in the graph
+            struc_pks = []
+            for band_pk in pks:
+                try:
+                    struc_pks.append(min( [_ for _ in structure_list_data if _[1]==band_pk], 
+                                          key=lambda x:x[1] 
+                                          )[0]
+                                     )
+                except ValueError: # no structure in input
+                    struc_pks.append(None)
+            
+            # query for the attributes needed for the structure formula
+            attr_query = Q(key__startswith='kinds') | Q(key__startswith='sites')
+            attrs = models.DbAttribute.objects.filter(attr_query,
+                        dbnode__in=struc_pks).values_list(
+                                'dbnode__pk', 'key', 'datatype', 'tval', 'fval',
+                                 'ival', 'bval', 'dval')                                   
+            
+            results = defaultdict(dict)
+            for attr in attrs:
+                results[attr[0]][attr[1]] = {"datatype": attr[2], 
+                                             "tval": attr[3],
+                                             "fval": attr[4], 
+                                             "ival": attr[5], 
+                                             "bval": attr[6], 
+                                             "dval": attr[7]}
+            # organize all of it in a dictionary
+            deser_data = {}
+            for k in results:
+                deser_data[k] = models.deserialize_attributes(results[k],
+                    sep=models.DbAttribute._sep)
+            
+            # prepare the printout
+            for ((band_pk,label,date),struc_pk) in zip(this_chunk,struc_pks):
+                if struc_pk is not None:
+                    # Exclude structures by the elements
+                    if parsed_args.element is not None:
+                        all_kinds = [k['symbols'] for k in deser_data[struc_pk]['kinds']]
+                        all_symbols = [ item for sublist in all_kinds for item in sublist ]
+                        if not any( [s in parsed_args.element for s in all_symbols]
+                                    ):
+                            continue
+                    if parsed_args.element_only is not None:
+                        all_kinds = [k['symbols'] for k in deser_data[struc_pk]['kinds']]
+                        all_symbols = [ item for sublist in all_kinds for item in sublist ]
+                        if not all( [s in all_symbols for s in parsed_args.element_only]
+                                   ):
+                            continue
+                    
+                    # build the formula
+                    symbol_dict = { k['name']:get_symbols_string(k['symbols'],
+                                                                 k['weights']) 
+                                    for k in deser_data[struc_pk]['kinds'] }
+                    try:
+                        symbol_list = [ symbol_dict[s['kind_name']] 
+                                        for s in deser_data[struc_pk]['sites'] ]
+                        formula = get_formula(symbol_list,
+                                              mode=parsed_args.formulamode)
+                    # If for some reason there is no kind with the name
+                    # referenced by the site
+                    except KeyError:
+                        formula = "<<UNKNOWN>>"
+                        # cycle if we imposed the filter on elements
+                        if parsed_args.element is not None or parsed_args.element_only is not None:
+                            continue
+                else:
+                    formula = "<<UNKNOWN>>"
+                    # cycle if we imposed the filter on elements
+                    if parsed_args.element is not None or parsed_args.element_only is not None:
+                        continue
+                
+                # print stuff in output
+                to_print = []
+                to_print.append('{}\t{}\t{}\t{}\n'.format(band_pk, 
+                                                          formula,
+                                                          date.strftime('%d %b %Y'),
+                                                          label,
+                                                      ))
+                sys.stdout.write("".join(to_print))
         
     
 class _Structure(VerdiCommandWithSubcommands):

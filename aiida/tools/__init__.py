02430d0af73d40ce0faee39f49ec7a82cca7df1a
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2014-12-15 22:29:51 +0100

    Changed version number, as we changed the schema: this is a big change.

--- aiida/tools/__init__.py
+++ aiida/tools/__init__.py
@@ -2,6 +2,6 @@
 
 __copyright__ = u"Copyright (c), 2014, École Polytechnique Fédérale de Lausanne (EPFL), Switzerland, Laboratory of Theory and Simulation of Materials (THEOS). All rights reserved."
 __license__ = "Non-Commercial, End-User Software License Agreement, see LICENSE.txt file"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 from aiida.tools.dbimporters import DbImporterFactory

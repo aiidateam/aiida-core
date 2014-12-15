41e62154b4af8accab13f76b0ee232eb1a11c10c
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2014-12-15 22:29:51 +0100

    Changed version number, as we changed the schema: this is a big change.

--- aiida/djsite/db/models.py
+++ aiida/djsite/db/models.py
@@ -17,7 +17,7 @@ from aiida.djsite.utils import long_field_length
 
 __copyright__ = u"Copyright (c), 2014, École Polytechnique Fédérale de Lausanne (EPFL), Switzerland, Laboratory of Theory and Simulation of Materials (THEOS). All rights reserved."
 __license__ = "Non-Commercial, End-User Software License Agreement, see LICENSE.txt file"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 # This variable identifies the schema version of this file.
 # Every time you change the schema below in *ANY* way, REMEMBER TO CHANGE
--- aiida/orm/calculation/job/quantumespresso/__init__.py
+++ aiida/orm/calculation/job/quantumespresso/__init__.py
@@ -15,7 +15,7 @@ from aiida.orm.data.remote import RemoteData
 
 __copyright__ = u"Copyright (c), 2014, École Polytechnique Fédérale de Lausanne (EPFL), Switzerland, Laboratory of Theory and Simulation of Materials (THEOS). All rights reserved."
 __license__ = "Non-Commercial, End-User Software License Agreement, see LICENSE.txt file"
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 class BasePwCpInputGenerator(object):
     """

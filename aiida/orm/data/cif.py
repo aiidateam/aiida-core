89d5cd8e5cac39ce8875f620daf39e424d457661
Andrius Merkys <andrius.merkys@epfl.ch>
2015-01-08 15:32:36 +0100

    Enabling the storage of intermediate InlineCalculation, which converts CifData to StructureData.

--- aiida/orm/data/cif.py
+++ aiida/orm/data/cif.py
@@ -317,19 +317,41 @@ class CifData(SinglefileData):
             else:        
                 return (cifs[0], False)
 
-    def _get_aiida_structure(self, **kwargs):
-        converter = 'ase'
-        if 'converter' in kwargs.keys():
-            converter = kwargs.pop('converter')
+    def _get_aiida_structure(self,converter='ase',store=False,**kwargs):
+        """
+        Creates :py:class:`aiida.orm.data.structure.StructureData`.
+
+        :param converter: specify the converter. Default 'ase'.
+        :param store: If True, intermediate calculation gets stored in the
+            AiiDA database for record. Default False.
+        :return: :py:class:`aiida.orm.data.structure.StructureData` node.
+        """
+        from aiida.orm.data.parameter import ParameterData
+        param = ParameterData(dict=kwargs)
         try:
-            conv_f = getattr(self, '_get_aiida_structure_{}'.format(converter))
-            return conv_f(**kwargs)
+            conv_f = getattr(self,
+                             '_get_aiida_structure_{}_inline'.format(converter))
+            ret_dict = None
+            if store:
+                from aiida.orm.calculation.inline import make_inline
+                _,ret_dict = make_inline(conv_f)(parameters=param)
+            else:
+                ret_dict = conv_f(parameters=param)
+            return ret_dict['structure']
         except AttributeError:
             raise ValueError("No such converter '{}' available".format(converter))
 
-    def _get_aiida_structure_ase(self, **kwargs):
+    def _get_aiida_structure_ase_inline(self,parameters=None):
+        """
+        Creates :py:class:`aiida.orm.data.structure.StructureData` using ASE.
+
+        :note: requires ASE module.
+        """
         from aiida.orm.data.structure import StructureData
-        return StructureData(ase=self.get_ase(**kwargs))
+        kwargs = {}
+        if parameters is not None:
+            kwargs = parameters.get_dict()
+        return {'structure': StructureData(ase=self.get_ase(**kwargs))}
 
     @property
     def ase(self):

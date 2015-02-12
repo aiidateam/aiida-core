9ff6fdf862fd00f59ad8a9aa19b22f73f4be0523
Andrea Cepellotti <andrea.cepellotti@epfl.ch>
2015-02-12 14:43:05 +0100

    Removed a comment on south

--- aiida/djsite/settings/settings.py
+++ aiida/djsite/settings/settings.py
@@ -199,7 +199,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-#    'south',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'aiida.djsite.db',

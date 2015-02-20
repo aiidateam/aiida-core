d080c7f3a066ee1542b5cfafdf9491ef194fe16a
Nicolas Mounet <nicolas.mounet@epfl.ch>
2015-02-20 16:33:29 +0100

    one file forgotten in the previous commit (load_workflow test)

---
+++ aiida/djsite/db/subtests/workflows.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for workflows
+"""
+from django.utils import unittest
+
+from aiida.orm import Workflow
+from aiida.djsite.db.testbase import AiidaTestCase
+from aiida.workflows.test import WorkflowTestEmpty
+ 
+__copyright__ = u"Copyright (c), 2014, École Polytechnique Fédérale de Lausanne (EPFL), Switzerland, Laboratory of Theory and Simulation of Materials (THEOS). All rights reserved."
+__license__ = "Non-Commercial, End-User Software License Agreement, see LICENSE.txt file"
+__version__ = "0.3.0"
+
+
+class TestWorkflowBasic(AiidaTestCase):
+    """
+    These tests check the basic features of workflows.
+    Now only the load_workflow function is tested.
+    """
+
+    def test_load_workflows(self):
+        """
+        Test for load_node() function.
+        """
+        from aiida.orm import load_workflow
+        from aiida.common.exceptions import NotExistent
+        a = WorkflowTestEmpty()
+        a.store()
+
+        self.assertEquals(a.pk,load_workflow(wf_id=a.pk).pk)
+        self.assertEquals(a.pk,load_workflow(wf_id=a.uuid).pk)
+        self.assertEquals(a.pk,load_workflow(pk=a.pk).pk)
+        self.assertEquals(a.pk,load_workflow(uuid=a.uuid).pk)
+
+        with self.assertRaises(ValueError):
+            load_workflow(wf_id=a.pk,pk=a.pk)
+        with self.assertRaises(ValueError):
+            load_workflow(pk=a.pk,uuid=a.uuid)
+        with self.assertRaises(ValueError):
+            load_workflow(pk=a.uuid)
+        with self.assertRaises(NotExistent):
+            load_workflow(uuid=a.pk)
+        with self.assertRaises(ValueError):
+            load_workflow()
+
+

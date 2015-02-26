6b5e2672e7999f02a3bb987ccf69fcdb390f3e6d
Andrea Cepellotti <andrea.cepellotti@epfl.ch>
2015-02-26 23:06:21 +0100

    Removed empty files.

--- aiida/scheduler/plugins/torque.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__copyright__ = u"Copyright (c), 2014, École Polytechnique Fédérale de Lausanne (EPFL), Switzerland, Laboratory of Theory and Simulation of Materials (THEOS). All rights reserved."
-__license__ = "Non-Commercial, End-User Software License Agreement, see LICENSE.txt file"
-__version__ = "0.3.0"
-

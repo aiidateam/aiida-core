65cf0c0682656447eefa06a3969feaf898f6a7a4
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2015-04-01 14:28:56 +0200

    Missing version change in docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ copyright = u'2015, ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE (Theory and Simulat
 # The short X.Y version.
 version = '0.4'
 # The full version, including alpha/beta/rc tags.
-release = '0.4.0'
+release = '0.4.1'
 
 author = "Giovanni Pizzi, Andrea Cepellotti, Riccardo Sabatini, Nicola Marzari, Boris Kozinsky"
 

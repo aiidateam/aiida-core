4aa3560099f2ddff68ac70c468a1d68d98ff3a55
Andrius Merkys <andrius.merkys@epfl.ch>
2015-04-01 11:50:43 +0200

    Adding (very basic) input plugin and parser for NWChem. Tested with SCF calculation of H2O.

---
+++ aiida/orm/calculation/job/nwchem/basic.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+
+from aiida.orm.calculation.job import JobCalculation
+from aiida.orm.data.parameter import ParameterData
+from aiida.orm.data.structure import StructureData
+from aiida.common.datastructures import CalcInfo
+from aiida.common.exceptions import InputValidationError
+from aiida.common.utils import classproperty
+
+__copyright__ = u"Copyright (c), 2015, ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE (Theory and Simulation of Materials (THEOS) and National Centre for Computational Design and Discovery of Novel Materials (NCCR MARVEL)), Switzerland and ROBERT BOSCH LLC, USA. All rights reserved."
+__license__ = "MIT license, see LICENSE.txt file"
+__version__ = "0.4.0"
+__contributors__ = "Andrea Cepellotti, Giovanni Pizzi, Andrius Merkys"
+
+class BasicCalculation(JobCalculation):
+    """
+    Generic input plugin for nwchem.
+    """
+    def _init_internal_params(self):
+        super(BasicCalculation, self)._init_internal_params()
+
+        # Name of the default parser
+        self._default_parser = 'nwchem.basic'
+
+        # Default input and output files
+        self._DEFAULT_INPUT_FILE  = 'aiida.in'
+        self._DEFAULT_OUTPUT_FILE = 'aiida.out'
+        self._DEFAULT_ERROR_FILE  = 'aiida.err'
+
+        # Default command line parameters
+        self._default_commandline_params = [ self._DEFAULT_INPUT_FILE ]
+
+    @classproperty
+    def _use_methods(cls):
+        retdict = JobCalculation._use_methods
+        retdict.update({
+            "structure": {
+               'valid_types': StructureData,
+               'additional_parameter': None,
+               'linkname': 'structure',
+               'docstring': "A structure to be processed",
+               },
+            "parameters": {
+               'valid_types': ParameterData,
+               'additional_parameter': None,
+               'linkname': 'parameters',
+               'docstring': "Parameters used to describe the calculation",
+               },
+            })
+        return retdict
+
+    def _prepare_for_submission(self,tempfolder,inputdict):
+        try:
+            struct = inputdict.pop(self.get_linkname('structure'))
+        except KeyError:
+            raise InputValidationError("no structure is specified for this calculation")
+        if not isinstance(struct, StructureData):
+            raise InputValidationError("struct is not of type StructureData")
+
+        atoms = struct.get_ase()
+
+        parameters = inputdict.pop(self.get_linkname('parameters'), None)
+        if parameters is None:
+            parameters = ParameterData(dict={})
+        if not isinstance(parameters, ParameterData):
+            raise InputValidationError("parameters is not of type ParameterData")
+        par = parameters.get_dict()
+
+        abbreviation = par.pop('abbreviation','aiida_calc')
+        title = par.pop('title','AiiDA NWChem calculation')
+        basis = par.pop('basis',None)
+        task = par.pop('task','scf')
+
+        if basis is None:
+            basis = dict()
+            for atom_type in set(atoms.get_chemical_symbols()):
+                basis[atom_type] = 'library 6-31g'
+
+        input_filename = tempfolder.get_abs_path(self._DEFAULT_INPUT_FILE)
+        with open(input_filename,'w') as f:
+            f.write('start {}\ntitle "{}"\n\n'.format(abbreviation,title))
+            f.write('geometry units au\n')
+            for i,atom_type in enumerate(atoms.get_chemical_symbols()):
+                f.write('    {} {} {} {}\n'.format(atom_type,
+                                               atoms.get_positions()[i][0],
+                                               atoms.get_positions()[i][1],
+                                               atoms.get_positions()[i][2]))
+            f.write('end\nbasis\n')
+            for atom_type,b in basis.iteritems():
+                f.write('    {} {}\n'.format(atom_type,b))
+            f.write('end\ntask {}\n'.format(task))
+            f.flush()
+
+        commandline_params = self._default_commandline_params
+
+        calcinfo = CalcInfo()
+        calcinfo.uuid = self.uuid
+        calcinfo.cmdline_params = commandline_params
+        calcinfo.local_copy_list = []
+        calcinfo.remote_copy_list = []
+        calcinfo.stdout_name = self._DEFAULT_OUTPUT_FILE
+        calcinfo.stderr_name = self._DEFAULT_ERROR_FILE
+        calcinfo.retrieve_list = [self._DEFAULT_OUTPUT_FILE,
+                                  self._DEFAULT_ERROR_FILE]
+        calcinfo.retrieve_singlefile_list = []
+
+        return calcinfo

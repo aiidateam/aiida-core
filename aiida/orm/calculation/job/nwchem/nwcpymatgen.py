133c5fc51fbebd2d9c03b8fb531cc06bd75dfae0
Andrius Merkys <andrius.merkys@epfl.ch>
2015-04-02 13:22:00 +0200

    Transforming structure from StructureData to coordinates, used by pymatgen's NwInput.

--- aiida/orm/calculation/job/nwchem/nwcpymatgen.py
+++ aiida/orm/calculation/job/nwchem/nwcpymatgen.py
@@ -14,6 +14,50 @@ __license__ = "MIT license, see LICENSE.txt file"
 __version__ = "0.4.0"
 __contributors__ = "Andrea Cepellotti, Giovanni Pizzi, Andrius Merkys"
 
+def _prepare_pymatgen_dict(par,atoms=None):
+    from pymatgen.io.nwchemio import NwTask,NwInput
+
+    add_cell = par.pop('add_cell',False)
+    if atoms:
+        if add_cell:
+            import numpy as np
+
+            lat_lengths = [
+                (atoms.cell[0]**2).sum()**0.5,
+                (atoms.cell[1]**2).sum()**0.5,
+                (atoms.cell[2]**2).sum()**0.5,
+            ]
+
+            lat_angles = list(np.arccos([
+                np.vdot(atoms.cell[1],atoms.cell[2])/lat_lengths[1]/lat_lengths[2],
+                np.vdot(atoms.cell[0],atoms.cell[2])/lat_lengths[0]/lat_lengths[2],
+                np.vdot(atoms.cell[0],atoms.cell[1])/lat_lengths[0]/lat_lengths[1],
+            ])/np.pi*180)
+
+            par['geometry_options'].append(
+                '\n  system crystal\n'
+                '    lat_a {}\n    lat_b {}\n    lat_c {}\n'
+                '    alpha {}\n    beta  {}\n    gamma {}\n'
+                '  end'.format(*(lat_lengths+lat_angles)))
+
+        if 'mol' not in par:
+            par['mol'] = {}
+        if 'sites' not in par['mol']:
+            par['mol']['sites'] = []
+
+        for i,atom_type in enumerate(atoms.get_chemical_symbols()):
+            xyz = []
+            if add_cell:
+                xyz = atoms.get_scaled_positions()[i]
+            else:
+                xyz = atoms.get_positions()[i]
+            par['mol']['sites'].append({
+                'species': [ { 'element': atom_type, 'occu': 1 } ],
+                'xyz': xyz
+            })
+
+    return str(NwInput.from_dict(par))
+
 class NwcpymatgenCalculation(JobCalculation):
     """
     Generic input plugin for NWChem, using pymatgen.
@@ -36,6 +80,12 @@ class NwcpymatgenCalculation(JobCalculation):
     def _use_methods(cls):
         retdict = JobCalculation._use_methods
         retdict.update({
+            "structure": {
+               'valid_types': StructureData,
+               'additional_parameter': None,
+               'linkname': 'structure',
+               'docstring': "A structure to be processed",
+               },
             "parameters": {
                'valid_types': ParameterData,
                'additional_parameter': None,
@@ -54,9 +104,16 @@ class NwcpymatgenCalculation(JobCalculation):
         if not isinstance(parameters, ParameterData):
             raise InputValidationError("parameters is not of type ParameterData")
 
+        atoms = None
+        if 'structure' in inputdict:
+            struct = inputdict.pop(self.get_linkname('structure'))
+            if not isinstance(struct, StructureData):
+                raise InputValidationError("structure is not of type StructureData")
+            atoms = struct.get_ase()
+
         input_filename = tempfolder.get_abs_path(self._DEFAULT_INPUT_FILE)
         with open(input_filename,'w') as f:
-            f.write(str(NwInput.from_dict(parameters.get_dict())))
+            f.write(_prepare_pymatgen_dict(parameters.get_dict(),atoms))
             f.flush()
 
         commandline_params = self._default_commandline_params

592ed883dc9dfa410407260e99b99dee23a6b4c1
Andrius Merkys <andrius.merkys@epfl.ch>
2015-04-10 10:58:28 +0200

    Moving common code for cod-tools parsers to a base class.

--- aiida/parsers/plugins/codtools/cifcodcheck.py
+++ aiida/parsers/plugins/codtools/cifcodcheck.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
-"""
-Plugin to parse outputs from the scripts from cod-tools package.
-This plugin is in the development stage. Andrius Merkys, 2014-10-29
-"""
-from aiida.parsers.plugins.codtools.ciffilter import CiffilterParser
+
+from aiida.parsers.plugins.codtools.baseclass import BaseCodtoolsParser
 from aiida.orm.calculation.job.codtools.cifcodcheck import CifcodcheckCalculation
-from aiida.orm.data.parameter import ParameterData
 
 __copyright__ = u"Copyright (c), 2015, ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE (Theory and Simulation of Materials (THEOS) and National Centre for Computational Design and Discovery of Novel Materials (NCCR MARVEL)), Switzerland and ROBERT BOSCH LLC, USA. All rights reserved."
 __license__ = "MIT license, see LICENSE.txt file"
 __version__ = "0.4.1"
 __contributors__ = "Andrea Cepellotti, Andrius Merkys, Giovanni Pizzi"
 
-class CifcodcheckParser(CiffilterParser):
+class CifcodcheckParser(BaseCodtoolsParser):
     """
     Specific parser for the output of cif_cod_check script.
     """
-    def _check_calc_compatibility(self,calc):
-        from aiida.common.exceptions import ParsingError
-        if not isinstance(calc,CifcodcheckCalculation):
-            raise ParsingError("Input calc must be a CifcodcheckCalculation")
+    def __init__(self,calc):
+        """
+        Initialize the instance of CifcodcheckParser
+        """
+        # Check for valid input:
+        self._supported_calculation_class = CifcodcheckCalculation
+        super(CifcodcheckParser, self).__init__(calc)
 
     def _get_output_nodes(self, output_path, error_path):
         """
         Extracts output nodes from the standard output and standard error
         files.
         """
+        from aiida.orm.data.parameter import ParameterData
         import re
 
         messages = []

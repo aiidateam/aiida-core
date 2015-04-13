c06f07a5c4cfe6e0db5fe69f63860955b70e56a1
Andrius Merkys <andrius.merkys@epfl.ch>
2015-04-13 11:33:49 +0200

    Fixing a bug with parameter passing for _prepare_tcod() method of TrajectoryData.

--- aiida/orm/data/array/trajectory.py
+++ aiida/orm/data/array/trajectory.py
@@ -379,12 +379,12 @@ class TrajectoryData(ArrayData):
             cif = cif + ciffile.WriteOut()
         return cif
 
-    def _prepare_tcod(self,step=None,**kwargs):
+    def _prepare_tcod(self,**kwargs):
         """
         Write the given trajectory to a string of format TCOD CIF.
         """
         from aiida.tools.dbexporters.tcod import export_cif
-        return export_cif(self,trajectory_index=step,**kwargs)
+        return export_cif(self,**kwargs)
 
     def _get_aiida_structure(self,store=False,**kwargs):
         """

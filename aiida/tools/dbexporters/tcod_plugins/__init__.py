ddeaf22b7a99f8bb76446827631c5be004488c96
Andrius Merkys <andrius.merkys@epfl.ch>
2015-04-16 16:52:26 +0200

    Updating the AiiDA version number.

--- aiida/tools/dbexporters/tcod_plugins/__init__.py
+++ aiida/tools/dbexporters/tcod_plugins/__init__.py
@@ -2,7 +2,7 @@
 
 __copyright__ = u"Copyright (c), 2014, École Polytechnique Fédérale de Lausanne (EPFL), Switzerland, Laboratory of Theory and Simulation of Materials (THEOS). All rights reserved."
 __license__ = "Non-Commercial, End-User Software License Agreement, see LICENSE.txt file"
-__version__ = "0.2.1"
+__version__ = "0.4.1"
 
 class BaseTcodtranslator(object):
     """

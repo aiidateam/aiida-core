aiidateam/aiida-core

commit 7c91c1a9a76e5c25983fa13f190fa5ce6d375be1

Moving the code of get_aiida_structure() from CodEntry to it's baseclass DbEntry.

aiida/tools/dbimporters/baseclasses.py
@@ -242,8 +242,13 @@ class DbEntry(object):
         """
         Returns AiiDA-compatible structure, representing the crystal
         structure from the CIF file.
+
+        .. note:: To be removed, as it is duplicated in
+            :py:class:`aiida.orm.data.cif.CifData`.
         """
-        raise NotImplementedError("not implemented in base class")
+        import ase.io.cif
+        import StringIO
+        return ase.io.cif.read_cif( StringIO.StringIO( self.cif ) )
 
     def get_parsed_cif(self):
         """

aiida/tools/dbimporters/plugins/cod.py
@@ -307,14 +307,3 @@ class CodEntry(DbEntry):
         """
         super(CodEntry, self).__init__(db_source=db_source,db_url=db_url,
                                        url=url,**kwargs)
-
-    def get_ase_structure(self):
-        """
-        Returns ASE representation of the CIF.
-
-        .. note:: To be removed, as it is duplicated in
-            :py:class:`aiida.orm.data.cif.CifData`.
-        """
-        import ase.io.cif
-        import StringIO
-        return ase.io.cif.read_cif( StringIO.StringIO( self.cif ) )

commit 94fabf2cba53eef8e56d58012e8454f0c8f13a2c

Fixing the bug introduced in the commit 7c91c1a: wrong name was given to a method.

aiida/tools/dbimporters/baseclasses.py
@@ -224,7 +224,9 @@ class DbEntry(object):
         .. note:: To be removed, as it is duplicated in
             :py:class:`aiida.orm.data.cif.CifData`.
         """
-        raise NotImplementedError("not implemented in base class")
+        import ase.io.cif
+        import StringIO
+        return ase.io.cif.read_cif( StringIO.StringIO( self.cif ) )
 
     def get_cif_node(self,store=False):
         """
@@ -242,14 +244,8 @@ class DbEntry(object):
         """
         Returns AiiDA-compatible structure, representing the crystal
         structure from the CIF file.
-
-        .. note:: To be removed, as it is duplicated in
-            :py:class:`aiida.orm.data.cif.CifData`.
         """
-        import ase.io.cif
-        import StringIO
-        return ase.io.cif.read_cif( StringIO.StringIO( self.cif ) )
-
+        raise NotImplementedError("not implemented in base class")
     def get_parsed_cif(self):
         """
         Returns data structure, representing the CIF file. Can be created
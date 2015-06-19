dd267ca839b14dbef9be2af9d8d0d7f2b3774c69
Andrius Merkys <andrius.merkys@epfl.ch>
2015-06-19 11:27:14 +0200

    Rewriting StructureData.get_pymatgen_structure() to perform less calls to get_kind().

--- aiida/orm/data/structure.py
+++ aiida/orm/data/structure.py
@@ -917,10 +917,11 @@ class StructureData(Data):
         """
         from pymatgen.core.structure import Structure
 
-        species = [{self.get_kind(x.kind_name).symbols[i]:
-                        self.get_kind(x.kind_name).weights[i]
-                    for i in range(0, len(self.get_kind(x.kind_name).symbols))}
-                   for x in self.sites]
+        species = []
+        for s in self.sites:
+            k = self.get_kind(s.kind_name)
+            species.append({s: w for s, w in zip(k.symbols, k.weights)})
+
         positions = [list(x.position) for x in self.sites]
         return Structure(self.cell, species, positions,
                          coords_are_cartesian=True)

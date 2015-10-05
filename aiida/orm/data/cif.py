443af92cb2f2d7058b0a94515f3295b3cea213d6
Andrius Merkys <andrius.merkys@epfl.ch>
2015-10-05 17:11:16 +0200

    Enhancing docstrings of encode_* and decode_* functions of CifData.

--- aiida/orm/data/cif.py
+++ aiida/orm/data/cif.py
@@ -38,7 +38,8 @@ def has_pycifrw():
 
 def encode_textfield_base64(content, foldwidth=76):
     """
-    Encodes the contents for CIF textfield in Base64.
+    Encodes the contents for CIF textfield in Base64 using standard Python
+    implementation (``base64.standard_b64encode()``).
 
     :param content: a string with contents
     :param foldwidth: maximum width of line (default is 76)
@@ -54,7 +55,8 @@ def encode_textfield_base64(content, foldwidth=76):
 
 def decode_textfield_base64(content):
     """
-    Decodes the contents for CIF textfield from Base64.
+    Decodes the contents for CIF textfield from Base64 using standard
+    Python implementation (``base64.standard_b64decode()``)
 
     :param content: a string with contents
     :return: decoded string
@@ -67,6 +69,12 @@ def decode_textfield_base64(content):
 def encode_textfield_quoted_printable(content):
     """
     Encodes the contents for CIF textfield in quoted-printable encoding.
+    In addition to non-ASCII characters, that are encoded by Python
+    function ``quopri.encodestring()``, following characters are encoded:
+
+        * '``;``', if encountered on the beginning of the line;
+        * '``\\t``'
+        * '``.``' and '``?``', if comprise the entire textfield.
 
     :param content: a string with contents
     :return: encoded string
@@ -110,6 +118,12 @@ def decode_textfield_quoted_printable(content):
 def encode_textfield_ncr(content):
     """
     Encodes the contents for CIF textfield in Numeric Character Reference.
+    Encoded characters:
+
+        * ``\\x09``, ``\\x0A``, ``\\x0D``, ``\\x20``--``\\x7E``;
+        * '``;``', if encountered on the beginning of the line;
+        * '``\\t``'
+        * '``.``' and '``?``', if comprise the entire textfield.
 
     :param content: a string with contents
     :return: encoded string

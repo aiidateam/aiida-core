b4db2a0fad0d5b7656e3a0057c9b8a24e407109a
Andrius Merkys <andrius.merkys@epfl.ch>
2015-10-13 10:03:42 +0200

    Adding a note on _inline_to_standalone_script().

--- aiida/tools/dbexporters/tcod.py
+++ aiida/tools/dbexporters/tcod.py
@@ -188,6 +188,16 @@ def _assert_same_parents(a, b):
 def _inline_to_standalone_script(calc):
     """
     Create executable bash script for execution of inline script.
+
+    .. note:: the output bash script may not always be correct, since it
+        is simply formed from:
+        * contents of the file, which contains the original ``\*_inline``
+          function;
+        * call of the original ``\*_inline`` function with input nodes;
+        * storing of the output nodes.
+        Execution of generated bash script should result in
+        ModificationNotAllowed exception, since the nodes, that are
+        created by the ``\*_inline`` function, are already stored.
     """
     input_dict = calc.get_inputs_dict()
     args = ["{}=Node.get_subclass_from_uuid('{}')".format(x, input_dict[x].uuid)

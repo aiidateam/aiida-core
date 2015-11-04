a840af7b360853d9aebf9ef241b2104e90f6490d
Mario <mario.zic.st@gmail.com>
2015-11-04 23:10:01 +0000

    Added a test to make sure that 'num_cores_per_node' does not affect the 'mpiprocs' property in the PBS header

--- aiida/scheduler/plugins/test_pbspro.py
+++ aiida/scheduler/plugins/test_pbspro.py
@@ -278,7 +278,7 @@ Job Id: 74164.mycluster
 	PBS_O_MAIL=/var/spool/mail/user3,PBS_O_QUEUE=P_share_queue,
 	PBS_O_HOST=mycluster.cluster
     comment = Job run at Mon Apr 22 at 07:17 on (b270:ncpus=16)+(b275:ncpus=16)
-	
+
     etime = Mon Apr 22 07:17:34 2013
     Submit_arguments = -W depend=afterany:74163 u-100-l-96.job
     project = _pbs_project_default
@@ -512,7 +512,7 @@ Job Id: 547637
     Checkpoint = u
     ctime = Fri Jun 19 14:00:43 2015
     Error_Path = server.domain:/path/to/error/file
-	
+
     Hold_Types = n
     Join_Path = oe
     Keep_Files = n
@@ -870,7 +870,7 @@ class TestParserQstat(unittest.TestCase):
 #            """
 #            The qstat -f command has received a retval != 0
 #            """
-#            s = PbsproScheduler()            
+#            s = PbsproScheduler()
 #            retval = 1
 #            stdout = text_qstat_f_to_test
 #            stderr = ''
@@ -885,7 +885,7 @@ class TestParserQstat(unittest.TestCase):
 #            """
 #            The qstat -f command has received a stderr
 #            """
-#            s = PbsproScheduler()            
+#            s = PbsproScheduler()
 #            retval = 0
 #            stdout = text_qstat_f_to_test
 #            stderr = 'A non empty error message'
@@ -921,3 +921,35 @@ class TestSubmitScript(unittest.TestCase):
         self.assertTrue("'mpirun' '-np' '23' 'pw.x' '-npool' '1'" + \
                         " < 'aiida.in'" in submit_script_text)
 
+    def test_submit_script_with_num_cores_per_node(self):
+        """
+        """
+        from aiida.scheduler.datastructures import JobTemplate
+
+        s = PbsproScheduler()
+
+        job_tmpl = JobTemplate()
+        job_tmpl.job_resource = s.create_job_resource(
+            num_machines=1,
+            num_mpiprocs_per_machine=2,
+            num_cores_per_node=24
+        )
+        job_tmpl.uuid = str(uuid.uuid4())
+        job_tmpl.max_wallclock_seconds = 24 * 3600
+        code_info = CodeInfo()
+        code_info.cmdline_params = ["mpirun", "-np", "23",
+                                    "pw.x", "-npool", "1"]
+        code_info.stdin_name = 'aiida.in'
+        job_tmpl.codes_info = [code_info]
+        job_tmpl.codes_run_mode = code_run_modes.SERIAL
+
+        submit_script_text = s.get_submit_script(job_tmpl)
+
+        self.assertTrue('#PBS -r n' in submit_script_text)
+        self.assertTrue(submit_script_text.startswith('#!/bin/bash'))
+
+        self.assertTrue('#PBS -l select=1:mpiprocs=2' in submit_script_text)
+        # Note: here 'num_cores_per_node' should NOT override the mpiprocs
+
+        self.assertTrue("'mpirun' '-np' '23' 'pw.x' '-npool' '1'" +
+                        " < 'aiida.in'" in submit_script_text)

139856ce0b96420c3c6d24358cbec7bbbfaeda33
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2015-11-18 13:02:33 +0100

    Copy the transitive closure table install into SQLA folder
    TODO: this should be moved or abstracted somewhere

--- aiida/backends/sqlalchemy/utils.py
+++ aiida/backends/sqlalchemy/utils.py
@@ -120,3 +120,200 @@ def get_configured_user_email():
         raise ConfigurationError("No 'default_user' key found in the "
                                  "AiiDA configuration file")
     return email
+
+
+def install_tc(session):
+    links_table_name = "db_dblink"
+    links_table_input_field = "input_id"
+    links_table_output_field = "output_id"
+    closure_table_name = "db_dbpath"
+    closure_table_parent_field = "parent_id"
+    closure_table_child_field = "child_id"
+
+    session.execute(get_pg_tc(links_table_name, links_table_input_field,
+                              links_table_output_field, closure_table_name,
+                              closure_table_parent_field,
+                              closure_table_child_field))
+
+#====================================
+#   Postgresql Transive Closure
+#====================================
+
+def get_pg_tc(links_table_name,
+              links_table_input_field,
+              links_table_output_field,
+              closure_table_name,
+              closure_table_parent_field,
+              closure_table_child_field):
+    from string import Template
+
+    pg_tc = Template("""
+
+DROP TRIGGER IF EXISTS autoupdate_tc ON $links_table_name;
+DROP FUNCTION IF EXISTS update_tc();
+
+CREATE OR REPLACE FUNCTION update_tc()
+  RETURNS trigger AS
+$$BODY$$
+DECLARE
+
+    new_id INTEGER;
+    old_id INTEGER;
+    num_rows INTEGER;
+
+BEGIN
+
+  IF tg_op = 'INSERT' THEN
+
+    IF EXISTS (
+      SELECT Id FROM $closure_table_name
+      WHERE $closure_table_parent_field = new.$links_table_input_field
+         AND $closure_table_child_field = new.$links_table_output_field
+         AND depth = 0
+         )
+    THEN
+      RETURN null;
+    END IF;
+
+    IF new.$links_table_input_field = new.$links_table_output_field
+    OR EXISTS (
+      SELECT id FROM $closure_table_name
+        WHERE $closure_table_parent_field = new.$links_table_output_field
+        AND $closure_table_child_field = new.$links_table_input_field
+        )
+    THEN
+      RETURN null;
+    END IF;
+
+    INSERT INTO $closure_table_name (
+         $closure_table_parent_field,
+         $closure_table_child_field,
+         depth)
+      VALUES (
+         new.$links_table_input_field,
+         new.$links_table_output_field,
+         0);
+
+    new_id := lastval();
+
+    UPDATE $closure_table_name
+      SET entry_edge_id = new_id
+        , exit_edge_id = new_id
+        , direct_edge_id = new_id
+      WHERE id = new_id;
+
+
+    INSERT INTO $closure_table_name (
+      entry_edge_id,
+      direct_edge_id,
+      exit_edge_id,
+      $closure_table_parent_field,
+      $closure_table_child_field,
+      depth)
+      SELECT id
+         , new_id
+         , new_id
+         , $closure_table_parent_field
+         , new.$links_table_output_field
+         , depth + 1
+        FROM $closure_table_name
+        WHERE $closure_table_child_field = new.$links_table_input_field;
+
+
+    INSERT INTO $closure_table_name (
+      entry_edge_id,
+      direct_edge_id,
+      exit_edge_id,
+      $closure_table_parent_field,
+      $closure_table_child_field,
+      depth)
+      SELECT new_id
+        , new_id
+        , id
+        , new.$links_table_input_field
+        , $closure_table_child_field
+        , depth + 1
+        FROM $closure_table_name
+        WHERE $closure_table_parent_field = new.$links_table_output_field;
+
+    INSERT INTO $closure_table_name (
+      entry_edge_id,
+      direct_edge_id,
+      exit_edge_id,
+      $closure_table_parent_field,
+      $closure_table_child_field,
+      depth)
+      SELECT A.id
+        , new_id
+        , B.id
+        , A.$closure_table_parent_field
+        , B.$closure_table_child_field
+        , A.depth + B.depth + 2
+     FROM $closure_table_name A
+        CROSS JOIN $closure_table_name B
+     WHERE A.$closure_table_child_field = new.$links_table_input_field
+       AND B.$closure_table_parent_field = new.$links_table_output_field;
+
+  END IF;
+
+  IF tg_op = 'DELETE' THEN
+
+    IF NOT EXISTS(
+        SELECT id FROM $closure_table_name
+        WHERE $closure_table_parent_field = old.$links_table_input_field
+        AND $closure_table_child_field = old.$links_table_output_field AND
+        depth = 0 )
+    THEN
+        RETURN NULL;
+    END IF;
+
+    CREATE TABLE PurgeList (Id int);
+
+    INSERT INTO PurgeList
+      SELECT id FROM $closure_table_name
+          WHERE $closure_table_parent_field = old.$links_table_input_field
+        AND $closure_table_child_field = old.$links_table_output_field AND
+        depth = 0;
+
+    WHILE (1 = 1)
+    loop
+
+      INSERT INTO PurgeList
+        SELECT id FROM $closure_table_name
+          WHERE depth > 0
+          AND ( entry_edge_id IN ( SELECT Id FROM PurgeList )
+          OR direct_edge_id IN ( SELECT Id FROM PurgeList )
+          OR exit_edge_id IN ( SELECT Id FROM PurgeList ) )
+          AND Id NOT IN (SELECT Id FROM PurgeList );
+
+      GET DIAGNOSTICS num_rows = ROW_COUNT;
+      if (num_rows = 0) THEN
+        EXIT;
+      END IF;
+    end loop;
+
+    DELETE FROM $closure_table_name WHERE Id IN ( SELECT Id FROM PurgeList);
+    DROP TABLE PurgeList;
+
+  END IF;
+
+  RETURN NULL;
+
+END
+$$BODY$$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+
+CREATE TRIGGER autoupdate_tc
+  AFTER INSERT OR DELETE OR UPDATE
+  ON $links_table_name FOR each ROW
+  EXECUTE PROCEDURE update_tc();
+
+""")
+    return pg_tc.substitute(links_table_name=links_table_name,
+                            links_table_input_field=links_table_input_field,
+                            links_table_output_field=links_table_output_field,
+                            closure_table_name=closure_table_name,
+                            closure_table_parent_field=closure_table_parent_field,
+                            closure_table_child_field=closure_table_child_field)

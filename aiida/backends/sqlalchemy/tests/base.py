86c1f8aa89cf9cb350d9150a60ae58a74701bb2e
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2015-11-18 20:41:56 +0100

    Fix various DB conn. issues, add inject_computer decorator for tests

--- aiida/backends/sqlalchemy/tests/base.py
+++ aiida/backends/sqlalchemy/tests/base.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import functools
 
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import sessionmaker
 
 
-
-from aiida.backends import sqlalchemy
+from aiida.backends import sqlalchemy as sa
 from aiida.backends.sqlalchemy.utils import (
     load_dbenv, is_dbenv_loaded, get_configured_user_email, get_automatic_user,
     install_tc
@@ -19,50 +20,87 @@ from aiida.orm.computer import Computer
 
 from aiida.common.setup import get_profile_config
 
+Session = sessionmaker()
+
 class SqlAlchemyTests(unittest.TestCase):
 
     # Specify the need to drop the table at the beginning of a test case
-    drop_all = False
+    drop_all = True
 
     @classmethod
     def setUpClass(cls):
-        if not is_dbenv_loaded():
-            load_dbenv()
 
-        cls._session = sqlalchemy.session
-        cls._engine = cls._session.bind
+        config = get_profile_config("tests")
+        engine_url = ("postgresql://{AIIDADB_USER}:{AIIDADB_PASS}@"
+                      "{AIIDADB_HOST}:{AIIDADB_PORT}/{AIIDADB_NAME}").format(**config)
+        engine = create_engine(engine_url)
+
+        cls.connection = engine.connect()
+
+        session = Session(bind=cls.connection)
+        sa.session = session
 
         if cls.drop_all:
-            Base.metadata.drop_all(cls._engine)
-        Base.metadata.create_all(cls._engine)
-        install_tc(cls._session)
+            Base.metadata.drop_all(cls.connection)
+        Base.metadata.create_all(cls.connection)
+        install_tc(cls.connection)
 
         email = get_configured_user_email()
 
-        user = DbUser.query.filter_by(email=email).first()
-        if not user:
-            user = DbUser(email, "foo", "bar", "tests")
-            cls._session.add(user)
+        user = DbUser(email, "foo", "bar", "tests")
+        session.add(user)
 
-        cls.computer = DbComputer.query.filter_by(name="localhost").first()
-        if not cls.computer:
-            cls.computer = Computer(name='localhost',
-                                    hostname='localhost',
-                                    transport_type='local',
-                                    scheduler_type='pbspro',
-                                    workdir='/tmp/aiida')
-            cls.computer.store()
+        session.commit()
 
-        cls._user = user
+        computer = Computer(name='localhost',
+                            hostname='localhost',
+                            transport_type='local',
+                            scheduler_type='pbspro',
+                            workdir='/tmp/aiida')
+        computer.store()
+
+        session.close()
+
+    @staticmethod
+    def inject_computer(f):
+        @functools.wraps(f)
+        def dec(*args, **kwargs):
+            computer = DbComputer.query.filter_by(name="localhost").first()
+            args = list(args)
+            args.insert(1, computer)
+            return f(*args, **kwargs)
+
+        return dec
+
 
     @classmethod
     def tearDownClass(cls):
-        cls._session.close()
+        # Clean what we added before
+        cls.connection.close()
 
     def setUp(self):
-        self.__class__._session.begin_nested()
+        connec = self.__class__.connection
+        self.trans = connec.begin()
+        self.session = Session(bind=connec)
+        sa.session = self.session
+
+
+        self.session.begin_nested()
+
+        # then each time that SAVEPOINT ends, reopen it
+        @event.listens_for(self.session, "after_transaction_end")
+        def restart_savepoint(session, transaction):
+            if transaction.nested and not transaction._parent.nested:
+
+                # ensure that state is expired the way
+                # session.commit() at the top level normally does
+                # (optional step)
+                # session.expire_all()
+
+                session.begin_nested()
 
     def tearDown(self):
-        self.__class__._session.rollback()
-
+        self.session.rollback()
+        self.session.close()
+        self.trans.rollback()
 

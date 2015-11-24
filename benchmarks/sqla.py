b0a78156149bc31fa0b2c625016227c610bb5645
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2015-11-24 08:10:27 +0100

    Fix wrong SqlA filter on groups name

--- benchmarks/sqla.py
+++ benchmarks/sqla.py
@@ -6,9 +6,12 @@ from aiida.backends.sqlalchemy.models.group import DbGroup
 
 
 def build_query_attr(filter_):
+    begin_group = 'N_elements_'
+    end_group = '_clean_cif_primitive_dup_filtered'
+
     q = DbNode.query.join(DbGroup, DbNode.dbgroups).filter(
         DbNode.attributes.has_key(filter_),
-        DbGroup.name.like("%{}%".format(filter_))
+        DbGroup.name.like("{}%{}".format(begin_group, end_group))
     )
 
     return lambda : q.all()

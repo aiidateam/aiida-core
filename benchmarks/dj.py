b7617f76c8b1d2a0e23a9f5626034c8828fade64
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2015-12-01 13:03:28 +0100

    Add complex queries (first try)

--- benchmarks/dj.py
+++ benchmarks/dj.py
@@ -7,6 +7,9 @@ from aiida.backends.profile import load_profile
 from aiida.backends.djsite.utils import load_dbenv
 from aiida.backends.djsite.db import models
 
+from aiida.orm import DataFactory, CalculationFactory
+from aiida.orm.implementation.general.calculation.inline import InlineCalculation
+
 
 def build_deserialized_dict(query_values):
     first_dict = defaultdict(dict)
@@ -42,10 +45,69 @@ def build_query_attr(filter_):
 
     return lambda: build_deserialized_dict(pks)
 
+
+def get_closest_cif():
+    """
+    Get closest cif data in the parents of all nodes
+    :param nodes: list of nodes
+    """
+    node_type = 'CifData'
+
+    depth = models.DbPath.objects.filter(
+        child__in=nodes, parent__type__contains=node_type
+    ).distinct().order_by('depth').values_list('depth')[0][0]
+
+    q = models.DbPath.objects.filter(
+        parent__type__contains=node_type, child__in=nodes,depth=depth
+    ).distinct()
+
+    return list(CifData.query(
+        children__in=nodes,child_paths__in=q).distinct().order_by('ctime'))
+
+
+def get_farthest_struc():
+    struc_type = DataFactory('structure')().__class__.__name__
+
+    depth = models.DbPath.objects.filter(
+        child=node, parent__type__contains=struc_type
+    ).distinct().order_by('-depth').values_list('depth')[0][0]
+
+    q = models.DbPath.objects.filter(
+        parent__type__contains=struc_type, child=node,depth=depth
+    ).distinct()
+
+    return list(DataFactory('structure').query(
+        children=node,child_paths__in=q).distinct().order_by('ctime'))
+
+def complex_query():
+    RemoteData = DataFactory('remote')
+    PwCalculation = CalculationFactory('quantumespresso.pw')
+
+    pws = models.DbNode.objects.filter(type=PwCalculation._plugin_type_string)
+    scratchremotes = models.DbNode.objects.filter(type=RemoteData._plugin_type_string,
+                                                  inputs__in=pws)
+    inlines = models.DbNode.objects.filter(type=InlineCalculation._plugin_type_string,
+                                           inputs__in=scratchremotes)
+    storeremotes = models.DbNode.objects.filter(type=RemoteData._plugin_type_string,
+                                                inputs__in=inlines)
+
+    q = storeremotes.values_list('uuid', flat=True)
+
+    return lambda: list(q)
+
+
 queries = {
     "attributes": {
         'kinds': build_query_attr('kinds'),
         'sites': build_query_attr('sites'),
         'cell': build_query_attr('cell')
+    },
+    "paths": {
+        "closest_cif": get_closest_cif,
+        "farthest_struc": get_farthest_struc,
+    },
+    "complex": {
+        "1": complex_query()
     }
+
 }
--- benchmarks/sqla.py
+++ benchmarks/sqla.py
@@ -5,24 +5,8 @@ from aiida.backends import sqlalchemy as sa
 from aiida.backends.sqlalchemy.models.node import DbNode
 from aiida.backends.sqlalchemy.models.group import DbGroup
 
-BEGIN_GROUP = 'N_elements_'
-END_GROUP = '_clean_cif_primitive_dup_filtered'
-
-def build_query_attr(filter_):
-    q = DbNode.query.join(DbGroup, DbNode.dbgroups).filter(
-        DbNode.attributes.has_key(filter_),
-        DbGroup.name.like("{}%{}".format(BEGIN_GROUP, END_GROUP))
-    )
-
-    return lambda: q.all()
-
-def build_query_attr_only(filter_):
-    q = (sa.session.query(DbNode.attributes[filter_])
-         .join(DbGroup, DbNode.dbgroups)
-         .filter(DbGroup.name.like("{}%{}".format(BEGIN_GROUP, END_GROUP)))
-         .filter(DbNode.attributes.has_key(filter_)))
-
-    return lambda: q.all()
+from aiida.orm import DataFactory, CalculationFactory
+from aiida.orm.implementation.general.calculation.inline import InlineCalculation
 
 
 INDEX_NAME = "dbnode_attributes_idx"
@@ -47,6 +31,49 @@ def _vacuum_analyaze():
     raw_conn.set_isolation_level(isolation_level)
 
 
+BEGIN_GROUP = 'N_elements_'
+END_GROUP = '_clean_cif_primitive_dup_filtered'
+
+def build_query_attr(filter_):
+    q = DbNode.query.join(DbGroup, DbNode.dbgroups).filter(
+        DbNode.attributes.has_key(filter_),
+        DbGroup.name.like("{}%{}".format(BEGIN_GROUP, END_GROUP))
+    )
+
+    return lambda: q.all()
+
+def build_query_attr_only(filter_):
+    q = (sa.session.query(DbNode.attributes[filter_])
+         .join(DbGroup, DbNode.dbgroups)
+         .filter(DbGroup.name.like("{}%{}".format(BEGIN_GROUP, END_GROUP)))
+         .filter(DbNode.attributes.has_key(filter_)))
+
+    return lambda: q.all()
+
+def complex_query():
+    RemoteData = DataFactory('remote')
+    PwCalculation = CalculationFactory('quantumespresso.pw')
+
+    pws = sa.session.query(DbNode.id).filter(
+        DbNode.type == PwCalculation._plugin_type_string).cte()
+
+    scratchremotes = sa.session.query(DbNode.id).filter(
+        DbNode.type == RemoteData._plugin_type_string
+    ).join(pws, DbNode.inputs).cte()
+
+    inlines = sa.session.query(DbNode.id).filter(
+        DbNode.type == InlineCalculation._plugin_type_string
+    ).join(scratchremotes, DbNode.inputs).cte()
+
+    storeremotes = sa.session.query(DbNode.uuid).filter(
+        DbNode.type == RemoteData._plugin_type_string
+    ).join(inlines, DbNode.inputs)
+
+    return lambda: storeremotes.all()
+
+
+
+
 queries = {
     "attributes": {
         'kinds': build_query_attr('kinds'),
@@ -57,6 +84,9 @@ queries = {
         'kinds': build_query_attr_only('kinds'),
         'sites': build_query_attr_only('sites'),
         'cell': build_query_attr_only('cell'),
+    },
+    "complex": {
+        "1": complex_query()
     }
 }
 

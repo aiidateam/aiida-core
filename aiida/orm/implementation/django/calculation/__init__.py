fa61d30213ad114b9f8b03f9bde13062d37fd25d
Jocelyn Boullier <jocelyn.boullier@epfl.ch>
2016-01-22 18:09:19 +0100

    Change relative import to absolute

--- aiida/orm/implementation/django/calculation/__init__.py
+++ aiida/orm/implementation/django/calculation/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from ..node import Node
+from aiida.orm.implementation.django.node import Node
 from aiida.orm.implementation.general.calculation import AbstractCalculation
 
 class Calculation(AbstractCalculation, Node):

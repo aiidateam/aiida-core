ebdac0efb7de8acd7edd320cac11eddd7f1edbda
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2016-03-18 18:13:32 +0100

    [Issue #14 - aiida_epfl] load_dbenv & is_dbenv_loaded can by loaded using the aiida package: e.g. from aiida import load_dbenv, is_dbenv_loaded

--- aiida/orm/__init__.py
+++ aiida/orm/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/.. All rights reserved."
-__license__ = "MIT license, see LICENSE.txt file"
-__version__ = "0.6.0"
-__authors__ = "The AiiDA team."
-
 from aiida.orm.calculation.job import JobCalculation
 from aiida.orm.implementation import *
 from aiida.orm.implementation.calculation import *
 from aiida.orm.data import *
 from aiida.orm.utils import *
+
+__copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/.. All rights reserved."
+__license__ = "MIT license, see LICENSE.txt file"
+__version__ = "0.6.0"
+__authors__ = "The AiiDA team."

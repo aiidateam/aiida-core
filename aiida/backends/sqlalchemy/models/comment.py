9e68d3a90bbf36eb6012649833d8e65c742168f0
Leonid Kahle <leonid.kahle@epfl.ch>
2016-04-06 11:13:38 +0200

    [Working on #181] Added column uuid in db_dbcomment in sqlalchemy schema to be consistent with schema defined in django, with respect to that table

--- aiida/backends/sqlalchemy/models/comment.py
+++ aiida/backends/sqlalchemy/models/comment.py
@@ -4,9 +4,11 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Integer, DateTime, Text
+from sqlalchemy.dialects.postgresql import UUID
 
 from aiida.utils import timezone
 from aiida.backends.sqlalchemy.models.base import Base
+from aiida.backends.sqlalchemy.models.utils import uuid_func
 
 __copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/.. All rights reserved."
 __license__ = "MIT license, see LICENSE.txt file"
@@ -18,16 +20,30 @@ class DbComment(Base):
 
     id = Column(Integer, primary_key=True)
 
-    dbnode_id = Column(Integer, ForeignKey('db_dbnode.id', ondelete="CASCADE", deferrable=True, initially="DEFERRED"))
-    dbnode = relationship('DbNode', backref='dbcomments')
+    uuid = Column(UUID(as_uuid=True), default=uuid_func)
+    dbnode_id = Column(
+            Integer,
+            ForeignKey(
+                'db_dbnode.id', ondelete="CASCADE",
+                deferrable=True, initially="DEFERRED"
+            )
+        )
 
     ctime = Column(DateTime(timezone=True), default=timezone.now)
     mtime = Column(DateTime(timezone=True), default=timezone.now)
 
-    user_id = Column(Integer, ForeignKey('db_dbuser.id', ondelete="CASCADE", deferrable=True, initially="DEFERRED"))
-    user = relationship("DbUser")
-
+    user_id = Column(
+            Integer,
+            ForeignKey(
+                'db_dbuser.id', ondelete="CASCADE",
+                deferrable=True, initially="DEFERRED"
+            )
+        )
     content = Column(Text, nullable=True)
+
+
+    dbnode = relationship('DbNode', backref='dbcomments')
+    user = relationship("DbUser")
 
     def __str__(self):
         return "DbComment for [{} {}] on {}".format(

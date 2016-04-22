87955474be6c2db47bbe299d1346e5f8c98c4ba4
Boris Kozinsky <bkoz37@gmail.com>
2016-04-22 00:29:48 -0400

    moved aiida2 examples and lib to .../specs

---
+++ aiida/workflows2/specs/examples/wftest.py
@@ -0,0 +1,185 @@
+"""
+Testing the workfunction wrappers
+"""
+
+#from sqlalchemy.orm import sessionmaker, aliased
+import time
+
+from aiida.workflows2.specs.lib.wflib import *
+
+@threadit
+@workit_factory()
+def top_func(self, **inp):
+    self.test = True
+    x = inp['x']
+    print x.content
+#    session4 = Session()
+#    session4.add(self)
+#    session4.add(x)
+#    print "I am", self   # direct access to self will not work unless there is a session during execution
+    print inp['x'].content
+#    session4.close()
+    er = IntData(5)
+    print er.content['value']
+    a = func10(self)
+    b = func20(self, d=er)
+    #c = a + b
+    c = w4(a).update(w4(b))
+    a1 = func10(self, e=er)
+    a2 = func10(self, e=er)
+    a3 = func10(self, e=er)
+    a4 = func10(self)
+    a5 = func10(self)
+
+    #k = w4(a1)
+    result = c
+    print c
+    return result
+
+@threadit
+@workit_factory()
+def ex1(self):
+    a = func10()
+    b = func20()
+    c = func10(w4(a))
+    d = func10(w4(b))
+
+
+@threadit
+@workit_factory()
+def func10(self, **inp):
+    d = IntData(5)
+    inp3 = {'a':IntData(3), 'b':d}
+    result2 = w4(func20(self, **inp3))
+    print inp3['a'].content['value']
+    print result2
+    time.sleep(1)
+    return result2
+
+
+@threadit
+@workit_factory()
+def func20(self, **inp):
+    v= firstcomp(self, c=IntData(6))
+    #result = StringData('result_2 ' + str(inp))
+    result = {'f20res': StringData('result_3 ')}
+    #print w4(v)
+    #a = func10(self)
+    #time.sleep(5)
+    #print "---cdcd", inp['a'].content
+    return result
+
+
+@threadit
+@workit_factory()
+def firstcomp(self, c, d=IntData(8)):
+    return {'first': IntData(6)}
+
+#session5 = Session()
+#c = Calc(name='start')
+#x = IntData(88)
+#session5.add_all([c, x])
+#session5.commit()
+
+#top_func(c, x=x)
+#res = w4(top_func(c, x=x))
+#print "result {0}".format(res)
+#for item in res:
+#    item.roots.append(x)
+#session5.commit()
+
+#*******************************
+
+@threadit
+@workit_factory()
+def func0(self, **inp):
+    N = inp['x']
+    M = inp['y']
+    K = IntData(37)
+    output = {'N':N, 'M':M, 'K':K}
+    time.sleep(5)
+    return output
+
+
+@threadit
+@workit_factory()
+def func1(self, **inp):
+    n = inp['number']
+    double = n * 2
+    triple = n * 3
+    output = {'outA':double, 'outB':triple}
+    time.sleep(n)
+    return output
+
+
+@threadit
+@workit_factory()
+def func2(self, **inp):
+    n = inp['number']
+    square = n * n
+    cube = n * n * n
+    output = {'outC':square, 'outD':cube}
+    time.sleep(n)
+    return output
+
+
+@threadit
+@workit_factory()
+def func3(self, inE, inF):
+    #e = inp['inE']
+    #f = inp['inF']
+    e = inE
+    f = inF
+    output= {'combined':e + f}
+    return output
+
+
+@threadit
+@workit_factory()
+def Workflow3(self, **inp):
+    step0 = func0(self, x=inp['first'], y=inp['second'])
+    result0 = w4(step0)
+
+    step1 = func1(self, number=result0['N'])  #these two are started in parallel
+    step2 = func2(self, number=result0['M'])
+
+    result1 = w4(step1)
+    result2 = w4(step2)   # block and wait for both results before proceeding
+
+    step3 = func3(self, inE=result1['outB'], inF=result2['outC'])
+    result3 = w4(step3)
+    output = {'subsum':result3['combined']}
+    return output
+
+@threadit
+@workit_factory()
+def collect4(self, arr):
+    output = {'sum': sum(arr)}
+    return output
+
+@threadit
+@workit_factory()
+def Workflow4(self, **inp):
+    step = []   #initiate lists
+    result = []
+    N = inp['Nmax']
+
+    # start multiple parallel runs
+    for n in xrange(N):
+        step.append(Workflow3(self, first=n, second=n))
+
+    # collect the results once they are all finished
+    for n in xrange(N):
+        x = w4(step[n])
+        result.append(x['subsum'])
+
+    print result
+    output = collect4(self, arr = tuple(result))   #tuple to make hashable value
+    return w4(output)
+
+
+#b = Workflow4(c, Nmax=IntData(5))
+#print w4(b)
+
+#b = Workflow4(Wstart, Nmax=4)
+#print w4(b)
---
+++ aiida/workflows2/specs/lib/model_def.py
@@ -0,0 +1,356 @@
+import datetime, hashlib
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.dialects.postgresql import JSONB, UUID
+from sqlalchemy.schema import Index
+from uuid import uuid4
+
+Base = declarative_base()
+loading_type="dynamic"
+#TODO investigate lazy loading
+
+#------------------------ Object Classes ------------------------
+class Calc(Base):
+    __tablename__ = "calc"
+
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), default=uuid4)
+    name = Column(String)
+    type = Column(String)
+    state = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    content = Column(JSONB)
+    hash = Column(String)
+    extra = Column(JSONB)
+
+    code_id = Column(Integer, ForeignKey("code.id"))
+    code = relationship("Code", backref="calcs")
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref=backref("calcs", lazy=loading_type))
+    #master_id = Column(Integer, ForeignKey("calc.id"))
+    #slaves = relationship("Calc", backref=backref("master", remote_side=[id]))
+    inputs = relationship("Data", secondary="inputlink", backref=backref("takers", lazy=loading_type))
+    outputs = relationship("Data", secondary="outputlink", backref=backref("givers", lazy=loading_type))
+    masters = relationship("Calc", secondary="control",
+                           primaryjoin="Calc.id==Control.slave_id",
+                           secondaryjoin="Calc.id==Control.master_id",
+                           backref="slaves")
+
+    def __init__(self, name=None, state='New', content=None, extra=None, code=None):
+        self.name = name
+        self.state = state
+        self.content = content
+        self.extra = extra
+        self.code = code
+        self.hash = hashlib.sha224(str(self.content)).hexdigest()
+
+    def __repr__(self):
+        return "calc[{}]".format(self.name)
+
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'calc'}
+
+
+class Data(Base):
+    __tablename__ = "data"
+
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True), default=uuid4)
+    name = Column(String)
+    type = Column(String)
+    state = Column(String)
+    validity = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    content = Column(JSONB)
+    hash = Column(String)
+    extra = Column(JSONB)
+
+    #parent_id = Column(Integer, ForeignKey("calc.id"), nullable=True)   #change to False if parents are required
+    #parent = relationship("Calc", backref=backref("children", lazy="dynamic"))
+    #calcs = relationship("Calc", secondary="inputlink", backref=backref("inputs", lazy="dynamic"))
+    roots = relationship("Data", secondary="derive",
+                         primaryjoin="Data.id==Derive.fruit_id",
+                         secondaryjoin="Data.id==Derive.root_id",
+                         backref="fruits")
+
+    #__table_args__ = (UniqueConstraint('parent_id', 'content_hash', name='parent_hash_uc'),)
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'data'}
+
+    def __init__(self, name=None, state=None, validity='New',
+        content=None, extra=None):
+        self.name = name
+        self.state = state
+        self.validity = validity
+        self.content = content
+        self.hash = hashlib.sha224(str(self.content)).hexdigest()
+        self.extra = extra
+
+    def __repr__(self):
+        return "data({})".format(self.name)
+
+
+class Code(Base):
+    __tablename__ = "code"
+
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True),  default=uuid4)
+    name = Column(String, unique=False) #non-unique for testing
+    type = Column(String)
+    state = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    content = Column(JSONB)
+    hash = Column(String, unique=True)
+    extra = Column(JSONB)
+    computer_id = Column(Integer, ForeignKey("computer.id"), nullable=True)
+    computer = relationship("Computer", backref=backref("codes", lazy=loading_type))
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref=backref("codes", lazy=loading_type))
+    wrappers = relationship("Code", secondary="relative",
+                         primaryjoin="Code.id==Relative.core_id",
+                         secondaryjoin="Code.id==Relative.wrapper_id",
+                         backref="cores")
+
+    def __init__(self, name=None, state=None, content=None, extra=None, computer=None,
+        user=None):
+        self.name = name
+        self.state = state
+        self.content = content
+        self.extra = extra
+        self.computer = computer
+        self.hash = hashlib.sha224(str(self.content)).hexdigest()
+
+    def __repr__(self):
+        return "code<{}>".format(self.name)
+
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'code'}
+
+
+class Computer(Base):
+    __tablename__ = "computer"
+
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True),  default=uuid4)
+    name = Column(String, unique=True)
+    type = Column(String)
+    state = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    content = Column(JSONB)
+    hash = Column(String, unique=True)
+    extra = Column(JSONB)
+
+    def __init__(self, name=None, state=None, content=None, extra=None):
+        self.name = name
+        self.state = state
+        self.content = content
+        self.extra = extra
+        self.hash = hashlib.sha224(str(self.content)).hexdigest()
+
+    def __repr__(self):
+        return "Computer <{}>".format(self.name)
+
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'computer'}
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    uuid = Column(UUID(as_uuid=True),  default=uuid4)
+    name = Column(String, unique=True)
+    type = Column(String)
+    state = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    content = Column(JSONB)
+    hash = Column(String, unique=True)
+    extra = Column(JSONB)
+
+    def __init__(self, name=None, state=None, content=None, extra=None):
+        self.name = name
+        self.state = state
+        self.content = content
+        self.extra = extra
+        self.hash = hashlib.sha224(str(self.content)).hexdigest()
+
+    def __repr__(self):
+        return "User <{}>".format(self.name)
+
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'user'}
+
+# -------------- Link Classes --------------------------
+
+class InputLink(Base):
+    """
+    Link between a in input data and a calc, for a m2m relationship
+    """
+    __tablename__ = "inputlink"
+
+    input_id = Column(Integer, ForeignKey("data.id"), primary_key=True)
+    taker_id = Column(Integer, ForeignKey("calc.id"), primary_key=True)
+    type = Column(String)
+    label = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    extra = Column(JSONB)
+    input = relationship("Data", backref=backref('taker_links', lazy=loading_type))
+    taker = relationship("Calc", backref=backref('input_links', lazy=loading_type))
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'inputlink'}
+
+    def __init__(self, d=None, c=None, label=None):
+        self.input = d
+        self.taker = c
+        self.label = label
+
+    def __repr__(self):
+        return "<InputLink {0} {1} -> {2}>".format(self.label, self.input, self.taker)
+
+
+class OutputLink(Base):
+    """
+    Link for connecting a calc to the data it returns. This is not always the same as the data it creates (children)
+    """
+    __tablename__ = "outputlink"
+
+    giver_id = Column(Integer, ForeignKey("calc.id"), primary_key=True)
+    output_id = Column(Integer, ForeignKey("data.id"), primary_key=True)
+    type = Column(String)
+    label = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    extra = Column(JSONB)
+    giver = relationship("Calc", backref=backref('output_links', lazy=loading_type))
+    output = relationship("Data", backref=backref('giver_links', lazy=loading_type))
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'outputlink'}
+
+    def __init__(self, c=None, d=None, label=None):
+        self.label = label
+        self.giver = c
+        self.output = d
+
+    def __repr__(self):
+        return "<OutputLink {0} {1} -> {2}>".format(self.label, self.giver, self.output)
+
+
+class Control(Base):
+    __tablename__ = "control"
+
+    master_id = Column(Integer, ForeignKey("calc.id"), primary_key=True)
+    slave_id = Column(Integer, ForeignKey("calc.id"), primary_key=True)
+    type = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    extra = Column(JSONB)
+    master = relationship("Calc", primaryjoin="Control.master_id==Calc.id", backref='slave_controls')
+    slave = relationship("Calc", primaryjoin="Control.slave_id==Calc.id", backref='master_controls')
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'control'}
+
+    def __init__(self, n1=None, n2=None):
+        self.master = n1
+        self.slave = n2
+
+    def __repr__(self):
+        return "<Control {0} -> {1}>".format(self.caller, self.helper)
+
+
+class Derive(Base):
+    __tablename__ = "derive"
+
+    root_id = Column(Integer, ForeignKey("data.id"), primary_key=True)
+    fruit_id = Column(Integer, ForeignKey("data.id"), primary_key=True)
+    type = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    extra = Column(JSONB)
+    #label = Column(String)
+    root = relationship("Data", primaryjoin="Derive.root_id==Data.id", backref='fruit_derivations')
+    fruit = relationship("Data", primaryjoin="Derive.fruit_id==Data.id", backref='root_derivations')
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'derive'}
+
+    def __init__(self, n1=None, n2=None):
+        self.root = n1
+        self.fruit = n2
+
+    def __repr__(self):
+        return "<Derive {0} -> {1}>".format(self.root, self.fruit)
+
+
+class Relative(Base):
+    __tablename__ = "relative"
+
+    core_id = Column(Integer, ForeignKey("code.id"), primary_key=True)
+    wrapper_id = Column(Integer, ForeignKey("code.id"), primary_key=True)
+    type = Column(String)
+    time = Column(DateTime(timezone=False), default=datetime.datetime.now)
+    extra = Column(JSONB)
+    #label = Column(String)
+    core = relationship("Code", primaryjoin="Relative.core_id==Code.id", backref='wrapper_relations')
+    wrapper = relationship("Code", primaryjoin="Relative.wrapper_id==Code.id", backref='core_relations')
+    __mapper_args__ = {'polymorphic_on': type, 'polymorphic_identity': 'relative'}
+
+    def __init__(self, n1=None, n2=None):
+        self.core = n1
+        self.wrapper = n2
+
+    def __repr__(self):
+        return "<Relative {0} -> {1}>".format(self.core, self.wrapper)
+
+
+#--------------- Subclasses --------------------------
+
+class Create(OutputLink):
+    """
+    A Data can only be Created by one parent calculation, but then given as an output by many others.
+    """
+    def __repr__(self):
+        return "<Create {0} {1} -> {2}>".format(self.label, self.giver, self.output)
+    __mapper_args__ = {'polymorphic_identity': 'create'}
+
+
+class Return(OutputLink):
+    """
+    """
+    def __repr__(self):
+        return "<Return {0} {1} -> {2}>".format(self.label, self.giver, self.output)
+    __mapper_args__ = {'polymorphic_identity': 'return'}
+
+
+class Recall(Control):
+    """
+    """
+    def __repr__(self):
+        return "<Call {0} -> {1}>".format(self.master, self.slave)
+    __mapper_args__ = {'polymorphic_identity': 'recall'}
+
+
+class Call(Control):
+    """
+    A Calc can only be called once, the first time it is created due to a call from the first caller.
+    """
+    def __repr__(self):
+        return "<Summon {0} -> {1}>".format(self.master, self.slave)
+    __mapper_args__ = {'polymorphic_identity': 'call'}
+
+
+class Suspend(Control):
+    """
+    """
+    def __repr__(self):
+        return "<Suspend {0} -> {1}>".format(self.master, self.slave)
+    __mapper_args__ = {'polymorphic_identity': 'summon'}
+
+
+class Equivalent(Derive):
+    """
+    """
+    def __repr__(self):
+        return "<Equivalent {0} -> {1}>".format(self.root, self.fruit)
+    __mapper_args__ = {'polymorphic_identity': 'equivalent'}
+
+
+class Include(Derive):
+    """
+    """
+    def __repr__(self):
+        return "<Include {0} -> {1}>".format(self.root, self.fruit)
+    __mapper_args__ = {'polymorphic_identity': 'include'}
+
+
+#------------------ Indexing ------------------
+### indexing for faster queries, needs testing
+#Index('my_index', Data.content, postgresql_using='gin')

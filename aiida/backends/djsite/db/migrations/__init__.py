cae6a601b00040bc5f3259f8c1ded329f8948499
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2016-06-07 11:43:10 +0200

    [Issue #192] Disabling very early backend check. Has to re-implemented more properly.

--- aiida/backends/djsite/db/migrations/__init__.py
+++ aiida/backends/djsite/db/migrations/__init__.py
@@ -9,7 +9,7 @@ LATEST_MIGRATION = '0002_db_state_change'
 
 
 def _update_schema_version(version, apps, schema_editor):
-    from aiida.backends.djsite.utils import set_db_schema_version
+    from aiida.backends.utils import set_db_schema_version
     set_db_schema_version(version)
 
 

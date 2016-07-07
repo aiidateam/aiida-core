f0c202230eac8cf908372b6295a4e2e36843a588
Martin Uhrin <martin.uhrin@gmail.com>
2016-07-07 10:17:50 +0200

    Updated workflow examples a little.

--- examples/workflows2/wf_test.py
+++ examples/workflows2/wf_test.py
@@ -6,17 +6,6 @@ if not is_dbenv_loaded():
 from aiida.workflows2.wf import wf
 from aiida.orm.data.simple import Int
 from aiida.workflows2.db_types import to_db_type, SimpleData
-from aiida.workflows2.process import Process
-from aiida.workflows2.workflow import Workflow
-
-
-def add(a, b):
-    return a + b
-
-
-def multiply(a, b):
-    return a * b
-
 
 @wf
 def sum(a, b):
@@ -33,42 +22,6 @@ def add_multiply_wf(a, b, c):
     return prod(sum(a, b), c)
 
 
-class Add(Process):
-    @classmethod
-    def _define(cls, spec):
-        spec.input('a', default=0)
-        spec.input('b', default=0)
-        spec.output('value')
-
-    def _run(self, a, b):
-        self.out('value', to_db_type(a.value + b.value))
-
-
-class Mul(Process):
-    @classmethod
-    def _define(cls, spec):
-        spec.input('a', default=1)
-        spec.input('b', default=1)
-        spec.output('value')
-
-    def _run(self, a, b):
-        self.out('value', to_db_type(a.value * b.value))
-
-
-class MulAdd(Workflow):
-    @classmethod
-    def _define(cls, spec):
-        spec.process(Mul)
-        spec.process(Add)
-
-        spec.exposed_inputs('Add')
-        spec.exposed_outputs('Mul')
-        spec.input('c')
-
-        spec.link(':c', 'Mul:a')
-        spec.link('Add:value', 'Mul:b')
-
-
 if __name__ == '__main__':
     two = Int(2)
     three = Int(3)
@@ -77,11 +30,5 @@ if __name__ == '__main__':
     print "WORKFUNCTION:"
 
     simpledata = add_multiply_wf(two, three, four)
-    print "output pk:", simpledata.pk
-    print "output value:", simpledata.value
-
-    print "PROCESS:"
-    outs = MulAdd.run(inputs={'a': two, 'b': three, 'c': four})
-    simpledata = outs['value']
     print "output pk:", simpledata.pk
     print "output value:", simpledata.value

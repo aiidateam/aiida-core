bf4bdb85cbd22ffa681e7abc05abf8ae127fe1d1
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2016-08-10 14:58:56 +0200

    [Issue #239] Fixes for the correct compilation of readthedocs.

--- aiida/backends/settings.py
+++ aiida/backends/settings.py
@@ -31,5 +31,5 @@ DUMMY_CONF_FILE = (
                               "default_user_email": "aiida@epfl.ch",
                               "TIMEZONE": "Europe/Zurich",
                               "AIIDADB_REPOSITORY_URI":
-                                  "file:///",
+                                  "file:///repository",
                               "AIIDADB_USER": "aiida"}}})

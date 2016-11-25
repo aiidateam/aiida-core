1d0fd1259427e0e1ca50e37f51321f4140647d90
Fernando Gargiulo <fernando.gargiulo@epfl.ch>
2016-11-25 12:15:10 +0100

    Added sqlachemy test folder. Minor changes in some comments

---
+++ aiida/backends/sqlalchemy/tests/nwchem.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for the NWChem input plugins.
+"""
+
+__copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/. All rights reserved."
+__license__ = "MIT license, see LICENSE.txt file."
+__version__ = "0.7.0"
+__authors__ = "The AiiDA team."
+
+#import the generic test class for nwchem
+from aiida.backends.sqlalchemy.tests.testbase import SqlAlchemyTests
+from aiida.backends.tests.nwchem import TestNwchem
+
+
+class TestNwchemSqla(SqlAlchemyTests, TestNwchem):
+    """
+    nwchem tests that do need to be specified for sqlalchemy backend
+    """
+    pass
+

24eb5826203d2303a2039ab7a86187da5b97926a
DropD <r.haeuselmann@gmx.ch>
2016-12-06 14:37:08 +0100

    update requirements, separate optional ones, remove fallback to disttools.

--- setup.py
+++ setup.py
@@ -6,10 +6,7 @@ __license__ = "MIT license, see LICENSE.txt file."
 __version__ = "0.7.1"
 __authors__ = "The AiiDA team."
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 # Get the version number
 aiida_folder = os.path.split(os.path.abspath(__file__))[0]
@@ -19,19 +16,11 @@ with open(fname) as aiida_init:
     exec (aiida_init.read(), ns)
     aiida_version = ns['__version__']
 
-if os.path.exists('CHANGELOG_EPFL.txt'):
-    # EPFL version
-    aiida_name = 'aiida-epfl'
-    aiida_license = 'MIT and EPFL licenses, see LICENSE.txt'
-else:
-    aiida_name = 'aiida'
-    aiida_license = 'MIT license, see LICENSE.txt'
-
 bin_folder = os.path.join(aiida_folder, 'bin')
 setup(
-    name=aiida_name,
+    name='aiida',
     url='http://www.aiida.net/',
-    license=aiida_license,
+    license='MIT license, see LICENSE.txt',
     version=aiida_version,
     # Abstract dependencies.  Concrete versions are listed in
     # requirements.txt
@@ -40,11 +29,45 @@ setup(
     # http://blog.miguelgrinberg.com/post/the-package-dependency-blues
     # for a useful discussion
     install_requires=[
-        'django', 'django_extensions', 'pytz', 'django-celery',
-        'celery', 'billiard', 'anyjson', 'six', 'supervisor',
-        'meld3', 'paramiko', 'ecdsa', 'pycrypto', 'numpy', 'django-tastypie',
-        'python-dateutil', 'python-mimeparse', 'plum', 'enum34', 'voluptuous',
-        'click', 'ujson', 'uritools',
+        'django==1.7.4',
+        'django_extensions==1.5',
+        'pytz==2014.10',
+        'django-celery==3.1.16',
+        'celery==3.1.17',
+        'billiard==3.3.0.19',
+        'anyjson==0.3.3',
+        'six==1.9',
+        'supervisor==3.1.3',
+        'meld3==1.0.0',
+        'numpy',
+        'plum==0.4.3'
+        'SQLAlchemy==1.0.12',
+        'SQLAlchemy-Utils==0.31.2',
+        'ujson==1.35',
+        'enum34==1.1.2',
+        'voluptuous==0.8.11',
+        'aldjemy',
+        'passlib',
+        'validate_email',
+        'click==6.6',
+        'tabulate==0.7.5',
+        'ete3==3.0.0b35',
+    ],
+    extra_require={
+        'verdi_shell': ['ipython'],
+        'ssh': [
+            'paramiko==1.15.2',
+            'ecdsa==0.13',
+            'pycrypto==2.6.1',
+        ],
+        'REST': [
+            'django-tastypie==0.12.1',
+            'python-dateutil==2.4.0',
+            'python-mimeparse==0.1.4',
+        ],
+    },
+    dependency_links=[
+        'git+https://bitbucket.org/aiida_team/plum.git@v0.4.3#egg=plum',
     ],
     packages=find_packages(),
     scripts=[os.path.join(bin_folder, f) for f in os.listdir(bin_folder)

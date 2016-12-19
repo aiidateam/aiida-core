433a66774abdb6c24b26b9686e9c47665ebb75f2
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2016-12-19 11:59:38 +0100

    [Issue #276] Moving test_deletion to the right place

--- aiida/backends/djsite/db/subtests/generic.py
+++ aiida/backends/djsite/db/subtests/generic.py
@@ -12,6 +12,40 @@ __version__ = "0.7.0"
 __authors__ = "The AiiDA team."
 
 
+class TestComputer(AiidaTestCase):
+    """
+    Test the Computer class.
+    """
+
+    def test_deletion(self):
+        from aiida.orm.computer import Computer
+        from aiida.orm import delete_computer, JobCalculation
+        from aiida.common.exceptions import InvalidOperation
+
+        newcomputer = Computer(name="testdeletioncomputer", hostname='localhost',
+                               transport_type='local',
+                               scheduler_type='pbspro',
+                               workdir='/tmp/aiida').store()
+
+        # # This should be possible, because nothing is using this computer
+        delete_computer(newcomputer)
+
+        calc_params = {
+            'computer': self.computer,
+            'resources': {'num_machines': 1,
+                          'num_mpiprocs_per_machine': 1}
+        }
+
+        _ = JobCalculation(**calc_params).store()
+
+        print "Node stored with pk:",  _.dbnode.pk
+
+        # This should fail, because there is at least a calculation
+        # using this computer (the one created just above)
+        with self.assertRaises(InvalidOperation):
+            delete_computer(self.computer)
+
+
 class TestGroupsDjango(AiidaTestCase):
     """
     Test groups.

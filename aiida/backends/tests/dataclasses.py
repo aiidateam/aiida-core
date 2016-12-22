71c8ac567ab330578eb649c4dfde9a02ba49499e
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2016-12-22 12:15:00 +0100

    Added missing refs to python's unittest

--- aiida/backends/tests/dataclasses.py
+++ aiida/backends/tests/dataclasses.py
@@ -5,6 +5,7 @@ Tests for specific subclasses of Data
 from aiida.orm import load_node
 from aiida.common.exceptions import ModificationNotAllowed, ValidationError
 from aiida.backends.testbase import AiidaTestCase
+import unittest
 
 __copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/. All rights reserved."
 __license__ = "MIT license, see LICENSE.txt file."
--- aiida/backends/tests/dbimporters.py
+++ aiida/backends/tests/dbimporters.py
@@ -3,6 +3,7 @@
 Tests for subclasses of DbImporter, DbSearchResults and DbEntry
 """
 from aiida.backends.testbase import AiidaTestCase
+import unittest
 
 __copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/. All rights reserved."
 __license__ = "MIT license, see LICENSE.txt file."

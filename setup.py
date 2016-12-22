1843437c59643529d112e25fff7e152197bfbda4
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2016-12-22 10:38:14 +0100

    Fixing dependencies

--- setup.py
+++ setup.py
@@ -1,109 +1,52 @@
 # -*- coding: utf-8 -*-
-
 from os import path
 from setuptools import setup, find_packages
+from setup_requirements import install_requires, extras_require, dependency_links
 
 __copyright__ = u"Copyright (c), This file is part of the AiiDA platform. For further information please visit http://www.aiida.net/. All rights reserved."
 __license__ = "MIT license, see LICENSE.txt file."
-__version__ = "0.7.1"
-__authors__ = "The AiiDA team."
 
-# Get the version number
-aiida_folder = path.split(path.abspath(__file__))[0]
-fname = path.join(aiida_folder, 'aiida', '__init__.py')
-with open(fname) as aiida_init:
-    ns = {}
-    exec(aiida_init.read(), ns)
-    aiida_version = ns['__version__']
 
-bin_folder = path.join(aiida_folder, 'bin')
-setup(
-    name='aiida',
-    url='http://www.aiida.net/',
-    license='MIT License',
-    author=__authors__,
-    author_email='developers@aiida.net',
-    include_package_data=True, # puts non code files into the distribution, reads list from MANIFEST.in
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-    ],
-    version=aiida_version,
-    # Abstract dependencies.  Concrete versions are listed in
-    # requirements.txt
-    # See https://caremad.io/2013/07/setup-vs-requirement/ for an explanation
-    # of the difference and
-    # http://blog.miguelgrinberg.com/post/the-package-dependency-blues
-    # for a useful discussion
-    install_requires=[
-        'python-dateutil~=2.4.0',
-        'django==1.7.4',
-        'django_extensions==1.5',
-        'tzlocal',
-        'pytz==2014.10',
-        'django-celery==3.1.16',
-        'celery==3.1.17',
-        'billiard==3.3.0.19',
-        'anyjson==0.3.3',
-        'six==1.9',
-        'supervisor==3.1.3',
-        'meld3==1.0.0',
-        'numpy',
-        'plum==0.4.3',
-        'SQLAlchemy==1.0.12',
-        'SQLAlchemy-Utils==0.31.2',
-        'ujson==1.35',
-        'enum34==1.1.2',
-        'voluptuous==0.8.11',
-        'aldjemy',
-        'passlib',
-        'validate_email',
-        'click==6.6',
-        'tabulate==0.7.5',
-        'ete3==3.0.0b35',
-        'uritools==1.0.2',
-        'psycopg2==2.6.1',
-    ],
-    extras_require={
-        'verdi_shell': ['ipython'],
-        'ssh': [
-            'paramiko==1.15.2',
-            'ecdsa==0.13',
-            'pycrypto==2.6.1',
+
+if __name__ == '__main__':
+    # Get the version number
+    aiida_folder = path.split(path.abspath(__file__))[0]
+    fname = path.join(aiida_folder, 'aiida', '__init__.py')
+    with open(fname) as aiida_init:
+        ns = {}
+        exec(aiida_init.read(), ns)
+        aiida_version = ns['__version__']
+
+    bin_folder = path.join(aiida_folder, 'bin')
+    setup(
+        name='aiida',
+        url='http://www.aiida.net/',
+        license='MIT License',
+        author="The AiiDA team",
+        author_email='developers@aiida.net',
+        include_package_data=True, # puts non-code files into the distribution, reads list from MANIFEST.in
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
         ],
-        'REST': [
-            'flask==0.10.1',
-            'flask_restful==0.3.5',
-            'flask-cors==3.0.1',
-            'pyparsing==2.1.8',
-            'pattern==2.6',
-            'Flask-SqlAlchemy==2.1',
-            'SQLAlchemy-migrate==0.10.0',
-            'marshmallow-sqlalchemy==0.10.0',
-            'flask-marshmallow==0.7.0',
-            'itsdangerous==0.24',
-            'flask-httpauth==3.2.0',
-            'flask-cache==0.13.1',
-            'python-memcached==1.58',
-        ],
-    },
-    dependency_links=[
-        'https://bitbucket.org/aiida_team/plum/get/v0.4.3.zip#egg=plum-0.4.3',
-    ],
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'verdi=aiida.cmdline.verdilib:run'
-        ],
-        # following are AiiDA plugin entry points:
-        'aiida.calculations': [],
-        'aiida.parsers': [],
-        'aiida.cmdline': [],
-        'aiida.schedulers': [],
-        'aiida.transports': [],
-        'aiida.workflows': [],
-    },
-    scripts=['bin/runaiida'],
-    long_description=open(path.join(aiida_folder, 'README.rst')).read(),
-)
+        version=aiida_version,
+        install_requires=install_requires,
+        extras_require=extras_require,
+        dependency_links=dependency_links,
+        packages=find_packages(),
+        entry_points={
+            'console_scripts': [
+                'verdi=aiida.cmdline.verdilib:run'
+            ],
+            # following are AiiDA plugin entry points:
+            'aiida.calculations': [],
+            'aiida.parsers': [],
+            'aiida.cmdline': [],
+            'aiida.schedulers': [],
+            'aiida.transports': [],
+            'aiida.workflows': [],
+        },
+        scripts=['bin/runaiida'],
+        long_description=open(path.join(aiida_folder, 'README.rst')).read(),
+    )

a9152ef311548df2b75d80a64f93d0162b0f456b
leo@pegasus <leonid.kahle@epfl.ch>
2016-12-30 21:25:12 +0100

    Working on the QueryBuilder abstract interface, #305. Refactored the code into general implementation (for user) and backend specific implementation. There is no more inheritance, the general implementation chooses a backend specific implementation as a member upon instantiation. An abstract class provides an interface, but more documentation and more checks are needed.

---
+++ aiida/backends/interfaces/iquerybuilder.py
@@ -0,0 +1,88 @@
+from abc import abstractmethod, ABCMeta
+
+
+class IQueryBuilder():
+    __metaclass__ = ABCMeta
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        pass
+    @abstractmethod
+    def _prepare_with_dbpath(self):
+        """
+        A method to use the DbPath, if this is supported, or throw an
+        exception if not.
+        The overrider must fill add the DbPath-ORM as an attribute to self::
+
+            from aiida.backends.implementation.model import DbPath
+            self.path = DbPath
+
+        """
+        pass
+    @abstractmethod
+    def _get_session(self):
+        pass
+
+    @abstractmethod
+    def _modify_expansions(self, alias, expansions):
+        """
+        Modify names of projections if ** was specified.
+        This is important for the schema having attributes in a different table.
+        """
+        pass
+
+    @abstractmethod
+    def _get_filter_expr_from_attributes(
+            cls, operator, value, attr_key,
+            column=None, column_name=None, alias=None):
+        pass
+    @abstractmethod
+    def _get_projectable_attribute(
+            self, alias, column_name, attrpath,
+            cast=None, **kwargs):
+        pass
+    @abstractmethod
+    def _get_aiida_res(self, key, res):
+        """
+        Some instance returned by ORM (django or SA) need to be converted
+        to Aiida instances (eg nodes)
+
+        :param key: the key that this entry would be returned with
+        :param res: the result returned by the query
+
+        :returns: an aiida-compatible instance
+        """
+        pass
+    @abstractmethod
+    def _yield_per(self, batch_size):
+        """
+        :param count: Number of rows to yield per step
+
+        Yields *count* rows at a time
+
+        :returns: a generator
+        """
+        pass
+    @abstractmethod
+    def _all(self):
+        pass
+    @abstractmethod
+    def _first(self):
+        """
+        Executes query in the backend asking for one instance.
+
+        :returns: One row of aiida results
+        """
+
+        pass
+    #~ @abstractmethod
+    #~ def iterall(self, batch_size=100):
+
+        #~ pass
+    #~ @abstractmethod
+    #~ def iterdict(self, batch_size=100):
+
+        #~ pass
+    @abstractmethod
+    def _get_ormclass(self,  cls, ormclasstype):
+        pass
+

aa9d733748dab57634ad1ec5ac54ddf8ebc796bf
Fernando Gargiulo <fernando.gargiulo@epfl.ch>
2017-02-03 11:47:36 +0100

    create_requirements script more robust.Missing features: create different files for different classes of optional requirements as well as documentation

--- utils/create_requirements.py
+++ utils/create_requirements.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+import sys, os
 
 # Import requirements specs
 from setup_requirements import install_requires, dependency_links, extras_require
 
+parent_directory = os.path.join(os.path.split(os.path.abspath(__file__))[0],os.pardir)
+requirement_file = os.path.join(parent_directory, 'requirements.txt')
+optional_requirement_file = os.path.join(parent_directory, 'optional_requirements.txt')
+
 # generate basic requirements and dependency links,
-with open('../requirements.txt', 'w') as fout:
+with open(requirement_file, 'w') as fout:
     
     fout.write('##Requirements\n')    
     for req in install_requires:
@@ -15,7 +20,7 @@ with open('../requirements.txt', 'w') as fout:
         fout.write(dep + '\n')
 
 # generate optional requirements
-with open('../optional_requirements.txt', 'w') as fout:
+with open(optional_requirement_file, 'w') as fout:
     for k, v in extras_require.iteritems():
         fout.write('##' + k + '\n')
         fout.write('\n'.join(v) + '\n\n')

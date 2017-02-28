e0a826dec8635262c5919b1fe599d68f465ea117
Martin Uhrin <martin.uhrin@gmail.com>
2017-02-28 15:48:43 +0100

    Changed AiiDA to use plumpy from pypi so now it can be put in pypi itself.

--- setup_requirements.py
+++ setup_requirements.py
@@ -16,7 +16,7 @@ install_requires = [
     'supervisor==3.1.3',
     'meld3==1.0.0',
     'numpy==1.12.0',
-    'plum==0.7.5',
+    'plumpy==0.7.6',
     'SQLAlchemy==1.0.12',  # upgrade to SQLalchemy 1.1.5 does break tests
     'SQLAlchemy-Utils==0.31.2',
     'ujson==1.35',
@@ -101,7 +101,3 @@ extras_require = {
     ]
 }
 
-# Dependency for workflow engine
-dependency_links = [
-    'https://bitbucket.org/aiida_team/plum/get/v0.7.5.zip#egg=plum-0.7.5',
-]

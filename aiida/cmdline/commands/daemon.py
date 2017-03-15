fa41a7bd3f22b60bf46c6cc55d2959705716f311
DropD <r.haeuselmann@gmx.ch>
2017-03-15 11:59:18 +0000

    find venv bin through sys.executable, prepend to PATH

--- aiida/cmdline/commands/daemon.py
+++ aiida/cmdline/commands/daemon.py
@@ -29,8 +29,11 @@ def is_daemon_user():
 
 
 def _get_env_with_venv_bin():
+    pybin = os.path.dirname(sys.executable)
+    print pybin
     currenv = os.environ.copy()
-    currenv['PATH'] += ':' + ':'.join([i for i in sys.path if i.endswith('bin')])
+    currenv['PATH'] = pybin + ':' + currenv['PATH']
+    print currenv['PATH']
     return currenv
 
 class Daemon(VerdiCommandWithSubcommands):

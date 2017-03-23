adab2a1acd261f64dd8de15ca3f9a3d7f24b9180
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2017-03-23 19:21:42 +0100

    More progress on the verdi cmdline tests.

--- aiida/backends/tests/verdi_commands.py
+++ aiida/backends/tests/verdi_commands.py
@@ -18,6 +18,7 @@ from StringIO import StringIO
 import sys
 from aiida.common import utils
 from aiida.utils.capturing import Capturing
+import mock
 
 @contextmanager
 def captured_output():
@@ -28,6 +29,59 @@ def captured_output():
         yield sys.stdout, sys.stderr
     finally:
         sys.stdout, sys.stderr = old_out, old_err
+
+computer_name_1 = "torquessh1"
+computer_setup_input_1 = [computer_name_1,
+                          "localhost",
+                          "",
+                          "True",
+                          "ssh",
+                          "torque",
+                          "/scratch/{username}/aiida_run",
+                          "mpirun -np {tot_num_mpiprocs}",
+                          "1",
+                          EOFError,
+                          EOFError,
+                          ]
+
+
+computer_name_2 = "torquessh2"
+computer_setup_input_2 = [computer_name_2,
+                          "localhost",
+                          "",
+                          "True",
+                          "ssh",
+                          "torque",
+                          "/scratch/{username}/aiida_run",
+                          "mpirun -np {tot_num_mpiprocs}",
+                          "1",
+                          EOFError,
+                          EOFError,
+                          ]
+
+code_name_1 = "doubler"
+code_setup_input_1 = [code_name_1,
+                      "simple script that doubles a number "
+                      "and sleeps for a given number of seconds",
+                      "False",
+                      "simpleplugins.templatereplacer",
+                      computer_name_1,
+                      "/usr/local/bin/doubler.sh",
+                      EOFError,
+                      EOFError,
+                      ]
+
+code_name_2 = "doubler"
+code_setup_input_2 = [code_name_2,
+                      "simple script that doubles a number "
+                      "and sleeps for a given number of seconds",
+                      "False",
+                      "simpleplugins.templatereplacer",
+                      computer_name_2,
+                      "/usr/local/bin/doubler.sh",
+                      EOFError,
+                      EOFError,
+                      ]
 
 
 class TestVerdiCommands(AiidaTestCase):
@@ -49,47 +103,77 @@ class TestVerdiCommands(AiidaTestCase):
 
 class TestVerdiCodeCommands(AiidaTestCase):
 
-    def tearDown(self):
-        utils.raw_input = None
+    @classmethod
+    def setUpClass(cls):
+        """
+        Create the computer and setup a code
+        """
+        super(TestVerdiCodeCommands, cls).setUpClass()
+
+        # Setup computer #1
+        from aiida.cmdline.commands.computer import Computer
+        cmd_comp = Computer()
+        with mock.patch('__builtin__.raw_input',
+                        side_effect=computer_setup_input_1):
+            cmd_comp.computer_setup()
+
+        # Setup a code for computer #1
+        from aiida.cmdline.commands.code import Code
+        code_cmd = Code()
+        with mock.patch('__builtin__.raw_input',
+                        side_effect=code_setup_input_2):
+            code_cmd.code_setup()
+
+        # Setup computer #2
+        from aiida.cmdline.commands.computer import Computer
+        cmd_comp = Computer()
+        with mock.patch('__builtin__.raw_input',
+                        side_effect=computer_setup_input_2):
+            cmd_comp.computer_setup()
+
+        # Setup a code for computer #2
+        from aiida.cmdline.commands.code import Code
+        code_cmd = Code()
+        with mock.patch('__builtin__.raw_input',
+                        side_effect=code_setup_input_2):
+            code_cmd.code_setup()
+
 
     def test_code_list(self):
-        # from aiida.orm.code import Code
-        # # Create a code
-        # code1 = Code()
-        # code1.set_remote_computer_exec((self.computer, '/bin/true'))
-        # code1.label = 'test_code1'
-        # code1.store()
-
         from aiida.cmdline.commands.code import Code
-        codeCmd = Code()
-        # codeCmd.code_setup()
-        code_setup_input = ["doubler",
-                       "simple script that doubles a number "
-                       "and sleeps for a given number of seconds",
-                       "False",
-                       "simpleplugins.templatereplacer",
-                       "torquessh",
-                       "/usr/local/bin/doubler.sh",
-                       "",
-                       ""]
+        code_cmd = Code()
 
-        import mock
-        with mock.patch('__builtin__.raw_input', side_effect=code_setup_input):
-            codeCmd.code_setup()
+        # Run a simple verdi code list, capture the output and check the result
+        with Capturing() as output:
+            code_cmd.code_list()
+        out_str_1 = ''.join(output)
+        self.assertTrue(computer_name_1 in out_str_1,
+                        "The computer 1 name should be included into "
+                        "this list")
+        self.assertTrue(code_name_1 in out_str_1,
+                        "The code 1 name should be included into this list")
+        self.assertTrue(computer_name_2 in out_str_1,
+                        "The computer 2 name should be included into "
+                        "this list")
+        self.assertTrue(code_name_2 in out_str_1,
+                        "The code 2 name should be included into this list")
 
-        original_raw_input = __builtins__.raw_input
-        __builtins__.raw_input = lambda _:  code_setup_input[ac.array_counter()]
-        codeCmd.code_setup()
-        __builtins__.raw_input = original_raw_input
+        # Run a verdi code list -a, capture the output and check if the result
+        # is the same as the previous one
+        with Capturing() as output:
+            code_cmd.code_list(*['-a'])
+        out_str_2 = ''.join(output)
+        self.assertEqual(out_str_1, out_str_2,
+                         "verdi code list & verdi code list -a should provide "
+                         "the same output in this experiment.")
 
-
-        ac = utils.ArrayCounter()
-        utils.raw_input = lambda _: code_setup_input[ac.array_counter()]
-        # with Capturing():
-        bk_vars = codeCmd.code_setup()
-
-
-
-        # codeCmd.code_list()
-
-
+        # Run a verdi code list -c, capture the output and check the result
+        with Capturing() as output:
+            code_cmd.code_list(*['-c', computer_name_1])
+        out_str = ''.join(output)
+        self.assertTrue(computer_name_1 in out_str,
+                        "The computer 1 name should be included into "
+                        "this list")
+        self.assertFalse(computer_name_2 in out_str,
+                         "The computer 2 name should not be included into "
+                         "this list")

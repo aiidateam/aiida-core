770364d60852ea8c6fd4226be2257291d0c1ad3d
Fernando Gargiulo <fernando.gargiulo@epfl.ch>
2017-04-20 18:03:56 +0200

    Added user_email as a hybrid property in both sqlalchemy and dummy_model DbNode class

--- aiida/backends/sqlalchemy/models/node.py
+++ aiida/backends/sqlalchemy/models/node.py
@@ -33,6 +33,7 @@ from aiida.common.exceptions import DbContentError, MissingPluginError
 from aiida.common.datastructures import calc_states, _sorted_datastates, sort_states
 
 from aiida.backends.sqlalchemy.models.user import DbUser
+from aiida.backends.sqlalchemy.models.computer import DbComputer
 
 
 class DbCalcState(Base):
@@ -264,21 +265,36 @@ class DbNode(Base):
     @hybrid_property
     def user_email(self):
         """
-
         Returns: the email of the user
-
         """
         return self.user.email
 
     @user_email.expression
     def user_email(cls):
         """
-
         Returns: the email of the user at a class level (i.e. in the database)
-
         """
         return select([DbUser.email]).where(DbUser.id == cls.user_id).label(
             'user_email')
+
+    # Computer name
+    @hybrid_property
+    def computer_name(self):
+        """
+        Returns: the of the computer
+        """
+        return self.dbcomputer.name
+
+    @computer_name.expression
+    def computer_name(cls):
+        """
+        Returns: the name of the computer at a class level (i.e. in the 
+        database)
+        """
+        return select([DbComputer.name]).where(DbComputer.id ==
+                                                 cls.dbcomputer_id).label(
+            'computer_name')
+
 
     @hybrid_property
     def state(self):

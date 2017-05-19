058b806d85328123dd03d8577d7997311fcd6231
Sebastiaan Huber <mail@sphuber.net>
2017-05-19 15:23:37 +0200

    Bump the required version of plumpy to v0.7.8
    The recent fixes and improvements in the WorkChains rely on
    fixes in Plum that are included in v0.7.8

--- setup_requirements.py
+++ setup_requirements.py
@@ -35,7 +35,7 @@ install_requires = [
     'supervisor==3.1.3',
     'meld3==1.0.0',
     'numpy==1.12.0',
-    'plumpy==0.7.6',
+    'plumpy==0.7.8',
     'SQLAlchemy==1.0.12',  # upgrade to SQLalchemy 1.1.5 does break tests, see #465
     'SQLAlchemy-Utils==0.31.2',
     'ujson==1.35',

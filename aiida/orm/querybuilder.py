6e2b3b5718a890f1773796484592a1d688477bed
leo@pegasus <leonid.kahle@epfl.ch>
2017-05-31 12:20:50 +0200

    This resolved 527 by changing the load_node function such that it accepts the beginning of a uuid. Used this opportunity to clean up the the way load_node works. It raises now TypeError and InputValidationError instead of ValueError and uses the querybuilder instead of subclass_from_pk.

--- aiida/orm/querybuilder.py
+++ aiida/orm/querybuilder.py
@@ -2264,6 +2264,21 @@ class QueryBuilder(object):
         return returnval
 
 
+    def one(self):
+        """
+        Executes the query asking for exactly one results. Will raise an exception if this is not the case
+        :raises: MultipleObjectsError if more then one row can be returned
+        :raises: NotExistent if no result was found
+        """
+        from aiida.common.exceptions import MultipleObjectsError, NotExistent
+        self.limit(2)
+        res = self.all()
+        if len(res) > 1:
+            raise MultipleObjectsError("More than one result was found")
+        elif len(res) == 0:
+            raise NotExistent("No result was found")
+        return res[0]
+
 
     def count(self):
         """
@@ -2389,6 +2404,7 @@ class QueryBuilder(object):
 
         """
         return list(self.iterdict(batch_size=batch_size))
+
 
 
     def get_results_dict(self):

4371bf17f9a906424d12a21c98a64f7bf8dbd935
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2017-06-27 20:43:54 +0200

    Created a correct first migration. The schema record in the DbSetting table is not needed any more and it is removed.

--- aiida/backends/sqlalchemy/migrations/versions/470e57bc0936_create_account_table.py
+++ aiida/backends/sqlalchemy/migrations/versions/470e57bc0936_create_account_table.py
@@ -5,9 +5,8 @@ Revises:
 Create Date: 2017-06-16 17:53:10.920345
 
 """
-from alembic import op
-import sqlalchemy as sa
-
+from aiida.backends import sqlalchemy as sa
+from aiida.backends.sqlalchemy.models.settings import DbSetting
 
 # revision identifiers, used by Alembic.
 revision = '470e57bc0936'
@@ -17,12 +16,12 @@ depends_on = None
 
 
 def upgrade():
-    op.create_table(
-        'account',
-        sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('name', sa.String(50), nullable=False),
-        sa.Column('description', sa.Unicode(200)),
-    )
+    # Delete the old schema version
+    sa.get_scoped_session().query(DbSetting).filter(
+        DbSetting.key == "db|schemaversion").delete()
+
 
 def downgrade():
-    op.drop_table('account')
+    #  Add the old schema version
+    DbSetting.set_value('db|schemaversion', '0.1', other_attribs={
+        "description": 'The version of the schema used in this database.'})

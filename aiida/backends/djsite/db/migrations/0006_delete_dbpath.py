59a1d8cd998aa190a236a2b91d8b4cbde4f526d9
Leonid Kahle <leonid.kahle@epfl.ch>
2017-07-12 15:11:29 +0200

    #258 Wrote a better migration script, forcing Django to remove the DbPath model and the corresponding relationshio fields

--- aiida/backends/djsite/db/migrations/0006_delete_dbpath.py
+++ aiida/backends/djsite/db/migrations/0006_delete_dbpath.py
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
-###########################################################################
-# Copyright (c), The AiiDA team. All rights reserved.                     #
-# This file is part of the AiiDA code.                                    #
-#                                                                         #
-# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
-# For further information on the license, see the LICENSE.txt file        #
-# For further information please visit http://www.aiida.net               #
-###########################################################################
 from __future__ import unicode_literals
 
-import django_extensions.db.fields
-from django.db import migrations
-
+from django.db import models, migrations
 from aiida.backends.djsite.db.migrations import update_schema_version
 
 
 SCHEMA_VERSION = "1.0.6"
 
-
 class Migration(migrations.Migration):
+
     dependencies = [
         ('db', '0005_add_cmtime_indices'),
     ]
 
     operations = [
-        # Create the index that speeds up the daemon queries
-        # We use the RunSQL command because Django interface
-        # doesn't seem to support partial indexes
+        migrations.RemoveField(
+            model_name='dbpath',
+            name='child',
+        ),
+        migrations.RemoveField(
+            model_name='dbpath',
+            name='parent',
+        ),
+        migrations.RemoveField(
+            model_name='dbnode',
+            name='children',
+        ),
+        migrations.DeleteModel(
+            name='DbPath',
+        ),
         migrations.RunSQL("""
             DROP TRIGGER IF EXISTS autoupdate_tc ON db_dblink;
             DROP FUNCTION IF EXISTS update_tc();
-            DROP TABLE db_dbpath; 
         """),
         update_schema_version(SCHEMA_VERSION)
+
     ]

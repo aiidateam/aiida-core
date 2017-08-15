aiidateam/aiida-core

commit aeac53b5b390d0093cad82053e30129c328df483

Changed the internal structure of _inputlinks_cache of Node
The old input links cache used a dictionary with the link label
as the key, to store links that were to be created between two
nodes. This clearly put a uniqueness constraint on the link label
regardless of the node and link type, which is clearly undesirable.
This would for example directly disallow two links labeled 'parameters'
one INPUT link and the other a CREATE, even though this should be
perfectly acceptable in the current view and design of nodes and links.

For that reason the structure of the _inputlinks_cache is changed to
use a LinkKey tuple which consists of the source node uuid and the link
label. The values are LinkInfo tuples which contain the full required
information of source node, label and link type.

I removed some stale code, that was not used anywhere: _remove_link_from

aiida/orm/implementation/general/node.py
@@ -27,6 +27,9 @@ from aiida.backends.utils import validate_attribute_key
 
 _NO_DEFAULT = tuple()
 
+LinkKey = collections.namedtuple('LinkKey', ['src_uuid', 'label'])
+LinkInfo = collections.namedtuple('LinkInfo', ['src', 'label', 'link_type'])
+
 
 def clean_value(value):
     """
@@ -451,22 +454,16 @@ class AbstractNode(object):
         """
         assert src is not None, "You must provide a valid Node to link"
 
-        # Check that the label does not already exist
+        # Check if the link for the same label/node/type combination already
+        # exists in the link cache, in which case we don't have to do anything
+        if LinkInfo(src, label, link_type) in self._inputlinks_cache.itervalues():
+            return
 
-        # This can happen also if both nodes are stored, e.g. if one first
-        # stores the output node and then the input node. Therefore I check
-        # it here.
-        if label in self._inputlinks_cache:
-            raise UniquenessError("Input link with name '{}' already present "
-                                  "in the internal cache".format(label))
-
-        # See if I am pointing to already saved nodes and I am already
-        # linking to a given node
-        if src.uuid in [_[0].uuid for _ in self._inputlinks_cache.values()]:
-            raise UniquenessError(
-                "A link from node with UUID={} and "
-                "the current node (UUID={}) already exists!".format(
-                    src.uuid, self.uuid))
+        # The combination label/link_type should be unique
+        for link_info in self._inputlinks_cache.itervalues():
+            if label == link_info.label and link_type == link_info.link_type:
+                raise UniquenessError('A link with the label {} and type {} already'
+                    'exists'.format(label, link_type))
 
         # Check if the source allows output links from this node
         # (will raise ValueError if this is not the case)
@@ -495,7 +492,7 @@ class AbstractNode(object):
             raise UniquenessError("Input link with name '{}' already present "
                                   "in the internal cache".format(label))
 
-        self._inputlinks_cache[label] = (src, link_type)
+        self._inputlinks_cache[LinkKey(src.uuid, label)] = LinkInfo(src, label, link_type)
 
     def _replace_link_from(self, src, label, link_type=LinkType.UNSPECIFIED):
         """
@@ -508,6 +505,8 @@ class AbstractNode(object):
         :param src: the source object
         :param str label: the name of the label to set the link from src.
         """
+        link_key = LinkKey(src.uuid, label)
+
         # If both are stored, write directly on the DB
         if self.is_stored and src.is_stored:
             self._replace_dblink_from(src, label, link_type)
@@ -515,48 +514,33 @@ class AbstractNode(object):
             # (this could happen if I first store the output node, then
             # the input node.
             try:
-                del self._inputlinks_cache[label]
+                del self._inputlinks_cache[link_key]
             except KeyError:
                 pass
-        else:  # at least one is not stored: set in the internal cache
-            # See if I am pointing to already saved nodes and I am already
-            # linking to a given node
-            # It is similar to the 'add' method, but if I am replacing the
-            # same node, I will not complain (k!=label)
-            if src.uuid in [v[0].uuid for k, v in
-                            self._inputlinks_cache.iteritems() if k != label]:
-                raise UniquenessError(
-                    "A link from node with UUID={} and "
-                    "the current node (UUID={}) already exists!".format(
-                        src.uuid, self.uuid))
+        else:
+            # At least one node is not stored: set in the internal cache
+
+            # The combination label/link_type should still be unique just
+            # as is checked for in the 'add_link_from' method.
+            for link_info in self._inputlinks_cache.itervalues():
+                if label == link_info.label and link_type == link_info.link_type:
+                    raise UniquenessError('A link with the label {} and type {} already'
+                        'exists'.format(label, link_type))
+
+            # Remove any potential pre-existing links with the same source node and type
+            existing_link_key = None
+            for key, link_info in self._inputlinks_cache.iteritems():
+                if link_info.src == src and link_info.link_type == link_type:
+                    existing_link_key = key
+
+            if existing_link_key:
+                del self._inputlinks_cache[existing_link_key]
+
             # I insert the link directly in the cache rather than calling
             # _add_cachelink_from because this latter performs an undesired check
-            self._inputlinks_cache[label] = (src, link_type)
+            self._inputlinks_cache[link_key] = LinkInfo(src, label, link_type)
 
-           # self._add_cachelink_from(src, label, link_type)
-
-    def _remove_link_from(self, label):
-        """
-        Remove from the DB the input link with the given label.
-
-        :note: In subclasses, change only this. Moreover, remember to call
-            the super() method in order to properly use the caching logic!
-
-        :note: No error is raised if the link does not exist.
-
-        :param str label: the name of the label to set the link from src.
-        :param link_type: The type of link, must be one of the enum values form
-          :class:`~aiida.common.links.LinkType`
-        """
-        # Try to remove from the local cache, no problem if none is present
-        try:
-            del self._inputlinks_cache[label]
-        except KeyError:
-            pass
-
-        # If both are stored, remove also from the DB
-        if self.is_stored:
-            self._remove_dblink_from(label)
+            # self._add_cachelink_from(src, label, link_type)
 
     @abstractmethod
     def _replace_dblink_from(self, src, label, link_type):
@@ -575,7 +559,7 @@ class AbstractNode(object):
         pass
 
     @abstractmethod
-    def _remove_dblink_from(self, label):
+    def _remove_dblink_from(self, label, link_type):
         """
         Remove from the DB the input link with the given label.
 
@@ -690,13 +674,12 @@ class AbstractNode(object):
             # Needed for the check
             input_list_keys = [i[0] for i in inputs_list]
 
-            for label, v in self._inputlinks_cache.iteritems():
-                src = v[0]
-                if label in input_list_keys:
+            for link_info in self._inputlinks_cache.itervalues():
+                if link_info.label in input_list_keys:
                     raise InternalError("There exist a link with the same name "
                                         "'{}' both in the DB and in the internal "
-                                        "cache for node pk= {}!".format(label, self.pk))
-                inputs_list.append((label, src))
+                                        "cache for node pk= {}!".format(link_info.label, self.pk))
+                inputs_list.append((link_info.label, link_info.src))
 
         if node_type is None:
             filtered_list = inputs_list
@@ -1417,8 +1400,8 @@ class AbstractNode(object):
                 "_store_input_nodes can be called only if the node is "
                 "unstored (node {} is stored, instead)".format(self.pk))
 
-        for label in self._inputlinks_cache:
-            parent = self._inputlinks_cache[label][0]
+        for link_info in self._inputlinks_cache.itervalues():
+            parent = link_info.src
             if not parent.is_stored:
                 parent.store(with_transaction=False)
 
@@ -1430,13 +1413,13 @@ class AbstractNode(object):
           stored.
         """
         # Preliminary check to verify that inputs are stored already
-        for label in self._inputlinks_cache:
-            if not self._inputlinks_cache[label][0].is_stored:
+        for link_info in self._inputlinks_cache.itervalues():
+            if not link_info.src.is_stored:
                 raise ModificationNotAllowed(
                     "Cannot store the input link '{}' because the "
                     "source node is not stored. Either store it first, "
                     "or call _store_input_links with the store_parents "
-                    "parameter set to True".format(label))
+                    "parameter set to True".format(link_info.label))
 
     @abstractmethod
     def _store_cached_input_links(self, with_transaction=True):

aiida/backends/tests/nodes.py
@@ -1657,20 +1657,22 @@ class TestSubNodesAndLinks(AiidaTestCase):
 
         # Caching the links
         n2.add_link_from(n1, label='l1')
+        n3.add_link_from(n1, label='l1')
         n3.add_link_from(n2, label='l2')
+
+        # Add link with same label and link_type, regardless of src node
+        with self.assertRaises(UniquenessError):
+            n3.add_link_from(n4, label='l1')
+
+        # Twice the link to the same node but different label is fine
         n3.add_link_from(n1, label='l3')
 
-        # Twice the same link name
-        with self.assertRaises(UniquenessError):
-            n3.add_link_from(n4, label='l2')
+        # Same should be allowed in _replace_link_from
+        n3._replace_link_from(n2, label='l4')
 
-        # Twice the link to the same node
+        # However, replacing with existing link of identical label and link_type
         with self.assertRaises(UniquenessError):
-            n3.add_link_from(n2, label='l4')
-
-        # Same error also in _replace_link_from
-        with self.assertRaises(UniquenessError):
-            n3._replace_link_from(n2, label='l4')
+            n3._replace_link_from(n1, label='l4')
 
         n2.store_all()
         n3.store_all()
@@ -1678,17 +1680,21 @@ class TestSubNodesAndLinks(AiidaTestCase):
         n2_in_links = [(l, n.uuid) for l, n in n2.get_inputs_dict().iteritems()]
         self.assertEquals(sorted(n2_in_links), sorted([('l1', n1.uuid),
                                                        ]))
+
         n3_in_links = [(l, n.uuid) for l, n in n3.get_inputs_dict().iteritems()]
-        self.assertEquals(sorted(n3_in_links), sorted([('l2', n2.uuid),
-                                                       ('l3', n1.uuid),
+        self.assertEquals(sorted(n3_in_links), sorted([('l1', n1.uuid),
+                                                       ('l4', n2.uuid),
+                                                       ('l3', n1.uuid)
                                                        ]))
 
         n1_out_links = [(l, n.pk) for l, n in n1.get_outputs(also_labels=True)]
         self.assertEquals(sorted(n1_out_links), sorted([('l1', n2.pk),
-                                                        ('l3', n3.pk),
+                                                        ('l1', n3.pk),
+                                                        ('l3', n3.pk)
                                                         ]))
         n2_out_links = [(l, n.pk) for l, n in n2.get_outputs(also_labels=True)]
-        self.assertEquals(sorted(n2_out_links), sorted([('l2', n3.pk)]))
+        self.assertEquals(sorted(n2_out_links), sorted([('l4', n3.pk)
+                                                        ]))
 
     def test_valid_links(self):
         import tempfile

aiida/orm/implementation/django/node.py
@@ -165,11 +165,11 @@ class Node(AbstractNode):
         except UniquenessError:
             # I have to replace the link; I do it within a transaction
             with transaction.atomic():
-                self._remove_dblink_from(label)
+                self._remove_dblink_from(label, link_type)
                 self._add_dblink_from(src, label, link_type)
 
-    def _remove_dblink_from(self, label):
-        DbLink.objects.filter(output=self.dbnode, label=label).delete()
+    def _remove_dblink_from(self, label, link_type):
+        DbLink.objects.filter(output=self.dbnode, label=label, type=link_type).delete()
 
     def _add_dblink_from(self, src, label=None, link_type=LinkType.UNSPECIFIED):
         from aiida.backends.djsite.db.models import DbPath
@@ -488,9 +488,9 @@ class Node(AbstractNode):
                 "Node with pk= {} was already stored".format(self.pk))
 
         # For each parent, check that all its inputs are stored
-        for label in self._inputlinks_cache:
+        for link in self._inputlinks_cache.itervalues():
             try:
-                parent_node = self._inputlinks_cache[label][0]
+                parent_node = link.src
                 parent_node._check_are_parents_stored()
             except ModificationNotAllowed:
                 raise ModificationNotAllowed(
@@ -546,10 +546,9 @@ class Node(AbstractNode):
             self._check_are_parents_stored()
             # I have to store only those links where the source is already
             # stored
-            links_to_store = list(self._inputlinks_cache.keys())
+            links_to_store = list(self._inputlinks_cache.itervalues())
 
-            for label in links_to_store:
-                src, link_type = self._inputlinks_cache[label]
+            for src, label, link_type in links_to_store:
                 self._add_dblink_from(src, label, link_type)
             # If everything went smoothly, clear the entries from the cache.
             # I do it here because I delete them all at once if no error

aiida/orm/implementation/sqlalchemy/node.py
@@ -180,17 +180,17 @@ class Node(AbstractNode):
         except UniquenessError:
             # I have to replace the link; I do it within a transaction
             try:
-                self._remove_dblink_from(label)
+                self._remove_dblink_from(label, link_type)
                 self._add_dblink_from(src, label, link_type)
                 session.commit()
             except:
                 session.rollback()
                 raise
 
-    def _remove_dblink_from(self, label):
+    def _remove_dblink_from(self, label, link_type):
         from aiida.backends.sqlalchemy import get_scoped_session
         session = get_scoped_session()
-        link = DbLink.query.filter_by(label=label).first()
+        link = DbLink.query.filter_by(label=label, type=link_type).first()
         if link is not None:
             session.delete(link)
 
@@ -524,9 +524,9 @@ class Node(AbstractNode):
                 "Node with pk= {} was already stored".format(self.id))
 
         # For each parent, check that all its inputs are stored
-        for link in self._inputlinks_cache:
+        for link in self._inputlinks_cache.itervalues():
             try:
-                parent_node = self._inputlinks_cache[link][0]
+                parent_node = link.src
                 parent_node._check_are_parents_stored()
             except ModificationNotAllowed:
                 raise ModificationNotAllowed("Parent node (UUID={}) has "
@@ -579,10 +579,9 @@ class Node(AbstractNode):
         self._check_are_parents_stored()
         # I have to store only those links where the source is already
         # stored
-        links_to_store = list(self._inputlinks_cache.keys())
+        links_to_store = list(self._inputlinks_cache.itervalues())
 
-        for label in links_to_store:
-            src, link_type = self._inputlinks_cache[label]
+        for src, label, link_type in links_to_store:
             self._add_dblink_from(src, label, link_type)
         # If everything went smoothly, clear the entries from the cache.
         # I do it here because I delete them all at once if no error

aiida/orm/implementation/general/calculation/job/__init__.py
@@ -531,22 +531,6 @@ class AbstractJobCalculation(object):
                                                                       label,
                                                                       link_type)
 
-    def _remove_link_from(self, label):
-        """
-        Remove a link. Only possible if the calculation is in state NEW.
-
-        :param str label: Name of the link to remove.
-        """
-        valid_states = [calc_states.NEW]
-
-        if self.get_state() not in valid_states:
-            raise ModificationNotAllowed(
-                "Can remove an input link to a calculation only if it is in one "
-                "of the following states:\n   {}\n it is instead {}".format(
-                    valid_states, self.get_state()))
-
-        return super(AbstractJobCalculation, self)._remove_link_from(label)
-
     @abstractmethod
     def _set_state(self, state):
         """

commit 627a7648e453b34c7865e373237113a5fe532826

Fix bug introduced in aeac53b5b390d0093cad82053e30129c328df483
In the reference commit, the internal structure of the _inputlinks_cache
of the Node class was changed by replacing the key of the dictionary
from being just the link label, to a tuple of the link label and source
node uuid. There used to be a unqiueness constraint therefore just on the
label, which was a problem and the original aim of the fix of the referenced
commit. However, in the _replace_link_from a check for uniqueness on the
link label + link type pair, was kept in place. However, when replacing
this check is not necessary and actually breaks the restart functionality
of the legacy workflows. Therefore, this commit removes that uniqueness
check

aiida/orm/implementation/general/node.py
@@ -518,15 +518,6 @@ class AbstractNode(object):
             except KeyError:
                 pass
         else:
-            # At least one node is not stored: set in the internal cache
-
-            # The combination label/link_type should still be unique just
-            # as is checked for in the 'add_link_from' method.
-            for link_info in self._inputlinks_cache.itervalues():
-                if label == link_info.label and link_type == link_info.link_type:
-                    raise UniquenessError('A link with the label {} and type {} already'
-                        'exists'.format(label, link_type))
-
             # Remove any potential pre-existing links with the same source node and type
             existing_link_key = None
             for key, link_info in self._inputlinks_cache.iteritems():

aiida/backends/tests/nodes.py
@@ -1670,10 +1670,6 @@ class TestSubNodesAndLinks(AiidaTestCase):
         # Same should be allowed in _replace_link_from
         n3._replace_link_from(n2, label='l4')
 
-        # However, replacing with existing link of identical label and link_type
-        with self.assertRaises(UniquenessError):
-            n3._replace_link_from(n1, label='l4')
-
         n2.store_all()
         n3.store_all()
 
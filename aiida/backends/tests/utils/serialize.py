c3b0c187f5cd57584ab1d49d9aa02c90e920c9b8
Sebastiaan Huber <mail@sphuber.net>
2017-10-09 18:00:34 +0200

    Add general purpose (de)serialize function for Node containers
    Data structures that may contain AiiDA nodes that need to be
    serialized, for example JSON serialization, need to convert the
    node instances to their UUID's and a deserialize function needs
    to be able to reinstantiate the nodes from the UUID. The general
    purpose functions added here can do this for arbirtrary data values
    or data structures.

---
+++ aiida/backends/tests/utils/serialize.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from aiida.orm import Node
+from aiida.utils.serialize import serialize_data, deserialize_data
+from aiida.backends.testbase import AiidaTestCase
+
+class TestSerialize(AiidaTestCase):
+
+    def test_serialize_round_trip(self):
+        """
+        """
+        node_a = Node().store()
+        node_b = Node().store()
+
+        data = {
+            'test': 1,
+            'list': (1, 2, 3, node_a),
+            'dict': {
+                ('Si',): node_b,
+                'foo': 'bar'
+            },
+            'baz': 'aar'
+        }
+
+        serialized_data = serialize_data(data)
+        deserialized_data = deserialize_data(serialized_data)
+
+        # For now manual element-for-element comparison until we come up with general
+        # purpose function that can equate two node instances properly
+        self.assertEqual(data['test'], deserialized_data['test'])
+        self.assertEqual(data['baz'], deserialized_data['baz'])
+        self.assertEqual(data['list'][:3], deserialized_data['list'][:3])
+        self.assertEqual(data['list'][3].uuid, deserialized_data['list'][3].uuid)
+        self.assertEqual(data['dict'][('Si',)].uuid, deserialized_data['dict'][('Si',)].uuid)
---
+++ aiida/utils/serialize.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import collections
+from ast import literal_eval
+from aiida.orm import Node, load_node
+
+_PREFIX_KEY_TUPLE = 'tuple():'
+_PREFIX_VALUE_NODE = 'aiida_node:'
+
+def encode_key(key):
+    """
+    Helper function for the serialize_data function which may need to serialize a
+    dictionary that uses tuples as keys. This function will encode the tuple into
+    a string such that it is JSON serializable
+
+    :param key: the key to encode
+    :return: the encoded key
+    """
+    if isinstance(key, tuple):
+        return '{}{}'.format(_PREFIX_KEY_TUPLE, key)
+    else:
+        return key
+
+def decode_key(key):
+    """
+    Helper function for the deserialize_data function which can undo the key encoding
+    of tuple keys done by the encode_key function
+
+    :param key: the key to decode
+    :return: the decoded key
+    """
+    if key.startswith(_PREFIX_KEY_TUPLE):
+        return literal_eval(key[len(_PREFIX_KEY_TUPLE):])
+    else:
+        return key
+
+def serialize_data(data):
+    """
+    Serialize a value or collection that may potentially contain AiiDA nodes, which
+    will be serialized to their UUID. Keys encountered in any mappings, such as a dictionary,
+    will also be encoded if necessary. An example is where tuples are used as keys in the
+    pseudo potential input dictionaries. These operations will ensure that the returned data is 
+    JSON serializable.
+
+    :param data: a single value or collection
+    :return: the serialized data with the same internal structure
+    """
+    if isinstance(data, collections.Mapping):
+        return {encode_key(key): serialize_data(value) for key, value in data.iteritems()}
+    elif isinstance(data, collections.Sequence) and not isinstance(data, (str, unicode)):
+        return tuple(serialize_data(value) for value in data)
+    elif isinstance(data, Node):
+        return '{}{}'.format(_PREFIX_VALUE_NODE, data.uuid)
+    else:
+        return data
+
+def deserialize_data(data):
+    """
+    Deserialize a single value or a collection that may contain serialized AiiDA nodes. This is
+    essentially the inverse operation of serialize_data which will reload node instances from
+    the serialized UUID data. Encoded tuples that are used as dictionary keys will be decoded.
+
+    :param data: serialized data
+    :return: the deserialized data with keys decoded and node instances loaded from UUID's
+    """
+    if isinstance(data, collections.Mapping):
+        return {decode_key(key): deserialize_data(value) for key, value in data.iteritems()}
+    elif isinstance(data, collections.Sequence) and not isinstance(data, (str, unicode)):
+        return tuple(deserialize_data(value) for value in data)
+    elif isinstance(data, (str, unicode)) and data.startswith(_PREFIX_VALUE_NODE):
+        return load_node(uuid=data[len(_PREFIX_VALUE_NODE):])
+    else:
+        return data

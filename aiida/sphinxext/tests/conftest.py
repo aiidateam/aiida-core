7b1908fbe08d00ebd36136b31aaeddec21afc667
Dominik Gresch <greschd@gmx.ch>
2017-10-09 16:11:07 +0200

    Explicitly compare XML for the sphinxext test

--- aiida/sphinxext/tests/conftest.py
+++ aiida/sphinxext/tests/conftest.py
@@ -1,13 +1,18 @@
+import os
 import sys
-import json
 import shutil
 import tempfile
-import operator
 import subprocess
-from os.path import join
+from os.path import join, dirname
+import xml.etree.ElementTree as ET
 
 import pytest
 
+@pytest.fixture
+def reference_result():
+    def inner(name):
+        return join(dirname(__file__), 'reference_results', name)
+    return inner
 
 @pytest.fixture
 def build_dir():
@@ -38,28 +43,23 @@ def build_sphinx(build_dir):
 
 
 @pytest.fixture
-def test_name(request):
-    """Returns module_name.function_name for a given test"""
-    return request.module.__name__ + '/' + request._parent_request._pyfuncitem.name
-
-
-@pytest.fixture
-def compare_data(request, test_name, scope="session"):
-    """Returns a function which either saves some data to a file or (if that file exists already) compares it to pre-existing data using a given comparison function."""
-
-    def inner(compare_fct, data, tag=None):
-        full_name = test_name + (tag or '')
-        val = request.config.cache.get(full_name, None)
-        if val is None:
-            request.config.cache.set(full_name, json.loads(json.dumps(data)))
-            raise ValueError('Reference data does not exist.')
-        else:
-            assert compare_fct(val, json.loads(json.dumps(data))
-                               )  # get rid of json-specific quirks
-
+def xml_equal():
+    def inner(test_file, reference_file):
+        if not os.path.isfile(reference_file):
+            shutil.copyfile(test_file, reference_file)
+            raise ValueError('Reference file does not exist!')
+        assert _flatten_xml(test_file) == _flatten_xml(reference_file)
+        print(_flatten_xml(test_file))
+        # assert ET.parse(test_file) == ET.parse(reference_file)
     return inner
 
 
-@pytest.fixture
-def compare_equal(compare_data):
-    return lambda data, tag=None: compare_data(operator.eq, data, tag)
+def _flatten_xml(filename):
+    return [
+        (
+            el.tag,
+            {k: v for k, v in el.attrib.items() if k not in ['source']},
+            el.text
+        )
+        for el in ET.parse(filename).iter()
+    ]

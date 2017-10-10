c1a3e1018b8867a6eab0c3c4fd26ca6d9c261444
DropD <r.haeuselmann@gmx.ch>
2017-10-10 12:51:40 +0200

    add test for quicksetup (only for travis)

---
+++ .travis-data/test_setup.py
@@ -0,0 +1,36 @@
+"""Integration tests for setup and quicksetup"""
+import unittest
+
+from click.testing import TestRunner
+
+from aiida.cmdline.verdilib import _setup_cmd, Quicksetup
+
+
+class QuicksetupTestCase(unittest.TestCase):
+    """Test ``verdi setup``"""
+
+    def setUp(self):
+        self.runner = TestRunner()
+
+    def _get_msg(self, result):
+        msg = '{}\n---\nOutput:\n{}'
+        return msg.format(result.exception, result.output)
+
+    def test_user_setup(self):
+        result = self.runner.invoke(
+            'verdi', [
+                'quicksetup',
+                '--non-interactive',
+                '--profile=giuseppe'
+                '--email=giuseppe.verdi@ope.ra',
+                '--first-name=Giuseppe',
+                '--last-name=Verdi',
+                '--institution=Scala',
+                '--db-name=aiida_giuseppe',
+                '--repo=aiida_giuseppe',
+                '--no-set-default'])
+        self.assertFalse(result.exception, msg=self._get_msg(result))
+
+
+if __name__ == '__main__':
+    unittest.main()

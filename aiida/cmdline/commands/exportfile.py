926acb7244d6c3355d706a65a3c411501eaff779
Sebastiaan Huber <mail@sphuber.net>
2017-10-17 11:22:30 +0200

    Migrate existing verdi export command to use click
    Before there was only a single command for 'verdi export', but this
    commit is in preparation of a new entry point 'verdi export migrate'
    which means that the original function had to be moved as well
    and is now located at 'verdi export create'

--- aiida/cmdline/commands/exportfile.py
+++ aiida/cmdline/commands/exportfile.py
@@ -8,162 +8,130 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 import sys
+import click
 
 from aiida.backends.utils import load_dbenv
-from aiida.cmdline.baseclass import VerdiCommand
+from aiida.cmdline.commands import verdi, export
+from aiida.cmdline.baseclass import VerdiCommand, VerdiCommandWithSubcommands
 
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-
-class Export(VerdiCommand):
+class Export(VerdiCommandWithSubcommands):
     """
-    Export nodes and group of nodes
-
-    This command allows to export to file nodes and group of nodes, for backup
-    purposes or to share data with collaborators.
-    Call this command with the '-h' option for some documentation of its usage.
+    Create and manage AiiDA export archives
     """
+    def __init__(self):
+        self.valid_subcommands = {
+            'create': (self.cli, self.complete_none)
+        }
 
-    def run(self, *args):
-        load_dbenv()
+    def cli(self, *args):
+        verdi()
 
-        import argparse
 
-        from aiida.orm.querybuilder import QueryBuilder
-        from aiida.orm import Group, Node, Computer
-        from aiida.orm.importexport import export, export_zip
+@export.command('create', context_settings=CONTEXT_SETTINGS)
+@click.argument('outfile', type=click.Path())
+@click.option('-n', '--nodes', multiple=True, type=int,
+    help='Export the given nodes by pk')
+@click.option('-c', '--computers', multiple=True, type=int,
+    help='Export the given computers by pk')
+@click.option('-g', '--groups', multiple=True, type=int,
+    help='Export the given groups by pk')
+@click.option('-G', '--group_names', multiple=True, type=str,
+    help='Export the given groups by group name')
+@click.option('-P', '--no-parents', is_flag=True, default=False,
+    help='Store only the nodes that are explicitly given, without exporting the parents')
+@click.option('-O', '--no-calc-outputs', is_flag=True, default=False,
+    help='If a calculation is included in the list of nodes to export, do not export its outputs')
+@click.option('-y', '--overwrite', is_flag=True, default=False,
+    help='Overwrite the output file, if it exists')
+@click.option('-z', '--zipfile-compressed', is_flag=True, default=False,
+    help='tore as zip file (experimental, should be faster')
+@click.option('-Z', '--zipfile-uncompressed', is_flag=True, default=False,
+    help='Store as uncompressed zip file (experimental, should be faster')
+def create(outfile, computers, groups, nodes, group_names, no_parents, no_calc_outputs,
+    overwrite, zipfile_compressed, zipfile_uncompressed):
+    """
+    Export nodes and groups of nodes to an archive file for backup or sharing purposes
+    """
+    load_dbenv()
+    from aiida.orm import Group, Node, Computer
+    from aiida.orm.querybuilder import QueryBuilder
+    from aiida.orm.importexport import export, export_zip
 
-        parser = argparse.ArgumentParser(
-            prog=self.get_full_command_name(),
-            description='Export data from the DB.')
-        parser.add_argument('-c', '--computers', nargs='+', type=int,
-                            metavar="PK", help="Export the given computers")
-        parser.add_argument('-n', '--nodes', nargs='+', type=int, metavar="PK",
-                            help="Export the given nodes")
-        parser.add_argument('-g', '--groups', nargs='+', metavar="GROUPNAME",
-                            help="Export all nodes in the given group(s), "
-                                 "identified by name.",
-                            type=str)
-        parser.add_argument('-G', '--group_pks', nargs='+', metavar="PK",
-                            help="Export all nodes in the given group(s), "
-                                 "identified by pk.",
-                            type=str)
-        parser.add_argument('-P', '--no-parents',
-                            dest='no_parents', action='store_true',
-                            help="Store only the nodes that are explicitly "
-                                 "given, without exporting the parents")
-        parser.set_defaults(no_parents=False)
-        parser.add_argument('-O', '--no-calc-outputs',
-                            dest='no_calc_outputs', action='store_true',
-                            help="If a calculation is included in the list of "
-                                 "nodes to export, do not export its outputs")
-        parser.set_defaults(no_calc_outputs=False)
-        parser.add_argument('-y', '--overwrite',
-                            dest='overwrite', action='store_true',
-                            help="Overwrite the output file, if it exists")
-        parser.set_defaults(overwrite=False)
+    node_id_set = set(nodes)
+    group_dict = dict()
 
-        zipsubgroup = parser.add_mutually_exclusive_group()
-        zipsubgroup.add_argument('-z', '--zipfile-compressed',
-                            dest='zipfilec', action='store_true',
-                            help="Store as zip file (experimental, should be "
-                                 "faster")
-        zipsubgroup.add_argument('-Z', '--zipfile-uncompressed',
-                            dest='zipfileu', action='store_true',
-                            help="Store as uncompressed zip file "
-                                 "(experimental, should be faster")
-        parser.set_defaults(zipfilec=False)
-        parser.set_defaults(zipfileu=False)
+    if group_names:
+        qb = QueryBuilder()
+        qb.append(Group, tag='group', project=['*'], filters={'name': {'in': group_names}})
+        qb.append(Node, tag='node', member_of='group', project=['id'])
+        res = qb.dict()
 
-        parser.add_argument('output_file', type=str,
-                            help='The output file name for the export file')
+        group_dict.update({group['group']['*'].name: group['group']['*'].dbgroup for group in res})
+        node_id_set.update([node['node']['id'] for node in res])
 
-        parsed_args = parser.parse_args(args)
+    if groups:
+        qb = QueryBuilder()
+        qb.append(Group, tag='group', project=['*'], filters={'id': {'in': groups}})
+        qb.append(Node, tag='node', member_of='group', project=['id'])
+        res = qb.dict()
 
-        if parsed_args.nodes is None:
-            node_id_set = set()
-        else:
-            node_id_set = set(parsed_args.nodes)
+        group_dict.update({group['group']['*'].name: group['group']['*'].dbgroup for group in res})
+        node_id_set.update([node['node']['id'] for node in res])
 
-        group_dict = dict()
+    # The db_groups that correspond to what was searched above
+    dbgroups_list = group_dict.values()
 
-        if parsed_args.groups is not None:
-            qb = QueryBuilder()
-            qb.append(Group, tag='group', project=['*'],
-                      filters={'name': {'in': parsed_args.groups}})
-            qb.append(Node, tag='node', member_of='group', project=['id'])
-            res = qb.dict()
+    # Getting the nodes that correspond to the ids that were found above
+    if len(node_id_set) > 0:
+        qb = QueryBuilder()
+        qb.append(Node, tag='node', project=['*'], filters={'id': {'in': node_id_set}})
+        node_list = [node[0] for node in qb.all()]
+    else:
+        node_list = list()
 
-            group_dict.update({_['group']['*'].name: _['group']['*'].dbgroup
-                               for _ in res})
-            node_id_set.update([_['node']['id'] for _ in res])
+    # Check if any of the nodes wasn't found in the database.
+    missing_nodes = node_id_set.difference(node.id for node in node_list)
+    for node_id in missing_nodes:
+        print >> sys.stderr, ('WARNING! Node with pk={} not found, skipping'.format(node_id))
 
-        if parsed_args.group_pks is not None:
-            qb = QueryBuilder()
-            qb.append(Group, tag='group', project=['*'],
-                      filters={'id': {'in': parsed_args.group_pks}})
-            qb.append(Node, tag='node', member_of='group', project=['id'])
-            res = qb.dict()
+    # The dbnodes of the above node list
+    dbnode_list = [node.dbnode for node in node_list]
 
-            group_dict.update({_['group']['*'].name: _['group']['*'].dbgroup
-                               for _ in res})
-            node_id_set.update([_['node']['id'] for _ in res])
+    if computers:
+        qb = QueryBuilder()
+        qb.append(Computer, tag='comp', project=['*'], filters={'id': {'in': set(computers)}})
+        computer_list = [computer[0] for computer in qb.all()]
+        missing_computers = set(computers).difference(computer.id for computer in computer_list)
 
-        # The db_groups that correspond to what was searched above
-        dbgroups_list = group_dict.values()
+        for computer_id in missing_computers:
+            print >> sys.stderr, ('WARNING! Computer with pk={} not found, skipping'.format(computer_id))
+    else:
+        computer_list = []
 
-        # Getting the nodes that correspond to the ids that were found above
-        if len(node_id_set) > 0:
-            qb = QueryBuilder()
-            qb.append(Node, tag='node', project=['*'],
-                      filters={'id': {'in': node_id_set}})
-            node_list = [_[0] for _ in qb.all()]
-        else:
-            node_list = list()
+    # The dbcomputers of the above computer list
+    dbcomputer_list = [computer.dbcomputer for computer in computer_list]
 
-        # Check if any of the nodes wasn't found in the database.
-        missing_nodes = node_id_set.difference(_.id for _ in node_list)
-        for id in missing_nodes:
-            print >> sys.stderr, ("WARNING! Node with pk= {} "
-                                  "not found, skipping.".format(id))
+    what_list = dbnode_list + dbcomputer_list + dbgroups_list
 
-        # The dbnodes of the above node list
-        dbnode_list = [_.dbnode for _ in node_list]
+    export_function = export
+    additional_kwargs = {}
 
-        if parsed_args.computers is not None:
-            qb = QueryBuilder()
-            qb.append(Computer, tag='comp', project=['*'],
-                      filters={'id': {'in': set(parsed_args.computers)}})
-            computer_list = [_[0] for _ in qb.all()]
-            missing_computers = set(parsed_args.computers).difference(
-                _.id for _ in computer_list)
-            for id in missing_computers:
-                print >> sys.stderr, ("WARNING! Computer with pk= {} "
-                                      "not found, skipping.".format(id))
-        else:
-            computer_list = []
+    if zipfile_uncompressed:
+        export_function = export_zip
+        additional_kwargs.update({'use_compression': False})
+    elif zipfile_compressed:
+        export_function = export_zip
+        additional_kwargs.update({'use_compression': True})
 
-        # The dbcomputers of the above computer list
-        dbcomputer_list = [_.dbcomputer for _ in computer_list]
-
-        what_list = dbnode_list + dbcomputer_list + dbgroups_list
-
-        export_function = export
-        additional_kwargs = {}
-        if parsed_args.zipfileu:
-            export_function = export_zip
-            additional_kwargs.update({"use_compression": False})
-        elif parsed_args.zipfilec:
-            export_function = export_zip
-            additional_kwargs.update({"use_compression": True})
-        try:
-            export_function(
-                what=what_list, also_parents=not parsed_args.no_parents,
-                also_calc_outputs=not parsed_args.no_calc_outputs,
-                outfile=parsed_args.output_file,
-                overwrite=parsed_args.overwrite,**additional_kwargs)
-        except IOError as e:
-            print >> sys.stderr, "IOError: {}".format(e.message)
-            sys.exit(1)
-
-    def complete(self, subargs_idx, subargs):
-        return ""
+    try:
+        export_function(
+            what=what_list, also_parents=not no_parents,
+            also_calc_outputs=not no_calc_outputs,
+            outfile=outfile, overwrite=overwrite,
+            **additional_kwargs
+        )
+    except IOError as e:
+        print >> sys.stderr, 'IOError: {}'.format(e.message)
+        sys.exit(1)

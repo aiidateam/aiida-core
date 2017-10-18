fc33ee32565e4c84c179b8016d3d8e9134bbeae4
DropD <r.haeuselmann@gmx.ch>
2017-10-18 16:17:52 +0200

    setup test: add yet more input

--- .travis-data/test_setup.py
+++ .travis-data/test_setup.py
@@ -99,7 +99,7 @@ class SetupTestCase(unittest.TestCase):
         result = self.runner.invoke(
             _setup_cmd,
             ['radames2', '--only-config'],
-            input='yes\nradames.verdi@ope.ra\npostgresql_psycopg2\n\n\n\n\n\n{repo}\nRadames2\nVerdi2\nScala2\nyes\n'.format(
+            input='yes\nradames.verdi@ope.ra\npostgresql_psycopg2\n\n\n\n\n\n{repo}\nRadames2\nVerdi2\nScala2\nyes\nno\n'.format(
                 repo=self.repo
             ),
             catch_exceptions=False

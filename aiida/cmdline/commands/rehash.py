7df259fd61f4afaac86872eae48192e0d114563d
Dominik Gresch <greschd@gmx.ch>
2017-10-20 00:34:21 +0200

    Add option to specify PKs (or --all) in verdi rehash.

--- aiida/cmdline/commands/rehash.py
+++ aiida/cmdline/commands/rehash.py
@@ -1,6 +1,9 @@
+import sys
+
 import click
 
 from plum.util import load_class
+from plum.exceptions import ClassNotFoundException
 
 from aiida import try_load_dbenv
 from aiida.cmdline.baseclass import VerdiCommand
@@ -22,15 +25,35 @@ class Rehash(VerdiCommand):
         print ""
 
 @click.command('rehash')
-@click.argument('classname', type=str, default='aiida.orm.node.Node')
-def _rehash_cmd(classname):
+@click.option('--all', '-a', is_flag=True, help='Rehash all nodes of the given Node class.')
+@click.option('--class-name', type=str, default='aiida.orm.node.Node', help='Restrict nodes which are re-hashed to instances of this class.')
+@click.argument('pks', type=int, nargs=-1)
+def _rehash_cmd(all, class_name, pks):
     try_load_dbenv()
     from aiida.orm.querybuilder import QueryBuilder
-    node_class = load_class(classname)
+
+    # Get the Node class to match
+    try:
+        node_class = load_class(class_name)
+    except ClassNotFoundException:
+        click.echo("Could not load class '{}'.\nAborted!".format(class_name))
+        sys.exit(1)
+
+    # Add the filters for the class and PKs.
     qb = QueryBuilder()
-    qb.append(node_class)
+    qb.append(node_class, tag='node')
+    if pks:
+        qb.add_filter('node', {'id': {'in': pks}})
+    else:
+        if not all:
+            click.echo("Nothing specified, nothing re-hashed.\nExplicitly specify the PK of the nodes, or use '--all'.")
+            return
+
+    i = -1
     for i, n in enumerate(qb.iterall()):
         if i % 100 == 0:
             click.echo('.', nl=False)
         n[0].rehash()
-    click.echo('\nAll done! {} nodes re-hashed.'.format(i + 1))
+    if i > 0:
+        click.echo('')
+    click.echo('All done! {} nodes re-hashed.'.format(i + 1))

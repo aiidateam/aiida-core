a3176d297810e25e06454c4dd699f18418ee6933
Sebastiaan Huber <mail@sphuber.net>
2017-10-19 09:38:00 +0200

    Fix the version numbers of dependencies to most recent version
    The process of finding the most recent compatible version of the
    dependencies was to first set all requirements to >= than the
    current version and try to install aiida_core in a clean virtual
    environment, resetting dependency versions where necessary until
    all tests run. At that point, run pip freeze to get a list of the
    currently installed dependencies and their version numbers. Through
    a utility script those version are then placed back into the
    setup_requirements.py file using == notation, except where not
    possible, indicated by a blacklist of dependencies. This utility
    script will be uploaded in the aiida-helpers repository

--- setup_requirements.py
+++ setup_requirements.py
@@ -9,20 +9,20 @@
 ###########################################################################
 # Requirements for core AiiDA functionalities
 install_requires = [
-    'pip>=9.0.1',
-    'setuptools>=18.5',
-    'reentry>=1.0.2',
-    'wheel>=0.29.0',
-    'python-dateutil>=2.6.0',
-    'python-mimeparse>=0.1.4',
+    'pip==9.0.1',
+    'setuptools==36.6.0',
+    'reentry==1.0.2',
+    'wheel==0.29.0',
+    'python-dateutil==2.6.0',
+    'python-mimeparse==0.1.4',
     'django==1.7.11',  # upgrade to Django 1.9 does prevent AiiDA functioning
-    'django-extensions<1.8',
-    'tzlocal>=1.3',
-    'pytz>=2014.10',
-    'six>=1.10',
-    'future',
-    'pathlib2',
-    'singledispatch>=3.4.0.0',
+    'django-extensions==1.5.0',
+    'tzlocal==1.3',
+    'pytz==2014.10',
+    'six==1.10.0',
+    'future==0.16.0',
+    'pathlib2==2.3.0',
+    'singledispatch==3.4.0.3',
     # We need for the time being to stay with an old version
     # of celery, including the versions of the AMQP libraries below,
     # because the support for a SQLA broker has been dropped in later
@@ -37,30 +37,30 @@ install_requires = [
     # as well
     'billiard==3.3.0.23',
     'amqp==1.4.9',
-    'anyjson>=0.3.3',
-    'supervisor>=3.1.3',
-    'meld3>=1.0.0',
-    'numpy>=1.12.0',
-    'plumpy>=0.7.9',
-    'portalocker>=1.1.0',
+    'anyjson==0.3.3',
+    'supervisor==3.1.3',
+    'meld3==1.0.0',
+    'numpy==1.12.0',
+    'plumpy==0.7.9',
+    'portalocker==1.1.0',
     'SQLAlchemy==1.0.12',  # upgrade to SQLalchemy 1.1.5 does break tests, see #465
-    'SQLAlchemy-Utils>=0.31.2',
-    'alembic>=0.9.2',
-    'ujson>=1.35',
-    'enum34>=1.1.6',
-    'voluptuous>=0.8.11',
-    'aldjemy>=0.6.0',
-    'passlib>=1.7.1',
-    'validate-email>=1.3',
-    'click>=6.7',
-    'tabulate>=0.7.5',
-    'ete3>=3.0.0b35',
-    'uritools>=1.0.2',
-    'psycopg2>=2.7.1',
+    'SQLAlchemy-Utils==0.31.2',
+    'alembic==0.9.6',
+    'ujson==1.35',
+    'enum34==1.1.6',
+    'voluptuous==0.8.11',
+    'aldjemy==0.6.0',
+    'passlib==1.7.1',
+    'validate-email==1.3',
+    'click==6.7',
+    'tabulate==0.7.5',
+    'ete3==3.0.0b35',
+    'uritools==1.0.2',
+    'psycopg2==2.7.1',
     # Requirements for ssh transport
-    'paramiko>=2.1.2',
-    'ecdsa>=0.13',
-    'pycrypto>=2.6.1',
+    'paramiko==2.1.2',
+    'ecdsa==0.13',
+    'pycrypto==2.6.1',
     # Requirements for verdi shell (version of ipython non enforced, because
     # there are people who still prefer version 4 rather than the latest)
     'ipython<6.0',
@@ -72,46 +72,46 @@ extras_require = {
     # N. B.: you need to install first libffi and MIT kerberos GSSAPI including header files.
     # E.g. for Ubuntu 14.04: sudo apt-get install libffi-dev libkrb5-dev
     'ssh_kerberos': [
-        'pyasn1>=0.1.9',
+        'pyasn1==0.3.7',
         'python-gssapi>=0.6.4',
     ],
     # Requirements for RESTful API
     'REST': [
-        'flask>=0.10.1',
-        'flask-restful>=0.3.5',
-        'flask-cors>=3.0.1',
-        'pyparsing>=2.1.10',
-        'pattern>=2.6',
-        'Flask-SqlAlchemy>=2.1',
-        'SQLAlchemy-migrate>=0.10.0',
-        'marshmallow-sqlalchemy>=0.10.0',
-        'flask-marshmallow>=0.7.0',
-        'itsdangerous>=0.24',
-        'flask-httpauth>=3.2.0',
-        'flask-cache>=0.13.1',
-        'python-memcached>=1.58',
+        'Flask==0.10.1',
+        'Flask-RESTful==0.3.5',
+        'Flask-Cors==3.0.1',
+        'pyparsing==2.1.10',
+        'Pattern==2.6',
+        'Flask-SQLAlchemy==2.1',
+        'sqlalchemy-migrate==0.10.0',
+        'marshmallow-sqlalchemy==0.10.0',
+        'flask-marshmallow==0.7.0',
+        'itsdangerous==0.24',
+        'Flask-HTTPAuth==3.2.0',
+        'Flask-Cache==0.13.1',
+        'python-memcached==1.58',
     ],
     # Requirements to buiilding documentation
     'docs': [
-        'Sphinx>=1.5.2',
-        'pygments>=2.2.0',
-        'docutils>=0.13.1',
-        'jinja2>=2.9.5',
-        'markupsafe>=0.23',
+        'Sphinx==1.5.2',
+        'Pygments==2.2.0',
+        'docutils==0.13.1',
+        'Jinja2==2.9.5',
+        'MarkupSafe==0.23',
         # Required by readthedocs
-        'sphinx-rtd-theme>=0.1.9',
+        'sphinx-rtd-theme==0.1.9',
     ],
     # Requirements for non-core funciontalities that rely on external atomic
     # manipulation/processing software
     'atomic_tools': [
-        'spglib>=1.9',
+        'spglib==1.9.10.1',
         # support for symmetry detection in aiida.orm.data.structure. Has no
         # easily accessible version number
-        'pymatgen>=4.5.3',  # support for NWChem I/O
+        'pymatgen==4.5.3',  # support for NWChem I/O
         'ase==3.12.0',  # support for crystal structure manipulation
-        'PyMySQL>=0.7.9',  # required by ICSD tools
+        'PyMySQL==0.7.9',  # required by ICSD tools
         'PyCifRW==3.6.2.1',
-        'seekpath>=1.4.0',
+        'seekpath==1.6.0',
         # support for the AiiDA CifData class. Update to version 4 ddoes
         # break tests
     ],
@@ -121,8 +121,8 @@ extras_require = {
     ],
     # Requirements for testing
     'testing': [
-        'mock',
-        'pgtest'
+        'mock==2.0.0',
+        'pgtest==1.1.0'
     ],
     'dev_precommit': [
         'pre-commit',

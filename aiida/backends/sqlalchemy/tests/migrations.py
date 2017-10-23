208174f8753f3dd1731e335e9f50545536b6a7dd
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2017-10-23 15:26:36 +0200

    More progress in having a proper way to create a test database

--- aiida/backends/sqlalchemy/tests/migrations.py
+++ aiida/backends/sqlalchemy/tests/migrations.py
@@ -279,6 +279,24 @@ class TestExample(unittest.TestCase):
         # new_db(self.uri_left)
         # new_db(self.uri_right)
 
+
+    def create_database(self, existing_db_uri, new_db_uri):
+        from sqlalchemy.engine import create_engine
+
+        # Create engine and connect to the existing database
+        engine = create_engine(existing_db_uri)
+        conn = engine.connect()
+        # Do an initial commit to exit the transaction procedure
+        conn.execute("commit")
+
+        #
+
+        conn.execute("create database temp_07f07f0a599d4c6d8f658ffc892e368c")
+        conn.close()
+
+
+
+
     def tearDown(self):
         destroy_database(self.db_url_left)
         destroy_database(self.db_url_right)
@@ -335,3 +353,53 @@ class TestExample(unittest.TestCase):
 
         return create_engine(engine_url, json_serializer=dumps_json,
                              json_deserializer=loads_json)
+
+
+def create_database(url, encoding='utf8'):
+    """Issue the appropriate CREATE DATABASE statement.
+
+    This is a modification of sqlalchemy_utils.functions.database.create_database
+    since the latter one did not correctly work with SQLAlchemy and PostgreSQL.
+
+    It was removed the template argument that was causing problems.
+
+    :param url: A SQLAlchemy engine URL.
+    :param encoding: The encoding to create the database as.
+
+
+    It currently supports only PostgreSQL and the psycopg2 driver.
+    """
+
+    from sqlalchemy.engine.url import make_url
+    from sqlalchemy_utils.functions.orm import quote
+    from copy import copy
+    import sqlalchemy as sa
+
+    url = copy(make_url(url))
+
+    database = url.database
+
+    if url.drivername.startswith('postgresql'):
+        url.database = 'template1'
+    elif not url.drivername.startswith('sqlite'):
+        url.database = None
+
+    engine = sa.create_engine(url)
+
+    if engine.dialect.name == 'postgresql':
+        if engine.driver == 'psycopg2':
+            from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+            engine.raw_connection().set_isolation_level(
+                ISOLATION_LEVEL_AUTOCOMMIT
+            )
+
+        text = "CREATE DATABASE {0} ENCODING '{1}'".format(
+            quote(engine, database),
+            encoding
+        )
+
+        engine.execute(text)
+
+    else:
+        raise Exception("Only PostgreSQL with the psycopg2 driver is "
+                        "supported.")

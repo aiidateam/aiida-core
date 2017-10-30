d809be64ae92f6037aa4029b5b87aa91b6d567f2
Dominik Gresch <greschd@gmx.ch>
2017-10-30 14:07:42 +0100

    Update plumpy requirement to 0.7.11

--- setup_requirements.py
+++ setup_requirements.py
@@ -41,7 +41,7 @@ install_requires = [
     'supervisor==3.1.3',
     'meld3==1.0.0',
     'numpy==1.12.0',
-    'plumpy==0.7.10',
+    'plumpy==0.7.11',
     'portalocker==1.1.0',
     'SQLAlchemy==1.0.12',  # upgrade to SQLalchemy 1.1.5 does break tests, see #465
     'SQLAlchemy-Utils==0.31.2',

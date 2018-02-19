a2444ef310da9757f7f3eb1e683983534a823a79
Leopold Talirz <leopold.talirz@gmail.com>
2018-02-19 15:59:45 +0100

    fix #1143
    * disable matplotlib requirement for rtd (was not needed)
    * make sure that travis installs the same python packages as
      rtd for building the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ needs_sphinx = '1.5.0'
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'IPython.sphinxext.ipython_console_highlighting', 'IPython.sphinxext.ipython_directive']
+ipython_mplbackend = ""
 
 todo_include_todos = True
 

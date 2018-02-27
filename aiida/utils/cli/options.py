47d76afd58b9b4b208e9f0be11e305ebf7df4646
Sebastiaan Huber <mail@sphuber.net>
2018-02-27 20:18:06 +0100

    Remove overly specific default cli options
    Also renamed the 'parent_calc' option to 'calculation' which
    is clearer and more accurate

--- aiida/utils/cli/options.py
+++ aiida/utils/cli/options.py
@@ -44,11 +44,13 @@ class overridable_option(object):
 
         return click.option(*args_copy, **kw_copy)
 
+
 code = overridable_option(
     '-c', '--code', type=click.STRING, required=True,
     callback=validators.validate_code,
     help='the label of the AiiDA code object to use'
 )
+
 
 structure = overridable_option(
     '-s', '--structure', type=click.INT, required=True,
@@ -56,11 +58,13 @@ structure = overridable_option(
     help='the node pk of the structure'
 )
 
+
 pseudo_family = overridable_option(
     '-p', '--pseudo-family', type=click.STRING, required=True,
     callback=validators.validate_pseudo_family,
     help='the name of the pseudo potential family to use'
 )
+
 
 kpoint_mesh = overridable_option(
     '-k', '--kpoint-mesh', 'kpoints', nargs=3, type=click.INT, default=[2, 2, 2], show_default=True,
@@ -68,36 +72,31 @@ kpoint_mesh = overridable_option(
     help='the number of points in the kpoint mesh along each basis vector'
 )
 
-parent_calc = overridable_option(
-    '-r', '--parent-calc', type=click.INT, required=True,
-    callback=validators.validate_parent_calc,
-    help='the node pk of the parent calculation'
+
+calculation = overridable_option(
+    '-r', '--calculation', type=click.INT, required=True,
+    callback=validators.validate_calculation,
+    help='the pk of a calculation node'
 )
+
 
 max_num_machines = overridable_option(
     '-m', '--max-num-machines', type=click.INT, default=1, show_default=True,
     help='the maximum number of machines (nodes) to use for the calculations'
 )
 
+
 max_wallclock_seconds = overridable_option(
     '-w', '--max-wallclock-seconds', type=click.INT, default=1800, show_default=True,
     help='the maximum wallclock time in seconds to set for the calculations'
 )
 
-automatic_parallelization = overridable_option(
-    '-a', '--automatic-parallelization', is_flag=True, default=False, show_default=True,
-    help='enable the automatic parallelization option of the workchain'
-)
 
 daemon = overridable_option(
     '-d', '--daemon', is_flag=True, default=False, show_default=True,
     help='submit the workchain to the daemon instead of running it locally'
 )
 
-clean_workdir = overridable_option(
-    '-x', '--clean-workdir', is_flag=True, default=False, show_default=True,
-    help='clean the remote folder of all the launched calculations after completion of the workchain'
-)
 
 group = overridable_option(
     '-g', '--group', type=click.STRING, required=True,

5be898c54b26dcce1b883cdad76ea55cea88f66a
Sebastiaan Huber <mail@sphuber.net>
2018-03-11 15:49:58 +0100

    Refactor circus functionality into CircusClient
    This cleans up the code in verdi daemon commands by centralizing all
    the code that interacts with the circus deamon through a CircusClient
    instance that can be configured for a particular profile. I had to
    get rid of the `@only_if_daemon_pid` decorator on the function that
    called the circus endpoint for any given daemon, because this prevented
    the implementation of `--all` flags such as for `verdi daemon status`.
    The decorator would call `sys.exit(1)` straight away if a daemon was
    not running. Instead we check manually if the daemon is running and
    just return a status if it is not instead of killing the process

--- aiida/cmdline/commands/daemon.py
+++ aiida/cmdline/commands/daemon.py
@@ -13,62 +13,16 @@ Verdi daemon commands
 import os
 import sys
 import subprocess
+import time
 
 import click
-import tabulate
-from circus import get_arbiter
-from circus import logger as circus_logger
-from circus.circusd import daemonize
-from circus.exc import CallError
-from circus.pidfile import Pidfile
-from circus.util import check_future_exception_and_log, configure_logger
-from click_spinner import spinner as cli_spinner
+from click_spinner import spinner
 
 from aiida.cmdline.baseclass import VerdiCommandWithSubcommands
 from aiida.cmdline.commands import verdi, daemon_cmd
 from aiida.cmdline.utils import decorators
 from aiida.cmdline.utils.common import format_local_time, get_env_with_venv_bin
 from aiida.cmdline.utils.daemon import CircusClient
-from aiida.daemon.client import ProfileDaemonClient
-
-
-@decorators.only_if_daemon_pid
-def try_calling_running_client(client, cmd):
-    """
-    Call a given circus client with a given command only if pid file exists, handle timeout
-    """
-    result = None
-
-    try:
-        with cli_spinner():
-            result = client.call(cmd)
-    except CallError as err:
-        if str(err) == 'Timed out.':
-            click.echo('Daemon was not running but a PID file was found. '
-                       'This indicates the daemon was terminated unexpectedly; '
-                       'no action is required but proceed with caution.')
-            sys.exit(0)
-        raise err
-
-    return result
-
-
-def try_calling_client(client, cmd):
-    """
-    Call a given circus client with a given command with optional timeout
-    """
-    result = None
-
-    try:
-        with cli_spinner():
-            result = client.call(cmd)
-    except CallError as err:
-        if str(err) == 'Timed out.':
-            click.echo('failed, timed out')
-            sys.exit(0)
-        raise err
-
-    return result
 
 
 class Daemon(VerdiCommandWithSubcommands):
@@ -119,8 +73,7 @@ def start(foreground):
     """
     Start the daemon
     """
-    profile = ProfileDaemonClient()
-    client = profile.get_client()
+    client = CircusClient()
 
     click.echo('Starting the daemon... ', nl=False)
 
@@ -136,15 +89,16 @@ def start(foreground):
         click.echo('failed: {}'.format(exception))
         sys.exit(1)
 
-    status_cmd = {'command': 'status', 'properties': {'name': profile.daemon_name, 'waiting': True}}
+    # We add a small timeout to give the pid-file a chance to be created
+    with spinner():
+        time.sleep(1)
+        response = client.get_status()
 
-    response = try_calling_client(client, status_cmd)
     click.echo(response['status'])
 
 
 @daemon_cmd.command()
-@decorators.only_if_daemon_pid
-@click.option('-a', '--all', 'all_profiles', is_flag=True, help='Show status for all daemons')
+@click.option('--all', 'all_profiles', is_flag=True, help='Show all daemons')
 def status(all_profiles):
     """
     Print the status of the current daemon or all daemons
@@ -153,60 +107,52 @@ def status(all_profiles):
     from aiida.common.setup import get_profiles_list
 
     if all_profiles is True:
-        profile_list = [p for p in get_profiles_list() if not p.startswith('test_')]
+        profiles = [p for p in get_profiles_list() if not p.startswith('test_')]
     else:
-        profile_list = [get_current_profile_name()]
+        profiles = [get_current_profile_name()]
 
-    for profile_name in profile_list:
+    for profile_name in profiles:
         click.secho('Profile: ', fg='red', bold=True, nl=False)
         click.secho('{}'.format(profile_name), bold=True)
-        print_daemon_status(profile_name)
-        click.echo()
+        result = get_daemon_status(profile_name)
+        click.echo(result)
 
 
 @daemon_cmd.command()
-@click.argument('num', default=1, type=int)
+@click.argument('number', default=1, type=int)
 @decorators.only_if_daemon_pid
-def incr(num):
+def incr(number):
     """
-    Add NUM [default=1] workers to the running daemon
+    Add NUMBER [default=1] workers to the running daemon
     """
-    profile = ProfileDaemonClient()
-    client = profile.get_client()
-
-    incr_cmd = {'command': 'incr', 'properties': {'name': profile.daemon_name, 'nb': num}}
-
-    response = try_calling_running_client(client, incr_cmd)
+    client = CircusClient()
+    response = client.increase_workers(number)
     click.echo(response['status'])
 
 
 @daemon_cmd.command()
-@click.argument('num', default=1, type=int)
+@click.argument('number', default=1, type=int)
 @decorators.only_if_daemon_pid
-def decr(num):
+def decr(number):
     """
-    Remove NUM [default=1] workers from the running daemon
+    Remove NUMBER [default=1] workers from the running daemon
     """
-    profile = ProfileDaemonClient()
-    client = profile.get_client()
-
-    incr_cmd = {'command': 'decr', 'properties': {'name': profile.daemon_name, 'nb': num}}
-
-    response = try_calling_running_client(client, incr_cmd)
+    client = CircusClient()
+    response = client.decrease_workers(number)
     click.echo(response['status'])
 
 
 @daemon_cmd.command()
-@decorators.with_dbenv()
-@decorators.only_if_daemon_pid
 def logshow():
     """
     Show the log of the daemon, press CTRL+C to quit
     """
-    profile = ProfileDaemonClient()
+    client = CircusClient()
+    logfile = client.daemon_client.daemon_log_file
+
     try:
         currenv = get_env_with_venv_bin()
-        process = subprocess.Popen(['tail', '-f', profile.daemon_log_file], env=currenv)
+        process = subprocess.Popen(['tail', '-f', logfile], env=currenv)
         process.wait()
     except KeyboardInterrupt:
         process.kill()
@@ -219,19 +165,16 @@ def stop(no_wait):
     """
     Stop the daemon
     """
-    profile = ProfileDaemonClient()
-    client = profile.get_client()
+    client = CircusClient()
 
     wait = not no_wait
-
-    quit_cmd = {'command': 'quit', 'properties': {'waiting': wait}}
 
     if wait:
         click.echo('Waiting for the daemon to shut down... ', nl=False)
     else:
         click.echo('Shutting the daemon down')
 
-    response = try_calling_running_client(client, quit_cmd)
+    response = client.stop_daemon(wait)
 
     if wait:
         click.echo(response['status'])
@@ -250,8 +193,7 @@ def restart(ctx, reset, no_wait):
     is passed, however, the full circus daemon will be stopped and restarted with just
     a single worker
     """
-    profile = ProfileDaemonClient()
-    client = profile.get_client()
+    client = CircusClient()
 
     wait = not no_wait
 
@@ -259,14 +201,13 @@ def restart(ctx, reset, no_wait):
         ctx.invoke(stop)
         ctx.invoke(start)
     else:
-        restart_cmd = {'command': 'restart', 'properties': {'name': profile.daemon_name, 'waiting': wait}}
 
         if wait:
             click.echo('Restarting the daemon... ', nl=False)
         else:
             click.echo('Restarting the daemon')
 
-        response = try_calling_running_client(client, restart_cmd)
+        response = client.restart_daemon(wait)
 
         if wait:
             click.echo(response['status'])
@@ -281,9 +222,16 @@ def _start_circus(foreground):
     This will actually launch the circus daemon, either daemonized in the background
     or in the foreground, printing all logs to stdout.
 
-    ..: Note: this should not be called directly from the commandline!
+    .. note:: this should not be called directly from the commandline!
     """
-    client = ProfileDaemonClient()
+    from circus import get_arbiter
+    from circus import logger as circus_logger
+    from circus.circusd import daemonize
+    from circus.pidfile import Pidfile
+    from circus.util import check_future_exception_and_log, configure_logger
+
+    circus_client = CircusClient()
+    daemon_client = circus_client.daemon_client
 
     env = get_env_with_venv_bin()
     env['PYTHONUNBUFFERED'] = 'True'
@@ -291,25 +239,25 @@ def _start_circus(foreground):
     logoutput = '-'
 
     if not foreground:
-        logoutput = client.circus_log_file
+        logoutput = daemon_client.circus_log_file
 
     arbiter_config = {
-        'controller': client.get_endpoint(0),
-        'pubsub_endpoint': client.get_endpoint(1),
-        'stats_endpoint': client.get_endpoint(2),
+        'controller': circus_client.get_endpoint(0),
+        'pubsub_endpoint': circus_client.get_endpoint(1),
+        'stats_endpoint': circus_client.get_endpoint(2),
         'logoutput': logoutput,
         'loglevel': loglevel,
         'debug': False,
         'statsd': True,
-        'pidfile': client.circus_pid_file,
+        'pidfile': daemon_client.circus_pid_file,
         'watchers': [{
-            'name': client.daemon_name,
-            'cmd': client.cmd_string,
-            'virtualenv': client.virtualenv,
+            'name': daemon_client.name,
+            'cmd': daemon_client.cmd_string,
+            'virtualenv': daemon_client.virtualenv,
             'copy_env': True,
             'stdout_stream': {
                 'class': 'FileStream',
-                'filename': client.daemon_log_file
+                'filename': daemon_client.daemon_log_file
             },
             'env': env,
         }]
@@ -355,26 +303,25 @@ def _start_circus(foreground):
     sys.exit(0)
 
 
-def print_daemon_status(profile_name):
+def get_daemon_status(profile_name):
     """
     Print the status information of the daemon for a given profile
 
     :param profile_name: the name of the profile
     """
+    from tabulate import tabulate
+
     client = CircusClient(profile_name)
 
     if not client.is_daemon_running:
-        click.echo('The daemon is not running')
-        return
+        return 'The daemon is not running'
 
     status_response = client.get_status()
 
     if status_response['status'] == 'stopped':
-        click.echo('The daemon is paused')
-        return
+        return 'The daemon is paused'
     elif status_response['status'] == 'error':
-        click.echo('The daemon is in an unexpected state. Please try restarting or stopping and then starting')
-        return
+        return 'The daemon is in an unexpected state, try verdi daemon restart --reset'
 
     worker_response = client.get_worker_info()
     daemon_response = client.get_daemon_info()
@@ -385,7 +332,7 @@ def print_daemon_status(profile_name):
         workers.append(worker_row)
 
     if len(workers) > 1:
-        workers_info = tabulate.tabulate(workers, headers='firstrow', tablefmt='simple')
+        workers_info = tabulate(workers, headers='firstrow', tablefmt='simple')
     else:
         workers_info = '--> No workers are running. Use verdi daemon incr to start some!\n'
 
@@ -396,7 +343,7 @@ def print_daemon_status(profile_name):
         'workers': workers_info
     }
 
-    message_tpl = ('Daemon is running as PID {pid} since {time}\nActive workers [{nworkers}]:\n{workers}\n'
-                   'Use verdi daemon [incr | decr] [num] to increase / decrease the amount of workers')
+    template = ('Daemon is running as PID {pid} since {time}\nActive workers [{nworkers}]:\n{workers}\n'
+                'Use verdi daemon [incr | decr] [num] to increase / decrease the amount of workers')
 
-    click.echo(message_tpl.format(**info))
+    return template.format(**info)
--- aiida/cmdline/utils/daemon.py
+++ aiida/cmdline/utils/daemon.py
@@ -1,51 +1,58 @@
 # -*- coding: utf-8 -*-
-import click
-import sys
-
+from circus.client import CircusClient as LibCircusClient
 from circus.exc import CallError
-from click_spinner import spinner as cli_spinner
 
-from aiida.cmdline.utils import decorators
-from aiida.daemon.client import ProfileDaemonClient
-
-
-@decorators.only_if_daemon_pid
-def try_calling_running_client(client, cmd):
-    """
-    Call a given circus client with a given command only if pid file exists, handle timeout
-    """
-    result = None
-
-    try:
-        with cli_spinner():
-            result = client.call(cmd)
-    except CallError as err:
-        if str(err) == 'Timed out.':
-            click.echo('Daemon was not running but a PID file was found. '
-                       'This indicates the daemon was terminated unexpectedly; '
-                       'no action is required but proceed with caution.')
-            sys.exit(0)
-        raise err
-
-    return result
+from aiida.daemon.client import DaemonClient
 
 
 class CircusClient(object):
 
-    def __init__(self, profile_name):
-        self._profile = ProfileDaemonClient(profile_name)
+    _ENDPOINT_TPL = 'tcp://127.0.0.1:{port}'
+
+    def __init__(self, profile_name=None):
+        self._daemon_client = DaemonClient(profile_name)
+
+    def get_endpoint(self, port_incr=0):
+        return self._ENDPOINT_TPL.format(port=self.daemon_client.circus_port + port_incr)
 
     @property
-    def profile(self):
-        return self._profile
+    def client(self):
+        return LibCircusClient(endpoint=self.get_endpoint(), timeout=0.5)
 
     @property
-    def profile_client(self):
-        return self.profile.get_client()
+    def daemon_client(self):
+        return self._daemon_client
+
+    @property
+    def daemon_name(self):
+        return self._daemon_client.name
 
     @property
     def is_daemon_running(self):
-        return self.profile.get_daemon_pid is not None
+        return self.daemon_client.get_daemon_pid is not None
+
+    def call_client(self, command):
+        """
+        Call the client with a specific command. Will check whether the daemon is running first
+        by checking for the pid file. When the pid is found yet the call still fails with a
+        timeout, this means the daemon was actually not running and it was terminated unexpectedly
+        causing the pid file to not be cleaned up properly
+        """
+        if not self.daemon_client.get_daemon_pid:
+            return {'status': 'The daemon is not running'}
+
+        try:
+            result = self.client.call(command)
+        except CallError as exception:
+            if str(exception) == 'Timed out.':
+                return {
+                    'status': 'Daemon was not running but a PID file was found. '
+                    'This indicates the daemon was terminated unexpectedly; '
+                    'no action is required but proceed with caution.'
+                }
+            raise exception
+
+        return result
 
     def get_status(self):
         """
@@ -54,11 +61,11 @@ class CircusClient(object):
         command = {
             'command': 'status',
             'properties': {
-                'name': self.profile.daemon_name
+                'name': self.daemon_name
             }
         }
 
-        return try_calling_running_client(self.profile_client, command)
+        return self.call_client(command)
 
     def get_worker_info(self):
         """
@@ -67,11 +74,11 @@ class CircusClient(object):
         command = {
             'command': 'stats',
             'properties': {
-                'name': self.profile.daemon_name
+                'name': self.daemon_name
             }
         }
 
-        return try_calling_running_client(self.profile_client, command)
+        return self.call_client(command)
 
     def get_daemon_info(self):
         """
@@ -82,4 +89,67 @@ class CircusClient(object):
             'properties': {}
         }
 
-        return try_calling_running_client(self.profile_client, command)
+        return self.call_client(command)
+
+    def increase_workers(self, number):
+        """
+        Increase the number of workers
+
+        :param number: the number of workers to add
+        """
+        command = {
+            'command': 'incr',
+            'properties': {
+                'name': self.daemon_name,
+                'nb': number
+            }
+        }
+
+        return self.call_client(command)
+
+    def decrease_workers(self, number):
+        """
+        Decrease the number of workers
+
+        :param number: the number of workers to remove
+        """
+        command = {
+            'command': 'decr',
+            'properties': {
+                'name': self.daemon_name,
+                'nb': number
+            }
+        }
+
+        return self.call_client(command)
+
+    def stop_daemon(self, wait):
+        """
+        Stop the daemon
+
+        :param wait: boolean to indicate whether to wait for the result of the command
+        """
+        command = {
+            'command': 'quit',
+            'properties': {
+                'waiting': wait
+            }
+        }
+
+        return self.call_client(command)
+
+    def restart_daemon(self, wait):
+        """
+        Restart the daemon
+
+        :param wait: boolean to indicate whether to wait for the result of the command
+        """
+        command = {
+            'command': 'restart',
+            'properties': {
+                'name': self.daemon_name,
+                'waiting': wait
+            }
+        }
+
+        return self.call_client(command)

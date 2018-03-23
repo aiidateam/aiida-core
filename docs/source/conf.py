18f8f43079a1ca033bdb445112b90b7ef4461905
Leopold Talirz <leopold.talirz@gmail.com>
2018-03-23 21:28:02 +0100

    docs: work around built-in type bug
    See https://stackoverflow.com/questions/11417221/sphinx-autodoc-gives-warning-pyclass-reference-target-not-found-type-warning
    
    and https://bugs.python.org/issue11975

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -343,6 +343,7 @@ epub_copyright = copyright
 # Warnings to ignore when using the -n (nitpicky) option
 # We should ignore any python built-in exception, for instance
 nitpick_ignore = [
+    ('py:class', 'type'),
     ('py:exc', 'ArithmeticError'),
     ('py:exc', 'AssertionError'),
     ('py:exc', 'AttributeError'),

aiidateam/aiida-core

commit f0e2b614c1ba593fccc1caeaa3da4d69e6fcd6ff

Changed AiiDA user to use new backend concept
The old system (which is still in place for most AiiDA types) uses
an abstract that is subclassed by the concrete backendclass e.g.
AbstractUser and DjangoUser and then DjangoUser is then imported
at load_dbenv.  This has a lot of problems, including that you have
to load_dbenv at the right time, it prohibits having more than one
backend in play at a time and it means that we use global variables
which make debuggin harder.

This bring User into conformance with the new system which is so
far used just by Log.

aiida/cmdline/verdilib.py
@@ -27,7 +27,6 @@ import click
 
 import aiida
 import aiida.cmdline.commands.user
-import aiida.control.user
 from aiida.common.exceptions import (
     AiidaException, ConfigurationError, ProfileConfigurationError)
 from aiida.cmdline.baseclass import VerdiCommand, VerdiCommandRouter
@@ -35,7 +34,7 @@ from aiida.cmdline import pass_to_django_manage
 from aiida.backends import settings as settings_profile
 from aiida.control.postgres import Postgres, manual_setup_instructions, prompt_db_info
 from aiida.cmdline.commands import verdi
-from  aiida.backends.profile import (BACKEND_DJANGO, BACKEND_SQLA)
+from aiida.backends.profile import (BACKEND_DJANGO, BACKEND_SQLA)
 
 # Import here from other files; once imported, it will be found and
 # used as a command-line parameter
@@ -176,39 +175,41 @@ class CompletionCommand(VerdiCommand):
           and then, no substitution is suggested.
         """
 
-        print r"""
-function _aiida_verdi_completion
-{
-    OUTPUT=$( $1 completion "$COMP_CWORD" "${COMP_WORDS[@]}" ; echo 'x')
-    OUTPUT=${OUTPUT%x}
-    if [ -z "$OUTPUT" ]
-    then
-    # Only newline is a valid separator
-        local IFS=$'\n'
-
-        COMPREPLY=( $(compgen -o default -- "${COMP_WORDS[COMP_CWORD]}" ) )
-    # Add either a slash or a space, depending on whether it is a folder
-    # or a file. printf %q escapes the filename if there are spaces.
-        for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
-            [ -d "${COMPREPLY[$i]}" ] && \
-               COMPREPLY[$i]=$(printf %q%s "${COMPREPLY[$i]}" "/") || \
-               COMPREPLY[$i]=$(printf %q%s "${COMPREPLY[$i]}" " ")
-        done
-
-    else
-        COMPREPLY=( $(compgen -W "$OUTPUT" -- "${COMP_WORDS[COMP_CWORD]}" ) )
-        # Always add a space after each command
-        for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
-            COMPREPLY[$i]="${COMPREPLY[$i]} "
-        done
-    fi
-}
-complete -o nospace -F _aiida_verdi_completion verdi
-"""
+        print
+        r"""
+       function _aiida_verdi_completion
+       {
+           OUTPUT=$( $1 completion "$COMP_CWORD" "${COMP_WORDS[@]}" ; echo 'x')
+           OUTPUT=${OUTPUT%x}
+           if [ -z "$OUTPUT" ]
+           then
+           # Only newline is a valid separator
+               local IFS=$'\n'
+       
+               COMPREPLY=( $(compgen -o default -- "${COMP_WORDS[COMP_CWORD]}" ) )
+           # Add either a slash or a space, depending on whether it is a folder
+           # or a file. printf %q escapes the filename if there are spaces.
+               for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
+                   [ -d "${COMPREPLY[$i]}" ] && \
+                      COMPREPLY[$i]=$(printf %q%s "${COMPREPLY[$i]}" "/") || \
+                      COMPREPLY[$i]=$(printf %q%s "${COMPREPLY[$i]}" " ")
+               done
+       
+           else
+               COMPREPLY=( $(compgen -W "$OUTPUT" -- "${COMP_WORDS[COMP_CWORD]}" ) )
+               # Always add a space after each command
+               for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
+                   COMPREPLY[$i]="${COMPREPLY[$i]} "
+               done
+           fi
+       }
+       complete -o nospace -F _aiida_verdi_completion verdi
+       """
 
     def complete(self, subargs_idx, subargs):
         # disable further completion
-        print ""
+        print
+        ""
 
 
 class Completion(VerdiCommand):
@@ -243,7 +244,8 @@ class Completion(VerdiCommand):
             cword_offset = command_position - 1
 
         if cword == 1 + cword_offset:
-            print " ".join(sorted(short_doc.keys()))
+            print
+            " ".join(sorted(short_doc.keys()))
             return
         else:
             try:
@@ -271,7 +273,8 @@ class ListParams(VerdiCommand):
     """
 
     def run(self, *args):
-        print get_listparams()
+        print
+        get_listparams()
 
 
 class Help(VerdiCommand):
@@ -285,23 +288,23 @@ class Help(VerdiCommand):
         try:
             command = args[0]
         except IndexError:
-            print get_listparams()
-            print ""
-            print (
+            print(get_listparams())
+            print("")
+            print(
                 "Before each command you can specify the AiiDA profile to use,"
                 " with 'verdi -p <profile> <command>' or "
                 "'verdi --profile=<profile> <command>'")
-            print ""
-            print ("Use '{} help <command>' for more information "
-                   "on a specific command.".format(execname))
+            print("")
+            print("Use '{} help <command>' for more information "
+                  "on a specific command.".format(execname))
             sys.exit(1)
 
         if command in short_doc:
-            print "Description for '%s %s'" % (execname, command)
-            print ""
-            print "**", short_doc[command]
+            print("Description for '{} {}'".format(execname, command))
+            print("")
+            print("**{}".format(short_doc[command]))
             if command in long_doc:
-                print long_doc[command]
+                print(long_doc[command])
         else:
             print >> sys.stderr, (
                 "{}: '{}' is not a valid command. "
@@ -312,9 +315,10 @@ class Help(VerdiCommand):
 
     def complete(self, subargs_idx, subargs):
         if subargs_idx == 0:
-            print " ".join(sorted(short_doc.keys()))
+            print
+            " ".join(sorted(short_doc.keys()))
         else:
-            print ""
+            print("")
 
 
 class Install(VerdiCommand):
@@ -337,7 +341,8 @@ class Install(VerdiCommand):
         """
         No completion after 'verdi install'.
         """
-        print ""
+        print
+        ""
 
 
 class Setup(VerdiCommand):
@@ -363,7 +368,8 @@ class Setup(VerdiCommand):
         """
         No completion after 'verdi install'.
         """
-        print ""
+        print
+        ""
 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -373,7 +379,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.argument('profile', default='', type=str)
 @click.option('--only-config', is_flag=True)
 @click.option('--non-interactive', is_flag=True, help='never prompt the user for input, read values from options')
-@click.option('--backend', type=click.Choice(['django', 'sqlalchemy']),)
+@click.option('--backend', type=click.Choice(['django', 'sqlalchemy']), )
 @click.option('--email', type=str)
 @click.option('--db_host', type=str)
 @click.option('--db_port', type=int)
@@ -486,7 +492,9 @@ def setup(profile, only_config, non_interactive=False, **kwargs):
         except KeyError as e:
             import traceback
             click.echo(traceback.format_exc())
-            click.echo("--non-interactive requires all values to be given on the commandline! Missing argument: {}".format(e.message), err=True)
+            click.echo(
+                "--non-interactive requires all values to be given on the commandline! Missing argument: {}".format(
+                    e.message), err=True)
             sys.exit(1)
     else:
         try:
@@ -500,10 +508,11 @@ def setup(profile, only_config, non_interactive=False, **kwargs):
         set_default_profile(gprofile, force_rewrite=False)
 
     if only_user_config:
-        print ("Only user configuration requested, "
-               "skipping the migrate command")
+        print("Only user configuration requested, "
+              "skipping the migrate command")
     else:
-        print "Executing now a migrate command..."
+        print
+        "Executing now a migrate command..."
 
         backend_choice = created_conf['AIIDADB_BACKEND']
         if backend_choice == BACKEND_DJANGO:
@@ -555,10 +564,12 @@ def setup(profile, only_config, non_interactive=False, **kwargs):
         else:
             raise InvalidOperation("Not supported backend selected.")
 
-    print "Database was created successfully"
+    print
+    "Database was created successfully"
 
     # I create here the default user
-    print "Loading new environment..."
+    print
+    "Loading new environment..."
     if only_user_config:
         from aiida.backends.utils import load_dbenv, is_dbenv_loaded
         # db environment has not been loaded in this case
@@ -566,25 +577,27 @@ def setup(profile, only_config, non_interactive=False, **kwargs):
             load_dbenv()
 
     from aiida.common.setup import DEFAULT_AIIDA_USER
-    from aiida.orm.user import User as AiiDAUser
+    from aiida.orm.backend import construct_backend
 
-    if not AiiDAUser.search_for_users(email=DEFAULT_AIIDA_USER):
-        print "Installing default AiiDA user..."
-        nuser = AiiDAUser(email=DEFAULT_AIIDA_USER)
+    backend = construct_backend()
+    if not backend.users.find(email=DEFAULT_AIIDA_USER):
+        print("Installing default AiiDA user...")
+        nuser = backend.users.create(email=DEFAULT_AIIDA_USER)
         nuser.first_name = "AiiDA"
         nuser.last_name = "Daemon"
-        nuser.is_staff = True
         nuser.is_active = True
-        nuser.is_superuser = True
-        nuser.force_save()
+        nuser.store()
 
     from aiida.common.utils import get_configured_user_email
     email = get_configured_user_email()
-    print "Starting user configuration for {}...".format(email)
+    print
+    "Starting user configuration for {}...".format(email)
     if email == DEFAULT_AIIDA_USER:
-        print "You set up AiiDA using the default Daemon email ({}),".format(
+        print
+        "You set up AiiDA using the default Daemon email ({}),".format(
             email)
-        print "therefore no further user configuration will be asked."
+        print
+        "therefore no further user configuration will be asked."
     else:
         # Ask to configure the new user
         if not non_interactive:
@@ -592,16 +605,17 @@ def setup(profile, only_config, non_interactive=False, **kwargs):
         else:
             # or don't ask
             aiida.cmdline.commands.user.do_configure(
+                backend,
                 email=kwargs['email'],
                 first_name=kwargs.get('first_name'),
                 last_name=kwargs.get('last_name'),
                 institution=kwargs.get('institution'),
                 no_password=True,
                 non_interactive=non_interactive,
-                force=True
+                force_reconfigure=True
             )
 
-    print "Setup finished."
+    print("Setup finished.")
 
 
 class Quicksetup(VerdiCommand):
@@ -612,7 +626,7 @@ class Quicksetup(VerdiCommand):
     doesn't exist). Creates a database '<profile>_<username>' (if it exists,
     prompts user to use or change the name).
     '''
-    from  aiida.backends.profile import (BACKEND_DJANGO, BACKEND_SQLA)
+    from aiida.backends.profile import (BACKEND_DJANGO, BACKEND_SQLA)
 
     def run(self, *args):
         ctx = self._ctx(args)
@@ -624,10 +638,11 @@ class Quicksetup(VerdiCommand):
     def _ctx(args, info_name='verdi quicksetup', **kwargs):
         return quicksetup.make_context(info_name, list(args), **kwargs)
 
+
 @verdi.command('quicksetup', context_settings=CONTEXT_SETTINGS)
 @click.option('--profile', prompt='Profile name', type=str, default='quicksetup')
 @click.option('--email', prompt='Email Address (identifies your data when sharing)', type=str,
-                help='This email address will be associated with your data and will be exported along with it, should you choose to share any of your work')
+              help='This email address will be associated with your data and will be exported along with it, should you choose to share any of your work')
 @click.option('--first-name', prompt='First Name', type=str)
 @click.option('--last-name', prompt='Last Name', type=str)
 @click.option('--institution', prompt='Institution', type=str)
@@ -637,11 +652,12 @@ class Quicksetup(VerdiCommand):
 @click.option('--db-user-pw', type=str)
 @click.option('--db-name', type=str)
 @click.option('--repo', type=str)
-@click.option('--set-default/--no-set-default', default=None, help='Whether to set new profile as default for shell and daemon.')
+@click.option('--set-default/--no-set-default', default=None,
+              help='Whether to set new profile as default for shell and daemon.')
 @click.option('--non-interactive', is_flag=True, help='never prompt the user for input, read values from options')
 @click.pass_obj
 def quicksetup(self, profile, email, first_name, last_name, institution, backend, db_port, db_user, db_user_pw, db_name,
-                    repo, set_default, non_interactive):
+               repo, set_default, non_interactive):
     '''Set up a sane aiida configuration with as little interaction as possible.'''
     from aiida.common.setup import create_base_dirs, AIIDA_CONFIG_FOLDER
     create_base_dirs()
@@ -677,7 +693,8 @@ def quicksetup(self, profile, email, first_name, last_name, institution, backend
     for v in profs.itervalues():
         if v.get('AIIDADB_USER', '') == dbuser and not db_user_pw:
             dbpass = v.get('AIIDADB_PASS')
-            print 'using found password for {}'.format(dbuser)
+            print
+            'using found password for {}'.format(dbuser)
             break
 
     try:
@@ -755,6 +772,7 @@ def quicksetup(self, profile, email, first_name, last_name, institution, backend
 
     if do_set_default:
         set_default_profile(profile_name, force_rewrite=True)
+
 
 def _check_db_name(dbname, postgres):
     '''looks up if a database with the name exists, prompts for using or creating a differently named one'''
@@ -955,9 +973,9 @@ def exec_from_cmdline(argv):
         verdi_subcmd.get_command_name(): verdi_subcmd
         for verdi_subcmd in verdilib_namespace.itervalues()
         if inspect.isclass(verdi_subcmd) and not verdi_subcmd == VerdiCommand
-        and issubclass(verdi_subcmd, VerdiCommand)
-        and not verdi_subcmd.__name__.startswith('_')
-        and not verdi_subcmd._abstract
+           and issubclass(verdi_subcmd, VerdiCommand)
+           and not verdi_subcmd.__name__.startswith('_')
+           and not verdi_subcmd._abstract
     }
 
     # Retrieve the list of docstrings, managing correctly the
@@ -1026,7 +1044,7 @@ def exec_from_cmdline(argv):
         else:
             print >> sys.stderr, ("{}: '{}' is not a valid command. "
                                   "See '{} help' for more help.".format(
-                                      execname, command, execname))
+                execname, command, execname))
             get_command_suggestion(command)
             sys.exit(1)
     except ProfileConfigurationError as err:

commit 2d0770b05e3d1b7de7043290977a637074346cb6

Fix verdi completioncommand that was broken in f0e2b614c1ba59

aiida/cmdline/verdilib.py
@@ -175,41 +175,41 @@ class CompletionCommand(VerdiCommand):
           and then, no substitution is suggested.
         """
 
-        print
-        r"""
-       function _aiida_verdi_completion
-       {
-           OUTPUT=$( $1 completion "$COMP_CWORD" "${COMP_WORDS[@]}" ; echo 'x')
-           OUTPUT=${OUTPUT%x}
-           if [ -z "$OUTPUT" ]
-           then
-           # Only newline is a valid separator
-               local IFS=$'\n'
-       
-               COMPREPLY=( $(compgen -o default -- "${COMP_WORDS[COMP_CWORD]}" ) )
-           # Add either a slash or a space, depending on whether it is a folder
-           # or a file. printf %q escapes the filename if there are spaces.
-               for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
-                   [ -d "${COMPREPLY[$i]}" ] && \
-                      COMPREPLY[$i]=$(printf %q%s "${COMPREPLY[$i]}" "/") || \
-                      COMPREPLY[$i]=$(printf %q%s "${COMPREPLY[$i]}" " ")
-               done
-       
-           else
-               COMPREPLY=( $(compgen -W "$OUTPUT" -- "${COMP_WORDS[COMP_CWORD]}" ) )
-               # Always add a space after each command
-               for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
-                   COMPREPLY[$i]="${COMPREPLY[$i]} "
-               done
-           fi
-       }
-       complete -o nospace -F _aiida_verdi_completion verdi
-       """
+        print(
+            r"""
+            function _aiida_verdi_completion
+            {
+                OUTPUT=$( $1 completion "$COMP_CWORD" "${COMP_WORDS[@]}" ; echo 'x')
+                OUTPUT=${OUTPUT%x}
+                if [ -z "$OUTPUT" ]
+                then
+                # Only newline is a valid separator
+                    local IFS=$'\n'
+
+                    COMPREPLY=( $(compgen -o default -- "${COMP_WORDS[COMP_CWORD]}" ) )
+                # Add either a slash or a space, depending on whether it is a folder
+                # or a file. printf %q escapes the filename if there are spaces.
+                    for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
+                        [ -d "${COMPREPLY[$i]}" ] && \
+                           COMPREPLY[$i]=$(printf %q%s "${COMPREPLY[$i]}" "/") || \
+                           COMPREPLY[$i]=$(printf %q%s "${COMPREPLY[$i]}" " ")
+                    done
+
+                else
+                    COMPREPLY=( $(compgen -W "$OUTPUT" -- "${COMP_WORDS[COMP_CWORD]}" ) )
+                    # Always add a space after each command
+                    for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
+                        COMPREPLY[$i]="${COMPREPLY[$i]} "
+                    done
+                fi
+            }
+            complete -o nospace -F _aiida_verdi_completion verdi
+            """
+            )
 
     def complete(self, subargs_idx, subargs):
         # disable further completion
-        print
-        ""
+        print("")
 
 
 class Completion(VerdiCommand):
@@ -244,8 +244,7 @@ class Completion(VerdiCommand):
             cword_offset = command_position - 1
 
         if cword == 1 + cword_offset:
-            print
-            " ".join(sorted(short_doc.keys()))
+            print(" ".join(sorted(short_doc.keys())))
             return
         else:
             try:
@@ -273,8 +272,7 @@ class ListParams(VerdiCommand):
     """
 
     def run(self, *args):
-        print
-        get_listparams()
+        print(get_listparams())
 
 
 class Help(VerdiCommand):
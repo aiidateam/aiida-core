ff6688564fdb68795154884f43eafd8ff0570229
Sebastiaan Huber <mail@sphuber.net>
2018-04-18 21:34:54 +0200

    Documentation: advanced concepts of workchain design
    Wrote extensive documentation on all the features of the WorkChain
    and how to write robust, maintainable and modular workchains. Described
    all the features of the ProcessSpec with the newly added Ports and
    PortNamespaces, the context and how to add futures, the reporting system
    and how to abort workchains and set their finish status.

---
+++ docs/source/concepts/include/snippets/workflows/workchains/run_workchain_submit_append.py
@@ -0,0 +1,21 @@
+from aiida.orm.data.int import Int
+from aiida.work.workchain import WorkChain, ToContext, append_
+
+class SomeWorkChain(WorkChain):
+
+    @classmethod
+    def define(cls, spec):
+        super(SomeWorkChain, cls).define(spec)
+        spec.outline(
+            cls.submit_workchains,
+            cls.inspect_workchains,
+        )
+
+    def submit_workchains(self)
+    	for i in range(3):
+        	future = self.submit(SomeWorkChain)
+        	self.to_context(workchains=append_(future))
+
+    def inspect_workchains(self)
+    	for workchain in self.ctx.workchains:
+        	assert workchain.is_finished_ok

3a2ad44881ef60b3fe667066f3b866442fb854d3
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2018-04-30 10:37:08 +0200

    Fixing the server name for checking the jenkins syntax

--- .jenkins-data/check-jenkinsfile.sh
+++ .jenkins-data/check-jenkinsfile.sh
@@ -2,7 +2,7 @@
 
 # curl (REST API)
 # Assuming "anonymous read access" has been enabled on your Jenkins instance.
-JENKINS_URL=http://localhost:8080
+JENKINS_URL=http://theossrv6.epfl.ch/jenkins
 # JENKINS_CRUMB is needed if your Jenkins master has CRSF protection enabled as it should
 JENKINS_CRUMB=`curl "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"`
 echo CRUMB: $JENKINS_CRUMB

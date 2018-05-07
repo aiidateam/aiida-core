a568bbbf13354b850d52faba07b416c1c8acf0ab
Sebastiaan Huber <mail@sphuber.net>
2018-05-07 12:52:59 +0200

    Change the test_rpn.sh script to use predefined expressions
    For these expressions we know that the timeout will not be
    hit. We lose the randomness of testing but the tests were
    timing out too often and would otherwise take too long.
    The script is written such that just commenting out the line
    with the definition of EXPRESSIONS variable, will return to
    the old behavior of running with randomly generated expressions.

--- .jenkins-data/test_rpn.sh
+++ .jenkins-data/test_rpn.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-NUMBER_WORKCHAINS=5
-TIMEOUT=600
-CODE='add@torquessh'
 
 # Be verbose, and stop with error as soon there's one
 set -ev
+
+declare -a EXPRESSIONS=("1 -2 -1 4 -5 -5 * * * * +" "2 1 3 3 -1 + ^ ^ +" "3 -5 -1 -4 + * ^" "2 4 2 -4 * * +" "3 1 1 5 ^ ^ ^" "3 1 3 4 -4 2 * + + ^ ^")
+NUMBER_WORKCHAINS=5
+TIMEOUT=600
+CODE='add@torquessh'
 
 # Needed on Jenkins
 if [ -e ~/.bashrc ] ; then source ~/.bashrc ; fi
@@ -19,8 +21,12 @@ export PYTHONPATH="${PYTHONPATH}:${JENKINS_DATA_DIR}/polish"
 # Get the absolute path for verdi
 VERDI=$(which verdi)
 
-for i in $(seq 1 $NUMBER_WORKCHAINS); do
-
-    coverage run -a $VERDI -p ${TEST_AIIDA_BACKEND} run "${CLI_SCRIPT}" -c $CODE -C -W -d -t $TIMEOUT
-
-done
+if [ -n "$EXPRESSIONS" ]; then
+    for expression in "${EXPRESSIONS[@]}"; do
+        coverage run -a $VERDI -p ${TEST_AIIDA_BACKEND} run "${CLI_SCRIPT}" -c $CODE -C -W -d -t $TIMEOUT "$expression"
+    done
+else
+    for i in $(seq 1 $NUMBER_WORKCHAINS); do
+        coverage run -a $VERDI -p ${TEST_AIIDA_BACKEND} run "${CLI_SCRIPT}" -c $CODE -C -W -d -t $TIMEOUT
+    done
+fi

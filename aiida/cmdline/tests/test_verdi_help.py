993194a9dee3d806b6646f64aa77a8c4041d4015
Sebastiaan Huber <mail@sphuber.net>
2018-05-09 21:19:54 +0200

    Replace ad-hoc implementation of output capture for Capturing in verdi tests
    The Capture class is more widely used, so we replace the local solution
    capture_output with that one, to reduce cluttering

--- aiida/cmdline/tests/test_verdi_help.py
+++ aiida/cmdline/tests/test_verdi_help.py
@@ -11,8 +11,7 @@
 import unittest
 
 from aiida.cmdline.verdilib import Help, Import
-
-from .common import captured_output
+from aiida.utils.capturing import Capturing
 
 
 class VerdiHelpTest(unittest.TestCase):
@@ -26,16 +25,15 @@ class VerdiHelpTest(unittest.TestCase):
         """
         Prevent regression of bug #700
 
-        ``verdi help`` was printing only the first letter of the docstring
-        of non-click commands
+        ``verdi help`` was printing only the first letter of the docstring of non-click commands
         """
         self.assertFalse(hasattr(Import, '_ctx'), 'This test must use a non-click verdi subcommand')
-        fail_msg = 'Has the docstring for ``verdi import`` changed? ' 'If not, this is a regression of #700'
-        with captured_output() as (out, _):
+        fail_msg = 'Has the docstring for ``verdi import`` changed? If not, this is a regression of #700'
+
+        with Capturing() as output:
             try:
                 self.help_cmd.run()
             except SystemExit:
                 pass
-            finally:
-                output = [l.strip() for l in out.getvalue().split('\n')]
-                self.assertIn('* import        Import nodes and group of nodes', output, fail_msg)
+
+        self.assertIn('  * import        Import nodes and group of nodes', output, fail_msg)

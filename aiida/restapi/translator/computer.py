56224258d3f10afa1d6750ab5810f080dc456b30
Snehal Kumbhar <snehal.kumbhar@epfl.ch>
2018-05-11 12:14:18 +0200

    Moved schema from file to orm class for computer [broke the functionality for other node type to get schema]

--- aiida/restapi/translator/computer.py
+++ aiida/restapi/translator/computer.py
@@ -30,19 +30,32 @@ class ComputerTranslator(BaseTranslator):
 
     _result_type = __label__
 
-    _default_projections = None
-
-    # Extract the default projections from custom_schema if they are defined
-    if custom_schema is not None:
-        try:
-            # check if schema is defined for given node type
-            if __label__ in custom_schema['columns'].keys():
-                _default_projections = custom_schema['columns'][__label__]
-        except KeyError:
-            pass
-
-    if _default_projections is None:
-        _default_projections = ['**']
+    ## computer schema
+    # All the values from column_order must present in additional info dict
+    _default_projections = {
+        "column_order": [
+            "id",
+            "name",
+            "hostname",
+            "description",
+            "enabled",
+            "scheduler_type",
+            "transport_type",
+            "transport_params",
+            "uuid"
+        ],
+        "additional_info": {
+            "id": {"is_display": True},
+            "name": {"is_display": True},
+            "hostname": {"is_display": True},
+            "description": {"is_display": False},
+            "enabled": {"is_display": True},
+            "scheduler_type": {"is_display": True},
+            "transport_type": {"is_display": False},
+            "transport_params": {"is_display": False},
+            "uuid": {"is_display": False}
+        }
+    }
 
     def __init__(self, **kwargs):
         """

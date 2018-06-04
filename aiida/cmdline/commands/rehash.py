b1e6cedc34fe0eb517ccd45ff34b8149cf5ad21e
Sebastiaan Huber <mail@sphuber.net>
2018-06-04 11:06:10 +0200

    Migrate `verdi rehash` to use the click infrastructure
    Also changed the interface options of the command. The --class-name has
    been replaced by -e/--entry-point, since that is a more robust way of
    defining a class in AiiDA. Additionally, the --all option has been
    removed, since that would be implied if no identifier arguments would
    be specified but only an entry point.

--- aiida/cmdline/commands/rehash.py
+++ aiida/cmdline/commands/rehash.py
@@ -7,63 +7,59 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-import sys
-
 import click
 
-from aiida import try_load_dbenv
 from aiida.cmdline.baseclass import VerdiCommand
+from aiida.cmdline.commands import verdi_rehash
+from aiida.cmdline.params import arguments
+from aiida.cmdline.utils import decorators, echo
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 class Rehash(VerdiCommand):
     """
-    Re-hash all nodes.
+    Re-hash nodes filtered by identifier and or node class
     """
 
     def run(self, *args):
-        ctx = _rehash_cmd.make_context('rehash', list(args))
+        ctx = rehash.make_context('rehash', list(args))
         with ctx:
-            _rehash_cmd.invoke(ctx)
-
-    def complete(self, subargs_idx, subargs):
-        """
-        No completion after 'verdi rehash'.
-        """
-        print ""
+            rehash.invoke(ctx)
 
 
-@click.command('rehash')
-@click.option('--all', '-a', is_flag=True, help='Rehash all nodes of the given Node class.')
-@click.option('--class-name', type=str, default='aiida.orm.node.Node',
-              help='Restrict nodes which are re-hashed to instances of this class.')
-@click.argument('pks', type=int, nargs=-1)
-def _rehash_cmd(all, class_name, pks):
-    try_load_dbenv()
+@verdi_rehash.command('rehash', context_settings=CONTEXT_SETTINGS)
+@arguments.NODES()
+@click.option('-e', '--entry-point', 'entry_point_string', type=click.STRING, default='aiida.node:node',
+    help='restrict nodes which are re-hashed to instances that are a sub class of the class identified by this entry point')
+@decorators.with_dbenv()
+def rehash(nodes, entry_point_string):
+    """
+    Rehash all nodes in the database filtered by their identifier and/or based on their class
+    """
+    from aiida.common.exceptions import MissingEntryPointError, MultipleEntryPointError, LoadingEntryPointError
     from aiida.orm.querybuilder import QueryBuilder
-    from aiida.common.utils import get_object_from_string
+    from aiida.plugins.entry_point import load_entry_point, parse_entry_point_string
 
-    # Get the Node class to match
     try:
-        node_class = get_object_from_string(class_name)
-    except ValueError:
-        click.echo("Could not load class '{}'.\nAborted!".format(class_name))
-        sys.exit(1)
+        group, name = parse_entry_point_string(entry_point_string)
+        entry_point = load_entry_point(group, name)
+    except (TypeError, ValueError, MissingEntryPointError, MultipleEntryPointError, LoadingEntryPointError) as exception:
+        echo.echo_critical('{}'.format(exception))
 
-    # Add the filters for the class and PKs.
-    qb = QueryBuilder()
-    qb.append(node_class, tag='node')
-    if pks:
-        qb.add_filter('node', {'id': {'in': pks}})
+    if nodes:
+        to_hash = [(node, ) for node in nodes if isinstance(node, entry_point)]
     else:
-        if not all:
-            click.echo("Nothing specified, nothing re-hashed.\nExplicitly specify the PK of the nodes, or use '--all'.")
-            return
+        qb = QueryBuilder()
+        qb.append(entry_point, tag='node')
+        to_hash = qb.all()
 
-    if not qb.count():
-        click.echo('No matching nodes found.')
-        return
-    for i, (node,) in enumerate(qb.iterall()):
+    if not to_hash:
+        echo.echo_critical('no matching nodes found')
+
+    for i, (node,) in enumerate(to_hash):
         if i % 100 == 0:
-            click.echo('.', nl=False)
+            echo.echo('.', nl=False)
         node.rehash()
-    click.echo('\nAll done! {} node(s) re-hashed.'.format(i + 1))
+
+    echo.echo_success('{} nodes re-hashed'.format(i + 1), prefix='\n')

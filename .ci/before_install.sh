e6b7c5dfda550fde8ac09668f22b37da3d6a7b92
Sebastiaan Huber <mail@sphuber.net>
2018-06-11 09:53:15 +0200

    Merge the .travis-data and .jenkins-data into single folder
    These folders contain the files necessary to run the continuous
    integration on the Travis and Jenkins platforms. So far they had
    separate repositories but a lot of the functionality and scripts
    were shared. Merging the folders will reduce the maintenance load.

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -7,7 +7,7 @@ if [[ "$TEST_TYPE" == "tests" ]]
 then
     THEKEY=`cat ${HOME}/.ssh/id_rsa.pub`
     echo 'AUTHORIZED_KEY='"$THEKEY" > ${TRAVIS_BUILD_DIR}/torquessh.env
-    docker build -t torquessh ${TRAVIS_BUILD_DIR}/.travis-data/torquessh-doubler
+    docker build -t torquessh ${TRAVIS_BUILD_DIR}/.ci/torquessh-doubler
     # Run it in the background, mapping port 22 of the container
     # to port 10022 outside, and passing the environment variable
     docker run -d --privileged -p=10022:22 --name="torquesshmachine" --env-file ${TRAVIS_BUILD_DIR}/torquessh.env torquessh
@@ -15,7 +15,7 @@ then
     echo "Running docker ps to see if the 'torquessh' docker image is up..."
     docker ps    
     # Wait for SSH to be up
-    ${TRAVIS_BUILD_DIR}/.travis-data/wait-for-it.sh localhost:10022 -t 0
+    ${TRAVIS_BUILD_DIR}/.ci/wait-for-it.sh localhost:10022 -t 0
 
     # I will add the key to the known_hosts later, to give the time to ssh
     # to be really up - see the before_script script

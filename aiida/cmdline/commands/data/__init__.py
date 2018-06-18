83d401d8d4c99f6908665c44bc857cde0e7a68ca
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-06-18 11:40:56 +0200

    Data subcommand move to click:
    1) create a folder for data click commands
    2) place subcommands of verdi data in different files

---
+++ aiida/cmdline/commands/data/__init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+import click
+from aiida.cmdline.baseclass import VerdiCommandWithSubcommands
+from aiida.cmdline.commands import verdi, verdi_data
+from aiida.cmdline.params import arguments
+from aiida.cmdline.params import options
+from aiida.cmdline.utils import echo
+from aiida.common.exceptions import DanglingLinkError
+
+from aiida.cmdline.commands.data import upf
+from aiida.cmdline.commands.data import structure
+from aiida.cmdline.commands.data import bands
+from aiida.cmdline.commands.data import cif
+from aiida.cmdline.commands.data import trajectory
+from aiida.cmdline.commands.data import parameter
+from aiida.cmdline.commands.data import array
+from aiida.cmdline.commands.data import label
+from aiida.cmdline.commands.data import remote
+from aiida.cmdline.commands.data import description
+
+
+class Data(VerdiCommandWithSubcommands):
+    """
+    Setup and manage data specific types
+
+    There is a list of subcommands for managing specific types of data.
+    For instance, 'data upf' manages pseudopotentials in the UPF format.
+    """
+
+    def __init__(self):
+        self.valid_subcommands = {
+            'upf': (self.cli, self.complete_none),
+            'structure': (self.cli, self.complete_none),
+            'bands': (self.cli, self.complete_none),
+            'cif': (self.cli, self.complete_none),
+            'trajectory': (self.cli, self.complete_none),
+            'parameter': (self.cli, self.complete_none),
+            'array': (self.cli, self.complete_none),
+            'label': (self.cli, self.complete_none),
+            'remote': (self.cli, self.complete_none),
+            'description': (self.cli, self.complete_none),
+        }
+
+    def cli(self, *args):
+        verdi()
---
+++ aiida/cmdline/commands/data/label.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+import click
+from aiida.cmdline.baseclass import VerdiCommandWithSubcommands
+from aiida.cmdline.commands import verdi, verdi_data
+from aiida.cmdline.params import arguments
+from aiida.cmdline.params import options
+from aiida.cmdline.utils import echo
+from aiida.common.exceptions import DanglingLinkError
+
+
+        
+@verdi_data.group('label')
+@click.pass_context
+def cif(ctx):
+    """help"""
+    pass
+    
+
+@cif.command()
+def show():
+    """help"""
+    click.echo("Test")

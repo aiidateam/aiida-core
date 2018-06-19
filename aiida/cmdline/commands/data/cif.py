89673612786ecad2a3cb90ae390448eadbf03154
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2018-06-19 14:13:30 +0200

    Grouping the list option arguments

--- aiida/cmdline/commands/data/cif.py
+++ aiida/cmdline/commands/data/cif.py
@@ -14,15 +14,29 @@ from aiida.cmdline.params import arguments
 from aiida.cmdline.params import options
 from aiida.cmdline.utils import echo
 from aiida.common.exceptions import DanglingLinkError
+from aiida.cmdline.params.options.multivalue import MultipleValueOption
+from aiida.cmdline.commands.data.list import _list
+from aiida.orm.data.cif import CifData
+from aiida.cmdline.commands.data.list import list_options
 
 
-        
 @verdi_data.group('cif')
 @click.pass_context
 def cif(ctx):
     """help"""
     pass
     
+
+@cif.command('list')
+@list_options
+def list_cif(elements, elements_only, formulamode, past_days, groups, all_users):
+    """
+    List stored BandData objects
+    """
+    project = ['ID', 'Ctime', 'Label', 'Formula']
+    _list(CifData, project, elements, elements_only, formulamode, past_days, groups, all_users)
+
+
 
 @cif.command()
 def show():

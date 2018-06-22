98eaacf11ab6825260b00b3aee2c322e2df2f568
Spyros Zoupanos <spyros.zoupanos@epfl.ch>
2018-06-22 15:32:44 +0200

    Adding indexes and uniqueconstrains of issues 1678

---
+++ aiida/backends/sqlalchemy/migrations/versions/59edaf8a8b79_adding_indexes_and_constraints_to_the_.py
@@ -0,0 +1,32 @@
+"""Adding indexes and constraints to the dbnode-dbgroup relationship table
+
+Revision ID: 59edaf8a8b79
+Revises: a514d673c163
+Create Date: 2018-06-22 14:50:18.447211
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '59edaf8a8b79'
+down_revision = 'a514d673c163'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_index('db_dbgroup_dbnodes_dbnode_id_idx', 'db_dbgroup_dbnodes',
+                    ['dbnode_id'])
+    op.create_index('db_dbgroup_dbnodes_dbgroup_id_idx', 'db_dbgroup_dbnodes',
+                    ['dbgroup_id'])
+    op.create_unique_constraint(
+        'db_dbgroup_dbnodes_dbgroup_id_dbnode_id_key', 'db_dbgroup_dbnodes',
+        ['dbgroup_id', 'dbnode_id'])
+
+
+def downgrade():
+    op.drop_index('db_dbgroup_dbnodes_dbnode_id_idx', 'db_dbgroup_dbnodes')
+    op.drop_index('db_dbgroup_dbnodes_dbgroup_id_idx', 'db_dbgroup_dbnodes')
+    op.drop_constraint('db_dbgroup_dbnodes_dbgroup_id_dbnode_id_key',
+                       'db_dbgroup_dbnodes')

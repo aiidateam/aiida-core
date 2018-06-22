147d54c3022cb4b33a357fb14e900c790eab152e
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-06-22 17:50:46 +0200

    setup.py: make `setup.py` py3 compatible

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
+
+from __future__ import print_function
+
 import fastentrypoints
 import re
 import sys
@@ -27,7 +30,7 @@ if __name__ == '__main__':
     try:
         import pip
     except ImportError:
-        print 'Could not import pip, which is required for installation'
+        print('Could not import pip, which is required for installation')
         sys.exit(1)
 
     PIP_REQUIRED_VERSION = '10.0.0'
@@ -35,7 +38,7 @@ if __name__ == '__main__':
     installed_version = StrictVersion(pip.__version__)
 
     if installed_version < required_version:
-        print 'The installation requires pip>={}, whereas currently {} is installed'.format(required_version, installed_version)
+        print('The installation requires pip>={}, whereas currently {} is installed'.format(required_version, installed_version))
         sys.exit(1)
 
     bin_folder = path.join(aiida_folder, 'bin')

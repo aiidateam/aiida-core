6d8e7d004803248b18e77900fed2522d7a5c1391
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-06-27 11:20:39 +0200

    Deposit function in a separate file

---
+++ aiida/cmdline/commands/data/deposit.py
@@ -0,0 +1,11 @@
+def deposit_tcod(node, deposit_type, parameter_data=None, **kwargs):
+    """
+    Deposition plugin for TCOD.
+    """
+    from aiida.tools.dbexporters.tcod import deposit
+    parameters = None
+    if parameter_data is not None:
+        from aiida.orm import DataFactory
+        ParameterData = DataFactory('parameter')
+        parameters = load_node(parameter_data, sub_class=ParameterData)
+    return deposit(node, deposit_type, parameters, **kwargs)

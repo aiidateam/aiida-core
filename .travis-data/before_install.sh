c3cdcf6ebf687ace4d99b8b25d505681621e0e34
Tiziano Müller <tm@dev-zero.ch>
2018-07-10 14:11:10 +0200

    travis: fix pgtest failures (release-branch) (#1736)
    * travis: explicitly install postgresql-server-dev-9.5
    
    Based on https://github.com/travis-ci/travis-ci/issues/9011 it seems we
    have to specify explicitly the dependency on the postgresql-dev package
    by now.
    
    * travis: add proper postgresql bin-dir to path
    
    fixes #1722
    
    * travis: fix ssh-keygen for private instances

--- .travis-data/before_install.sh
+++ .travis-data/before_install.sh
@@ -5,17 +5,17 @@ set -ev
 
 if [[ "$TEST_TYPE" == "tests" ]]
 then
-    THEKEY=`cat ${HOME}/.ssh/id_rsa.pub`
-    echo 'AUTHORIZED_KEY='"$THEKEY" > ${TRAVIS_BUILD_DIR}/torquessh.env
-    docker build -t torquessh ${TRAVIS_BUILD_DIR}/.travis-data/torquessh-doubler
+    THEKEY=`ssh-keygen -y -f "${HOME}/.ssh/id_rsa"`
+    echo 'AUTHORIZED_KEY='"$THEKEY" > "${TRAVIS_BUILD_DIR}/torquessh.env"
+    docker build -t torquessh "${TRAVIS_BUILD_DIR}/.travis-data/torquessh-doubler"
     # Run it in the background, mapping port 22 of the container
     # to port 10022 outside, and passing the environment variable
-    docker run -d --privileged -p=10022:22 --env-file ${TRAVIS_BUILD_DIR}/torquessh.env torquessh
+    docker run -d --privileged -p=10022:22 --env-file "${TRAVIS_BUILD_DIR}/torquessh.env" torquessh
     # Docker ps to see what is going on
     echo "Running docker ps to see if the 'torquessh' docker image is up..."
     docker ps    
     # Wait for SSH to be up
-    ${TRAVIS_BUILD_DIR}/.travis-data/wait-for-it.sh localhost:10022 -t 0
+    "${TRAVIS_BUILD_DIR}"/.travis-data/wait-for-it.sh localhost:10022 -t 0
 
     # I will add the key to the known_hosts later, to give the time to ssh
     # to be really up - see the before_script script

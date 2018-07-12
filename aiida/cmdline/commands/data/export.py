d0ccf22f0a1ad3cd983a3105d557c3952f572f41
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2018-07-12 18:29:14 +0200

    Adapt export.py to python format convention

--- aiida/cmdline/commands/data/export.py
+++ aiida/cmdline/commands/data/export.py
@@ -1,53 +1,78 @@
-from aiida.cmdline.utils import echo
-from aiida.backends.utils import load_dbenv, is_dbenv_loaded
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+"""
+This module provides export functionality to all data types
+"""
 import click
+from aiida.cmdline.utils import echo
 from aiida.cmdline.params import arguments
 from aiida.cmdline.params import options
 
-
-_export_options = [ click.option('--reduce-symmetry/--no-reduce-symmetry', 'reduce_symmetry',
-                 is_flag=True,
-                 default=None,
-                 help='Do (default) or do not perform symmetry reduction.'),
-    click.option('--parameter-data', type=click.INT,
-                 default=None,
-                 help="ID of the ParameterData to be exported alongside the"
-                      " StructureData instance. By default, if StructureData"
-                      " originates from a calculation with single"
-                      " ParameterData in the output, aforementioned"
-                      " ParameterData is picked automatically. Instead, the"
-                      " option is used in the case the calculation produces"
-                      " more than a single instance of ParameterData."),
-    click.option('--dump-aiida-database/--no-dump-aiida-database',
-                 'dump_aiida_database', is_flag=True,
-                 default=None,
-                 help='Export (default) or do not export AiiDA database to the CIF file.'),
-    click.option('--exclude-external-contents/--no-exclude-external-contents', 'exclude_external_contents', is_flag=True,
-                 default=None,
-                 help='Do not (default) or do save the contents for external resources even if URIs are provided'),
-    click.option('--gzip/--no-gzip', is_flag=True,
-                 default=None,
-                 help='Do or do not (default) gzip large files.'),
-    click.option('--gzip-threshold', type=click.INT,
-                 default=None,
-                 help="Specify the minimum size of exported file which should"
-                      " be gzipped."),
-    click.option('-o', '--output', type=click.STRING,
-                 default=None,
-                 help="If present, store the output directly on a file "
-                      "with the given name. It is essential to use this option "
-                      "if more than one file needs to be created."),
+EXPORT_OPTIONS = [
+    click.option(
+        '--reduce-symmetry/--no-reduce-symmetry',
+        'reduce_symmetry',
+        is_flag=True,
+        default=None,
+        help='Do (default) or do not perform symmetry reduction.'),
+    click.option(
+        '--parameter-data',
+        type=click.INT,
+        default=None,
+        help="ID of the ParameterData to be exported alongside the"
+        " StructureData instance. By default, if StructureData"
+        " originates from a calculation with single"
+        " ParameterData in the output, aforementioned"
+        " ParameterData is picked automatically. Instead, the"
+        " option is used in the case the calculation produces"
+        " more than a single instance of ParameterData."),
+    click.option(
+        '--dump-aiida-database/--no-dump-aiida-database',
+        'dump_aiida_database',
+        is_flag=True,
+        default=None,
+        help='Export (default) or do not export AiiDA database to the CIF file.'),
+    click.option(
+        '--exclude-external-contents/--no-exclude-external-contents',
+        'exclude_external_contents',
+        is_flag=True,
+        default=None,
+        help='Do not (default) or do save the contents for external resources even if URIs are provided'),
+    click.option('--gzip/--no-gzip', is_flag=True, default=None, help='Do or do not (default) gzip large files.'),
+    click.option(
+        '--gzip-threshold',
+        type=click.INT,
+        default=None,
+        help="Specify the minimum size of exported file which should"
+        " be gzipped."),
+    click.option(
+        '-o',
+        '--output',
+        type=click.STRING,
+        default=None,
+        help="If present, store the output directly on a file "
+        "with the given name. It is essential to use this option "
+        "if more than one file needs to be created."),
     options.FORCE(help="If passed, overwrite files without checking."),
     arguments.NODE(),
 ]
 
+
 def export_options(func):
-    for option in reversed(_export_options):
+    for option in reversed(EXPORT_OPTIONS):
         func = option(func)
 
     return func
 
-def _export(node, output_fname, fileformat, other_args={}, overwrite=False):
+
+def _export(node, output_fname, fileformat, other_args=None, overwrite=False):
     """
     Depending on the parameters, either print the (single) output file on
     screen, or store the file(s) on disk.
@@ -63,24 +88,25 @@ def _export(node, output_fname, fileformat, other_args={}, overwrite=False):
     :note: this function calls directly sys.exit(1) when an error occurs (or e.g. if
         check_overwrite is True and a file already exists).
     """
+    if other_args is None:
+        other_args = {}
     try:
+        # pylint: disable=protected-access
         if output_fname:
             try:
-                node.export(
-                    output_fname, fileformat=fileformat, overwrite=overwrite, **other_args)
-            except OSError as e:
-                echo.echo_critical ("verdi: ERROR while exporting file:\n" + e.message)
+                node.export(output_fname, fileformat=fileformat, overwrite=overwrite, **other_args)
+            except OSError as err:
+                echo.echo_critical("verdi: ERROR while exporting file:\n" + err.message)
         else:
-            filetext, extra_files = node._exportstring(
-                fileformat, main_file_name=output_fname, **other_args)
+            filetext, extra_files = node._exportstring(fileformat, main_file_name=output_fname, **other_args)
             if extra_files:
-                echo.echo_critical ("This format requires to write more than one file.\n"
-                                    "You need to pass the -o option to specify a file name.")
+                echo.echo_critical("This format requires to write more than one file.\n"
+                                   "You need to pass the -o option to specify a file name.")
             else:
                 print filetext
-    except TypeError as e:
+    except TypeError as err:
         # This typically occurs for parameters that are passed down to the
         # methods in, e.g., BandsData, but they are not accepted
         echo.echo_critical("verdi: ERROR, probably a parameter is not "
                            "supported by the specific format.\nError "
-                           "message: {}".format(e.message))
+                           "message: {}".format(err.message))

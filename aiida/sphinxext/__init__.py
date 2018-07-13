26ea74ca93b3d76fbfd3e45c8c067853f64cd367
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2018-07-13 18:03:51 +0200

    Fixing the missing headers

--- aiida/sphinxext/__init__.py
+++ aiida/sphinxext/__init__.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
 """
 Defines reStructuredText directives to simplify documenting AiiDA and its plugins.
 """
 
-__version__ = '0.1.0'
 
 from . import workchain
 

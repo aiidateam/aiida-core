9eb94f2307735d63e77ce99f43d896d7cff738de
Sebastiaan Huber <mail@sphuber.net>
2018-07-28 17:37:10 +0200

    Add the option `--version` to `verdi` to display current version (#1811)

--- aiida/cmdline/commands/__init__.py
+++ aiida/cmdline/commands/__init__.py
@@ -16,11 +16,20 @@ import click_completion
 click_completion.init()
 
 
-@click.group()
-@click.option('-p', '--profile', help='Execute the command for this profile instead of the default profile')
+@click.group(invoke_without_command=True)
+@click.option('-p', '--profile', help='Execute the command for this profile instead of the default profile.')
+@click.option('--version', is_flag=True, default=False, help='Print the version of AiiDA that is currently installed.')
 @click.pass_context
-def verdi(ctx, profile):
+def verdi(ctx, profile, version):
     """The command line interface of AiiDA."""
+    import sys
+    import aiida
+    from aiida.cmdline.utils import echo
+
+    if version:
+    	echo.echo('AiiDA version {}'.format(aiida.__version__))
+    	sys.exit(0)
+
     if profile is not None:
         from aiida.backends import settings
         settings.AIIDADB_PROFILE = profile

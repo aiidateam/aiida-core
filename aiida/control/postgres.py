71df10191c1c98ea6eb12bebdf7092dbccc12248
DropD <r.haeuselmann@gmx.ch>
2018-08-27 16:50:15 +0200

    fix #1900 by removing `preexec_fn=os.setsid`

--- aiida/control/postgres.py
+++ aiida/control/postgres.py
@@ -20,7 +20,6 @@ try:
 except ImportError:
     import subprocess
 
-import os
 import click
 
 _CREATE_USER_COMMAND = 'CREATE USER "{}" WITH PASSWORD \'{}\''
@@ -322,7 +321,7 @@ def _pg_execute_sh(command, user='postgres', **kwargs):
         options += '-p {}'.format(port)
 
     # Build command line
-    sudo_cmd = ['sudo', '-S']
+    sudo_cmd = ['sudo']
     non_interactive = kwargs.pop('non_interactive', None)
     if non_interactive:
         sudo_cmd += ['-n']
@@ -330,7 +329,7 @@ def _pg_execute_sh(command, user='postgres', **kwargs):
     from aiida.common.utils import escape_for_bash
     psql_cmd = ['psql {options} -tc {}'.format(escape_for_bash(command), options=options)]
     sudo_su_psql = sudo_cmd + su_cmd + psql_cmd
-    result = subprocess.check_output(sudo_su_psql, preexec_fn=os.setsid, **kwargs)
+    result = subprocess.check_output(sudo_su_psql, **kwargs)
 
     if isinstance(result, str):
         result = result.strip().split('\n')

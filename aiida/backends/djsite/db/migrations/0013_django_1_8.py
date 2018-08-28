c90a0758efb1fcf0e0f1e4ace89de1de1c9cdd39
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-08-28 10:16:37 +0200

    setup: bump version of Django to 1.8.19 for py3 support
    include a migration to drop the 'not null' constraint on
    `auth_user.last_login` and increase the field length for the email as
    documented in the release notes.

---
+++ aiida/backends/djsite/db/migrations/0013_django_1_8.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+from __future__ import unicode_literals
+
+from __future__ import absolute_import
+from django.db import models, migrations
+from aiida.backends.djsite.db.migrations import update_schema_version
+
+
+SCHEMA_VERSION = "1.0.13"
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('db', '0012_drop_dblock'),
+    ]
+
+    # An amalgamation from django:django/contrib/auth/migrations/
+    # these changes are already the default for SQLA at this point
+    operations = [
+        migrations.AlterField(
+            model_name='dbuser',
+            name='last_login',
+            field=models.DateTimeField(null=True, verbose_name='last login', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='dbuser',
+            name='email',
+            field=models.EmailField(max_length=254, verbose_name='email address', blank=True),
+        ),
+        update_schema_version(SCHEMA_VERSION)
+    ]

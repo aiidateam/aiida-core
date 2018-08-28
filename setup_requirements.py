74b1d29b5de1b8c39182aec08c3666f510a00110
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-08-28 22:34:04 +0200

    setup: do not install enum34 on python >= 3.5
    Installing `enum34` breaks things inside Python 3.5+.
    
    Use new-style environment markers instead of semi-supported
    `extras_require` section. Can be passed directly to the
    `requirements.txt`.

--- setup_requirements.py
+++ setup_requirements.py
@@ -21,12 +21,12 @@ install_requires = [
     'anyjson==0.3.3',
     'psutil==5.4.5',
     'meld3==1.0.2',
+    'mock==2.0.0',
     'numpy==1.14.3',
     'SQLAlchemy==1.0.19',  # Upgrade to SQLalchemy 1.1.5 does break tests, see #465
     'SQLAlchemy-Utils==0.33.0',
     'alembic==0.9.9',
     'ujson==1.35',
-    'enum34==1.1.6',
     'aldjemy==0.8.0',
     'passlib==1.7.1',
     'validate-email==1.3',
@@ -45,12 +45,13 @@ install_requires = [
     'plumpy==0.10.6',
     'circus==0.14.0',
     'tornado==4.5.3',  # As of 2018/03/06 Tornado released v5.0 which breaks circus 0.14.0
+    'chainmap; python_version<"3.5"',
+    'pathlib2; python_version<"3.5"',
+    'singledispatch>=3.4.0.3; python_version<"3.5"',
+    'enum34==1.1.6; python_version<"3.5"',
 ]
 
 extras_require = {
-    ':python_version < "3.3"': ['mock'],
-    ':python_version < "3"': ['chainmap', 'pathlib2', 'singledispatch >= 3.4.0.3'],
-
     # Requirements for ssh transport with authentification through Kerberos token
     # N. B.: you need to install first libffi and MIT kerberos GSSAPI including header files.
     # E.g. for Ubuntu 14.04: sudo apt-get install libffi-dev libkrb5-dev
@@ -98,7 +99,6 @@ extras_require = {
     ],
     # Requirements for testing
     'testing': [
-        'mock==2.0.0',
         'pgtest==1.1.0',
         'sqlalchemy-diff==0.1.3',
         'coverage==4.5.1',

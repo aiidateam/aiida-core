9fbdfd1261a2b63a9d696e9a061ab4db20557c31
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-08-31 16:11:42 +0200

    sphinxext/tests/workchain: use separate reference output for py3
    Python 3 does not know the `basestring` type anymore, only `str`.

--- aiida/sphinxext/tests/test_workchain.py
+++ aiida/sphinxext/tests/test_workchain.py
@@ -12,6 +12,7 @@
 from __future__ import absolute_import
 from os.path import join, dirname
 
+import six
 import pytest
 
 WORKCHAIN = join(dirname(__file__), 'workchain_source')
@@ -20,4 +21,7 @@ WORKCHAIN = join(dirname(__file__), 'workchain_source')
 def test_workchain_build(build_sphinx, xml_equal, reference_result):
     out_dir = build_sphinx(WORKCHAIN)
     index_file = join(out_dir, 'index.xml')
-    xml_equal(index_file, reference_result('workchain.xml'))    
+    if six.PY2:
+        xml_equal(index_file, reference_result('workchain.py2.xml'))
+    else:
+        xml_equal(index_file, reference_result('workchain.py3.xml'))

9c7b717a64184e88a64e4405fe9c14c6ece28fa5
Sebastiaan Huber <mail@sphuber.net>
2018-09-20 17:50:22 +0200

    Add a node UUID uniqueness check before applying migration
    A migration was recently added to apply a uniqueness constraint on
    the UUID column of the Node table. However, for databases that violate
    this constraint, the migration will fail and this will have to be
    addressed first. In a previous commit a cli end point was added to
    investigate the database for this problem and automatically fix it
    to remove this integrity violation. Here we add a check before the
    migration, to see if duplicate UUIDs exist. If that is the case, the
    migration is interrupted and the user is instructed to run the
    relevant verdi command to fix the problem first manually.

--- aiida/backends/sqlalchemy/migrations/versions/62fe0d36de90_add_node_uuid_unique_constraint.py
+++ aiida/backends/sqlalchemy/migrations/versions/62fe0d36de90_add_node_uuid_unique_constraint.py
@@ -5,8 +5,9 @@ Revises: 59edaf8a8b79
 Create Date: 2018-07-02 17:50:42.929382
 
 """
+from __future__ import absolute_import
+from __future__ import print_function
 from alembic import op
-
 
 # revision identifiers, used by Alembic.
 revision = '62fe0d36de90'
@@ -15,7 +16,30 @@ branch_labels = None
 depends_on = None
 
 
+def verify_node_uuid_uniqueness():
+    """Check whether the database contains nodes with duplicate UUIDS.
+
+    Note that we have to redefine this method from aiida.manage.database.integrity.verify_node_uuid_uniqueness
+    because that uses the default database connection, while here the one created by Alembic should be used instead.
+
+    :raises: IntegrityError if database contains nodes with duplicate UUIDS.
+    """
+    from alembic import op
+    from sqlalchemy.sql import text
+    from aiida.common.exceptions import IntegrityError
+
+    query = text(
+        'SELECT s.id, s.uuid FROM (SELECT *, COUNT(*) OVER(PARTITION BY uuid) AS c FROM db_dbnode) AS s WHERE c > 1')
+    conn = op.get_bind()
+    duplicates = conn.execute(query).fetchall()
+
+    if duplicates:
+        raise IntegrityError('your database contains nodes with duplicate UUIDS: '
+                             'run `verdi database integrity duplicate-node-uuid` to return to a consistent state')
+
+
 def upgrade():
+    verify_node_uuid_uniqueness()
     op.create_unique_constraint('db_dbnode_uuid_key', 'db_dbnode', ['uuid'])
 
 

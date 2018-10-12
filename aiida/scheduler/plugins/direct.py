65500462f827e4d8bdbc203bfccf31049494df4e
Marco Borelli <borellim@users.noreply.github.com>
2018-10-12 15:09:39 +0200

    Fix type in variable virtual_memory_kb in direct scheduler. (#2050)

--- aiida/scheduler/plugins/direct.py
+++ aiida/scheduler/plugins/direct.py
@@ -148,8 +148,8 @@ class DirectScheduler(aiida.scheduler.Scheduler):
             except ValueError:
                 raise ValueError("max_memory_kb must be "
                                  "a positive integer (in kB)! It is instead '{}'"
-                                 "".format((job_tmpl.MaxMemoryKb)))
-            lines.append("ulimit -v {}".format(virtualMemoryKb))
+                                 "".format((job_tmpl.max_memory_kb)))
+            lines.append("ulimit -v {}".format(virtual_memory_kb))
         if not job_tmpl.import_sys_environment:
             lines.append("env --ignore-environment \\")
 

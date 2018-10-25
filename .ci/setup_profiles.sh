a9400ea314acfb9c11a740bf33a988d90e11d153
szoupanos <szoupanos@users.noreply.github.com>
2018-10-25 12:50:51 +0200

    Forcing UTF-8 encoding to AiiDA and DB (#2115)
    * Enforcing SQLA to use UTF-8, adding to quicksetup the utf-8 encoding and the same for the automatic database creation on Travis
    
    * Making necessary changes to the documentation
    
    * Fixing identation
    
    * Minor changes in the setup_profiles.py for Travis

--- .ci/setup_profiles.sh
+++ .ci/setup_profiles.sh
@@ -8,10 +8,10 @@ if [[ "$TEST_TYPE" != "pre-commit" ]]
     # no setup at all required for pre-commit to run
 then
     # Here I create the actual DB for submission
-    psql -h localhost -c "CREATE DATABASE $TEST_AIIDA_BACKEND;" -U postgres -w
+    psql -h localhost -c "CREATE DATABASE $TEST_AIIDA_BACKEND ENCODING \"UTF8\" LC_COLLATE=\"en_US.UTF-8\" LC_CTYPE=\"en_US.UTF-8\" TEMPLATE=template0;" -U postgres -w
 
     # Here I create the test DB
-    psql -h localhost -c "CREATE DATABASE test_$TEST_AIIDA_BACKEND;" -U postgres -w
+    psql -h localhost -c "CREATE DATABASE test_$TEST_AIIDA_BACKEND ENCODING \"UTF8\" LC_COLLATE=\"en_US.UTF-8\" LC_CTYPE=\"en_US.UTF-8\" TEMPLATE=template0;" -U postgres -w
 
     # Here I setup the actual AiiDA profile, non-interactively
     verdi setup --non-interactive --backend=$TEST_AIIDA_BACKEND --email="aiida@localhost" --db-host="localhost" --db-port=5432 --db-name="$TEST_AIIDA_BACKEND" --db-username=postgres --db-password='' --repository="/tmp/repository_${TEST_AIIDA_BACKEND}/" --first-name=AiiDA --last-name=test --institution="AiiDA Team" $TEST_AIIDA_BACKEND

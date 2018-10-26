0cb86c3a1d7e6f8acdd7ea859a435b74dae34833
Andrius Merkys <andrius.merkys@gmail.com>
2018-10-26 12:44:44 +0200

    Moving LogCollection to Collection inside Log class.

--- aiida/orm/log.py
+++ aiida/orm/log.py
@@ -25,93 +25,94 @@ DESCENDING = -1
 
 OrderSpecifier = namedtuple("OrderSpecifier", ['field', 'direction'])
 
+__all_ = ['Log']
+
 
 @six.add_metaclass(ABCMeta)
-class LogCollection(Collection):
-    """
-    This class represents the collection of logs and can be used to create
-    and retrieve logs.
-    """
-
-    @abstractmethod
-    def create_entry(self, time, loggername, levelname, objname,
-                     objpk=None, message="", metadata=None):
-        """
-        Create a log entry.
-
-        :param time: The time of creation for the entry
-        :type time: :class:`!datetime.datetime`
-        :param loggername: The name of the logger that generated the entry
-        :type loggername: basestring
-        :param levelname: The log level
-        :type levelname: basestring
-        :param objname: The object name (if any) that emitted the entry
-        :type objname: basestring
-        :param objpk: The object id that emitted the entry
-        :type objpk: int
-        :param message: The message to log
-        :type message: basestring
-        :param metadata: Any (optional) metadata, should be JSON serializable dictionary
-        :type metadata: :class:`dict`
-        :return: An object implementing the log entry interface
-        :rtype: :class:`aiida.orm.log.Log`
-        """
-        pass
-
-    def create_entry_from_record(self, record):
-        """
-        Helper function to create a log entry from a record created as by the
-        python logging lobrary
-
-        :param record: The record created by the logging module
-        :type record: :class:`logging.record`
-        :return: An object implementing the log entry interface
-        :rtype: :class:`aiida.orm.log.Log`
-        """
-        from datetime import datetime
-
-        objpk = record.__dict__.get('objpk', None)
-        objname = record.__dict__.get('objname', None)
-
-        # Do not store if objpk and objname are not set
-        if objpk is None or objname is None:
-            return None
-
-        return self.create_entry(
-            time=timezone.make_aware(datetime.fromtimestamp(record.created)),
-            loggername=record.name,
-            levelname=record.levelname,
-            objname=objname,
-            objpk=objpk,
-            message=record.getMessage(),
-            metadata=record.__dict__
-        )
-
-    @abstractmethod
-    def find(self, filter_by=None, order_by=None, limit=None):
-        """
-        Find all entries in the Log collection that conforms to the filter and
-        optionally sort and/or apply a limit.
-
-        :param filter_by: A dictionary of key value pairs where the entries have
-            to match all the criteria (i.e. an AND operation)
-        :type filter_by: :class:`dict`
-        :param order_by: A list of tuples of type :class:`OrderSpecifier`
-        :type order_by: list
-        :param limit: The number of entries to retrieve
-        :return: An iterable of the matching entries
-        """
-        pass
-
-    @abstractmethod
-    def delete_many(self, filter):
-        """
-        Delete all the log entries matching the given filter
-        """
-        pass
-
-
 class Log(entities.Entity):
+    class Collection(entities.Collection):
+        """
+        This class represents the collection of logs and can be used to create
+        and retrieve logs.
+        """
+
+        @abstractmethod
+        def create_entry(self, time, loggername, levelname, objname,
+                         objpk=None, message="", metadata=None):
+            """
+            Create a log entry.
+
+            :param time: The time of creation for the entry
+            :type time: :class:`!datetime.datetime`
+            :param loggername: The name of the logger that generated the entry
+            :type loggername: basestring
+            :param levelname: The log level
+            :type levelname: basestring
+            :param objname: The object name (if any) that emitted the entry
+            :type objname: basestring
+            :param objpk: The object id that emitted the entry
+            :type objpk: int
+            :param message: The message to log
+            :type message: basestring
+            :param metadata: Any (optional) metadata, should be JSON serializable dictionary
+            :type metadata: :class:`dict`
+            :return: An object implementing the log entry interface
+            :rtype: :class:`aiida.orm.log.Log`
+            """
+            pass
+
+        def create_entry_from_record(self, record):
+            """
+            Helper function to create a log entry from a record created as by the
+            python logging lobrary
+
+            :param record: The record created by the logging module
+            :type record: :class:`logging.record`
+            :return: An object implementing the log entry interface
+            :rtype: :class:`aiida.orm.log.Log`
+            """
+            from datetime import datetime
+
+            objpk = record.__dict__.get('objpk', None)
+            objname = record.__dict__.get('objname', None)
+
+            # Do not store if objpk and objname are not set
+            if objpk is None or objname is None:
+                return None
+
+            return self.create_entry(
+                time=timezone.make_aware(datetime.fromtimestamp(record.created)),
+                loggername=record.name,
+                levelname=record.levelname,
+                objname=objname,
+                objpk=objpk,
+                message=record.getMessage(),
+                metadata=record.__dict__
+            )
+
+        @abstractmethod
+        def find(self, filter_by=None, order_by=None, limit=None):
+            """
+            Find all entries in the Log collection that conforms to the filter and
+            optionally sort and/or apply a limit.
+
+            :param filter_by: A dictionary of key value pairs where the entries have
+                to match all the criteria (i.e. an AND operation)
+            :type filter_by: :class:`dict`
+            :param order_by: A list of tuples of type :class:`OrderSpecifier`
+            :type order_by: list
+            :param limit: The number of entries to retrieve
+            :return: An iterable of the matching entries
+            """
+            pass
+
+        @abstractmethod
+        def delete_many(self, filter):
+            """
+            Delete all the log entries matching the given filter
+            """
+            pass
+
     def __init__(self, backend=None):
         backend = backend or construct_backend()
         model = backend.logs.create()

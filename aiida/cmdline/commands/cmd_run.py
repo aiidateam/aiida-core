60bc39862d0a6f435830cc6f1f7458a42cb9838b
Marco Borelli <borellim@users.noreply.github.com>
2018-11-20 13:52:35 +0100

    Verdi run: don't inject future statements into the target script (#2218)
    Thanks @borellim!
    
    Note: I'm merging this directly into develop (and not provenance_redesign) because it's an important bug fix and shouldn't create merge conflicts. (@sphuber)

--- aiida/cmdline/commands/cmd_run.py
+++ aiida/cmdline/commands/cmd_run.py
@@ -109,7 +109,7 @@ def run(scriptname, varargs, group, group_name, exclude, excludesubclasses, incl
                 # Add local folder to sys.path
                 sys.path.insert(0, os.path.abspath(os.curdir))
                 # Compile the script for execution and pass it to exec with the globals_dict
-                exec(compile(handle.read(), scriptname, 'exec'), globals_dict)  # yapf: disable # pylint: disable=exec-used
+                exec(compile(handle.read(), scriptname, 'exec', dont_inherit=True), globals_dict)  # yapf: disable # pylint: disable=exec-used
         except SystemExit:
             # Script called sys.exit()
             # Re-raise the exception to have the error code properly returned at the end

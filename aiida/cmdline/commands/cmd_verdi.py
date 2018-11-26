446ce157123fdf5b17ef3573bbadfe4ced632562
Sebastiaan Huber <mail@sphuber.net>
2018-11-26 18:01:14 +0100

    Make `verdi` callable without sub command (#2240)
    Because the `verdi` group was not marked as callable without
    subcommand, the `--version` option was broken as it would throw
    a `Missing command` error. Adding `invoke_without_command=True` to
    the `verdi` group declaration and making the `--version` an eager
    option will ensure it works without specifying a sub command.
    
    A test has been added to test this functionality.

--- aiida/cmdline/commands/cmd_verdi.py
+++ aiida/cmdline/commands/cmd_verdi.py
@@ -8,26 +8,26 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """The main `verdi` click group."""
-
 from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
+
 import click
 
 
-@click.group()
+@click.group(invoke_without_command=True)
 @click.option(
     '-p', '--profile', metavar='PROFILE', help='Execute the command for this profile instead of the default profile.')
-@click.option('--version', is_flag=True, default=False, help='Print the version of AiiDA that is currently installed.')
+@click.option('--version', is_flag=True, is_eager=True, help='Print the version of AiiDA that is currently installed.')
 @click.pass_context
 def verdi(ctx, profile, version):
     """The command line interface of AiiDA."""
     import sys
-    import aiida
+    from aiida import get_version
     from aiida.cmdline.utils import echo
 
     if version:
-        echo.echo('AiiDA version {}'.format(aiida.__version__))
+        echo.echo('AiiDA version {}'.format(get_version()))
         sys.exit(0)
 
     if profile is not None:

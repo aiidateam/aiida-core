cecd1db580d71f8fc8e2ad7ec33913b8d0d88962
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2018-12-07 00:19:52 +0100

    Actually one of the removed with_dbenvs was needed
    (and does not impact performance for TAB-completion)

--- aiida/cmdline/commands/cmd_devel.py
+++ aiida/cmdline/commands/cmd_devel.py
@@ -170,6 +170,7 @@ def devel_run_daemon():
 @verdi_devel.command('tests')
 @click.argument('paths', nargs=-1, type=TestModuleParamType(), required=False)
 @options.VERBOSE(help='Print the class and function name for each test.')
+@decorators.with_dbenv()
 def devel_tests(paths, verbose):  # pylint: disable=too-many-locals,too-many-statements,too-many-branches
     """Run the unittest suite or parts of it."""
     import os

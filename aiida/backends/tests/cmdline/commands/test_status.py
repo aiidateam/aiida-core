a7219d2fa77d25e4ed027374ed1d7508d5714591
Leopold Talirz <leopold.talirz@gmail.com>
2019-03-23 13:13:38 +0100

    fix test

--- aiida/backends/tests/cmdline/commands/test_status.py
+++ aiida/backends/tests/cmdline/commands/test_status.py
@@ -15,6 +15,7 @@ from click.testing import CliRunner
 
 from aiida.backends.testbase import AiidaTestCase
 from aiida.cmdline.commands import cmd_status
+from aiida.backends.tests.utils.configuration import with_temporary_config_instance
 
 
 class TestVerdiStatus(AiidaTestCase):
@@ -23,9 +24,16 @@ class TestVerdiStatus(AiidaTestCase):
     def setUp(self):
         self.cli_runner = CliRunner()
 
+    @with_temporary_config_instance
     def test_status_1(self):
-        """Check running the command."""
+        """Test running verdi status.
+
+        Note: The exit status may differ depending on the environment in which the tests are run.
+        """
         options = []
         result = self.cli_runner.invoke(cmd_status.verdi_status, options)
-        self.assertClickResultNoException(result)
-        self.assertTrue(result.exit_code == 0)
+        self.assertIsInstance(result.exception, SystemExit)
+        self.assertIn("profile", result.output)
+        self.assertIn("postgres", result.output)
+        self.assertIn("rabbitmq", result.output)
+        self.assertIn("daemon", result.output)

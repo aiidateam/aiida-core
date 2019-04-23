61dc6a61d174ef73dec684faa0b61b943f5248a6
Leopold Talirz <leopold.talirz@gmail.com>
2019-04-23 14:29:05 +0200

    remove code duplication

--- aiida/cmdline/commands/cmd_verdi.py
+++ aiida/cmdline/commands/cmd_verdi.py
@@ -19,10 +19,9 @@ from aiida.common.extendeddicts import AttributeDict
 from aiida.common import exceptions
 
 
-@click.group(invoke_without_command=True)
+@click.group(invoke_without_command=True, context_settings={'help_option_names': ['-h', '--help']})
 @options.PROFILE()
 @click.option('--version', is_flag=True, is_eager=True, help='Print the version of AiiDA that is currently installed.')
-@click.help_option('-h', '--help')  # Note: needed only for the top level command
 @click.pass_context
 def verdi(ctx, profile, version):
     """The command line interface of AiiDA."""
@@ -51,5 +50,3 @@ def verdi(ctx, profile, version):
 
     ctx.obj.config = config
     ctx.obj.profile = profile
-
-    ctx.help_option_names = ['-h', '--help']

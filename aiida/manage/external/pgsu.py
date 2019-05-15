1a60c355adba2d9e7deadc67c42db6480e039c30
Sebastiaan Huber <mail@sphuber.net>
2019-05-15 14:37:58 +0200

    Release `v1.0.0b3`

--- aiida/manage/external/pgsu.py
+++ aiida/manage/external/pgsu.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
 """Connect to an existing PostgreSQL cluster as the `postgres` superuser and execute SQL commands.
 
 Note: Once the API of this functionality has converged, this module should be moved out of aiida-core and into a

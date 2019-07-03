ae049bb2926775e4ef0fd5cd5f9cd8f4565b175c
Chris Sewell <chrisj_sewell@hotmail.com>
2019-07-03 23:39:20 +0100

    add import of Graph to top-level visualization

--- aiida/tools/visualization/__init__.py
+++ aiida/tools/visualization/__init__.py
@@ -7,3 +7,10 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from .graph import Graph
+
+__all__ = ('Graph',)

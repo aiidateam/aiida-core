39b2b888c371da87ef506c034923a0b0174c3904
Chris Sewell <chrisj_sewell@hotmail.com>
2019-07-03 14:46:03 +0100

    add review fixes

--- aiida/cmdline/commands/cmd_graph.py
+++ aiida/cmdline/commands/cmd_graph.py
@@ -15,15 +15,13 @@ from __future__ import absolute_import
 import click
 
 from aiida.cmdline.commands.cmd_verdi import verdi
-from aiida.cmdline.params import arguments
+from aiida.cmdline.params import arguments, options
 from aiida.cmdline.utils import decorators, echo
 
 
 @verdi.group('graph')
 def verdi_graph():
-    """
-    Create visual representations of part of the provenance graph.
-    Requires that `python-graphviz<https://graphviz.readthedocs.io>` be installed.
+    """Create visual representations of part of the provenance graph.
     """
 
 
@@ -47,7 +45,7 @@ def verdi_graph():
     type=click.IntRange(min=0))
 @click.option('-o', '--process-out', is_flag=True, help='Show outgoing links for all processes.')
 @click.option('-i', '--process-in', is_flag=True, help='Show incoming links for all processes.')
-@click.option('-p', '--print-info', is_flag=True, help="Print verbose information of the graph traversal.")
+@options.VERBOSE(help="Print verbose information of the graph traversal.")
 @click.option(
     '-e',
     '--engine',
@@ -55,16 +53,16 @@ def verdi_graph():
     "(see http://www.graphviz.org/doc/info/output.html)",
     default='dot')
 @click.option('-f', '--output-format', help="The output format used for rendering ('pdf', 'png', etc.).", default='pdf')
-@click.option('-v', '--view', is_flag=True, help="Open the rendered result with the default application.")
+@click.option('-s', '--show', is_flag=True, help="Open the rendered result with the default application.")
 @decorators.with_dbenv()
-def generate(root_node, link_types, ancestor_depth, descendant_depth, process_out, process_in, engine, print_info,
-             output_format, view):
+def generate(root_node, link_types, ancestor_depth, descendant_depth, process_out, process_in, engine, verbose,
+             output_format, show):
     """
     Generate a graph from a ROOT_NODE (specified by pk or uuid).
     """
     # pylint: disable=too-many-arguments
     from aiida.tools.visualization.graph import Graph
-    print_func = echo.echo_info if print_info else None
+    print_func = echo.echo_info if verbose else None
     link_types = {"all": (), "logic": ("input_work", "return"), "data": ("input_calc", "create")}[link_types]
 
     echo.echo_info("Initiating graphviz engine: {}".format(engine))
@@ -86,6 +84,6 @@ def generate(root_node, link_types, ancestor_depth, descendant_depth, process_ou
         include_process_inputs=process_in,
         print_func=print_func)
     output_file_name = graph.graphviz.render(
-        filename='{}.{}'.format(root_node.pk, engine), format=output_format, view=view, cleanup=True)
+        filename='{}.{}'.format(root_node.pk, engine), format=output_format, view=show, cleanup=True)
 
     echo.echo_success("Output file: {}".format(output_file_name))

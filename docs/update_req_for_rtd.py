b07685d56d8cc8d1780ffac4fd73ddc955132432
Sebastiaan Huber <mail@sphuber.net>
2019-10-02 17:48:02 +0200

    Fix loading of database environment in ReadTheDocs environment
    The environment of ReadTheDocs does not actually have a database and so
    the typical configuration and profile loading had to be hotwired. A
    special global flag `IN_RTD_DOC_MODE` in `aiida.manage.configuration`
    would circumvent loading an actual configuration for a dummy one. Since
    the actual database environment never had to be loaded, the fact that
    this contained non-sensical database connection parameters was
    irrelevant. However, recently the documentation was changed to use the
    Sphinx extension to document the `CalcJob` class. This now requires
    loading the `aiida.orm` module, which in turn requires the database
    environment being loaded.
    
    The fix is to actually load the database environment through the backend
    manager, but without performing the schema check. This will work despite
    the unusable database configuration because no actual connection is
    established in this way.

--- docs/update_req_for_rtd.py
+++ docs/update_req_for_rtd.py
@@ -33,8 +33,7 @@ def update_req_for_rtd(pre_commit):
 
     extras = setup_json['extras_require']
     reqs = set(extras['testing'] + extras['docs'] + extras['rest'] + extras['atomic_tools'] +
-               # To avoid that it requires also the postgres libraries
-               [p for p in setup_json['install_requires'] if not p.startswith('psycopg2')])
+        setup_json['install_requires'])
     reqs_str = '\n'.join(sorted(reqs))
 
     basename = 'requirements_for_rtd.txt'

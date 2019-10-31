625bc57a54651d9c7ad76a1b26127d1f24776356
Sebastiaan Huber <mail@sphuber.net>
2019-10-31 10:20:11 +0100

    Release preparation: fix file headers

--- .ci/pytest/test_unittest_example.py
+++ .ci/pytest/test_unittest_example.py
@@ -1,4 +1,15 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida-core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
 """Test running unittest test cases through pytest."""
+from __future__ import division
+from __future__ import print_function
 from __future__ import absolute_import
 import unittest
 import pytest

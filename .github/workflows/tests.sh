a227bd30a1543de59cbdc970a9194a0cb805ac18
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2019-12-09 15:48:24 +0100

    (re-)implement coverage reports via codecov.io
    fixes #3602

--- .github/workflows/tests.sh
+++ .github/workflows/tests.sh
@@ -10,6 +10,14 @@ verdi daemon start 4
 verdi -p test_${AIIDA_TEST_BACKEND} run .ci/test_daemon.py
 verdi daemon stop
 
+# Make sure
+# - omissions are taken from the top-level .coveragerc
+# - coverage is reported as XML and in terminal including
+#   the numbers/ranges of lines which are not covered,
+# - coverage results of multiple tests are collected,
+# - the base directory for files to consider is aiida/
+export PYTEST_ADDOPTS="--cov-config=.coveragerc --cov-report xml --cov-report term-missing --cov-append --cov=aiida"
+
 AIIDA_TEST_PROFILE=test_$AIIDA_TEST_BACKEND pytest tests
 AIIDA_TEST_PROFILE=test_$AIIDA_TEST_BACKEND pytest .ci/pytest
 pytest --noconftest .ci/test_test_manager.py

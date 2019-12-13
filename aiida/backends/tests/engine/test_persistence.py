f0521148f0667daed7f660d11e64eb750b0f4c2e
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-12-13 12:09:36 +0100

    Update aiida/backends/tests/engine/test_persistence.py
    Co-Authored-By: Leopold Talirz <leopold.talirz@gmail.com>

--- aiida/backends/tests/engine/test_persistence.py
+++ aiida/backends/tests/engine/test_persistence.py
@@ -7,7 +7,7 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-"""Test persistance module."""
+"""Test persisting via the AiiDAPersister."""
 import plumpy
 
 from aiida.backends.testbase import AiidaTestCase

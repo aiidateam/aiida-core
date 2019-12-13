fc42510333edb3aced6c5ada4e3ebacd3c7f3e73
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-12-13 12:09:18 +0100

    Update aiida/backends/tests/engine/test_class_loader.py
    Co-Authored-By: Leopold Talirz <leopold.talirz@gmail.com>

--- aiida/backends/tests/engine/test_class_loader.py
+++ aiida/backends/tests/engine/test_class_loader.py
@@ -7,7 +7,7 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-"""A module to test class loader."""
+"""A module to test class loader factories."""
 import aiida
 
 from aiida.backends.testbase import AiidaTestCase

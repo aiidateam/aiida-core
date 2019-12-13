582ea90e49a1168e34bc9a2d6ea69388a1382a01
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-12-13 12:10:34 +0100

    Update aiida/backends/tests/engine/test_runners.py
    Co-Authored-By: Leopold Talirz <leopold.talirz@gmail.com>

--- aiida/backends/tests/engine/test_runners.py
+++ aiida/backends/tests/engine/test_runners.py
@@ -7,7 +7,7 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-"""Module to test runners."""
+"""Module to test process runners."""
 import plumpy
 
 from aiida.backends.testbase import AiidaTestCase

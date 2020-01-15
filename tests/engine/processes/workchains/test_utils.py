1ba87c021d6d914c3b649426ddd1ccde9c8d213d
Sebastiaan Huber <mail@sphuber.net>
2020-01-15 12:42:45 +0100

    Add `exit_codes` argument to `register_process_handler`
    The `exit_codes` argument takes a single or list of `ExitCode`
    instances. If defined, the handler will return `None` if the exit
    code set on the `node` does not appear in the `exit_codes`. This is
    useful to have a handler called only when the process failed with a
    specific (set of) exit code(s).

--- tests/engine/processes/workchains/test_utils.py
+++ tests/engine/processes/workchains/test_utils.py
@@ -27,11 +27,11 @@ class TestRegisterProcessHandler(AiidaTestCase):
         with self.assertRaises(TypeError):
 
             @register_process_handler(BaseRestartWorkChain, 400)  # pylint: disable=too-many-function-args
-            def _(self, node):  # pylint: disable=unused-argument
+            def _(self, node):
                 pass
 
         @register_process_handler(BaseRestartWorkChain, priority=400)
-        def _(self, node):  # pylint: disable=unused-argument
+        def _(self, node):
             pass
 
     def test_priority_type(self):
@@ -39,7 +39,7 @@ class TestRegisterProcessHandler(AiidaTestCase):
         with self.assertRaises(TypeError):
 
             @register_process_handler(BaseRestartWorkChain, priority='400')
-            def _(self, node):  # pylint: disable=unused-argument
+            def _(self, node):
                 pass
 
     def test_priority(self):
@@ -92,3 +92,71 @@ class TestRegisterProcessHandler(AiidaTestCase):
         # Last called handler should be `handler_01` which returned `ExitCode(100)`
         assert process.inspect_process() == ExitCode(100)
         assert child.get_attribute(attribute_key, []) == ['handler_04', 'handler_03', 'handler_02', 'handler_01']
+
+    def test_exit_codes_keyword_only(self):
+        """The `exit_codes` should be keyword only."""
+        with self.assertRaises(TypeError):
+
+            @register_process_handler(BaseRestartWorkChain, ExitCode())  # pylint: disable=too-many-function-args
+            def _(self, node):
+                pass
+
+        @register_process_handler(BaseRestartWorkChain, exit_codes=ExitCode())
+        def _(self, node):
+            pass
+
+    def test_exit_codes_type(self):
+        """The `exit_codes` should be single or list of `ExitCode` instances."""
+        incorrect_types = [
+            'test',
+            ['test'],
+            400,
+            [400],
+        ]
+
+        with self.assertRaises(TypeError):
+            for incorrect_type in incorrect_types:
+
+                @register_process_handler(BaseRestartWorkChain, exit_codes=incorrect_type)
+                def _(self, node):
+                    pass
+
+        @register_process_handler(BaseRestartWorkChain, exit_codes=ExitCode(400, 'Some exit code'))
+        def _(self, node):
+            pass
+
+        @register_process_handler(BaseRestartWorkChain, exit_codes=[ExitCode(400, 'a'), ExitCode(401, 'b')])
+        def _(self, node):
+            pass
+
+    def test_exit_codes_filter(self):
+        """Test that the `exit_codes` argument properly filters, returning `None` if the `node` has different status."""
+
+        class ArithmeticAddBaseWorkChain(BaseRestartWorkChain):
+
+            _process_class = ArithmeticAddCalculation
+
+        exit_code_filter = ExitCode(400)
+
+        # This process node should match the exit code filter of the error handler
+        node_match = ProcessNode()
+        node_match.set_exit_status(exit_code_filter.status)
+
+        # This one should not because it has a different exit status
+        node_skip = ProcessNode()
+        node_skip.set_exit_status(200)  # Some other exit status
+
+        @register_process_handler(ArithmeticAddBaseWorkChain, exit_codes=exit_code_filter)
+        def _(self, node):
+            return ProcessHandlerReport()
+
+        # Create dummy process instance
+        process = ArithmeticAddBaseWorkChain()
+
+        # Loop over all handlers, which should be just the one, and call it with the two different nodes
+        for handler in process._handlers:  # pylint: disable=protected-access
+            # The `node_match` should match the `exit_codes` filter and so return a report instance
+            assert isinstance(handler.method(process, node_match), ProcessHandlerReport)
+
+            # The `node_skip` has a wrong exit status and so should get skipped, returning `None`
+            assert handler.method(process, node_skip) is None

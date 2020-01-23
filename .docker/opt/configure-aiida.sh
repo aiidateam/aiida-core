ecc19e0cdbe9a91ee5fb4b32889452d9e5515570
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-01-23 11:39:18 +0100

    Adress the comments by @ltalirz and @sphuber

--- .docker/opt/configure-aiida.sh
+++ .docker/opt/configure-aiida.sh
@@ -1,65 +1,69 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # This script is executed whenever the docker container is (re)started.
 
-#===============================================================================
-# debugging
-set -x
+# Debugging.
+#set -x
 
-#===============================================================================
-# environment
+# Environment.
 reentry scan
-
 export SHELL=/bin/bash
 
-#===============================================================================
-# setup AiiDA
-aiida_backend=django
+# Setup AiiDA autocompletion.
+grep _VERDI_COMPLETE .bash_profile &> /dev/null || echo 'eval "$(_VERDI_COMPLETE=source verdi)"' >> /home/$SYSTEM_USER/.bashrc
 
-if [ ! -d /home/$SYSTEM_USER/.aiida ]; then
-    verdi setup                                \
-        --profile default                      \
-        --non-interactive                      \
-        --email some.body@xyz.com              \
-        --first-name Some                      \
-        --last-name Body                       \
-        --institution XYZ                      \
-        --db-backend $aiida_backend            \
-        --db-username aiida                    \
-        --db-password aiida_db_passwd          \
-        --db-name aiidadb                      \
-        --db-host localhost                    \
-        --db-port 5432                         \
-        --repository /home/$SYSTEM_USER/aiida_repository
-
-   echo 'eval "$(verdi completioncommand)"' >> /home/$SYSTEM_USER/.bashrc
-
-   verdi profile setdefault default
+# Check if profile $PROFILE_NAME needs to be setup an if it exists already
+if [[ $SETUP_DEFAULT_PROFILE == true ]] && ! verdi profile show $PROFILE_NAME &> /dev/null; then
+    NEED_SETUP_PROFILE=true;
+else
+    NEED_SETUP_PROFILE=false;
 fi
 
-#===============================================================================
-# perform the database migration if needed
-#
-verdi daemon start || ( verdi daemon stop && verdi database migrate --force && verdi daemon start)
+# Setup AiiDA.
+if [[ $NEED_SETUP_PROFILE == true ]]; then
 
+    # Create postgres database first.
+    psql -h localhost -d template1 -c "CREATE USER $AIIDADB_USER WITH PASSWORD '$AIIDADB_PASS';"
+    psql -h localhost -d template1 -c "CREATE DATABASE $AIIDADB_NAME OWNER $AIIDADB_USER;"
+    psql -h localhost -d template1 -c "GRANT ALL PRIVILEGES ON DATABASE $AIIDADB_NAME to $AIIDADB_USER;"
 
-#===============================================================================
-# setup local computer
+    # Then create AiiDA profile.
+    verdi setup                                \
+        --profile $PROFILE_NAME                \
+        --non-interactive                      \
+        --email $USER_EMAIL                    \
+        --first-name $USER_FIRST_NAME          \
+        --last-name $USER_LAST_NAME            \
+        --institution $USER_INSTITUTION        \
+        --db-backend $AIIDADB_BACKEND          \
+        --db-username $AIIDADB_USER            \
+        --db-password $AIIDADB_PASS            \
+        --db-name $AIIDADB_NAME                \
+        --db-host $AIIDADB_HOST                \
+        --db-port $AIIDADB_PORT                \
 
-computer_name=localhost
-verdi computer show $computer_name || verdi computer setup \
-    --non-interactive                                      \
-    --label ${computer_name}                               \
-    --description "this computer"                          \
-    --hostname ${computer_name}                            \
-    --transport local                                      \
-    --scheduler direct                                     \
-    --work-dir /home/aiida/aiida_run/                      \
-    --mpirun-command "mpirun -np {tot_num_mpiprocs}"       \
-    --mpiprocs-per-machine 1 &&                            \
-verdi computer configure local ${computer_name}            \
-    --non-interactive                                      \
+    verdi profile setdefault $PROFILE_NAME
+
+    # Finally setup and configure local computer.
+    computer_name=localhost
+    verdi computer show $computer_name || verdi computer setup \
+        --non-interactive                                      \
+        --label ${computer_name}                               \
+        --description "this computer"                          \
+        --hostname ${computer_name}                            \
+        --transport local                                      \
+        --scheduler direct                                     \
+        --work-dir /home/aiida/aiida_run/                      \
+        --mpirun-command "mpirun -np {tot_num_mpiprocs}"       \
+        --mpiprocs-per-machine 1 &&                            \
+    verdi computer configure local ${computer_name}            \
+        --non-interactive                                      \
     --safe-interval 0.0
+fi
 
-#EOF
 
+# Perform the database migration if needed and start the daemon.
+verdi database migrate --force
+
+# Start AiiDA daemon.
+verdi daemon start
--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,20 @@ FROM aiidateam/aiida-prerequisites:latest
 
 USER root
 
+ENV SETUP_DEFAULT_PROFILE true
+
+ENV PROFILE_NAME default
+ENV USER_EMAIL some.body@xyz.com
+ENV USER_FIRST_NAME Some
+ENV USER_LAST_NAME Body
+ENV USER_INSTITUTION XYZ
+ENV AIIDADB_BACKEND django
+ENV AIIDADB_USER aiida
+ENV AIIDADB_PASS aiida_db_passwd
+ENV AIIDADB_NAME aiidadb
+ENV AIIDADB_HOST localhost
+ENV AIIDADB_PORT 5432
+
 # Copy and install AiiDA
 COPY . aiida-core
 RUN pip install ./aiida-core

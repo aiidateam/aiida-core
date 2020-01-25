38486493ceef5b3fbd2451760e6b225324c25d2a
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-01-25 10:23:12 +0100

    Add docker image with minimal running AiiDA instance (#3722)
    The docker file builds on the `aiida-prerequisites` base image to get
    a basic install of an AiiDA instance. This image then configures a full
    profile with a configured localhost computer and the daemon is started.
    The parameters of the profile can be controlled through environment
    variables.

---
+++ .docker/opt/configure-aiida.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This script is executed whenever the docker container is (re)started.
+
+# Debugging.
+set -x
+
+# Environment.
+reentry scan
+export SHELL=/bin/bash
+
+# Setup AiiDA autocompletion.
+grep _VERDI_COMPLETE .bash_profile &> /dev/null || echo 'eval "$(_VERDI_COMPLETE=source verdi)"' >> /home/$SYSTEM_USER/.bashrc
+
+# Check if user requested to set up AiiDA profile (and if it exists already)
+if [[ $SETUP_DEFAULT_PROFILE == true ]] && ! verdi profile show $PROFILE_NAME &> /dev/null; then
+    NEED_SETUP_PROFILE=true;
+else
+    NEED_SETUP_PROFILE=false;
+fi
+
+# Setup AiiDA profile if needed.
+if [[ $NEED_SETUP_PROFILE == true ]]; then
+
+    # Create AiiDA profile.
+    verdi quicksetup                           \
+        --non-interactive                      \
+        --profile "${PROFILE_NAME}"            \
+        --email "${USER_EMAIL}"                \
+        --first-name "${USER_FIRST_NAME}"      \
+        --last-name "${USER_LAST_NAME}"        \
+        --institution "${USER_INSTITUTION}"    \
+        --db-backend "${AIIDADB_BACKEND}"
+
+    # Setup and configure local computer.
+    computer_name=localhost
+    verdi computer show $computer_name || verdi computer setup \
+        --non-interactive                                      \
+        --label "${computer_name}"                             \
+        --description "${this computer}"                       \
+        --hostname "${computer_name}"                          \
+        --transport local                                      \
+        --scheduler direct                                     \
+        --work-dir /home/aiida/aiida_run/                      \
+        --mpirun-command "mpirun -np {tot_num_mpiprocs}"       \
+        --mpiprocs-per-machine 1 &&                            \
+    verdi computer configure local "${computer_name}"          \
+        --non-interactive                                      \
+        --safe-interval 0.0
+fi
+
+
+# Show the default profile
+verdi profile show || echo "The default profile is not set."
+
+# Migration will run for the default profile.
+verdi database migrate --force || echo "Database migration failed."
+
+# Daemon will start only if the database exists and is migrated to the latest version.
+verdi daemon start || echo "AiiDA daemon is not running."

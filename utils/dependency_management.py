aiidateam/aiida-core

commit 469cb37159b3a572e1741270f380700307298c0c

Apply suggestions from code review
Co-Authored-By: Leopold Talirz <leopold.talirz@gmail.com>

utils/dependency_management.py
@@ -303,12 +303,12 @@ def validate_all(ctx):
 
 
 @cli.command()
-@click.argument('extras', nargs=-1)
+@click.argument('extras', nargs=-1, help="extra requirements, e.g. 'atomic_tools'")
 def pip_install_extras(extras):
     """Install extra requirements.
 
-    Install extra requirements in isolation without triggering the installlation
-    of other installation requirements of the core package.
+    Install *only* extra requirements without triggering the installation of
+    of the main installation requirements of the aiida-core package.
     """
     # Read the requirements from 'setup.json'
     setup_cfg = _load_setup_cfg()

commit 17b7e2709cb46b18aba32bb1e6d62a7cdb6be244

Fix definition of pip-install-extras command.
Broken by 469cb37159b3a572e1741270f380700307298c0c.

utils/dependency_management.py
@@ -308,12 +308,16 @@ def validate_all(ctx):
 
 
 @cli.command()
-@click.argument('extras', nargs=-1, help="extra requirements, e.g. 'atomic_tools'")
+@click.argument('extras', nargs=-1)
 def pip_install_extras(extras):
     """Install extra requirements.
 
-    Install *only* extra requirements without triggering the installation of
-    of the main installation requirements of the aiida-core package.
+    For example:
+
+        pip-install-extras docs
+
+    This will install *only* the extra the requirements for docs, but without triggering
+    the installation of the main installations requirements of the aiida-core package.
     """
     # Read the requirements from 'setup.json'
     setup_cfg = _load_setup_cfg()
f7d0153656a3ddc4f0a727e8208aadc947eb8239
Carl Simon Adorf <csadorf@umich.edu>
2020-03-07 10:26:57 +0100

    Make numpy (instead of Cython) build requirement on Python 3.5.

--- utils/dependency_management.py
+++ utils/dependency_management.py
@@ -151,8 +151,11 @@ def generate_pyproject_toml():
 
     pyproject = {
         'build-system': {
-            'requires': ['setuptools>=40.8.0', 'wheel', str(reentry_requirement)],
-            'build-backend': 'setuptools.build_meta:__legacy__',
+            'requires':
+            ["numpy>=1.17.0,<1.17.5; python_version=='3.5'", 'setuptools>=40.8.0', 'wheel',
+             str(reentry_requirement)],
+            'build-backend':
+            'setuptools.build_meta:__legacy__',
         }
     }
     with open(ROOT / 'pyproject.toml', 'w') as file:

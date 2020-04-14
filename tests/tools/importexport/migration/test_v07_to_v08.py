9a75173869eca4925bcd44a19c81ba66dce9eca7
Sebastiaan Huber <mail@sphuber.net>
2020-04-14 11:31:59 +0200

    Deduplicate code for tests of archive migration code (#3924)
    Each archive migration was being tested on some archives included in this
    repo in `tools/fixtures/export/migrate` and the test was always the
    same. Simply take an archive, migrate it to the next version with the
    appropriate method and check that both metadata and data dictionaries of
    the migrated data match that of the reference archive. This same test
    was implemented for each migration method, but has now been centralized
    in a single test using pytest to parametrize the versions.
    
    The testing on external archives from `aiida-export-migration-testing`
    has also been streamlined a bit, by defining a new base class called
    `ArchiveMigrationTest` that provides some utility methods.

--- tests/tools/importexport/migration/test_v07_to_v08.py
+++ tests/tools/importexport/migration/test_v07_to_v08.py
@@ -8,76 +8,18 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """Test export file migration from export version 0.7 to 0.8"""
+from aiida.tools.importexport.migration.v07_to_v08 import migrate_v7_to_v8, migration_default_link_label
 
-from aiida.backends.testbase import AiidaTestCase
-from aiida.tools.importexport.migration.utils import verify_metadata_version
-from aiida.tools.importexport.migration.v07_to_v08 import (migrate_v7_to_v8, migration_default_link_label)
-
-from tests.utils.archives import get_json_files
+from . import ArchiveMigrationTest
 
 
-class TestMigrateV07toV08(AiidaTestCase):
-    """Test migration of export files from export version 0.7 to 0.8"""
+class TestMigrate(ArchiveMigrationTest):
+    """Tests specific for this archive migration."""
 
-    @classmethod
-    def setUpClass(cls, *args, **kwargs):
-        super().setUpClass(*args, **kwargs)
+    def test_migrate_external(self):
+        """Test the migration on the test archive provided by the external test package."""
+        _, data = self.migrate('export_v0.7_manual.aiida', '0.7', '0.8', migrate_v7_to_v8)
 
-        # Utility helpers
-        cls.external_archive = {'filepath': 'archives', 'external_module': 'aiida-export-migration-tests'}
-        cls.core_archive = {'filepath': 'export/migrate'}
-
-    def test_migrate_v7_to_v8(self):
-        """Test migration for file containing complete v0.7 era possibilities"""
-        from aiida import get_version
-
-        # Get metadata.json and data.json as dicts from v0.7 file archive
-        metadata_v7, data_v7 = get_json_files('export_v0.7_simple.aiida', **self.core_archive)
-        verify_metadata_version(metadata_v7, version='0.7')
-
-        # Get metadata.json and data.json as dicts from v0.8 file archive
-        metadata_v8, data_v8 = get_json_files('export_v0.8_simple.aiida', **self.core_archive)
-        verify_metadata_version(metadata_v8, version='0.8')
-
-        # Migrate to v0.8
-        migrate_v7_to_v8(metadata_v7, data_v7)
-        verify_metadata_version(metadata_v7, version='0.8')
-
-        # Remove AiiDA version, since this may change irregardless of the migration function
-        metadata_v7.pop('aiida_version')
-        metadata_v8.pop('aiida_version')
-
-        # Assert conversion message in `metadata.json` is correct and then remove it for later assertions
-        self.maxDiff = None  # pylint: disable=invalid-name
-        conversion_message = 'Converted from version 0.7 to 0.8 with AiiDA v{}'.format(get_version())
-        self.assertEqual(
-            metadata_v7.pop('conversion_info')[-1],
-            conversion_message,
-            msg='The conversion message after migration is wrong'
-        )
-        metadata_v8.pop('conversion_info')
-
-        # Assert changes were performed correctly
-        self.assertDictEqual(
-            metadata_v7,
-            metadata_v8,
-            msg='After migration, metadata.json should equal intended metadata.json from archives'
-        )
-        self.assertDictEqual(
-            data_v7, data_v8, msg='After migration, data.json should equal intended data.json from archives'
-        )
-
-    def test_migrate_v7_to_v8_complete(self):
-        """Test migration for file containing complete v0.7 era possibilities"""
-        # Get metadata.json and data.json as dicts from v0.7 file archive
-        metadata, data = get_json_files('export_v0.7_manual.aiida', **self.external_archive)
-        verify_metadata_version(metadata, version='0.7')
-
-        # Migrate to v0.8
-        migrate_v7_to_v8(metadata, data)
-        verify_metadata_version(metadata, version='0.8')
-
-        self.maxDiff = None  # pylint: disable=invalid-name
         # Check that no links have the label '_return', since it should now be 'result'
         illegal_label = '_return'
         for link in data.get('links_uuid'):

52f5046e6b9ca62d97d8399f4df7ec613db617cf
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-04-15 19:53:06 +0200

    Update logic of get_progress_bar

--- aiida/tools/importexport/common/progress_bar.py
+++ aiida/tools/importexport/common/progress_bar.py
@@ -18,50 +18,38 @@ def get_progress_bar(iterable=None, total=None, leave=None, **kwargs):
     """Set up, cache and return cached tqdm progress bar"""
     global PROGRESS_BAR
 
+    leave_default = False
+
     type_check(iterable, Iterable, allow_none=True)
     type_check(total, int, allow_none=True)
     type_check(leave, bool, allow_none=True)
 
-    if PROGRESS_BAR is None:
-        if iterable is not None and total is not None:
-            if len(iterable) == total:
-                kwargs['iterable'] = iterable
-            else:
-                raise ProgressBarError('You can not set both "iterable" and "total" for the progress bar.')
-        elif iterable is None and total is None:
-            kwargs['total'] = 1
-        elif iterable is not None:
+    # iterable and total are mutually exclusive
+    if iterable is not None and total is not None:
+        if len(iterable) == total:
             kwargs['iterable'] = iterable
-        elif total is not None:
-            kwargs['total'] = total
+        else:
+            raise ProgressBarError('You can not set both "iterable" and "total" for the progress bar.')
+    elif iterable is None and total is None:
+        if PROGRESS_BAR is None:
+            kwargs['total'] = 1
+        # Else pass: we guess the desired outcome is to retrieve the current progress bar
+    elif iterable is not None:
+        kwargs['iterable'] = iterable
+    elif total is not None:
+        kwargs['total'] = total
 
-        if ('iterable' not in kwargs and 'total' not in kwargs) or ('iterable' in kwargs and 'total' in kwargs):
-            raise ProgressBarError('Internal logic error in get_progress_bar')
-
+    if PROGRESS_BAR is None:
+        leave = leave if leave is not None else leave_default
+        PROGRESS_BAR = tqdm(bar_format=BAR_FORMAT, leave=leave, **kwargs)
+    elif 'iterable' in kwargs or 'total' in kwargs:
+        # Create a new progress bar
+        # We leave it up to the caller/creator to properly have set leave before we close the current progress bar
+        if leave is None:
+            leave = PROGRESS_BAR.leave if PROGRESS_BAR.leave is not None else leave_default
+        close_progress_bar()
         PROGRESS_BAR = tqdm(bar_format=BAR_FORMAT, leave=leave, **kwargs)
     else:
-        if iterable is not None and total is not None:
-            if len(iterable) == total:
-                kwargs['iterable'] = iterable
-            else:
-                raise ProgressBarError('You can not set both "iterable" and "total" for the progress bar.')
-        elif iterable is None and total is None:
-            pass
-        elif iterable is not None:
-            # Create a new progress bar
-            # We leave it up to the caller/creator to properly have set leave before we close the current progress bar
-            if leave is None:
-                leave = PROGRESS_BAR.leave if PROGRESS_BAR.leave is not None else False
-            close_progress_bar()
-            PROGRESS_BAR = tqdm(iterable=iterable, bar_format=BAR_FORMAT, leave=leave, **kwargs)
-        elif total is not None:
-            # Create a new progress bar
-            # We leave it up to the caller/creator to properly have set leave before we close the current progress bar
-            if leave is None:
-                leave = PROGRESS_BAR.leave if PROGRESS_BAR.leave is not None else False
-            close_progress_bar()
-            PROGRESS_BAR = tqdm(total=total, bar_format=BAR_FORMAT, leave=leave, **kwargs)
-
         for attribute, value in kwargs.items():
             try:
                 setattr(PROGRESS_BAR, attribute, value)

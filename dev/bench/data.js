f5168ee0c7d952b264bb40d45b9cac4c38cc4563
github-action-benchmark <github@users.noreply.github.com>
2020-07-16 14:35:29 +0000

    add Benchmark on ubuntu-latest (pytest) benchmark result for 9679afe7ce7b6fb5cc2f5ed53ebf4ba132a57bba

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1594910122022,
+  "lastUpdate": 1594910129002,
   "repoUrl": "https://github.com/aiidateam/aiida-core",
   "entries": {
     "Benchmark on ubuntu-latest": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00030139457489018116",
             "extra": "mean: 4.911265310002477 msec\nrounds: 100"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chrisj_sewell@hotmail.com",
+            "name": "Chris Sewell",
+            "username": "chrisjsewell"
+          },
+          "committer": {
+            "email": "chrisj_sewell@hotmail.com",
+            "name": "Chris Sewell",
+            "username": "chrisjsewell"
+          },
+          "distinct": true,
+          "id": "9679afe7ce7b6fb5cc2f5ed53ebf4ba132a57bba",
+          "message": "Update benchmark.yml",
+          "timestamp": "2020-07-16T15:31:39+01:00",
+          "tree_id": "795041457a0230860d8b1012909dee482b76a124",
+          "url": "https://github.com/aiidateam/aiida-core/commit/9679afe7ce7b6fb5cc2f5ed53ebf4ba132a57bba"
+        },
+        "date": 1594910125393,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmark/test_nodes.py::test_store",
+            "value": 104.9047126262413,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0007878892639694616",
+            "extra": "mean: 9.532460219998313 msec\nrounds: 100"
           }
         ]
       }

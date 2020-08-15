unkcpz/aiida_core

replace tornado with asyncio for modules independent from plumpy
This commit includes changes about replacing tornado with asyncio, only
touch the modules and functions which not interacting with plumpy.

 - logger configuration move tornado logger to asyncio logger
 - remove tornado loop from AiidaTestCase
 - Transports module move to asyncio
 - for py>=3.5, inspect.stack now support nametuple, use it
 - CalculationFuture class replace tornado with asyncio, use as_complete
   and wait the first completed future.
 - exponential backoff retry to asyncio, with some test case added

CalcJob related modules to asyncio, InterrupteFuture and interrupt_task
function.

 - test cases added for these two utils
 - If InterrupteFuture got set before its wrapped coroutine
 is done, raise a warning in logger of that exception.
 - Waiting state of calcjob migrate to asyncio
 - getting job list tasks migrate to asyncio

aiida/engine/processes/futures.py
@@ -9,15 +9,14 @@
 ###########################################################################
 # pylint: disable=cyclic-import
 """Futures that can poll or receive broadcasted messages while waiting for a task to be completed."""
-import tornado.gen
+import asyncio
 
-import plumpy
 import kiwipy
 
 __all__ = ('CalculationFuture',)
 
 
-class CalculationFuture(plumpy.Future):
+class CalculationFuture(asyncio.Future):
     """
     A future that waits for a calculation to complete using both polling and
     listening for broadcast events if possible
@@ -38,7 +37,10 @@ class CalculationFuture(plumpy.Future):
         from aiida.orm import load_node
         from .process import ProcessState
 
-        super().__init__()
+        # create future in specified event loop
+        loop = loop if loop is not None else asyncio.get_event_loop()
+        super().__init__(loop=loop)
+
         assert not (poll_interval is None and communicator is None), 'Must poll or have a communicator to use'
 
         calc_node = load_node(pk=pk)
@@ -58,7 +60,7 @@ class CalculationFuture(plumpy.Future):
 
             # Start polling
             if poll_interval is not None:
-                loop.add_callback(self._poll_calculation, calc_node, poll_interval)
+                asyncio.run_coroutine_threadsafe(self._poll_calculation(calc_node, poll_interval), loop)
 
     def cleanup(self):
         """Clean up the future by removing broadcast subscribers from the communicator if it still exists."""
@@ -67,11 +69,10 @@ class CalculationFuture(plumpy.Future):
             self._filtered = None
             self._communicator = None
 
-    @tornado.gen.coroutine
-    def _poll_calculation(self, calc_node, poll_interval):
+    async def _poll_calculation(self, calc_node, poll_interval):
         """Poll whether the calculation node has reached a terminal state."""
         while not self.done() and not calc_node.is_terminated:
-            yield tornado.gen.sleep(poll_interval)
+            await asyncio.sleep(poll_interval)
 
         if not self.done():
             self.set_result(calc_node)

aiidateam/aiida-core

aiida/engine/processes/futures.py
@@ -9,15 +9,14 @@
 ###########################################################################
 # pylint: disable=cyclic-import
 """Futures that can poll or receive broadcasted messages while waiting for a task to be completed."""
-import tornado.gen
+import asyncio
 
-import plumpy
 import kiwipy
 
 __all__ = ('ProcessFuture',)
 
 
-class ProcessFuture(plumpy.Future):
+class ProcessFuture(asyncio.Future):
     """Future that waits for a process to complete using both polling and listening for broadcast events if possible."""
 
     _filtered = None
@@ -36,7 +35,10 @@ class ProcessFuture(plumpy.Future):
         from aiida.orm import load_node
         from .process import ProcessState
 
-        super().__init__()
+        # create future in specified event loop
+        loop = loop if loop is not None else asyncio.get_event_loop()
+        super().__init__(loop=loop)
+
         assert not (poll_interval is None and communicator is None), 'Must poll or have a communicator to use'
 
         node = load_node(pk=pk)
@@ -56,7 +58,7 @@ class ProcessFuture(plumpy.Future):
 
             # Start polling
             if poll_interval is not None:
-                loop.add_callback(self._poll_process, node, poll_interval)
+                loop.create_task(self._poll_process(node, poll_interval))
 
     def cleanup(self):
         """Clean up the future by removing broadcast subscribers from the communicator if it still exists."""
@@ -65,11 +67,10 @@ class ProcessFuture(plumpy.Future):
             self._communicator = None
             self._broadcast_identifier = None
 
-    @tornado.gen.coroutine
-    def _poll_process(self, node, poll_interval):
+    async def _poll_process(self, node, poll_interval):
         """Poll whether the process node has reached a terminal state."""
         while not self.done() and not node.is_terminated:
-            yield tornado.gen.sleep(poll_interval)
+            await asyncio.sleep(poll_interval)
 
         if not self.done():
             self.set_result(node)
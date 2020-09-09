be94c5e2ac3e4c75181f03e2a7b711af340bc761
Chris Sewell <chrisj_sewell@hotmail.com>
2020-09-09 18:35:16 +0100

    update [ci skip]

--- tests/benchmark/test_nodes.py
+++ tests/benchmark/test_nodes.py
@@ -8,24 +8,45 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 # pylint: disable=unused-argument,protected-access
-"""Benchmark tests for nodes."""
+"""Performance benchmark tests for single nodes.
+
+The purpose of these tests is to benchmark and compare basic node interactions,
+such as storage and retrieval from the database.
+"""
+from io import StringIO
+
 import pytest
 
 from aiida.orm import Data
-from aiida.engine import calcfunction, run, WorkChain
+
+GROUP_NAME = 'Single Node'
 
 
-def get_node(store=True):
+def get_data_node(store=True):
+    """A function to create a simple data node."""
     data = Data()
     data.set_attribute_many({str(i): i for i in range(10)})
     if store:
         data.store()
-    return (), {"node": data}
+    return (), {'node': data}
+
+
+def get_data_node_and_object(store=True):
+    """A function to create a simple data node."""
+    data = Data()
+    data.set_attribute_many({str(i): i for i in range(10)})
+    data.put_object_from_filelike(StringIO('a' * 10000), 'key')
+    if store:
+        data.store()
+    return (), {'node': data}
 
 
 @pytest.mark.usefixtures('clear_database_before_test')
-@pytest.mark.benchmark(group='Node Manipulation', min_rounds=100)
+@pytest.mark.benchmark(group=GROUP_NAME, min_rounds=100)
 def test_store_backend(benchmark):
+    """Benchmark for creating and storing a node directly,
+    via the backend storage mechanism.
+    """
 
     def _run():
         data = Data()
@@ -36,70 +57,57 @@ def test_store_backend(benchmark):
 
 
 @pytest.mark.usefixtures('clear_database_before_test')
-@pytest.mark.benchmark(group='Node Manipulation', min_rounds=100)
+@pytest.mark.benchmark(group=GROUP_NAME, min_rounds=100)
 def test_store(benchmark):
-    """Store a node."""
-    benchmark(get_node)
+    """Benchmark for creating and storing a node,
+    via the full ORM mechanism.
+    """
+    benchmark(get_data_node)
 
 
 @pytest.mark.usefixtures('clear_database_before_test')
-@pytest.mark.benchmark(group='Node Manipulation')
+@pytest.mark.benchmark(group=GROUP_NAME, min_rounds=100)
+def test_store_with_object(benchmark):
+    """Benchmark for creating and storing a node,
+    including an object to be stored in the repository.
+    """
+    benchmark(get_data_node_and_object)
+
+
+@pytest.mark.usefixtures('clear_database_before_test')
+@pytest.mark.benchmark(group=GROUP_NAME)
 def test_delete_backend(benchmark):
+    """Benchmark for deleting a stored node directly,
+    via the backend deletion mechanism.
+    """
 
     def _run(node):
         Data.objects._backend.nodes.delete(node.pk)  # pylint: disable=no-member
 
-    benchmark.pedantic(_run, setup=get_node, iterations=1, rounds=100, warmup_rounds=1)
+    benchmark.pedantic(_run, setup=get_data_node, iterations=1, rounds=100, warmup_rounds=1)
 
 
 @pytest.mark.usefixtures('clear_database_before_test')
-@pytest.mark.benchmark(group='Node Manipulation')
+@pytest.mark.benchmark(group=GROUP_NAME)
 def test_delete(benchmark):
-    """Delete a node."""
+    """Benchmark for deleting a node,
+    via the full ORM mechanism.
+    """
+
     def _run(node):
         Data.objects.delete(node.pk)  # pylint: disable=no-member
 
-    benchmark.pedantic(_run, setup=get_node, iterations=1, rounds=100, warmup_rounds=1)
+    benchmark.pedantic(_run, setup=get_data_node, iterations=1, rounds=100, warmup_rounds=1)
 
 
 @pytest.mark.usefixtures('clear_database_before_test')
-@pytest.mark.benchmark(group='Computations')
-def test_calcfunction(benchmark):
-    """Run a simple calcfunction."""
-
-    @calcfunction
-    def _calcfunction(node):
-        return get_node(store=False)[1]["node"]
+@pytest.mark.benchmark(group=GROUP_NAME)
+def test_delete_with_object(benchmark):
+    """Benchmark for deleting a node,
+    including an object stored in the repository
+    """
 
     def _run(node):
-        return _calcfunction(node)
+        Data.objects.delete(node.pk)  # pylint: disable=no-member
 
-    result = benchmark.pedantic(_run, setup=get_node, iterations=1, rounds=50, warmup_rounds=1)
-    assert isinstance(result, Data)
-
-
-@pytest.mark.usefixtures('clear_database_before_test')
-@pytest.mark.benchmark(group='Computations')
-def test_workchain(benchmark):
-
-    @calcfunction
-    def _calcfunction(node):
-        return get_node(store=False)[1]["node"]
-
-    class _Wc(WorkChain):
-
-        @classmethod
-        def define(cls, spec):
-            super().define(spec)
-            spec.input('node')
-            spec.output('node')
-            spec.outline(cls.call_workfunction)
-
-        def call_workfunction(self):
-            self.out('node', _calcfunction(self.inputs.node))
-
-    def _run(node):
-        return run(_Wc({"node": node}))
-
-    result = benchmark.pedantic(_run, setup=get_node, iterations=1, rounds=50, warmup_rounds=1)
-    assert isinstance(result['node'], Data)
+    benchmark.pedantic(_run, setup=get_data_node_and_object, iterations=1, rounds=100, warmup_rounds=1)

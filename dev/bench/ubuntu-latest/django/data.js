c6b73acddb3e9152159d084bd9ba09abf537d7d6
github-action-benchmark <github@users.noreply.github.com>
2020-09-09 13:40:07 +0000

    add Pytest Benchmarks (ubuntu-latest, django) benchmark result for 45b1114af9d1d9a2e3eef05c8ce3c89dd3313d85 [ci skip]

--- dev/bench/ubuntu-latest/django/data.js
+++ dev/bench/ubuntu-latest/django/data.js
@@ -1,9 +1,9 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1599658489297,
+  "lastUpdate": 1599658807583,
   "repoUrl": "https://github.com/aiidateam/aiida-core",
   "xAxis": "id",
   "oneChartGroups": [
-    "node"
+    "Node Manipulation"
   ],
   "entries": {
     "Pytest Benchmarks (ubuntu-latest, django)": [
@@ -48,6 +48,58 @@ window.BENCHMARK_DATA = {
             "range": "stddev: 0.0013803",
             "group": "Node Manipulation",
             "extra": "mean: 5.4589 msec\nrounds: 100"
+          }
+        ]
+      },
+      {
+        "cpu": {
+          "speed": "2.60",
+          "cores": 2,
+          "physicalCores": 2,
+          "processors": 1
+        },
+        "extra": {
+          "pythonVersion": "3.8.5"
+        },
+        "commit": {
+          "id": "45b1114af9d1d9a2e3eef05c8ce3c89dd3313d85",
+          "message": "Pytest Benchmark",
+          "timestamp": "2020-09-08T05:32:02Z",
+          "url": "https://github.com/aiidateam/aiida-core/pull/4362/commits/45b1114af9d1d9a2e3eef05c8ce3c89dd3313d85"
+        },
+        "date": 1599658803865,
+        "benches": [
+          {
+            "name": "tests/benchmark/test_nodes.py::test_store_backend",
+            "value": 1398.0350909389886,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000087799",
+            "group": "Node Manipulation",
+            "extra": "mean: 715.29 usec\nrounds: 215"
+          },
+          {
+            "name": "tests/benchmark/test_nodes.py::test_store",
+            "value": 293.8502929177761,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00028604",
+            "group": "Node Manipulation",
+            "extra": "mean: 3.4031 msec\nrounds: 155"
+          },
+          {
+            "name": "tests/benchmark/test_nodes.py::test_delete_backend",
+            "value": 312.0641224866216,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00028478",
+            "group": "Node Manipulation",
+            "extra": "mean: 3.2045 msec\nrounds: 100"
+          },
+          {
+            "name": "tests/benchmark/test_nodes.py::test_delete",
+            "value": 62.757650790073946,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0010569",
+            "group": "Node Manipulation",
+            "extra": "mean: 15.934 msec\nrounds: 100"
           }
         ]
       }

0da7e1bb624a928a7a3105db67699d5d123014c3
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-09-18 14:07:15 +0200

    Convert if-block to a unittest assertion

--- aiida/tools/data/array/kpoints/legacy.py
+++ aiida/tools/data/array/kpoints/legacy.py
@@ -342,8 +342,8 @@ def get_explicit_kpoints_path(
     explicit_kpoints = [tuple(point_coordinates[path[0][0]])]
     labels = [(0, path[0][0])]
 
-    if not all([_.is_integer() for _ in num_points if isinstance(_, (float, numpy.float64))]):
-        raise ValueError('Could not determine number of points as a whole number. num_points={}'.format(num_points))
+    assert all([_.is_integer() for _ in num_points if isinstance(_, (float, numpy.float64))]
+               ), 'Could not determine number of points as a whole number. num_points={}'.format(num_points)
     num_points = [int(_) for _ in num_points]
 
     for count_piece, i in enumerate(path):

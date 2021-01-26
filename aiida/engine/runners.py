aiidateam/aiida-core

commit 2ae0f420e7466b68c61bf73f5b5e7008de07968e

Type checking: `aiida/engine` (+bug fixes) (#4669)
Added type checking for the modules

* `aiida.engine`
* `aiida.manage.manager`

Move `aiida.orm` imports to top of file in `aiida.engine` module. This should be
fine as `aiida.orm` should not import anything from `aiida.engine` and this way
we don't need import guards specifically for type checking.

aiida/engine/runners.py
@@ -9,23 +9,25 @@
 ###########################################################################
 # pylint: disable=global-statement
 """Runners that can run and submit processes."""
-import collections
+import asyncio
 import functools
 import logging
 import signal
 import threading
+from typing import Any, Callable, Dict, NamedTuple, Optional, Tuple, Type, Union
 import uuid
-import asyncio
 
 import kiwipy
-import plumpy
-from plumpy import set_event_loop_policy, reset_event_loop_policy
+from plumpy.persistence import Persister
+from plumpy.process_comms import RemoteProcessThreadController
+from plumpy.events import set_event_loop_policy, reset_event_loop_policy
+from plumpy.communications import wrap_communicator
 
 from aiida.common import exceptions
-from aiida.orm import load_node
+from aiida.orm import load_node, ProcessNode
 from aiida.plugins.utils import PluginVersionProvider
 
-from .processes import futures, ProcessState
+from .processes import futures, Process, ProcessBuilder, ProcessState
 from .processes.calcjobs import manager
 from . import transports
 from . import utils
@@ -34,28 +36,46 @@ __all__ = ('Runner',)
 
 LOGGER = logging.getLogger(__name__)
 
-ResultAndNode = collections.namedtuple('ResultAndNode', ['result', 'node'])
-ResultAndPk = collections.namedtuple('ResultAndPk', ['result', 'pk'])
+
+class ResultAndNode(NamedTuple):
+    node: ProcessNode
+    result: Dict[str, Any]
+
+
+class ResultAndPk(NamedTuple):
+    node: ProcessNode
+    pk: int
+
+
+TYPE_RUN_PROCESS = Union[Process, Type[Process], ProcessBuilder]  # pylint: disable=invalid-name
+# run can also be process function, but it is not clear what type this should be
+TYPE_SUBMIT_PROCESS = Union[Process, Type[Process], ProcessBuilder]  # pylint: disable=invalid-name
 
 
 class Runner:  # pylint: disable=too-many-public-methods
     """Class that can launch processes by running in the current interpreter or by submitting them to the daemon."""
 
-    _persister = None
-    _communicator = None
-    _controller = None
-    _closed = False
+    _persister: Optional[Persister] = None
+    _communicator: Optional[kiwipy.Communicator] = None
+    _controller: Optional[RemoteProcessThreadController] = None
+    _closed: bool = False
 
-    def __init__(self, poll_interval=0, loop=None, communicator=None, rmq_submit=False, persister=None):
+    def __init__(
+        self,
+        poll_interval: Union[int, float] = 0,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+        communicator: Optional[kiwipy.Communicator] = None,
+        rmq_submit: bool = False,
+        persister: Optional[Persister] = None
+    ):
         """Construct a new runner.
 
         :param poll_interval: interval in seconds between polling for status of active sub processes
         :param loop: an asyncio event loop, if none is suppled a new one will be created
         :param communicator: the communicator to use
-        :type communicator: :class:`kiwipy.Communicator`
         :param rmq_submit: if True, processes will be submitted to RabbitMQ, otherwise they will be scheduled here
         :param persister: the persister to use to persist processes
-        :type persister: :class:`plumpy.Persister`
+
         """
         assert not (rmq_submit and persister is None), \
             'Must supply a persister if you want to submit using communicator'
@@ -70,94 +90,86 @@ class Runner:  # pylint: disable=too-many-public-methods
         self._plugin_version_provider = PluginVersionProvider()
 
         if communicator is not None:
-            self._communicator = plumpy.wrap_communicator(communicator, self._loop)
-            self._controller = plumpy.RemoteProcessThreadController(communicator)
+            self._communicator = wrap_communicator(communicator, self._loop)
+            self._controller = RemoteProcessThreadController(communicator)
         elif self._rmq_submit:
             LOGGER.warning('Disabling RabbitMQ submission, no communicator provided')
             self._rmq_submit = False
 
-    def __enter__(self):
+    def __enter__(self) -> 'Runner':
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
     @property
-    def loop(self):
-        """
-        Get the event loop of this runner
-
-        :return: the asyncio event loop
-        """
+    def loop(self) -> asyncio.AbstractEventLoop:
+        """Get the event loop of this runner."""
         return self._loop
 
     @property
-    def transport(self):
+    def transport(self) -> transports.TransportQueue:
         return self._transport
 
     @property
-    def persister(self):
+    def persister(self) -> Optional[Persister]:
+        """Get the persister used by this runner."""
         return self._persister
 
     @property
-    def communicator(self):
-        """
-        Get the communicator used by this runner
-
-        :return: the communicator
-        :rtype: :class:`kiwipy.Communicator`
-        """
+    def communicator(self) -> Optional[kiwipy.Communicator]:
+        """Get the communicator used by this runner."""
         return self._communicator
 
     @property
-    def plugin_version_provider(self):
+    def plugin_version_provider(self) -> PluginVersionProvider:
         return self._plugin_version_provider
 
     @property
-    def job_manager(self):
+    def job_manager(self) -> manager.JobManager:
         return self._job_manager
 
     @property
-    def controller(self):
+    def controller(self) -> Optional[RemoteProcessThreadController]:
+        """Get the controller used by this runner."""
         return self._controller
 
     @property
-    def is_daemon_runner(self):
+    def is_daemon_runner(self) -> bool:
         """Return whether the runner is a daemon runner, which means it submits processes over RabbitMQ.
 
         :return: True if the runner is a daemon runner
-        :rtype: bool
         """
         return self._rmq_submit
 
-    def is_closed(self):
+    def is_closed(self) -> bool:
         return self._closed
 
-    def start(self):
+    def start(self) -> None:
         """Start the internal event loop."""
         self._loop.run_forever()
 
-    def stop(self):
+    def stop(self) -> None:
         """Stop the internal event loop."""
         self._loop.stop()
 
-    def run_until_complete(self, future):
+    def run_until_complete(self, future: asyncio.Future) -> Any:
         """Run the loop until the future has finished and return the result."""
         with utils.loop_scope(self._loop):
             return self._loop.run_until_complete(future)
 
-    def close(self):
+    def close(self) -> None:
         """Close the runner by stopping the loop."""
         assert not self._closed
         self.stop()
         reset_event_loop_policy()
         self._closed = True
 
-    def instantiate_process(self, process, *args, **inputs):
+    def instantiate_process(self, process: TYPE_RUN_PROCESS, *args, **inputs):
         from .utils import instantiate_process
         return instantiate_process(self, process, *args, **inputs)
 
-    def submit(self, process, *args, **inputs):
+    def submit(self, process: TYPE_SUBMIT_PROCESS, *args: Any, **inputs: Any):
         """
         Submit the process with the supplied inputs to this runner immediately returning control to
         the interpreter. The return value will be the calculation node of the submitted process
@@ -169,24 +181,26 @@ class Runner:  # pylint: disable=too-many-public-methods
         assert not utils.is_process_function(process), 'Cannot submit a process function'
         assert not self._closed
 
-        process = self.instantiate_process(process, *args, **inputs)
+        process_inited = self.instantiate_process(process, *args, **inputs)
 
-        if not process.metadata.store_provenance:
+        if not process_inited.metadata.store_provenance:
             raise exceptions.InvalidOperation('cannot submit a process with `store_provenance=False`')
 
-        if process.metadata.get('dry_run', False):
+        if process_inited.metadata.get('dry_run', False):
             raise exceptions.InvalidOperation('cannot submit a process from within another with `dry_run=True`')
 
         if self._rmq_submit:
-            self.persister.save_checkpoint(process)
-            process.close()
-            self.controller.continue_process(process.pid, nowait=False, no_reply=True)
+            assert self.persister is not None, 'runner does not have a persister'
+            assert self.controller is not None, 'runner does not have a controller'
+            self.persister.save_checkpoint(process_inited)
+            process_inited.close()
+            self.controller.continue_process(process_inited.pid, nowait=False, no_reply=True)
         else:
-            self.loop.create_task(process.step_until_terminated())
+            self.loop.create_task(process_inited.step_until_terminated())
 
-        return process.node
+        return process_inited.node
 
-    def schedule(self, process, *args, **inputs):
+    def schedule(self, process: TYPE_SUBMIT_PROCESS, *args: Any, **inputs: Any) -> ProcessNode:
         """
         Schedule a process to be executed by this runner
 
@@ -197,11 +211,11 @@ class Runner:  # pylint: disable=too-many-public-methods
         assert not utils.is_process_function(process), 'Cannot submit a process function'
         assert not self._closed
 
-        process = self.instantiate_process(process, *args, **inputs)
-        self.loop.create_task(process.step_until_terminated())
-        return process.node
+        process_inited = self.instantiate_process(process, *args, **inputs)
+        self.loop.create_task(process_inited.step_until_terminated())
+        return process_inited.node
 
-    def _run(self, process, *args, **inputs):
+    def _run(self, process: TYPE_RUN_PROCESS, *args: Any, **inputs: Any) -> Tuple[Dict[str, Any], ProcessNode]:
         """
         Run the process with the supplied inputs in this runner that will block until the process is completed.
         The return value will be the results of the completed process
@@ -213,24 +227,24 @@ class Runner:  # pylint: disable=too-many-public-methods
         assert not self._closed
 
         if utils.is_process_function(process):
-            result, node = process.run_get_node(*args, **inputs)
+            result, node = process.run_get_node(*args, **inputs)  # type: ignore[union-attr]
             return result, node
 
         with utils.loop_scope(self.loop):
-            process = self.instantiate_process(process, *args, **inputs)
+            process_inited = self.instantiate_process(process, *args, **inputs)
 
             def kill_process(_num, _frame):
                 """Send the kill signal to the process in the current scope."""
-                LOGGER.critical('runner received interrupt, killing process %s', process.pid)
-                process.kill(msg='Process was killed because the runner received an interrupt')
+                LOGGER.critical('runner received interrupt, killing process %s', process_inited.pid)
+                process_inited.kill(msg='Process was killed because the runner received an interrupt')
 
             signal.signal(signal.SIGINT, kill_process)
             signal.signal(signal.SIGTERM, kill_process)
 
-            process.execute()
-            return process.outputs, process.node
+            process_inited.execute()
+            return process_inited.outputs, process_inited.node
 
-    def run(self, process, *args, **inputs):
+    def run(self, process: TYPE_RUN_PROCESS, *args: Any, **inputs: Any) -> Dict[str, Any]:
         """
         Run the process with the supplied inputs in this runner that will block until the process is completed.
         The return value will be the results of the completed process
@@ -242,7 +256,7 @@ class Runner:  # pylint: disable=too-many-public-methods
         result, _ = self._run(process, *args, **inputs)
         return result
 
-    def run_get_node(self, process, *args, **inputs):
+    def run_get_node(self, process: TYPE_RUN_PROCESS, *args: Any, **inputs: Any) -> ResultAndNode:
         """
         Run the process with the supplied inputs in this runner that will block until the process is completed.
         The return value will be the results of the completed process
@@ -254,7 +268,7 @@ class Runner:  # pylint: disable=too-many-public-methods
         result, node = self._run(process, *args, **inputs)
         return ResultAndNode(result, node)
 
-    def run_get_pk(self, process, *args, **inputs):
+    def run_get_pk(self, process: TYPE_RUN_PROCESS, *args: Any, **inputs: Any) -> ResultAndPk:
         """
         Run the process with the supplied inputs in this runner that will block until the process is completed.
         The return value will be the results of the completed process
@@ -266,7 +280,7 @@ class Runner:  # pylint: disable=too-many-public-methods
         result, node = self._run(process, *args, **inputs)
         return ResultAndPk(result, node.pk)
 
-    def call_on_process_finish(self, pk, callback):
+    def call_on_process_finish(self, pk: int, callback: Callable[[], Any]) -> None:
         """Schedule a callback when the process of the given pk is terminated.
 
         This method will add a broadcast subscriber that will listen for state changes of the target process to be
@@ -276,6 +290,8 @@ class Runner:  # pylint: disable=too-many-public-methods
         :param pk: pk of the process
         :param callback: function to be called upon process termination
         """
+        assert self.communicator is not None, 'communicator not set for runner'
+
         node = load_node(pk=pk)
         subscriber_identifier = str(uuid.uuid4())
         event = threading.Event()
@@ -293,17 +309,17 @@ class Runner:  # pylint: disable=too-many-public-methods
                 callback()
             finally:
                 event.set()
-                self._communicator.remove_broadcast_subscriber(subscriber_identifier)
+                self.communicator.remove_broadcast_subscriber(subscriber_identifier)  # type: ignore[union-attr]
 
         broadcast_filter = kiwipy.BroadcastFilter(functools.partial(inline_callback, event), sender=pk)
         for state in [ProcessState.FINISHED, ProcessState.KILLED, ProcessState.EXCEPTED]:
             broadcast_filter.add_subject_filter(f'state_changed.*.{state.value}')
 
         LOGGER.info('adding subscriber for broadcasts of %d', pk)
-        self._communicator.add_broadcast_subscriber(broadcast_filter, subscriber_identifier)
+        self.communicator.add_broadcast_subscriber(broadcast_filter, subscriber_identifier)
         self._poll_process(node, functools.partial(inline_callback, event))
 
-    def get_process_future(self, pk):
+    def get_process_future(self, pk: int) -> futures.ProcessFuture:
         """Return a future for a process.
 
         The future will have the process node as the result when finished.

.pre-commit-config.yaml
@@ -43,7 +43,8 @@ repos:
         files: >-
             (?x)^(
                 aiida/common/progress_reporter.py|
-                aiida/engine/processes/calcjobs/calcjob.py|
+                aiida/manage/manager.py|
+                aiida/engine/.*py|
                 aiida/manage/database/delete/nodes.py|
                 aiida/tools/graph/graph_traversers.py|
                 aiida/tools/groups/paths.py|

aiida/engine/__init__.py
@@ -14,4 +14,4 @@ from .launch import *
 from .processes import *
 from .utils import *
 
-__all__ = (launch.__all__ + processes.__all__ + utils.__all__)
+__all__ = (launch.__all__ + processes.__all__ + utils.__all__)  # type: ignore[name-defined]

aiida/engine/daemon/client.py
@@ -16,12 +16,20 @@ import os
 import shutil
 import socket
 import tempfile
+from typing import Any, Dict, Optional, TYPE_CHECKING
 
 from aiida.manage.configuration import get_config, get_config_option
+from aiida.manage.configuration.profile import Profile
+
+if TYPE_CHECKING:
+    from circus.client import CircusClient
 
 VERDI_BIN = shutil.which('verdi')
 # Recent versions of virtualenv create the environment variable VIRTUAL_ENV
 VIRTUALENV = os.environ.get('VIRTUAL_ENV', None)
+
+# see https://github.com/python/typing/issues/182
+JsonDictType = Dict[str, Any]
 
 
 class ControllerProtocol(enum.Enum):
@@ -33,13 +41,13 @@ class ControllerProtocol(enum.Enum):
     TCP = 1
 
 
-def get_daemon_client(profile_name=None):
+def get_daemon_client(profile_name: Optional[str] = None) -> 'DaemonClient':
     """
     Return the daemon client for the given profile or the current profile if not specified.
 
     :param profile_name: the profile name, will use the current profile if None
     :return: the daemon client
-    :rtype: :class:`aiida.engine.daemon.client.DaemonClient`
+
     :raises aiida.common.MissingConfigurationError: if the configuration file cannot be found
     :raises aiida.common.ProfileConfigurationError: if the given profile does not exist
     """
@@ -65,7 +73,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
     _DAEMON_NAME = 'aiida-{name}'
     _ENDPOINT_PROTOCOL = ControllerProtocol.IPC
 
-    def __init__(self, profile):
+    def __init__(self, profile: Profile):
         """
         Construct a DaemonClient instance for a given profile
 
@@ -73,22 +81,22 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
         """
         config = get_config()
         self._profile = profile
-        self._SOCKET_DIRECTORY = None  # pylint: disable=invalid-name
-        self._DAEMON_TIMEOUT = config.get_option('daemon.timeout')  # pylint: disable=invalid-name
+        self._SOCKET_DIRECTORY: Optional[str] = None  # pylint: disable=invalid-name
+        self._DAEMON_TIMEOUT: int = config.get_option('daemon.timeout')  # pylint: disable=invalid-name
 
     @property
-    def profile(self):
+    def profile(self) -> Profile:
         return self._profile
 
     @property
-    def daemon_name(self):
+    def daemon_name(self) -> str:
         """
         Get the daemon name which is tied to the profile name
         """
         return self._DAEMON_NAME.format(name=self.profile.name)
 
     @property
-    def cmd_string(self):
+    def cmd_string(self) -> str:
         """
         Return the command string to start the AiiDA daemon
         """
@@ -101,42 +109,42 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
         return f'{VERDI_BIN} -p {self.profile.name} devel run_daemon'
 
     @property
-    def loglevel(self):
+    def loglevel(self) -> str:
         return get_config_option('logging.circus_loglevel')
 
     @property
-    def virtualenv(self):
+    def virtualenv(self) -> Optional[str]:
         return VIRTUALENV
 
     @property
-    def circus_log_file(self):
+    def circus_log_file(self) -> str:
         return self.profile.filepaths['circus']['log']
 
     @property
-    def circus_pid_file(self):
+    def circus_pid_file(self) -> str:
         return self.profile.filepaths['circus']['pid']
 
     @property
-    def circus_port_file(self):
+    def circus_port_file(self) -> str:
         return self.profile.filepaths['circus']['port']
 
     @property
-    def circus_socket_file(self):
+    def circus_socket_file(self) -> str:
         return self.profile.filepaths['circus']['socket']['file']
 
     @property
-    def circus_socket_endpoints(self):
+    def circus_socket_endpoints(self) -> Dict[str, str]:
         return self.profile.filepaths['circus']['socket']
 
     @property
-    def daemon_log_file(self):
+    def daemon_log_file(self) -> str:
         return self.profile.filepaths['daemon']['log']
 
     @property
-    def daemon_pid_file(self):
+    def daemon_pid_file(self) -> str:
         return self.profile.filepaths['daemon']['pid']
 
-    def get_circus_port(self):
+    def get_circus_port(self) -> int:
         """
         Retrieve the port for the circus controller, which should be written to the circus port file. If the
         daemon is running, the port file should exist and contain the port to which the controller is connected.
@@ -158,7 +166,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
 
             return port
 
-    def get_circus_socket_directory(self):
+    def get_circus_socket_directory(self) -> str:
         """
         Retrieve the absolute path of the directory where the circus sockets are stored if the IPC protocol is
         used and the daemon is running. If the daemon is running, the sockets file should exist and contain the
@@ -192,7 +200,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
             self._SOCKET_DIRECTORY = socket_dir_path
             return socket_dir_path
 
-    def get_daemon_pid(self):
+    def get_daemon_pid(self) -> Optional[int]:
         """
         Get the daemon pid which should be written in the daemon pid file specific to the profile
 
@@ -207,7 +215,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
             return None
 
     @property
-    def is_daemon_running(self):
+    def is_daemon_running(self) -> bool:
         """
         Return whether the daemon is running, which is determined by seeing if the daemon pid file is present
 
@@ -215,7 +223,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
         """
         return self.get_daemon_pid() is not None
 
-    def delete_circus_socket_directory(self):
+    def delete_circus_socket_directory(self) -> None:
         """
         Attempt to delete the directory used to store the circus endpoint sockets. Will not raise if the
         directory does not exist
@@ -321,7 +329,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
         return endpoint
 
     @property
-    def client(self):
+    def client(self) -> 'CircusClient':
         """
         Return an instance of the CircusClient with the endpoint defined by the controller endpoint, which
         used the port that was written to the port file upon starting of the daemon
@@ -334,7 +342,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
         from circus.client import CircusClient
         return CircusClient(endpoint=self.get_controller_endpoint(), timeout=self._DAEMON_TIMEOUT)
 
-    def call_client(self, command):
+    def call_client(self, command: JsonDictType) -> JsonDictType:
         """
         Call the client with a specific command. Will check whether the daemon is running first
         by checking for the pid file. When the pid is found yet the call still fails with a
@@ -358,47 +366,51 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
 
         return result
 
-    def get_status(self):
+    def get_status(self) -> JsonDictType:
         """
         Get the daemon running status
 
         :return: the client call response
+            If successful, will will contain 'status' key
         """
         command = {'command': 'status', 'properties': {'name': self.daemon_name}}
 
         return self.call_client(command)
 
-    def get_numprocesses(self):
+    def get_numprocesses(self) -> JsonDictType:
         """
         Get the number of running daemon processes
 
         :return: the client call response
+            If successful, will contain 'numprocesses' key
         """
         command = {'command': 'numprocesses', 'properties': {'name': self.daemon_name}}
 
         return self.call_client(command)
 
-    def get_worker_info(self):
+    def get_worker_info(self) -> JsonDictType:
         """
         Get workers statistics for this daemon
 
         :return: the client call response
+            If successful, will contain 'info' key
         """
         command = {'command': 'stats', 'properties': {'name': self.daemon_name}}
 
         return self.call_client(command)
 
-    def get_daemon_info(self):
+    def get_daemon_info(self) -> JsonDictType:
         """
         Get statistics about this daemon itself
 
         :return: the client call response
+            If successful, will contain 'info' key
         """
         command = {'command': 'dstats', 'properties': {}}
 
         return self.call_client(command)
 
-    def increase_workers(self, number):
+    def increase_workers(self, number: int) -> JsonDictType:
         """
         Increase the number of workers
 
@@ -409,7 +421,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
 
         return self.call_client(command)
 
-    def decrease_workers(self, number):
+    def decrease_workers(self, number: int) -> JsonDictType:
         """
         Decrease the number of workers
 
@@ -420,7 +432,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
 
         return self.call_client(command)
 
-    def stop_daemon(self, wait):
+    def stop_daemon(self, wait: bool) -> JsonDictType:
         """
         Stop the daemon
 
@@ -436,7 +448,7 @@ class DaemonClient:  # pylint: disable=too-many-public-methods
 
         return result
 
-    def restart_daemon(self, wait):
+    def restart_daemon(self, wait: bool) -> JsonDictType:
         """
         Restart the daemon
 

commit 2c5293ded4514c36a2754acbe67aa778e27c4886

Fix `run_get_node`/`run_get_pk` namedtuples (#4677)
Fix a regression made in #4669, whereby the namedtuple's were incorrectly named

aiida/engine/runners.py
@@ -38,12 +38,12 @@ LOGGER = logging.getLogger(__name__)
 
 
 class ResultAndNode(NamedTuple):
-    node: ProcessNode
     result: Dict[str, Any]
+    node: ProcessNode
 
 
 class ResultAndPk(NamedTuple):
-    node: ProcessNode
+    result: Dict[str, Any]
     pk: int
 
 
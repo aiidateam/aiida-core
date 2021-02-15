ce2310078a5959df703c6dcb3a835eb2f84d39ae
ramirezfranciscof <ramirezfranciscof@users.noreply.github.com>
2021-02-15 16:33:01 +0100

    Add migration + test for sqla (+ rename django)

---
+++ aiida/backends/sqlalchemy/migrations/versions/d102e96ea943_inline_calc_attributes.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida-core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+"""Migrate attributes from old InlineCalculation that have been transformed into CalcFunctions:
+
+* OLD `sealed` and `function_name` remain the same
+* OLD `namespace` becomes `function_namespace`
+* OLD `first_line_source_code` becomes `function_starting_line_number`
+* OLD `source_file` is moved to repository and dropped from DB
+* OLD `source_code` is used to calculate `function_number_of_lines` and dropped from DB
+* NEW `exit_status` is initialized at 0
+* NEW `process_state` is initialized at "finished"
+* NEW `process_label` is set as `Legacy InlineCalculation`
+
+Note 1: old InlineCalculation can be identified by the old keywords, and after migration they will be
+identified by having `function_number_of_lines` but no `version`.
+
+Note 2: `exit_status` and `process_state` are initialized to 0 and "finished" respectively because the
+CalcFunctions affected are old InlineCalculation, which were only stored if they succeeded. You can check
+the relevant code here (line 60 has the execution call, it is stored after that):
+
+https://github.com/aiidateam/aiida-core/blob/v0.12.5/aiida/orm/implementation/django/calculation/inline.py#L60
+
+Revision ID: d102e96ea943
+Revises: 0edcdd5a30f0
+Create Date: 2021-02-09 16:25:16.106176
+
+"""
+# pylint: disable=invalid-name,no-member
+# yapf: disable
+from alembic import op
+from sqlalchemy.sql import text
+from aiida.backends.general.migrations import utils
+
+# revision identifiers, used by Alembic.
+revision = 'd102e96ea943'
+down_revision = '0edcdd5a30f0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """Creates the new attributes"""
+    conn = op.get_bind()
+
+    # Creates new 'function_namespace' and copies old 'namespace'
+    conn.execute(text(
+        """
+        UPDATE db_dbnode
+        SET attributes = jsonb_set(attributes, '{function_namespace}', attributes->'namespace')
+        WHERE
+            attributes ? 'namespace' AND
+            attributes ? 'first_line_source_code' AND
+            node_type = 'process.calculation.calcfunction.CalcFunctionNode.';
+        """))
+
+    # Creates new 'function_starting_line_number' and copies old 'first_line_source_code'
+    conn.execute(text(
+        """
+        UPDATE db_dbnode
+        SET attributes = jsonb_set(attributes, '{function_starting_line_number}', attributes->'first_line_source_code')
+        WHERE
+            attributes ? 'namespace' AND
+            attributes ? 'first_line_source_code' AND
+            node_type = 'process.calculation.calcfunction.CalcFunctionNode.';
+        """))
+
+    # Sets new 'exit_status'
+    conn.execute(text(
+        """
+        UPDATE db_dbnode
+        SET attributes = jsonb_set(attributes, '{exit_status}', to_jsonb(:value))
+        WHERE
+            attributes ? 'namespace' AND
+            attributes ? 'first_line_source_code' AND
+            node_type = 'process.calculation.calcfunction.CalcFunctionNode.';
+        """), {'value': 0})
+
+    # Sets new 'process_state'
+    conn.execute(text(
+        """
+        UPDATE db_dbnode
+        SET attributes = jsonb_set(attributes, '{process_state}', to_jsonb('finished'::text))
+        WHERE
+            attributes ? 'namespace' AND
+            attributes ? 'first_line_source_code' AND
+            node_type = 'process.calculation.calcfunction.CalcFunctionNode.';
+        """))
+
+    conn.execute(text(
+        """
+        UPDATE db_dbnode
+        SET attributes = jsonb_set(attributes, '{process_label}', to_jsonb('Legacy InlineCalculation'::text))
+        WHERE
+            attributes ? 'namespace' AND
+            attributes ? 'first_line_source_code' AND
+            node_type = 'process.calculation.calcfunction.CalcFunctionNode.';
+        """))
+
+    # Sets new 'function_number_of_lines' with the information on 'source_code'
+    sqlquery_results = conn.execute(text(
+        """
+        SELECT id, attributes -> 'source_code' FROM db_dbnode;
+        """))
+
+    for data_pair in sqlquery_results:
+        node_pkid = data_pair[0]
+        source_code = data_pair[1]
+        num_lines = len(source_code.splitlines())
+        conn.execute(text(
+            """
+            UPDATE db_dbnode
+            SET attributes = jsonb_set(attributes, '{function_number_of_lines}', to_jsonb(:value))
+            WHERE id = :idnum;
+            """), {'value': num_lines, 'idnum': node_pkid})
+
+    # Saves the source file in the repository
+    sqlquery_results = conn.execute(text(
+        """
+        SELECT uuid, attributes -> 'source_file' FROM db_dbnode;
+        """))
+
+    for data_pair in sqlquery_results:
+        node_uuid = data_pair[0]
+        node_file = data_pair[1]
+        utils.put_object_from_string(node_uuid, 'source_file', node_file)
+
+    # Removes all the old attributes
+    conn.execute(text(
+        """
+        UPDATE db_dbnode
+        SET attributes = attributes - 'namespace' - 'source_code' - 'source_file' - 'first_line_source_code'
+        WHERE
+            attributes ? 'namespace' AND
+            attributes ? 'first_line_source_code' AND
+            node_type = 'process.calculation.calcfunction.CalcFunctionNode.';
+        """))
+
+    # ### end Alembic commands ###
+
+
+def downgrade():
+    """Deletes the old attributes"""
+    # ### end Alembic commands ###
--- tests/backends/aiida_sqlalchemy/test_migrations.py
+++ tests/backends/aiida_sqlalchemy/test_migrations.py
@@ -1747,3 +1747,131 @@ class TestGroupExtrasMigration(TestMigrationsSQLA):
                 self.assertEqual(group.extras, {})
             finally:
                 session.close()
+
+
+class TestCalcFunctionAttributesMigration(TestMigrationsSQLA):
+    """Test updates to attributes of InlineFunctions that have been transformed into CalcFunctions."""
+
+    migrate_from = '0edcdd5a30f0'  # 0edcdd5a30f0_dbgroup_extras.py
+    migrate_to = 'd102e96ea943'  # d102e96ea943_calcfunction_attributes.py
+
+    @staticmethod
+    def get_source_code(full_file=False):
+        """Returns a string containing the source code for the calcfunction"""
+
+        text_i = ('from aiida import orm\n'
+                  'from aiida.engine import calcfunction\n'
+        )
+        text_t = ('@calcfunction\n'
+                  'def sum_inline( int1, int2 ):\n'
+                  '    return int1 + int2\n'
+        )
+        text_f = ('num1 = orm.Int(2)\n'
+                  'num2 = orm.Int(3)\n'
+                  'num3 = sum_inline( int1, int2 )\n'
+                  'print(num3)\n'
+        )
+
+        if full_file:
+            text_t = text_i + '\n' + text_t + '\n' + text_f
+
+        return text_t
+
+    def setUpBeforeMigration(self):
+        """Create the process."""
+        from sqlalchemy.orm import Session  # pylint: disable=import-error,no-name-in-module
+
+        DbNode = self.get_auto_base().classes.db_dbnode  # pylint: disable=invalid-name
+        DbUser = self.get_auto_base().classes.db_dbuser  # pylint: disable=invalid-name
+
+        with sa.ENGINE.begin() as connection:
+            try:
+                session = Session(connection.engine)
+                user = DbUser(email=f'{self.id()}@aiida.net')
+                session.add(user)
+                session.commit()
+
+                calcnode = DbNode(
+                    node_type='process.calculation.calcfunction.CalcFunctionNode.',
+                    user_id=user.id,
+                    attributes={
+                        'sealed': True,
+                        'namespace': '__main__',
+                        'source_code': self.get_source_code(full_file=False),
+                        'source_file': self.get_source_code(full_file=True),
+                        'function_name': 'sum_inline',
+                        'first_line_source_code': 4,
+                    },
+                )
+
+                session.add(calcnode)
+                session.commit()
+                self.calcnode_id = calcnode.id
+
+            except Exception:
+                session.rollback()
+                raise
+            finally:
+                session.close()
+
+
+    def test_data_migrated(self):
+        """Test that the model now has an extras column with empty dictionary as default."""
+        from sqlalchemy.orm import Session  # pylint: disable=import-error,no-name-in-module
+
+        DbNode = self.get_auto_base().classes.db_dbnode  # pylint: disable=invalid-name
+
+        with sa.ENGINE.begin() as connection:
+            try:
+                session = Session(connection.engine)
+                calcnode = session.query(DbNode).filter(DbNode.id == self.calcnode_id).one()
+
+                query = session.query(DbNode).filter(
+                    DbNode.node_type == 'process.calculation.calcfunction.CalcFunctionNode.'
+                )
+                for calcnode in query:
+                    print(calcnode.attributes['sealed'])
+                    print(calcnode.node_type)
+
+                # Pre existing and renamed attributes
+                self.assertEqual(calcnode.attributes['sealed'], True)
+                self.assertEqual(calcnode.attributes['function_name'], 'sum_inline')
+                self.assertEqual(calcnode.attributes['function_namespace'], '__main__')
+                self.assertEqual(calcnode.attributes['function_starting_line_number'], 4)
+
+                # Newly determined attributes
+                self.assertEqual(calcnode.attributes['exit_status'], 0)
+                self.assertEqual(calcnode.attributes['process_state'], 'finished')  #USE ENUM?
+                self.assertEqual(calcnode.attributes['process_label'], 'Legacy InlineCalculation')
+                self.assertEqual(calcnode.attributes['function_number_of_lines'], 3)
+
+                # Old attributes that got renamed or removed
+                with self.assertRaises(KeyError):
+                    _ = calcnode.attributes['namespace']
+                with self.assertRaises(KeyError):
+                    _ = calcnode.attributes['source_code']
+                with self.assertRaises(KeyError):
+                    _ = calcnode.attributes['source_file']
+                with self.assertRaises(KeyError):
+                    _ = calcnode.attributes['first_line_source_code']
+
+                # New attributes that are not set
+                with self.assertRaises(KeyError):
+                    _ = calcnode.attributes['version']
+
+                # Source file in repository:
+                repo_data = utils.get_object_from_repository(calcnode.uuid, 'source_file')
+                self.assertEqual(repo_data, self.get_source_code(full_file=True))
+
+                # TEST TEMP
+                print()
+                result = connection.execute(
+                    """
+                    SELECT uuid,attributes FROM db_dbnode;
+                    """
+                )
+                for data in result:
+                    print(data)
+
+            finally:
+                session.close()

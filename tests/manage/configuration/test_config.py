aiidateam/aiida-core

commit e274ef2e065ac4aae03492b6736ae9fdea896ae4

`Config`: add the `delete_profile` method (#4901)
The class already has `remove_profile` that merely removes the profile
from the index and doesn't actually delete its associated resources. The
nrem `delete_profile` method will by default also delete the repository
and database that are configured for the profile, although this behavior
can be controlled with the `include_database` and `include_repository`
arguments.

The implementation of `verdi profile delete` is update to use this new
functionality instead of the loose functions that were defined in the
`aiida.manage.configuration.setup` module. The latter, since it was not
part of the public API and was only used by this CLI command, has been
removed from the codebase.

Note that the implementation for the deletion of the database is
slightly different. Not only does `Config.delete_profile` not contain
any echoing of information, as this should be done in the CLI command
and not in the generic API, the logic for deleting the database user
has also changed. The old function `delete_database` did delete the
database user configured for the profile, as long as no other profiles
in the config used the same. This is dangerous, however, because the
same database user can be used by a program outside of AiiDA and we
don't own those users necessarily. Now, the deletion of the database
user is optional and turned off by default. When it is used though, the
database user is deleted without checking the config for other profiles
that may also have configured it.

aiida/manage/configuration/config.py
@@ -342,6 +342,37 @@ class Config:  # pylint: disable=too-many-public-methods
         self._profiles.pop(name)
         return self
 
+    def delete_profile(
+        self,
+        name: str,
+        include_database: bool = True,
+        include_database_user: bool = False,
+        include_repository: bool = True
+    ):
+        """Delete a profile including its contents.
+
+        :param include_database: also delete the database configured for the profile.
+        :param include_database_user: also delete the database user configured for the profile.
+        :param include_repository: also delete the repository configured for the profile.
+        """
+        from aiida.manage.external.postgres import Postgres
+
+        profile = self.get_profile(name)
+
+        if include_repository:
+            repository = profile.get_repository()
+            repository.delete()
+
+        if include_database:
+            postgres = Postgres.from_profile(profile)
+            if postgres.db_exists(profile.database_name):
+                postgres.drop_db(profile.database_name)
+
+        if include_database_user and postgres.dbuser_exists(profile.database_username):
+            postgres.drop_dbuser(profile.database_username)
+
+        self.remove_profile(name)
+
     def set_default_profile(self, name, overwrite=False):
         """Set the given profile as the new default.
 

aiida/backends/general/migrations/utils.py
@@ -105,6 +105,9 @@ class NoopRepositoryBackend(AbstractRepositoryBackend):
         """Return whether the repository has been initialised."""
         return True
 
+    def erase(self):
+        raise NotImplementedError()
+
     def put_object_from_filelike(self, handle: io.BufferedIOBase) -> str:
         """Store the byte contents of a file in the repository.
 

aiida/cmdline/commands/cmd_profile.py
@@ -8,7 +8,6 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """`verdi profile` command."""
-
 import click
 import tabulate
 
@@ -83,7 +82,18 @@ def profile_setdefault(profile):
     help='Include deletion of entry in configuration file.'
 )
 @click.option(
-    '--include-db/--skip-db', default=True, show_default=True, help='Include deletion of associated database.'
+    '--include-db/--skip-db',
+    'include_database',
+    default=True,
+    show_default=True,
+    help='Include deletion of associated database.'
+)
+@click.option(
+    '--include-db-user/--skip-db-user',
+    'include_database_user',
+    default=False,
+    show_default=True,
+    help='Include deletion of associated database user.'
 )
 @click.option(
     '--include-repository/--skip-repository',
@@ -92,22 +102,41 @@ def profile_setdefault(profile):
     help='Include deletion of associated file repository.'
 )
 @arguments.PROFILES(required=True)
-def profile_delete(force, include_config, include_db, include_repository, profiles):
-    """
-    Delete one or more profiles.
+def profile_delete(force, include_config, include_database, include_database_user, include_repository, profiles):
+    """Delete one or more profiles.
 
-    You can specify more profile names (separated by spaces).
-    These will be removed from the aiida config file,
-    and the associated databases and file repositories will also be removed.
+    The PROFILES argument takes one or multiple profile names that will be deleted. Deletion here means that the profile
+    will be removed including its file repository and database. The various options can be used to control which parts
+    of the profile are deleted.
     """
-    from aiida.manage.configuration.setup import delete_profile
+    if not include_config:
+        echo.echo_deprecated('the `--skip-config` option is deprecated and is no longer respected.')
 
     for profile in profiles:
-        echo.echo_info(f"Deleting profile '{profile.name}'")
-        delete_profile(
-            profile,
-            non_interactive=force,
-            include_db=include_db,
-            include_repository=include_repository,
-            include_config=include_config
+
+        includes = {
+            'database': include_database,
+            'database user': include_database_user,
+            'file repository': include_repository
+        }
+
+        if not all(includes.values()):
+            excludes = [label for label in includes if not includes[label]]
+            message_suffix = f' excluding: {", ".join(excludes)}.'
+        else:
+            message_suffix = '.'
+
+        echo.echo_warning(f'deleting profile `{profile.name}`{message_suffix}')
+        echo.echo_warning('this operation cannot be undone, ', nl=False)
+
+        if not force and not click.confirm('are you sure you want to continue?'):
+            echo.echo_info(f'deleting of `{profile.name} cancelled.')
+            continue
+
+        get_config().delete_profile(
+            profile.name,
+            include_database=include_database,
+            include_database_user=include_database_user,
+            include_repository=include_repository
         )
+        echo.echo_success(f'profile `{profile.name}` was deleted{message_suffix}.')

aiida/manage/configuration/setup.py
@@ -1,137 +1,0 @@
-# -*- coding: utf-8 -*-
-###########################################################################
-# Copyright (c), The AiiDA team. All rights reserved.                     #
-# This file is part of the AiiDA code.                                    #
-#                                                                         #
-# The code is hosted on GitHub at https://github.com/aiidateam/aiida-core #
-# For further information on the license, see the LICENSE.txt file        #
-# For further information please visit http://www.aiida.net               #
-###########################################################################
-"""Module that defines methods required to setup a new AiiDA instance."""
-import os
-import urllib.parse
-
-import click
-
-from aiida.cmdline.utils import echo
-
-
-def delete_repository(profile, non_interactive=True):
-    """
-    Delete an AiiDA file repository associated with an AiiDA profile.
-
-    :param profile: AiiDA Profile
-    :type profile: :class:`aiida.manage.configuration.profile.Profile`
-    :param non_interactive: do not prompt for configuration values, fail if not all values are given as kwargs.
-    :type non_interactive: bool
-    """
-    repo_path = urllib.parse.urlparse(profile.repository_uri).path
-    repo_path = os.path.expanduser(repo_path)
-
-    if not os.path.isabs(repo_path):
-        echo.echo_info(f"Associated file repository '{repo_path}' does not exist.")
-        return
-
-    if not os.path.isdir(repo_path):
-        echo.echo_info(f"Associated file repository '{repo_path}' is not a directory.")
-        return
-
-    if non_interactive or click.confirm(
-        "Delete associated file repository '{}'?\n"
-        'WARNING: All data will be lost.'.format(repo_path)
-    ):
-        echo.echo_info(f"Deleting directory '{repo_path}'.")
-        import shutil
-        shutil.rmtree(repo_path)
-
-
-def delete_db(profile, non_interactive=True, verbose=False):
-    """
-    Delete an AiiDA database associated with an AiiDA profile.
-
-    :param profile: AiiDA Profile
-    :type profile: :class:`aiida.manage.configuration.profile.Profile`
-    :param non_interactive: do not prompt for configuration values, fail if not all values are given as kwargs.
-    :type non_interactive: bool
-    :param verbose: if True, print parameters of DB connection
-    :type verbose: bool
-    """
-    from aiida.manage.configuration import get_config
-    from aiida.manage.external.postgres import Postgres
-    from aiida.common import json
-
-    postgres = Postgres.from_profile(profile, interactive=not non_interactive, quiet=False)
-
-    if verbose:
-        echo.echo_info('Parameters used to connect to postgres:')
-        echo.echo(json.dumps(postgres.dbinfo, indent=4))
-
-    database_name = profile.database_name
-    if not postgres.db_exists(database_name):
-        echo.echo_info(f"Associated database '{database_name}' does not exist.")
-    elif non_interactive or click.confirm(
-        "Delete associated database '{}'?\n"
-        'WARNING: All data will be lost.'.format(database_name)
-    ):
-        echo.echo_info(f"Deleting database '{database_name}'.")
-        postgres.drop_db(database_name)
-
-    user = profile.database_username
-    config = get_config()
-    users = [available_profile.database_username for available_profile in config.profiles]
-
-    if not postgres.dbuser_exists(user):
-        echo.echo_info(f"Associated database user '{user}' does not exist.")
-    elif users.count(user) > 1:
-        echo.echo_info(
-            "Associated database user '{}' is used by other profiles "
-            'and will not be deleted.'.format(user)
-        )
-    elif non_interactive or click.confirm(f"Delete database user '{user}'?"):
-        echo.echo_info(f"Deleting user '{user}'.")
-        postgres.drop_dbuser(user)
-
-
-def delete_from_config(profile, non_interactive=True):
-    """
-    Delete an AiiDA profile from the config file.
-
-    :param profile: AiiDA Profile
-    :type profile: :class:`aiida.manage.configuration.profile.Profile`
-    :param non_interactive: do not prompt for configuration values, fail if not all values are given as kwargs.
-    :type non_interactive: bool
-    """
-    from aiida.manage.configuration import get_config
-
-    if non_interactive or click.confirm(
-        "Delete configuration for profile '{}'?\n"
-        'WARNING: Permanently removes profile from the list of AiiDA profiles.'.format(profile.name)
-    ):
-        echo.echo_info(f"Deleting configuration for profile '{profile.name}'.")
-        config = get_config()
-        config.remove_profile(profile.name)
-        config.store()
-
-
-def delete_profile(profile, non_interactive=True, include_db=True, include_repository=True, include_config=True):
-    """
-    Delete an AiiDA profile and AiiDA user.
-
-    :param profile: AiiDA profile
-    :type profile: :class:`aiida.manage.configuration.profile.Profile`
-    :param non_interactive: do not prompt for configuration values, fail if not all values are given as kwargs.
-    :param include_db: Include deletion of associated database
-    :type include_db: bool
-    :param include_repository: Include deletion of associated file repository
-    :type include_repository: bool
-    :param include_config: Include deletion of entry from AiiDA configuration file
-    :type include_config: bool
-    """
-    if include_db:
-        delete_db(profile, non_interactive)
-
-    if include_repository:
-        delete_repository(profile, non_interactive)
-
-    if include_config:
-        delete_from_config(profile, non_interactive)

aiida/orm/nodes/process/calculation/calcjob.py
@@ -418,7 +418,7 @@ class CalcJobNode(CalculationNode):
         if computer is None:
             raise exceptions.NotExistent('No computer has been set for this calculation')
 
-        return computer.get_authinfo(self.user)
+        return computer.get_authinfo(self.user)  # pylint: disable=no-member
 
     def get_transport(self) -> 'Transport':
         """Return the transport for this calculation.

aiida/repository/backend/abstract.py
@@ -40,6 +40,15 @@ class AbstractRepositoryBackend(metaclass=abc.ABCMeta):
     def is_initialised(self) -> bool:
         """Return whether the repository has been initialised."""
 
+    @abc.abstractmethod
+    def erase(self):
+        """Delete the repository itself and all its contents.
+
+        .. note:: This should not merely delete the contents of the repository but any resources it created. For
+            example, if the repository is essentially a folder on disk, the folder itself should also be deleted, not
+            just its contents.
+        """
+
     @staticmethod
     def is_readable_byte_stream(handle):
         return hasattr(handle, 'read') and hasattr(handle, 'mode') and 'b' in handle.mode

aiida/repository/backend/disk_object_store.py
@@ -2,6 +2,7 @@
 """Implementation of the ``AbstractRepositoryBackend`` using the ``disk-objectstore`` as the backend."""
 import contextlib
 import io
+import shutil
 import typing
 
 from disk_objectstore import Container
@@ -46,6 +47,13 @@ class DiskObjectStoreRepositoryBackend(AbstractRepositoryBackend):
     @property
     def container(self):
         return self._container
+
+    def erase(self):
+        """Delete the repository itself and all its contents."""
+        try:
+            shutil.rmtree(self.container.get_folder())
+        except FileNotFoundError:
+            pass
 
     def put_object_from_filelike(self, handle: io.BufferedIOBase) -> str:
         """Store the byte contents of a file in the repository.

aiida/repository/backend/sandbox.py
@@ -24,11 +24,7 @@ class SandboxRepositoryBackend(AbstractRepositoryBackend):
 
     def __del__(self):
         """Delete the entire sandbox folder if it was instantiated and still exists."""
-        if getattr(self, '_sandbox', None) is not None:
-            try:
-                shutil.rmtree(self.sandbox.abspath)
-            except FileNotFoundError:
-                pass
+        self.erase()
 
     @property
     def uuid(self) -> typing.Optional[str]:
@@ -61,6 +57,16 @@ class SandboxRepositoryBackend(AbstractRepositoryBackend):
             self._sandbox = SandboxFolder()
 
         return self._sandbox
+
+    def erase(self):
+        """Delete the repository itself and all its contents."""
+        if getattr(self, '_sandbox', None) is not None:
+            try:
+                shutil.rmtree(self.sandbox.abspath)
+            except FileNotFoundError:
+                pass
+            finally:
+                self._sandbox = None
 
     def put_object_from_filelike(self, handle: io.BufferedIOBase) -> str:
         """Store the byte contents of a file in the repository.

commit 3554f7ba864b36a858e804f368dfb205a4b5c956

`Config`: write config to disk at end of `delete_profile`
The `delete_profile` would successfully remove the profile from the
`Config` instance in memory, however, the changes would not be persisted
to disk. After reloading the config from disk, the deleted profile would
be back. A regression test is added for this behavior and the behavior
is fixed by calling `store` explictly at the end of the method, to
persist the new state to disk.

This regression was introduced in `e274ef2e065ac4aae03492b6736ae9fdea896ae4`
where the profile delete code was refactored and moved into the `Config`
class.

aiida/manage/configuration/config.py
@@ -372,6 +372,7 @@ class Config:  # pylint: disable=too-many-public-methods
             postgres.drop_dbuser(profile.database_username)
 
         self.remove_profile(name)
+        self.store()
 
     def set_default_profile(self, name, overwrite=False):
         """Set the given profile as the new default.

tests/manage/configuration/test_config.py
@@ -384,3 +384,23 @@ def test_store(config_with_profile):
         config_recreated = Config(config.filepath, json.load(handle))
 
         assert config.dictionary == config_recreated.dictionary
+
+
+def test_delete_profile(config_with_profile, profile_factory):
+    """Test the ``delete_profile`` method."""
+    config = config_with_profile
+    profile_name = 'to-be-deleted'
+    profile = profile_factory(name=profile_name)
+
+    config.add_profile(profile)
+    assert config.get_profile(profile_name) == profile
+
+    # Write the contents to disk so that the to-be-deleted profile is in the config file on disk
+    config.store()
+
+    config.delete_profile(profile_name)
+    assert profile_name not in config.profile_names
+
+    # Now reload the config from disk to make sure the changes after deletion were persisted to disk
+    config_on_disk = Config.from_file(config.filepath)
+    assert profile_name not in config_on_disk.profile_names
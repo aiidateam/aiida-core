014edffa31603c685f32d62a12f55eda9a1c45df
Sebastiaan Huber <mail@sphuber.net>
2021-09-19 15:15:01 +0200

    `Dict`: implement the `__eq__` method to compare plain dictionary
    The `__eq__` method is overridden to not only let a `Dict` node compare
    true to itself, but also to the plain dictionary that represents its
    value. That is to say, the following:
    
        node = Dict(dict={'a': 1'})
        assert node == node.value
    
    now passes the assert. This brings the behavior of `Dict` on par with
    all the other base types, `Bool`, `Float`, `Int`, `Str` and `Bool`.

--- tests/orm/data/test_dict.py
+++ tests/orm/data/test_dict.py
@@ -7,58 +7,91 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-"""Tests for the `Dict` class."""
+# pylint: disable=redefined-outer-name
+"""Tests for :class:`aiida.orm.nodes.data.dict.Dict` class."""
+import pytest
 
-from aiida.backends.testbase import AiidaTestCase
 from aiida.orm import Dict
 
 
-class TestDict(AiidaTestCase):
-    """Test for the `Dict` class."""
+@pytest.fixture
+def dictionary():
+    return {'value': 1, 'nested': {'dict': 'ionary'}}
 
-    @classmethod
-    def setUpClass(cls, *args, **kwargs):
-        super().setUpClass(*args, **kwargs)
-        cls.dictionary = {'value': 1, 'nested': {'dict': 'ionary'}}
-        cls.node = Dict(dict=cls.dictionary)
 
-    def test_keys(self):
-        """Test the `keys` method."""
-        self.assertEqual(sorted(self.node.keys()), sorted(self.dictionary.keys()))
+@pytest.mark.usefixtures('clear_database_before_test')
+def test_keys(dictionary):
+    """Test the ``keys`` method."""
+    node = Dict(dict=dictionary)
+    assert sorted(node.keys()) == sorted(dictionary.keys())
 
-    def test_get_dict(self):
-        """Test the `get_dict` method."""
-        self.assertEqual(self.node.get_dict(), self.dictionary)
 
-    def test_dict_property(self):
-        """Test the `dict` property."""
-        self.assertEqual(self.node.dict.value, self.dictionary['value'])
-        self.assertEqual(self.node.dict.nested, self.dictionary['nested'])
+@pytest.mark.usefixtures('clear_database_before_test')
+def test_get_dict(dictionary):
+    """Test the ``get_dict`` method."""
+    node = Dict(dict=dictionary)
+    assert node.get_dict() == dictionary
 
-    def test_get_item(self):
-        """Test the `__getitem__` method."""
-        self.assertEqual(self.node['value'], self.dictionary['value'])
-        self.assertEqual(self.node['nested'], self.dictionary['nested'])
 
-    def test_set_item(self):
-        """Test the methods for setting the item.
+@pytest.mark.usefixtures('clear_database_before_test')
+def test_dict_property(dictionary):
+    """Test the ``dict`` property."""
+    node = Dict(dict=dictionary)
+    assert node.dict.value == dictionary['value']
+    assert node.dict.nested == dictionary['nested']
 
-        * `__setitem__` directly on the node
-        * `__setattr__` through the `AttributeManager` returned by the `dict` property
-        """
-        self.node['value'] = 2
-        self.assertEqual(self.node['value'], 2)
-        self.node.dict.value = 3
-        self.assertEqual(self.node['value'], 3)
 
-    def test_correct_raises(self):
-        """Test that the methods for accessing the item raise the correct error.
+@pytest.mark.usefixtures('clear_database_before_test')
+def test_get_item(dictionary):
+    """Test the ``__getitem__`` method."""
+    node = Dict(dict=dictionary)
+    assert node['value'] == dictionary['value']
+    assert node['nested'] == dictionary['nested']
 
-        * `dictnode['inexistent']` should raise KeyError
-        * `dictnode.dict.inexistent` should raise AttributeError
-        """
-        with self.assertRaises(KeyError):
-            _ = self.node['inexistent_key']
 
-        with self.assertRaises(AttributeError):
-            _ = self.node.dict.inexistent_key
+@pytest.mark.usefixtures('clear_database_before_test')
+def test_set_item(dictionary):
+    """Test the methods for setting the item.
+
+    * ``__setitem__`` directly on the node
+    * ``__setattr__`` through the ``AttributeManager`` returned by the ``dict`` property
+    """
+    node = Dict(dict=dictionary)
+
+    node['value'] = 2
+    assert node['value'] == 2
+
+    node.dict.value = 3
+    assert node['value'] == 3
+
+
+@pytest.mark.usefixtures('clear_database_before_test')
+def test_correct_raises(dictionary):
+    """Test that the methods for accessing the item raise the correct error.
+
+    * ``node['inexistent']`` should raise ``KeyError``
+    * ``node.dict.inexistent`` should raise ``AttributeError``
+    """
+    node = Dict(dict=dictionary)
+
+    with pytest.raises(KeyError):
+        _ = node['inexistent_key']
+
+    with pytest.raises(AttributeError):
+        _ = node.dict.inexistent_key
+
+
+@pytest.mark.usefixtures('clear_database_before_test')
+def test_eq(dictionary):
+    """Test the ``__eq__`` method.
+
+    A node should compare equal to itself and to the plain dictionary that represents its value. However, it should not
+    compare equal to another node that has the same content. This is a hot issue and is being discussed in the following
+    ticket: https://github.com/aiidateam/aiida-core/issues/1917
+    """
+    node = Dict(dict=dictionary)
+    clone = Dict(dict=dictionary)
+
+    assert node is node  # pylint: disable=comparison-with-itself
+    assert node == dictionary
+    assert node != clone

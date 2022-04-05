0e2b45ca4977f5e2dd2ba08a0624de2fd11b403f
Chris Sewell <chrisj_sewell@hotmail.com>
2022-04-05 15:30:11 +0200

    🔧 MAINTAIN: Add `warn_deprecation` function

--- aiida/common/warnings.py
+++ aiida/common/warnings.py
@@ -8,6 +8,8 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """Define warnings that can be thrown by AiiDA."""
+import os
+import warnings
 
 
 class AiidaDeprecationWarning(Warning):
@@ -32,3 +34,17 @@ class AiidaTestWarning(Warning):
     """
     Class for warnings concerning the AiiDA testing infrastructure.
     """
+
+
+def warn_deprecation(message: str, version: int, stacklevel=2) -> None:
+    """Warns about a deprecation for a future aiida-core version.
+
+    Warnings are activated if the `AIIDA_WARN_v{major}` environment variable is set to `True`.
+
+    :param message: the message to be printed
+    :param version: the major version number of the future version
+    :param stacklevel: the stack level at which the warning is issued
+    """
+    if os.environ.get(f'AIIDA_WARN_v{version}'):
+        message = f'{message} (this will be removed in v{version})'
+        warnings.warn(message, AiidaDeprecationWarning, stacklevel=stacklevel)

0ac70b781a1bd403278502ab9cc0dfa0bd83a2c4
Sebastiaan Huber <mail@sphuber.net>
2022-05-23 22:31:16 +0200

    Engine: fix bug when caching from process with nested outputs (#5538)
    When the execution of a process is being skipped because it has a valid
    cache source and caching is enabled, the process would except if the
    process has nested output namespaces. Since the nesting cannot be
    literally preserved in the database, the nesting is representing by
    using double underscores between each level of nesting in the link
    label.
    
    The problem occurs when `Process._create_and_setup_db_record` would be
    called which would create the outputs of the cloned node, however, it
    would use these "collapsed" link labels containing the double
    underscores. Passing these to `self.out` would cause the validation to
    fail because the process port validation expects the actual nesting.
    
    The solution is to restore the database nesting representation to the
    one expected by `Process.out`, which amounts to replacing the namespace
    separators.
    
    Cherry-pick: b936da373499c63f4af4bcb9024500ffc9bf7f9e

---
+++ tests/engine/processes/test_caching.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""Test the caching functionality for a :class:`aiida.engine.processes.process.Process`."""
+from aiida.engine import Process, run
+from aiida.manage import enable_caching
+from aiida.orm import CalcJobNode, Int
+
+
+class NestedOutputsProcess(Process):
+    """Process with dynamic nested output namespace."""
+
+    _node_class = CalcJobNode
+
+    @classmethod
+    def define(cls, spec):
+        super().define(spec)
+        spec.input('a')
+        spec.output_namespace('nested', dynamic=True)
+
+    def run(self):
+        self.out('nested', {'a': self.inputs.a + 2})
+
+
+def test_caching_nested_output_namespace():
+    """Test that caching from a process with a nested output namespace works."""
+    _, node_original = run.get_node(NestedOutputsProcess, a=Int(1))
+    assert not node_original.is_created_from_cache
+
+    with enable_caching():
+        _, node_clone = run.get_node(NestedOutputsProcess, a=Int(1))
+
+    assert node_clone.is_created_from_cache
+    assert node_clone.get_cache_source() == node_original.uuid
+
+    outputs = node_clone.get_outgoing().nested()
+    assert list(outputs.keys()) == ['nested']
+    assert list(outputs['nested'].keys()) == ['a']
+    assert isinstance(outputs['nested']['a'], Int)

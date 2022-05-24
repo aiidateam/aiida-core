b98a56837cf2b87825668791b9cffdd074d396f8
Bonan Zhu <zhubonan@outlook.com>
2022-05-24 16:26:25 +0100

    Fix test typo

--- tests/cmdline/commands/test_storage.py
+++ tests/cmdline/commands/test_storage.py
@@ -148,7 +148,7 @@ def tests_storage_maintain_logging(run_cli_command, monkeypatch, caplog):
     with caplog.at_level(logging.INFO):
         _ = run_cli_command(cmd_storage.storage_maintain, user_input='Y', options=['--no-compress'])
 
-    message_list = caplog.records[0].msg.splitlines()
+    message_list = caplog.records[1].msg.splitlines()
     assert ' > full: False' in message_list
     assert ' > dry_run: False' in message_list
     assert ' > compress: False' in message_list

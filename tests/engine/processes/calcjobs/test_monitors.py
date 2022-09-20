307ffa639e5535df1fc0ca0e687d8b9a25fb3bdd
Sebastiaan Huber <mail@sphuber.net>
2022-09-20 18:11:01 +0200

    `CalcJobMonitor`: Add the `minimum_poll_interval` attribute
    The `minimum_poll_interval` is an optional positive integer that can be
    defined for a monitor. If defined, the engine will ensure that the
    interval between two successive calls of the monitor will at least be
    this long.
    
    In order to track the last time a monitor was called, the timestamp is
    added to the `call_timestamp` attribute when it is called by the
    `CalcJobMonitors.process` method.

--- tests/engine/processes/calcjobs/test_monitors.py
+++ tests/engine/processes/calcjobs/test_monitors.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """Tests for the :mod:`aiida.engine.processes.calcjobs.monitors` module."""
+import time
+
 import pytest
 
 from aiida.calculations.monitors import base
 from aiida.common.exceptions import EntryPointError
+from aiida.engine import run_get_node
 from aiida.engine.processes.calcjobs.monitors import CalcJobMonitor, CalcJobMonitors
-from aiida.orm import Dict
+from aiida.orm import Dict, Int
 
 
 def test_calc_job_monitor_constructor_invalid():
@@ -25,6 +28,12 @@ def test_calc_job_monitor_constructor_invalid():
     with pytest.raises(TypeError, match=r'Got object of type .*'):
         CalcJobMonitor(entry_point='core.always_kill', priority='one')
 
+    with pytest.raises(TypeError, match=r'Got object of type .*'):
+        CalcJobMonitor(entry_point='core.always_kill', minimum_poll_interval='one')
+
+    with pytest.raises(ValueError, match=r'The `minimum_poll_interval` must be a positive integer greater than zero.'):
+        CalcJobMonitor(entry_point='core.always_kill', minimum_poll_interval=-1)
+
     with pytest.raises(ValueError, match=r'The monitor `core.always_kill` does not accept the keywords.*'):
         CalcJobMonitor(entry_point='core.always_kill', kwargs={'unsupported': 1})
 
@@ -39,6 +48,8 @@ def test_calc_job_monitor_constructor_valid():
     assert monitor.entry_point == entry_point
     assert monitor.kwargs == {}
     assert monitor.priority == 0
+    assert monitor.minimum_poll_interval is None
+    assert monitor.call_timestamp is None
 
 
 def test_calc_job_monitor_load_entry_point():
@@ -66,3 +77,52 @@ def test_calc_job_monitors_monitors(monitors, expected):
         monitors_full[key] = Dict(monitors_full[key])
 
     assert list(CalcJobMonitors(monitors_full).monitors.keys()) == expected
+
+
+def test_calc_job_monitors_process_poll_interval(monkeypatch):
+    """Test the :meth:`aiida.engine.processes.calcjobs.monitors.CalcJobMonitors.process` method.
+
+    Test that the ``minimum_poll_interval`` of the monitors is respected.
+    """
+    monitors = CalcJobMonitors({'always_kill': Dict({'entry_point': 'core.always_kill', 'minimum_poll_interval': 1})})
+
+    def always_kill(*args, **kwargs):  # pylint: disable=unused-argument
+        return 'always_kill called'
+
+    monkeypatch.setattr(base, 'always_kill', always_kill)
+
+    # First call should simple go through and so raise
+    assert monitors.process(None, None) == 'Monitor `core.always_kill` killed job with message: always_kill called'
+
+    # Calling again should skip it since the minimum poll interval has not yet passed
+    assert monitors.process(None, None) is None
+
+    time.sleep(1)
+
+    # After the intervalhas passed, it should be called again
+    assert monitors.process(None, None) == 'Monitor `core.always_kill` killed job with message: always_kill called'
+
+
+def monitor_emit_warning(node, transport, **kwargs):  # pylint: disable=unused-argument
+    """Test monitor that logs a warning when called."""
+    from aiida.common.log import AIIDA_LOGGER
+    AIIDA_LOGGER.warning('monitor_emit_warning monitor was called')
+
+
+def test_calc_job_monitors_process_poll_interval_integrated(entry_points, aiida_local_code_factory, aiida_caplog):
+    """Test the ``minimum_poll_interval`` input by actually running through the engine."""
+    entry_points.add(monitor_emit_warning, 'aiida.calculations.monitors:core.emit_warning')
+
+    code = aiida_local_code_factory('core.arithmetic.add', '/bin/bash')
+    builder = code.get_builder()
+    builder.x = Int(1)
+    builder.y = Int(1)
+    builder.monitors = {'always_kill': Dict({'entry_point': 'core.emit_warning', 'minimum_poll_interval': 5})}
+    builder.metadata = {'options': {'sleep': 1, 'resources': {'num_machines': 1}}}
+
+    _, node = run_get_node(builder)
+    assert node.is_finished_ok
+
+    # Check that the number of log messages emitted by the monitor is just 1 as it should have been called just once.
+    logs = [rec.message for rec in aiida_caplog.records if rec.message == 'monitor_emit_warning monitor was called']
+    assert len(logs) == 1

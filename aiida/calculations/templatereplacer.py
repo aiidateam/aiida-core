d0fc4c4ac36116b5120a083ecc5f8cd1f6a9d4fe
Jusong Yu <jusong.yeu@gmail.com>
2022-09-27 01:26:14 +0200

    f-me

--- aiida/calculations/templatereplacer.py
+++ aiida/calculations/templatereplacer.py
@@ -63,7 +63,7 @@ class TemplatereplacerCalculation(CalcJob):
     def define(cls, spec):
         # yapf: disable
         super().define(spec)
-        spec.inputs['metadata']['options']['parser_name'].default = 'core.templatereplacer.doubler'
+        spec.inputs['metadata']['options']['parser_name'].default = 'core.templatereplacer'
         spec.input('template', valid_type=orm.Dict,
             help='A template for the input file.')
         spec.input('parameters', valid_type=orm.Dict, required=False,

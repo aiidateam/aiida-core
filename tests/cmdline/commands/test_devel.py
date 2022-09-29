51ba7c6e5f62e90143e5ca7b8462dd9ed0c5348d
Sebastiaan Huber <mail@sphuber.net>
2022-09-29 21:36:40 +0200

    CLI: Add the `verdi devel revive` command
    This method exposed the newly added `revive_processes` method from the
    process control module. It is put under `verdi devel` since this command
    should be used prudently and as a last resort, when the cause of the
    problem is known with high certainty, because incorrect use of the
    command can create bigger problems.

--- tests/cmdline/commands/test_devel.py
+++ tests/cmdline/commands/test_devel.py
@@ -8,8 +8,13 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """Tests for ``verdi devel``."""
+from plumpy.process_comms import RemoteProcessThreadController
+from plumpy.process_states import ProcessState
+import pytest
+
 from aiida.cmdline.commands import cmd_devel
-from aiida.orm import Node
+from aiida.engine import submit
+from aiida.orm import Int, Node
 
 
 def test_run_sql(run_cli_command):
@@ -17,3 +22,37 @@ def test_run_sql(run_cli_command):
     options = ['SELECT COUNT(*) FROM db_dbnode;']
     result = run_cli_command(cmd_devel.devel_run_sql, options)
     assert str(Node.collection.count()) in result.output, result.output
+
+
+@pytest.mark.usefixtures('stopped_daemon_client')
+def test_revive_without_daemon(run_cli_command):
+    """Test that ``verdi devel revive`` fails if no daemon is running."""
+    assert run_cli_command(cmd_devel.devel_revive, raises=True)
+
+
+@pytest.mark.usefixtures('aiida_profile', 'started_daemon_client')
+def test_revive(run_cli_command, monkeypatch, aiida_local_code_factory, submit_and_await):
+    """Test ``verdi devel revive``."""
+    code = aiida_local_code_factory('core.arithmetic.add', '/bin/bash')
+    builder = code.get_builder()
+    builder.x = Int(1)
+    builder.y = Int(1)
+    builder.metadata.options.resources = {'num_machines': 1}
+
+    # Temporarily patch the ``RemoteProcessThreadController.continue_process`` method to do nothing and just return a
+    # completed future. This ensures that the submission creates a process node in the database but no task is sent to
+    # RabbitMQ and so the daemon will not start running it.
+    with monkeypatch.context() as context:
+        context.setattr(RemoteProcessThreadController, 'continue_process', lambda *args, **kwargs: None)
+        node = submit(builder)
+
+    # The node should now be created in the database but "stuck"
+    assert node.process_state == ProcessState.CREATED
+
+    # Time to revive it by recreating the task and send it to RabbitMQ
+    run_cli_command(cmd_devel.devel_revive, [str(node.pk), '--force'])
+
+    # Wait for the process to be picked up by the daemon and completed. If there is a problem with the code, this call
+    # should timeout and raise an exception
+    submit_and_await(node, ProcessState.FINISHED)
+    assert node.is_finished_ok

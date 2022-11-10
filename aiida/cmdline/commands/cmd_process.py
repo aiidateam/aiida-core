aiidateam/aiida-core

commit 8bb7b343198370e1d23983f3e8b7dfb5b50304df

API: Add the `aiida.engine.processes.control` module
This module abstracts functionality that was implemented directly in the
`aiida.cmdline.commands.cmd_process` module that allows to interact with
active processes. By extracting it to a separate module, this
functionality can now also be easily used by users from interfaces other
than the CLI, such as the shell, or a notebook.

aiida/cmdline/commands/cmd_process.py
@@ -10,7 +10,6 @@
 # pylint: disable=too-many-arguments
 """`verdi process` command."""
 import click
-from kiwipy import communications
 
 from aiida.cmdline.commands.cmd_verdi import verdi
 from aiida.cmdline.params import arguments, options, types
@@ -193,24 +192,12 @@ def process_status(processes):
 @decorators.only_if_daemon_running(echo.echo_warning, 'daemon is not running, so process may not be reachable')
 def process_kill(processes, timeout, wait):
     """Kill running processes."""
+    from aiida.engine.processes import control
 
-    controller = get_manager().get_process_controller()
-
-    futures = {}
-    for process in processes:
-
-        if process.is_terminated:
-            echo.echo_error(f'Process<{process.pk}> is already terminated')
-            continue
-
-        try:
-            future = controller.kill_process(process.pk, msg='Killed through `verdi process kill`')
-        except communications.UnroutableError:
-            echo.echo_error(f'Process<{process.pk}> is unreachable')
-        else:
-            futures[future] = process
-
-    process_actions(futures, 'kill', 'killing', 'killed', wait, timeout)
+    try:
+        control.kill_processes(processes, timeout=timeout, wait=wait)
+    except control.ProcessTimeoutException as exception:
+        echo.echo_critical(str(exception))
 
 
 @verdi_process.command('pause')
@@ -222,33 +209,15 @@ def process_kill(processes, timeout, wait):
 @decorators.only_if_daemon_running(echo.echo_warning, 'daemon is not running, so process may not be reachable')
 def process_pause(processes, all_entries, timeout, wait):
     """Pause running processes."""
-    from aiida.orm import ProcessNode, QueryBuilder
-
-    controller = get_manager().get_process_controller()
+    from aiida.engine.processes import control
 
     if processes and all_entries:
         raise click.BadOptionUsage('all', 'cannot specify individual processes and the `--all` flag at the same time.')
 
-    if not processes and all_entries:
-        active_states = options.active_process_states()
-        builder = QueryBuilder().append(ProcessNode, filters={'attributes.process_state': {'in': active_states}})
-        processes = builder.all(flat=True)
-
-    futures = {}
-    for process in processes:
-
-        if process.is_terminated:
-            echo.echo_error(f'Process<{process.pk}> is already terminated')
-            continue
-
-        try:
-            future = controller.pause_process(process.pk, msg='Paused through `verdi process pause`')
-        except communications.UnroutableError:
-            echo.echo_error(f'Process<{process.pk}> is unreachable')
-        else:
-            futures[future] = process
-
-    process_actions(futures, 'pause', 'pausing', 'paused', wait, timeout)
+    try:
+        control.pause_processes(processes, all_entries=all_entries, timeout=timeout, wait=wait)
+    except control.ProcessTimeoutException as exception:
+        echo.echo_critical(str(exception))
 
 
 @verdi_process.command('play')
@@ -260,34 +229,15 @@ def process_pause(processes, all_entries, timeout, wait):
 @decorators.only_if_daemon_running(echo.echo_warning, 'daemon is not running, so process may not be reachable')
 def process_play(processes, all_entries, timeout, wait):
     """Play (unpause) paused processes."""
-    from aiida.orm import ProcessNode, QueryBuilder
-    from aiida.tools.query.calculation import CalculationQueryBuilder
-
-    controller = get_manager().get_process_controller()
+    from aiida.engine.processes import control
 
     if processes and all_entries:
         raise click.BadOptionUsage('all', 'cannot specify individual processes and the `--all` flag at the same time.')
 
-    if not processes and all_entries:
-        filters = CalculationQueryBuilder().get_filters(process_state=('created', 'waiting', 'running'), paused=True)
-        builder = QueryBuilder().append(ProcessNode, filters=filters)
-        processes = builder.all(flat=True)
-
-    futures = {}
-    for process in processes:
-
-        if process.is_terminated:
-            echo.echo_error(f'Process<{process.pk}> is already terminated')
-            continue
-
-        try:
-            future = controller.play_process(process.pk)
-        except communications.UnroutableError:
-            echo.echo_error(f'Process<{process.pk}> is unreachable')
-        else:
-            futures[future] = process
-
-    process_actions(futures, 'play', 'playing', 'played', wait, timeout)
+    try:
+        control.play_processes(processes, all_entries=all_entries, timeout=timeout, wait=wait)
+    except control.ProcessTimeoutException as exception:
+        echo.echo_critical(str(exception))
 
 
 @verdi_process.command('watch')
@@ -335,79 +285,3 @@ def process_watch(processes):
 
         # Reraise to trigger clicks builtin abort sequence
         raise
-
-
-def process_actions(futures_map, infinitive, present, past, wait=False, timeout=None):
-    """
-    Process the requested actions sent to a set of Processes given a map of the futures and the
-    corresponding processes.  This function will echo the correct information strings based on
-    the outcomes of the futures and the given verb conjugations.  You can optionally wait for
-    any pending actions to be completed before the functions returns and use a timeout to put
-    a maximum wait time on the actions.
-
-    :param futures_map: the map of action futures and the corresponding processes
-    :type futures_map: dict
-    :param infinitive: the infinitive form of the action verb
-    :type infinitive: str
-    :param present: the present tense form
-    :type present: str
-    :param past: the past tense form
-    :type past: str
-    :param wait: if True, waits for pending actions to be competed, otherwise just returns
-    :type wait: bool
-    :param timeout: the timeout (in seconds) to wait for actions to be completed
-    :type timeout: float
-    """
-    # pylint: disable=too-many-branches
-    from concurrent import futures
-
-    import kiwipy
-    from plumpy.futures import unwrap_kiwi_future
-
-    from aiida.manage.external.rmq import CommunicationTimeout
-
-    scheduled = {}
-    try:
-        for future in futures.as_completed(futures_map.keys(), timeout=timeout):
-            # Get the corresponding process
-            process = futures_map[future]
-
-            try:
-                # unwrap is need here since LoopCommunicator will also wrap a future
-                future = unwrap_kiwi_future(future)
-                result = future.result()
-            except CommunicationTimeout:
-                echo.echo_error(f'call to {infinitive} Process<{process.pk}> timed out')
-            except Exception as exception:  # pylint: disable=broad-except
-                echo.echo_error(f'failed to {infinitive} Process<{process.pk}>: {exception}')
-            else:
-                if result is True:
-                    echo.echo_success(f'{past} Process<{process.pk}>')
-                elif result is False:
-                    echo.echo_error(f'problem {present} Process<{process.pk}>')
-                elif isinstance(result, kiwipy.Future):
-                    echo.echo_success(f'scheduled {infinitive} Process<{process.pk}>')
-                    scheduled[result] = process
-                else:
-                    echo.echo_error(f'got unexpected response when {present} Process<{process.pk}>: {result}')
-
-        if wait and scheduled:
-            echo.echo_report(f"waiting for process(es) {','.join([str(proc.pk) for proc in scheduled.values()])}")
-
-            for future in futures.as_completed(scheduled.keys(), timeout=timeout):
-                process = scheduled[future]
-
-                try:
-                    result = future.result()
-                except Exception as exception:  # pylint: disable=broad-except
-                    echo.echo_error(f'failed to {infinitive} Process<{process.pk}>: {exception}')
-                else:
-                    if result is True:
-                        echo.echo_success(f'{past} Process<{process.pk}>')
-                    elif result is False:
-                        echo.echo_error(f'problem {present} Process<{process.pk}>')
-                    else:
-                        echo.echo_error(f'got unexpected response when {present} Process<{process.pk}>: {result}')
-
-    except futures.TimeoutError:
-        echo.echo_error(f'timed out trying to {infinitive} processes {futures_map.values()}')

aiida/engine/processes/control.py
@@ -1,0 +1,249 @@
+# -*- coding: utf-8 -*-
+"""Functions to control and interact with running processes."""
+from __future__ import annotations
+
+import concurrent
+import typing as t
+
+import kiwipy
+from kiwipy import communications
+from plumpy.futures import unwrap_kiwi_future
+
+from aiida.common.exceptions import AiidaException
+from aiida.common.log import AIIDA_LOGGER
+from aiida.engine.daemon.client import DaemonException, get_daemon_client
+from aiida.manage.external.rmq import CommunicationTimeout
+from aiida.manage.manager import get_manager
+from aiida.orm import ProcessNode, QueryBuilder
+from aiida.tools.query.calculation import CalculationQueryBuilder
+
+LOGGER = AIIDA_LOGGER.getChild('process_control')
+
+
+class ProcessTimeoutException(AiidaException):
+    """Raised when action to communicate with a process times out."""
+
+
+def get_active_processes(paused: bool = False) -> list[ProcessNode]:
+    """Return all active processes, i.e., those with a process state of created, waiting or running.
+
+    :param paused: Boolean, if True, filter for processes that are paused.
+    :return: A list of process nodes of active processes.
+    """
+    filters = CalculationQueryBuilder().get_filters(process_state=('created', 'waiting', 'running'), paused=paused)
+
+    builder = QueryBuilder().append(ProcessNode, filters=filters)
+    return builder.all(flat=True)
+
+
+def play_processes(
+    processes: list[ProcessNode] | None = None,
+    *,
+    all_entries: bool = False,
+    timeout: float = 5.0,
+    wait: bool = False
+) -> None:
+    """Play (unpause) paused processes.
+
+    .. note:: Requires the daemon to be running, or processes will be unresponsive.
+
+    :param processes: List of processes to play.
+    :param all_entries: Play all paused processes.
+    :param timeout: Raise a ``ProcessTimeoutException`` if the process does not respond within this amount of seconds.
+    :param wait: Set to ``True`` to wait for process response, for ``False`` the action is fire-and-forget.
+    :raises ``ProcessTimeoutException``: If the processes do not respond within the timeout.
+    """
+    if not get_daemon_client().is_daemon_running:
+        raise DaemonException('The daemon is not running.')
+
+    if processes and all_entries:
+        raise ValueError('cannot specify processes when `all_entries = True`.')
+
+    if not processes and all_entries:
+        processes = get_active_processes(paused=True)
+
+    if not processes:
+        LOGGER.report('no active processes selected.')
+        return
+
+    controller = get_manager().get_process_controller()
+    _perform_actions(processes, controller.play_process, 'play', 'playing', 'played', timeout, wait)
+
+
+def pause_processes(
+    processes: list[ProcessNode] | None = None,
+    *,
+    all_entries: bool = False,
+    timeout: float = 5.0,
+    wait: bool = False
+) -> None:
+    """Pause running processes.
+
+    .. note:: Requires the daemon to be running, or processes will be unresponsive.
+
+    :param processes: List of processes to play.
+    :param all_entries: Pause all playing processes.
+    :param timeout: Raise a ``ProcessTimeoutException`` if the process does not respond within this amount of seconds.
+    :param wait: Set to ``True`` to wait for process response, for ``False`` the action is fire-and-forget.
+    :raises ``ProcessTimeoutException``: If the processes do not respond within the timeout.
+    """
+    if not get_daemon_client().is_daemon_running:
+        raise DaemonException('The daemon is not running.')
+
+    if processes and all_entries:
+        raise ValueError('cannot specify processes when `all_entries = True`.')
+
+    if not processes and all_entries:
+        processes = get_active_processes()
+
+    if not processes:
+        LOGGER.report('no active processes selected.')
+        return
+
+    controller = get_manager().get_process_controller()
+    _perform_actions(processes, controller.pause_process, 'pause', 'pausing', 'paused', timeout, wait)
+
+
+def kill_processes(
+    processes: list[ProcessNode] | None = None,
+    *,
+    all_entries: bool = False,
+    timeout: float = 5.0,
+    wait: bool = False
+) -> None:
+    """Kill running processes.
+
+    .. note:: Requires the daemon to be running, or processes will be unresponsive.
+
+    :param processes: List of processes to play.
+    :param all_entries: Kill all active processes.
+    :param timeout: Raise a ``ProcessTimeoutException`` if the process does not respond within this amount of seconds.
+    :param wait: Set to ``True`` to wait for process response, for ``False`` the action is fire-and-forget.
+    :raises ``ProcessTimeoutException``: If the processes do not respond within the timeout.
+    """
+    if not get_daemon_client().is_daemon_running:
+        raise DaemonException('The daemon is not running.')
+
+    if processes and all_entries:
+        raise ValueError('cannot specify processes when `all_entries = True`.')
+
+    if not processes and all_entries:
+        processes = get_active_processes()
+
+    if not processes:
+        LOGGER.report('no active processes selected.')
+        return
+
+    controller = get_manager().get_process_controller()
+    _perform_actions(processes, controller.kill_process, 'kill', 'killing', 'killed', timeout, wait)
+
+
+def _perform_actions(
+    processes: list[ProcessNode],
+    action: t.Callable,
+    infinitive: str,
+    present: str,
+    past: str,
+    timeout: float = None,
+    wait: bool = False
+) -> None:
+    """Perform an action on a list of processes.
+
+    :param processes: The list of processes to perform the action on.
+    :param action: The action to perform.
+    :param infinitive: The infinitive of the verb that represents the action.
+    :param present: The present tense of the verb that represents the action.
+    :param past: The past tense of the verb that represents the action.
+    :param timeout: Raise a ``ProcessTimeoutException`` if the process does not respond within this amount of seconds.
+    :param wait: Set to ``True`` to wait for process response, for ``False`` the action is fire-and-forget.
+    :raises ``ProcessTimeoutException``: If the processes do not respond within the timeout.
+    """
+    futures = {}
+
+    for process in processes:
+
+        if process.is_terminated:
+            LOGGER.error(f'Process<{process.pk}> is already terminated.')
+            continue
+
+        try:
+            future = action(process.pk)
+        except communications.UnroutableError:
+            LOGGER.error(f'Process<{process.pk}> is unreachable.')
+        else:
+            futures[future] = process
+
+    _resolve_futures(futures, infinitive, present, past, wait, timeout)
+
+
+def _resolve_futures(
+    futures: dict[concurrent.futures.Future, ProcessNode],
+    infinitive: str,
+    present: str,
+    past: str,
+    wait: bool = False,
+    timeout: float = None
+) -> None:
+    """Process a mapping of futures representing an action on an active process.
+
+    This function will echo the correct information strings based on the outcomes of the futures and the given verb
+    conjugations. You can optionally wait for any pending actions to be completed before the functions returns and use a
+    timeout to put a maximum wait time on the actions.
+
+    :param futures: The map of action futures and the corresponding processes.
+    :param infinitive: The infinitive form of the action verb.
+    :param present: The present tense form of the action verb.
+    :param past: The past tense form of the action verb.
+    :param wait: Set to ``True`` to wait for process response, for ``False`` the action is fire-and-forget.
+    :param timeout: Raise a ``ProcessTimeoutException`` if the process does not respond within this amount of seconds.
+    """
+    scheduled = {}
+
+    def handle_result(result):
+        if result is True:
+            LOGGER.report(f'{past} Process<{process.pk}>')
+        elif result is False:
+            LOGGER.error(f'problem {present} Process<{process.pk}>')
+        elif isinstance(result, kiwipy.Future):
+            LOGGER.report(f'scheduled {infinitive} Process<{process.pk}>')
+            scheduled[result] = process
+        else:
+            LOGGER.error(f'got unexpected response when {present} Process<{process.pk}>: {result}')
+
+    try:
+        for future in concurrent.futures.as_completed(futures.keys(), timeout=timeout):
+
+            process = futures[future]
+
+            try:
+                # unwrap is need here since LoopCommunicator will also wrap a future
+                future = unwrap_kiwi_future(future)
+                result = future.result()
+            except CommunicationTimeout:
+                LOGGER.error(f'call to {infinitive} Process<{process.pk}> timed out')
+            except Exception as exception:  # pylint: disable=broad-except
+                LOGGER.error(f'failed to {infinitive} Process<{process.pk}>: {exception}')
+            else:
+                if isinstance(result, kiwipy.Future):
+                    LOGGER.report(f'scheduled {infinitive} Process<{process.pk}>')
+                    scheduled[result] = process
+                else:
+                    handle_result(result)
+
+        if not wait or not scheduled:
+            return
+
+        LOGGER.report(f"waiting for process(es) {','.join([str(proc.pk) for proc in scheduled.values()])}")
+
+        for future in concurrent.futures.as_completed(scheduled.keys(), timeout=timeout):
+            process = scheduled[future]
+
+            try:
+                result = future.result()
+            except Exception as exception:  # pylint: disable=broad-except
+                LOGGER.error(f'failed to {infinitive} Process<{process.pk}>: {exception}')
+            else:
+                handle_result(result)
+
+    except concurrent.futures.TimeoutError:
+        raise ProcessTimeoutException(f'timed out trying to {infinitive} processes {futures.values()}')

tests/engine/processes/test_control.py
@@ -1,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""Tests for the :mod:`aiida.engine.processes.control` module."""
+import pytest
+
+from aiida.engine.daemon.client import DaemonException
+from aiida.engine.processes import control
+from tests.utils.processes import WaitProcess
+
+
+@pytest.mark.usefixtures('started_daemon_client')
+@pytest.mark.parametrize('action', (control.pause_processes, control.play_processes, control.kill_processes))
+def test_processes_all_exclusivity(submit_and_await, action):
+    """Test that control methods raise if both ``processes`` is specified and ``all_entries=True``."""
+    node = submit_and_await(WaitProcess)
+    assert not node.paused
+
+    with pytest.raises(ValueError, match='cannot specify processes when `all_entries = True`.'):
+        action([node], all_entries=True)
+
+
+@pytest.mark.usefixtures('stopped_daemon_client')
+@pytest.mark.parametrize('action', (control.pause_processes, control.play_processes, control.kill_processes))
+def test_daemon_not_running(action):
+    """Test that control methods raise if the daemon is not running."""
+    with pytest.raises(DaemonException, match='The daemon is not running.'):
+        action(all_entries=True)
+
+
+@pytest.mark.usefixtures('started_daemon_client')
+def test_pause_processes(submit_and_await):
+    """Test :func:`aiida.engine.processes.control.pause_processes`."""
+    node = submit_and_await(WaitProcess)
+    assert not node.paused
+
+    control.pause_processes([node], wait=True)
+    assert node.paused
+
+
+@pytest.mark.usefixtures('started_daemon_client')
+def test_pause_processes_all_entries(submit_and_await):
+    """Test :func:`aiida.engine.processes.control.pause_processes` with ``all_entries=True``."""
+    node = submit_and_await(WaitProcess)
+    assert not node.paused
+
+    control.pause_processes(all_entries=True, wait=True)
+    assert node.paused
+
+
+@pytest.mark.usefixtures('started_daemon_client')
+def test_play_processes(submit_and_await):
+    """Test :func:`aiida.engine.processes.control.play_processes`."""
+    node = submit_and_await(WaitProcess)
+    assert not node.paused
+
+    control.pause_processes([node], wait=True)
+    assert node.paused
+
+    control.play_processes([node], wait=True)
+    assert not node.paused
+
+
+@pytest.mark.usefixtures('started_daemon_client')
+def test_play_processes_all_entries(submit_and_await):
+    """Test :func:`aiida.engine.processes.control.play_processes` with ``all_entries=True``."""
+    node = submit_and_await(WaitProcess)
+    assert not node.paused
+
+    control.pause_processes([node], wait=True)
+    assert node.paused
+
+    control.play_processes(all_entries=True, wait=True)
+    assert not node.paused
+
+
+@pytest.mark.usefixtures('started_daemon_client')
+def test_kill_processes(submit_and_await):
+    """Test :func:`aiida.engine.processes.control.kill_processes`."""
+    node = submit_and_await(WaitProcess)
+
+    control.kill_processes([node], wait=True)
+    assert node.is_terminated
+    assert node.is_killed
+
+
+@pytest.mark.usefixtures('started_daemon_client')
+def test_kill_processes_all_entries(submit_and_await):
+    """Test :func:`aiida.engine.processes.control.kill_processes` with ``all_entries=True``."""
+    node = submit_and_await(WaitProcess)
+
+    control.kill_processes(all_entries=True, wait=True)
+    assert node.is_terminated
+    assert node.is_killed

docs/source/conf.py
@@ -242,6 +242,7 @@ autodoc_aliases_typing = {
     'QueryBuilder': 'aiida.orm.querybuilder.QueryBuilder',
     'WorkChainNode': 'aiida.orm.nodes.process.workflow.workchain.WorkChainNode',
     'orm.ProcessNode': 'aiida.orm.nodes.process.process.ProcessNode',
+    'ProcessNode': 'aiida.orm.nodes.process.process.ProcessNode',
     'BackendAuthInfo': 'aiida.orm.implementation.authinfos.BackendAuthInfo',
     'BackendComment': 'aiida.orm.implementation.comments.BackendComment',
     'BackendComputer': 'aiida.orm.implementation.computers.BackendComputer',

commit d99bb9edeafe8897b797d4355d6ac9d3b3103823

Process control: Reinstate process status for paused/killed processes
The recent commit 8bb7b343198370e1d23983f3e8b7dfb5b50304df accidentally
removed the `process_status` being set on nodes when they were being
paused or killed through `verdi process pause/kill`.

This behavior is reinstated with tests to prevent future regression. The
process status is now also set when done directly through the Python API.

aiida/cmdline/commands/cmd_process.py
@@ -198,7 +198,8 @@ def process_kill(processes, timeout, wait):
     from aiida.engine.processes import control
 
     try:
-        control.kill_processes(processes, timeout=timeout, wait=wait)
+        message = 'Killed through `verdi process kill`'
+        control.kill_processes(processes, timeout=timeout, wait=wait, message=message)
     except control.ProcessTimeoutException as exception:
         echo.echo_critical(str(exception) + '\nFrom the CLI you can call `verdi devel revive <PID>`.')
 
@@ -218,7 +219,8 @@ def process_pause(processes, all_entries, timeout, wait):
         raise click.BadOptionUsage('all', 'cannot specify individual processes and the `--all` flag at the same time.')
 
     try:
-        control.pause_processes(processes, all_entries=all_entries, timeout=timeout, wait=wait)
+        message = 'Paused through `verdi process pause`'
+        control.pause_processes(processes, all_entries=all_entries, timeout=timeout, wait=wait, message=message)
     except control.ProcessTimeoutException as exception:
         echo.echo_critical(str(exception) + '\nFrom the CLI you can call `verdi devel revive <PID>`.')
 

aiida/engine/processes/control.py
@@ -112,6 +112,7 @@ def play_processes(
 def pause_processes(
     processes: list[ProcessNode] | None = None,
     *,
+    message: str = 'Paused through `aiida.engine.processes.control.pause_processes`',
     all_entries: bool = False,
     timeout: float = 5.0,
     wait: bool = False
@@ -140,12 +141,13 @@ def pause_processes(
         return
 
     controller = get_manager().get_process_controller()
-    _perform_actions(processes, controller.pause_process, 'pause', 'pausing', 'paused', timeout, wait)
+    _perform_actions(processes, controller.pause_process, 'pause', 'pausing', 'paused', timeout, wait, msg=message)
 
 
 def kill_processes(
     processes: list[ProcessNode] | None = None,
     *,
+    message: str = 'Killed through `aiida.engine.processes.control.kill_processes`',
     all_entries: bool = False,
     timeout: float = 5.0,
     wait: bool = False
@@ -174,7 +176,7 @@ def kill_processes(
         return
 
     controller = get_manager().get_process_controller()
-    _perform_actions(processes, controller.kill_process, 'kill', 'killing', 'killed', timeout, wait)
+    _perform_actions(processes, controller.kill_process, 'kill', 'killing', 'killed', timeout, wait, msg=message)
 
 
 def _perform_actions(
@@ -184,7 +186,8 @@ def _perform_actions(
     present: str,
     past: str,
     timeout: float = None,
-    wait: bool = False
+    wait: bool = False,
+    **kwargs: t.Any
 ) -> None:
     """Perform an action on a list of processes.
 
@@ -195,6 +198,7 @@ def _perform_actions(
     :param past: The past tense of the verb that represents the action.
     :param timeout: Raise a ``ProcessTimeoutException`` if the process does not respond within this amount of seconds.
     :param wait: Set to ``True`` to wait for process response, for ``False`` the action is fire-and-forget.
+    :param kwargs: Keyword arguments that will be passed to the method ``action``.
     :raises ``ProcessTimeoutException``: If the processes do not respond within the timeout.
     """
     futures = {}
@@ -206,7 +210,7 @@ def _perform_actions(
             continue
 
         try:
-            future = action(process.pk)
+            future = action(process.pk, **kwargs)
         except communications.UnroutableError:
             LOGGER.error(f'Process<{process.pk}> is unreachable.')
         else:

aiida/orm/nodes/data/code/legacy.py
@@ -22,7 +22,7 @@ __all__ = ('Code',)
 
 warn_deprecation(
     'The `Code` class is deprecated. To create an instance, use the `aiida.orm.nodes.data.code.installed.InstalledCode`'
-    ' or `aiida.orm.nodes.data.code.portable.PortableCode` for a "remote" or "local" code, respetively. If you are '
+    ' or `aiida.orm.nodes.data.code.portable.PortableCode` for a "remote" or "local" code, respectively. If you are '
     'using this class to compare type, e.g. in `isinstance`, use `aiida.orm.nodes.data.code.abstract.AbstractCode`.',
     version=3
 )

tests/cmdline/commands/test_process.py
@@ -452,6 +452,8 @@ def test_process_kill(submit_and_await, run_cli_command):
 
     run_cli_command(cmd_process.process_pause, [str(node.pk), '--wait'])
     await_condition(lambda: node.paused)
+    assert node.process_status == 'Paused through `verdi process pause`'
 
     run_cli_command(cmd_process.process_kill, [str(node.pk), '--wait'])
     await_condition(lambda: node.is_killed)
+    assert node.process_status == 'Killed through `verdi process kill`'

tests/engine/processes/test_control.py
@@ -38,6 +38,7 @@ def test_pause_processes(submit_and_await):
 
     control.pause_processes([node], wait=True)
     assert node.paused
+    assert node.process_status == 'Paused through `aiida.engine.processes.control.pause_processes`'
 
 
 @pytest.mark.usefixtures('started_daemon_client')
@@ -84,6 +85,7 @@ def test_kill_processes(submit_and_await):
     control.kill_processes([node], wait=True)
     assert node.is_terminated
     assert node.is_killed
+    assert node.process_status == 'Killed through `aiida.engine.processes.control.kill_processes`'
 
 
 @pytest.mark.usefixtures('started_daemon_client')
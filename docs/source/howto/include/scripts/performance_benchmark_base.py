f2c0f2c9258547356467f7e8b320d2cc6be4685b
Sebastiaan Huber <mail@sphuber.net>
2022-12-19 12:21:25 +0100

    Docs: Add `--without-daemon` flag to benchmark script (#5839)
    The benchmark script sends a number of `ArithmeticAddCalculation`s to
    the daemon and reports the time taken per process at the end. This is
    intended for users to run to get an idea of the performance of their
    setup. However, running over the daemon is not necessary the most
    straightforward when it comes to analysing the results and the timings
    can differ quite a bit. It makes more sense to simply run the N jobs
    serially, which should give a better more consistent picture of the
    average overhead per process.
    
    The option `--without-daemon/--with-daemon` is added to the command
    to allow switching the behavior. By default the jobs are now run
    sequentially, not over the daemon.

--- docs/source/howto/include/scripts/performance_benchmark_base.py
+++ docs/source/howto/include/scripts/performance_benchmark_base.py
@@ -10,8 +10,9 @@ from aiida.cmdline.utils import decorators, echo
 @click.command()
 @options.CODE(required=False, help='A code that can run the ``ArithmeticAddCalculation``, for example bash.')
 @click.option('-n', 'number', type=int, default=100, show_default=True, help='The number of processes to submit.')
+@click.option('--daemon/--without-daemon', default=False, is_flag=True, help='Submit to daemon or run synchronously.')
 @decorators.with_dbenv()
-def main(code, number):
+def main(code, number, daemon):
     """Submit a number of ``ArithmeticAddCalculation`` to the daemon and record time to completion.
 
     This command requires the daemon to be running.
@@ -28,14 +29,14 @@ def main(code, number):
 
     from aiida import orm
     from aiida.common import exceptions
-    from aiida.engine import submit
+    from aiida.engine import run_get_node, submit
     from aiida.engine.daemon.client import get_daemon_client
     from aiida.plugins import CalculationFactory
     from aiida.tools.graph.deletions import delete_nodes
 
     client = get_daemon_client()
 
-    if not client.is_daemon_running:
+    if daemon and not client.is_daemon_running:
         echo.echo_critical('The daemon is not running.')
 
     computer_created = False
@@ -72,33 +73,41 @@ def main(code, number):
     time_start = time.time()
     nodes = []
 
-    with click.progressbar(range(number), label=f'Submitting {number} calculations.') as bar:
-        for iteration in bar:
-            node = submit(builder)
-            nodes.append(node)
+    if daemon:
+        with click.progressbar(range(number), label=f'Submitting {number} calculations.') as bar:
+            for iteration in bar:
+                node = submit(builder)
+                nodes.append(node)
 
-    time_end = time.time()
-    echo.echo(f'Submission completed in {(time_end - time_start):.2f} seconds.')
+        time_end = time.time()
+        echo.echo(f'Submission completed in {(time_end - time_start):.2f} seconds.')
 
-    completed = 0
+        completed = 0
 
-    with click.progressbar(length=number, label='Waiting for calculations to complete') as bar:
-        while True:
-            time.sleep(0.2)
+        with click.progressbar(length=number, label='Waiting for calculations to complete') as bar:
+            while True:
+                time.sleep(0.2)
 
-            terminated = [node.is_terminated for node in nodes]
-            newly_completed = terminated.count(True) - completed
-            completed = terminated.count(True)
+                terminated = [node.is_terminated for node in nodes]
+                newly_completed = terminated.count(True) - completed
+                completed = terminated.count(True)
 
-            bar.update(newly_completed)
+                bar.update(newly_completed)
 
-            if all(terminated):
-                break
+                if all(terminated):
+                    break
+
+    else:
+        with click.progressbar(range(number), label=f'Running {number} calculations.') as bar:
+            for iteration in bar:
+                _, node = run_get_node(builder)
+                nodes.append(node)
 
     if any(node.is_excepted or node.is_killed for node in nodes):
         echo.echo_warning('At least one submitted calculation excepted or was killed.')
     else:
         echo.echo_success('All calculations finished successfully.')
+
 
     time_end = time.time()
     echo.echo(f'Elapsed time: {(time_end - time_start):.2f} seconds.')

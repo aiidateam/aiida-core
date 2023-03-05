7176fb4780f339c46be25c55633d024f3fac6e2c
Leopold Talirz <leopold.talirz@gmail.com>
2023-03-05 19:45:40 +0000

    back to Dockerfile

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,11 +1,5 @@
  #!/bin/bash
  set -e
 
- # Add test dependencies (not installed in image)
- pip install .[tests,pre-commit]
-
-# add aiida user
-/etc/my_init.d/10_create-system-user.sh
-
 # configure aiida
 /opt/configure-aiida.sh

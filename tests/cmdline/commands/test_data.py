aiidateam/aiida-core

commit dc7cdd0443ba18b13775fa84cd3391caa4d7af71

Move verdi data to click (#1738)
* Data subcommand move to click:

1) create a folder for data click commands
2) place subcommands of verdi data in different files

* data upf is moved to click

* Put list-related functions to a separate file

* data bands (not fully) transferred to click

* Grouping the list option arguments

* Removing not needed load_dbenv

* data bands are fully transferred to click

* More progress on cif

* Remove label and description from data lavel

* More progress on cif

* data parameter are transferred to click

* More progress on cif

* Put export in a separate file

* More progress on cif

* More progress on cif

* data remote is fully transferred to click

* More progress on cif deposit

* More progress on cif deposit

* Ported verdi data structure to click

* Transfer verdi data trajectory to click

* Transfer verdi data array to click

* Deposit function in a separate file

* More progress on data cif

* Small changes in data cif

* Fixing various errors

* More progress on tests

* More progress on verdi data cif tests

* Polish the verdi data code:

- remove unnecessary imports
- put show functionality in a separate file
- put commonly used click options in isolated containers

* More progress in tests

* Transfer verdi data cif to click

* Cleaning & correcting the code.

* Cleaning tests, cif & general code, fixing problems in argument grouping

* Adding tests for cif import

* More progress on cif tests

* Loading correctly db_env to not affect the tests

* Cif tests should be OK (need some cleaning but are complete). We should see why and if we need to have so many flags that are not used.

* Improving structure data listing. Starting structure data tests

* More progress on verdi data tests

* Structure listing test added

* More progress on TrajectoryData list testing

* Finished with array and bands tests

aiida/cmdline/commands/data/trajectory.py
@@ -1,0 +1,164 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+"""
+This allows to manage TrajectoryData objects from command line.
+"""
+import click
+from aiida.cmdline.commands import verdi_data
+from aiida.cmdline.commands.data.export import _export, export_options
+from aiida.cmdline.utils import echo
+from aiida.cmdline.commands.data.list import _list, list_options
+from aiida.backends.utils import load_dbenv, is_dbenv_loaded
+from aiida.cmdline.commands.data.show import show_options
+from aiida.cmdline.commands.data.deposit import deposit_options, deposit_tcod
+
+
+# pylint: disable=unused-argument
+@verdi_data.group('trajectory')
+@click.pass_context
+def trajectory(ctx):
+    """
+    View and manipulate TrajectoryData instances.
+    """
+    pass
+
+
+PROJECT_HEADERS = ["Id", "Label"]
+
+
+@trajectory.command('list')
+@list_options
+def list_trajections(raw, past_days, groups, all_users):
+    """
+    List trajectories stored in database.
+    """
+    from aiida.orm.data.array.trajectory import TrajectoryData
+    from tabulate import tabulate
+    elements = None
+    elements_only = False
+    formulamode = None
+    entry_list = _list(TrajectoryData, PROJECT_HEADERS, elements, elements_only, formulamode, past_days, groups,
+                       all_users)
+
+    counter = 0
+    struct_list_data = list()
+    if not raw:
+        struct_list_data.append(PROJECT_HEADERS)
+    for entry in entry_list:
+        for i, value in enumerate(entry):
+            if isinstance(value, list):
+                entry[i] = ",".join(value)
+        for i in range(len(entry), len(PROJECT_HEADERS)):
+            entry.append(None)
+        counter += 1
+    struct_list_data.extend(entry_list)
+    if raw:
+        echo.echo(tabulate(struct_list_data, tablefmt='plain'))
+    else:
+        echo.echo(tabulate(struct_list_data, headers="firstrow"))
+        echo.echo("\nTotal results: {}\n".format(counter))
+
+
+@trajectory.command('show')
+@show_options
+def show(**kwargs):
+    """
+    Visualize trajectory
+    """
+    from aiida.orm.data.array.trajectory import TrajectoryData
+    from aiida.cmdline.commands.data.show import _show_jmol
+    from aiida.cmdline.commands.data.show import _show_xcrysden
+    from aiida.cmdline.commands.data.show import _show_mpl_pos
+    from aiida.cmdline.commands.data.show import _show_mpl_heatmap
+    nodes = kwargs.pop('nodes')
+    given_format = kwargs.pop('show_format')
+    for node in nodes:
+        if not isinstance(node, TrajectoryData):
+            echo.echo_critical("Node {} is of class {} instead " "of {}".format(node, type(node), TrajectoryData))
+
+    for key, value in kwargs.items():
+        if value is None:
+            kwargs.pop(key)
+
+    if given_format == "jmol":
+        _show_jmol(given_format, nodes, **kwargs)
+    elif given_format == "xcrysden":
+        _show_xcrysden(given_format, nodes, **kwargs)
+    elif given_format == "mpl_pos":
+        _show_mpl_pos(given_format, nodes, **kwargs)
+    elif given_format == "mpl_heatmap":
+        _show_mpl_heatmap(given_format, nodes, **kwargs)
+    else:
+        raise NotImplementedError("The format {} is not yet implemented".format(given_format))
+
+
+SUPPORTED_FORMATS = ['cif', 'tcod', 'xsf']
+
+
+@trajectory.command('export')
+@click.option('-y', '--format', type=click.Choice(SUPPORTED_FORMATS), default='cif', help="Type of the exported file.")
+@click.option(
+    '--step',
+    'trajectory_index',
+    type=click.INT,
+    default=None,
+    help="ID of the trajectory step. If none is supplied, all"
+    " steps are explored.")
+@export_options
+def export(**kwargs):
+    """
+    Export trajectory
+    """
+    from aiida.orm.data.array.trajectory import TrajectoryData
+
+    node = kwargs.pop('node')
+    output = kwargs.pop('output')
+    export_format = kwargs.pop('format')
+    force = kwargs.pop('force')
+
+    for key, value in kwargs.items():
+        if value is None:
+            kwargs.pop(key)
+
+    if not isinstance(node, TrajectoryData):
+        echo.echo_critical("Node {} is of class {} instead of {}".format(node, type(node), TrajectoryData))
+    _export(node, output, export_format, other_args=kwargs, overwrite=force)
+
+
+@trajectory.command('deposit')
+@click.option(
+    '--step',
+    'trajectory_index',
+    type=click.INT,
+    default=1,
+    help="ID of the trajectory step. If none is "
+    "supplied, all steps are exported.")
+@deposit_options
+def deposit(**kwargs):
+    """
+    Deposit trajectory object
+    """
+    from aiida.orm.data.array.trajectory import TrajectoryData
+    if not is_dbenv_loaded():
+        load_dbenv()
+    node = kwargs.pop('node')
+    deposition_type = kwargs.pop('deposition_type')
+    parameter_data = kwargs.pop('parameter_data')
+
+    if kwargs['database'] is None:
+        echo.echo_critical("Default database is not defined, please specify.")
+
+    for key, value in kwargs.items():
+        if value is None:
+            kwargs.pop(key)
+
+    if not isinstance(node, TrajectoryData):
+        echo.echo_critical("Node {} is of class {} instead of {}".format(node, type(node), TrajectoryData))
+    deposit_tcod(node, deposition_type, parameter_data, **kwargs)

.pre-commit-config.yaml
@@ -16,6 +16,18 @@
         aiida/cmdline/commands/calculation.py|
         aiida/cmdline/commands/comment.py|
         aiida/cmdline/commands/daemon.py|
+        aiida/cmdline/commands/data/array.py|
+        aiida/cmdline/commands/data/bands.py|
+        aiida/cmdline/commands/data/cif.py|
+        aiida/cmdline/commands/data/deposit.py|
+        aiida/cmdline/commands/data/export.py|
+        aiida/cmdline/commands/data/list.py|
+        aiida/cmdline/commands/data/parameter.py|
+        aiida/cmdline/commands/data/remote.py|
+        aiida/cmdline/commands/data/show.py|
+        aiida/cmdline/commands/data/structure.py|
+        aiida/cmdline/commands/data/trajectory.py|
+        aiida/cmdline/commands/data/upf.py|
         aiida/cmdline/commands/code.py|
         aiida/cmdline/commands/graph.py|
         aiida/cmdline/commands/group.py|

aiida/backends/tests/__init__.py
@@ -71,6 +71,8 @@ db_test_list = {
         'cmdline.params.types.identifier': ['aiida.backends.tests.cmdline.params.types.test_identifier'],
         'cmdline.params.types.node': ['aiida.backends.tests.cmdline.params.types.test_node'],
         'cmdline.params.types.plugin': ['aiida.backends.tests.cmdline.params.types.test_plugin'],
+        'cmdline.commands.code': ['aiida.backends.tests.cmdline.commands.test_code'],
+        'cmdline.commands.data': ['aiida.backends.tests.cmdline.commands.test_data'],
         'cmdline.params.types.workflow': ['aiida.backends.tests.cmdline.params.types.test_workflow'],
         'daemon.client': ['aiida.backends.tests.daemon.test_client'],
         'orm.data.frozendict': ['aiida.backends.tests.orm.data.frozendict'],
@@ -94,7 +96,6 @@ db_test_list = {
         'work.job_processes': ['aiida.backends.tests.work.job_processes'],
         'plugin_loader': ['aiida.backends.tests.test_plugin_loader'],
         'daemon': ['aiida.backends.tests.daemon'],
-        'verdi_commands': ['aiida.backends.tests.verdi_commands'],
         'caching_config': ['aiida.backends.tests.test_caching_config'],
         'inline_calculation': ['aiida.backends.tests.inline_calculation'],
     }

aiida/cmdline/commands/data/__init__.py
@@ -1,0 +1,48 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+from aiida.cmdline.baseclass import VerdiCommandWithSubcommands
+from aiida.cmdline.commands import verdi, verdi_data
+
+
+class Data(VerdiCommandWithSubcommands):
+    """
+    Setup and manage data specific types
+
+    There is a list of subcommands for managing specific types of data.
+    For instance, 'data upf' manages pseudopotentials in the UPF format.
+    """
+
+    def __init__(self):
+        from aiida.backends.utils import load_dbenv, is_dbenv_loaded
+        if not is_dbenv_loaded():
+            load_dbenv()
+
+        from aiida.cmdline.commands.data import upf
+        from aiida.cmdline.commands.data import structure
+        from aiida.cmdline.commands.data import bands
+        from aiida.cmdline.commands.data import cif
+        from aiida.cmdline.commands.data import trajectory
+        from aiida.cmdline.commands.data import parameter
+        from aiida.cmdline.commands.data import array
+        from aiida.cmdline.commands.data import remote
+
+        self.valid_subcommands = {
+            'upf': (self.cli, self.complete_none),
+            'structure': (self.cli, self.complete_none),
+            'bands': (self.cli, self.complete_none),
+            'cif': (self.cli, self.complete_none),
+            'trajectory': (self.cli, self.complete_none),
+            'parameter': (self.cli, self.complete_none),
+            'array': (self.cli, self.complete_none),
+            'remote': (self.cli, self.complete_none),
+        }
+
+    def cli(self, *args):
+        verdi()

aiida/cmdline/commands/data/array.py
@@ -1,0 +1,44 @@
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (c), The AiiDA team. All rights reserved.                     #
+# This file is part of the AiiDA code.                                    #
+#                                                                         #
+# The code is hosted on GitHub at https://github.com/aiidateam/aiida_core #
+# For further information on the license, see the LICENSE.txt file        #
+# For further information please visit http://www.aiida.net               #
+###########################################################################
+"""
+This allows to manage ArrayData objects from command line.
+"""
+import click
+
+from aiida.cmdline.commands import verdi_data
+from aiida.cmdline.params import arguments
+from aiida.cmdline.utils import echo
+
+
+# pylint: disable=unused-argument
+@verdi_data.group('array')
+@click.pass_context
+def array(ctx):
+    """
+    Manipulate ArrayData objects
+    """
+    pass
+
+
+@array.command('show')
+@arguments.NODES()
+def show(nodes):
+    """
+    Visualize array object
+    """
+    from aiida.orm.data.array import ArrayData
+    from aiida.cmdline import print_dictionary
+    for node in nodes:
+        if not isinstance(node, ArrayData):
+            echo.echo_critical("Node {} is of class {} instead of" " {}".format(node, type(node), ArrayData))
+        the_dict = {}
+        for arrayname in node.arraynames():
+            the_dict[arrayname] = node.get_array(arrayname).tolist()
+        print_dictionary(the_dict, 'json+date')

commit 00d0b20e16afc9ca0fe8ab4de2bfb9b249cc0140

fix: verdi data trajectory show
The `verdi data trajectory show` command was broken at least since
dc7cdd0443ba18b13775fa84cd3391caa4d7af71 (Jul 2018).

The fact that most of its formats rely on external programs makes
it somewhat tedious to test.

.github/workflows/ci-code.yml
@@ -90,7 +90,7 @@ jobs:
         python-version: ${{ matrix.python-version }}
 
     - name: Install system dependencies
-      run: sudo apt update && sudo apt install postgresql graphviz
+      run: sudo apt update && sudo apt install postgresql graphviz jmol xcrysden
 
     - name: Upgrade pip and setuptools
       # Install specific version of setuptools, because 65.6.0 breaks a number of packages, such as numpy

aiida/cmdline/commands/cmd_data/cmd_show.py
@@ -12,56 +12,34 @@ This allows to manage showfunctionality to all data types.
 """
 import pathlib
 
-import click
-
-from aiida.cmdline.params import options
-from aiida.cmdline.params.options.multivalue import MultipleValueOption
 from aiida.cmdline.utils import echo
 from aiida.common.exceptions import MultipleObjectsError
 
-SHOW_OPTIONS = [
-    options.TRAJECTORY_INDEX(),
-    options.WITH_ELEMENTS(),
-    click.option('-c', '--contour', type=click.FLOAT, cls=MultipleValueOption, default=None, help='Isovalues to plot'),
-    click.option(
-        '--sampling-stepsize',
-        type=click.INT,
-        default=None,
-        help='Sample positions in plot every sampling_stepsize timestep'
-    ),
-    click.option(
-        '--stepsize',
-        type=click.INT,
-        default=None,
-        help='The stepsize for the trajectory, set it higher to reduce number of points'
-    ),
-    click.option('--mintime', type=click.INT, default=None, help='The time to plot from'),
-    click.option('--maxtime', type=click.INT, default=None, help='The time to plot to'),
-    click.option('--indices', type=click.INT, cls=MultipleValueOption, default=None, help='Show only these indices'),
-    click.option(
-        '--dont-block', 'block', is_flag=True, default=True, help="Don't block interpreter when showing plot."
-    ),
-]
+
+def has_executable(exec_name):
+    """
+    :return: True if executable can be found in PATH, False otherwise.
+    """
+    import shutil
+    if shutil.which(exec_name) is None:
+        return False
+    return True
 
 
-def show_options(func):
-    for option in reversed(SHOW_OPTIONS):
-        func = option(func)
-
-    return func
-
-
-def _show_jmol(exec_name, trajectory_list, **kwargs):
+def _show_jmol(exec_name, trajectory_list, **_kwargs):
     """
     Plugin for jmol
     """
     import subprocess
     import tempfile
 
+    if not has_executable(exec_name):
+        echo.echo_critical(f"No executable '{exec_name}' found. Add to the path, or try with an absolute path.")
+
     # pylint: disable=protected-access
     with tempfile.NamedTemporaryFile(mode='w+b') as handle:
         for trajectory in trajectory_list:
-            handle.write(trajectory._exportcontent('cif', **kwargs)[0])
+            handle.write(trajectory._exportcontent('cif')[0])
         handle.flush()
 
         try:
@@ -69,27 +47,26 @@ def _show_jmol(exec_name, trajectory_list, **kwargs):
         except subprocess.CalledProcessError:
             # The program died: just print a message
             echo.echo_error(f'the call to {exec_name} ended with an error.')
-        except OSError as err:
-            if err.errno == 2:
-                echo.echo_critical(f"No executable '{exec_name}' found. Add to the path, or try with an absolute path.")
-            else:
-                raise
 
 
-def _show_xcrysden(exec_name, object_list, **kwargs):
+def _show_xcrysden(exec_name, trajectory_list, **_kwargs):
     """
     Plugin for xcrysden
     """
     import subprocess
     import tempfile
 
-    if len(object_list) > 1:
+    if len(trajectory_list) > 1:
         raise MultipleObjectsError('Visualization of multiple trajectories is not implemented')
-    obj = object_list[0]
+    obj = trajectory_list[0]
+
+    if not has_executable(exec_name):
+        echo.echo_critical(f"No executable '{exec_name}' found.")
 
     # pylint: disable=protected-access
     with tempfile.NamedTemporaryFile(mode='w+b', suffix='.xsf') as tmpf:
-        tmpf.write(obj._exportcontent('xsf', **kwargs)[0])
+
+        tmpf.write(obj._exportcontent('xsf')[0])
         tmpf.flush()
 
         try:
@@ -97,11 +74,6 @@ def _show_xcrysden(exec_name, object_list, **kwargs):
         except subprocess.CalledProcessError:
             # The program died: just print a message
             echo.echo_error(f'the call to {exec_name} ended with an error.')
-        except OSError as err:
-            if err.errno == 2:
-                echo.echo_critical(f"No executable '{exec_name}' found. Add to the path, or try with an absolute path.")
-            else:
-                raise
 
 
 # pylint: disable=unused-argument

aiida/cmdline/commands/cmd_data/cmd_trajectory.py
@@ -14,7 +14,6 @@ import click
 from aiida.cmdline.commands.cmd_data import cmd_show, verdi_data
 from aiida.cmdline.commands.cmd_data.cmd_export import data_export, export_options
 from aiida.cmdline.commands.cmd_data.cmd_list import data_list, list_options
-from aiida.cmdline.commands.cmd_data.cmd_show import show_options
 from aiida.cmdline.params import arguments, options, types
 from aiida.cmdline.utils import decorators, echo
 
@@ -66,16 +65,38 @@ def trajectory_list(raw, past_days, groups, all_users):
 @trajectory.command('show')
 @arguments.DATA(type=types.DataParamType(sub_classes=('aiida.data:core.array.trajectory',)))
 @options.VISUALIZATION_FORMAT(type=click.Choice(VISUALIZATION_FORMATS), default='jmol')
-@show_options
+@options.TRAJECTORY_INDEX()
+@options.WITH_ELEMENTS()
+@click.option(
+    '-c', '--contour', type=click.FLOAT, cls=options.MultipleValueOption, default=None, help='Isovalues to plot'
+)
+@click.option(
+    '--sampling-stepsize',
+    type=click.INT,
+    default=None,
+    help='Sample positions in plot every sampling_stepsize timestep'
+)
+@click.option(
+    '--stepsize',
+    type=click.INT,
+    default=None,
+    help='The stepsize for the trajectory, set it higher to reduce number of points'
+)
+@click.option('--mintime', type=click.INT, default=None, help='The time to plot from')
+@click.option('--maxtime', type=click.INT, default=None, help='The time to plot to')
+@click.option(
+    '--indices', type=click.INT, cls=options.MultipleValueOption, default=None, help='Show only these indices'
+)
+@click.option('--dont-block', 'block', is_flag=True, default=True, help="Don't block interpreter when showing plot.")
 @decorators.with_dbenv()
-def trajectory_show(data, fmt):
+def trajectory_show(data, fmt, **kwargs):
     """Visualize a trajectory."""
     try:
         show_function = getattr(cmd_show, f'_show_{fmt}')
     except AttributeError:
         echo.echo_critical(f'visualization format {fmt} is not supported')
 
-    show_function(fmt, data)
+    show_function(exec_name=fmt, trajectory_list=data, **kwargs)
 
 
 @trajectory.command('export')

aiida/orm/nodes/data/array/trajectory.py
@@ -652,8 +652,6 @@ class TrajectoryData(ArrayData):
             from ase.data.colors import cpk_colors as colors
         else:
             raise ValueError(f'Unknown color spec {colors}')
-        if kwargs:
-            raise ValueError(f'Unrecognized keyword {kwargs.keys()}')
 
         if element_list is None:
             # If not all elements are allowed
@@ -703,12 +701,8 @@ class TrajectoryData(ArrayData):
             from mayavi import mlab
         except ImportError:
             raise ImportError(
-                'Unable to import the mayavi package, that is required to'
-                'use the plotting feature you requested. '
-                'Please install it first and then call this command again '
-                '(note that the installation of mayavi is quite complicated '
-                'and requires that you already installed the python numpy '
-                'package, as well as the vtk package'
+                'The plotting feature you requested requires the mayavi package.'
+                'Try `pip install mayavi` or consult the documentation.'
             )
         from ase.data import atomic_numbers
         from ase.data.colors import jmol_colors
@@ -847,7 +841,7 @@ def plot_positions_XYZ(  # pylint: disable=too-many-arguments,too-many-locals,in
         dont_block=False,
         mintime=None,
         maxtime=None,
-        label_sparsity=10):
+        n_labels=10):
     """
     Plot with matplotlib the positions of the coordinates of the atoms
     over time for a trajectory
@@ -862,14 +856,14 @@ def plot_positions_XYZ(  # pylint: disable=too-many-arguments,too-many-locals,in
     :param dont_block: passed to plt.show() as ``block=not dont_block``
     :param mintime: if specified, cut the time axis at the specified min value
     :param maxtime: if specified, cut the time axis at the specified max value
-    :param label_sparsity: how often to put a label with the pair (t, coord)
+    :param n_labels: how many labels (t, coord) to put
     """
     from matplotlib import pyplot as plt
     from matplotlib.gridspec import GridSpec
     import numpy as np
 
     tlim = [times[0], times[-1]]
-    index_range = [0, len(times)]
+    index_range = [0, len(times) - 1]
     if mintime is not None:
         tlim[0] = mintime
         index_range[0] = np.argmax(times > mintime)
@@ -896,7 +890,8 @@ def plot_positions_XYZ(  # pylint: disable=too-many-arguments,too-many-locals,in
     plt.ylabel(r'Z Position $\left[{}\right]$'.format(positions_unit))
     plt.xlabel(f'Time [{times_unit}]')
     plt.xlim(*tlim)
-    sparse_indices = np.linspace(*index_range, num=label_sparsity, dtype=int)
+    n_labels = np.minimum(n_labels, len(times))  # don't need more labels than times
+    sparse_indices = np.linspace(*index_range, num=n_labels, dtype=int)
 
     for index, traj in enumerate(trajectories):
         if index not in indices_to_show:

tests/cmdline/commands/test_data.py
@@ -26,6 +26,7 @@ from aiida.cmdline.commands.cmd_data import (
     cmd_cif,
     cmd_dict,
     cmd_remote,
+    cmd_show,
     cmd_singlefile,
     cmd_structure,
     cmd_trajectory,
@@ -506,7 +507,7 @@ class TestVerdiDataTrajectory(DummyVerdiDataListable, DummyVerdiDataExportable):
     def test_showhelp(self):
         res = self.cli_runner(cmd_trajectory.trajectory_show, ['--help'])
         assert b'Usage:' in res.stdout_bytes, 'The string "Usage: " was not found in the output' \
-            ' of verdi data trajecotry show --help'
+            ' of verdi data trajectory show --help'
 
     def test_list(self):
         self.data_listing_test(TrajectoryData, str(self.pks[DummyVerdiDataListable.NODE_ID_STR]), self.pks)
@@ -516,6 +517,15 @@ class TestVerdiDataTrajectory(DummyVerdiDataListable, DummyVerdiDataExportable):
     def test_export(self, output_flag, tmp_path):
         new_supported_formats = list(cmd_trajectory.EXPORT_FORMATS)
         self.data_export_test(TrajectoryData, self.pks, new_supported_formats, output_flag, tmp_path)
+
+    @pytest.mark.parametrize('fmt', cmd_trajectory.VISUALIZATION_FORMATS)
+    def test_trajectoryshow(self, fmt):
+        """Test showing the trajectory data in different formats"""
+        if fmt in ['xcrysden', 'jmol'] and not cmd_show.has_executable(fmt):
+            pytest.skip()
+        options = ['--format', fmt, str(self.ids[DummyVerdiDataListable.NODE_ID_STR])]
+        res = self.cli_runner(cmd_trajectory.trajectory_show, options, catch_exceptions=False)
+        assert res.exit_code == 0
 
 
 class TestVerdiDataStructure(DummyVerdiDataListable, DummyVerdiDataExportable):
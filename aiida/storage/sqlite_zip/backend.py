2ea5087c079417d6d0b37cbc0502ed7cab173c11
Sebastiaan Huber <mail@sphuber.net>
2023-10-03 10:01:31 +0200

    `PsqlDosBackend`: Fix changes not persisted after `iterall` and `iterdict` (#6134)
    The `iterall` and `iterdict` generators of the `QueryBuilder`
    implementation for the `PsqlDosBackend` would open a transaction in
    order for the `ModelWrapper` to not automatically commit the session
    upon any mutation as that would invalidate the cursor. However, it did
    not manually commit the session at the end of the iterator, causing any
    mutations to be lost when the storage backend was reloaded.
    
    This problem was not just present in the `iterall` and `iterdict`
    methods of the `QueryBuilder` but rather the `transaction` method of the
    `PsqlDosBackend` never commits the savepoint that is returned by the
    `Session.begin_nested` call. Now the `transaction` explicitly commits
    the savepoint after the yield and the `QueryBuilder` methods are updated
    to simply use the `transaction` method of the storage backend, rather
    than going directly to the session.
    
    This change also required a change in the `SqliteZipBackend`, since the
    `transaction` is now called during archive creation and import, but the
    backend raised a `NotImplementedError`. This is because it used to be a
    read-only backend, however, this limitation was recently lifted. The
    commit simply forgot to implement the `transaction` method.

--- aiida/storage/sqlite_zip/backend.py
+++ aiida/storage/sqlite_zip/backend.py
@@ -253,8 +253,19 @@ class SqliteZipBackend(StorageBackend):  # pylint: disable=too-many-public-metho
     def _clear(self) -> None:
         raise ReadOnlyError()
 
+    @contextmanager
     def transaction(self):
-        raise ReadOnlyError()
+        session = self.get_session()
+        if session.in_transaction():
+            with session.begin_nested() as savepoint:
+                yield session
+                savepoint.commit()
+            session.commit()
+        else:
+            with session.begin():
+                with session.begin_nested() as savepoint:
+                    yield session
+                    savepoint.commit()
 
     @property
     def in_transaction(self) -> bool:

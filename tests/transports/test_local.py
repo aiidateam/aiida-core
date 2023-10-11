6e11864d12de2d85a9e502fae8d4afc7f925b607
Sebastiaan Huber <mail@sphuber.net>
2023-10-11 23:35:47 +0200

    Tests: Refactor transport tests from `unittest` to `pytest`

--- tests/transports/test_local.py
+++ tests/transports/test_local.py
@@ -7,54 +7,38 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-"""Tests for the `LocalTransport`."""
-import unittest
+"""Test :mod:`aiida.transports.plugins.local`."""
+import getpass
+
+import pytest
 
 from aiida.transports.plugins.local import LocalTransport
 from aiida.transports.transport import TransportInternalError
 
-# This will be used by test_all_plugins
 
-plugin_transport = LocalTransport()
-
-
-class TestGeneric(unittest.TestCase):
-    """
-    Test whoami on localhost.
-    """
-
-    def test_whoami(self):
-        """Test the `whoami` command."""
-        import getpass
-
-        with LocalTransport() as transport:
-            self.assertEqual(transport.whoami(), getpass.getuser())
+def test_basic():
+    """Test constructor."""
+    with LocalTransport():
+        pass
 
 
-class TestBasicConnection(unittest.TestCase):
-    """
-    Test basic connections.
-    """
+def test_whoami():
+    """Test the `whoami` command."""
+    with LocalTransport() as transport:
+        assert transport.whoami() == getpass.getuser()
 
-    def test_closed_connection(self):
-        """Test running a command on a closed connection."""
 
-        with self.assertRaises(TransportInternalError):
-            transport = LocalTransport()
-            transport.listdir()
-
-    @staticmethod
-    def test_basic():
-        """Test constructor."""
-        with LocalTransport():
-            pass
+def test_closed_connection():
+    """Test running a command on a closed connection."""
+    with pytest.raises(TransportInternalError):
+        transport = LocalTransport()
+        transport.listdir()
 
 
 def test_gotocomputer():
     """Test gotocomputer"""
     with LocalTransport() as transport:
         cmd_str = transport.gotocomputer_command('/remote_dir/')
-
         expected_str = (
             """bash -c  "if [ -d '/remote_dir/' ] ;"""
             """ then cd '/remote_dir/' ; bash -l  ; else echo '  ** The directory' ; """
--- tests/transports/test_ssh.py
+++ tests/transports/test_ssh.py
@@ -7,103 +7,93 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-"""Test the `SshTransport` plugin on localhost."""
+"""Test :mod:`aiida.transports.plugins.ssh`."""
 import logging
-import unittest
 
 import paramiko
+import pytest
 
 from aiida.transports.plugins.ssh import SshTransport
 from aiida.transports.transport import TransportInternalError
 
-# This will be used by test_all_plugins
 
-plugin_transport = SshTransport(machine='localhost', timeout=30, load_system_host_keys=True, key_policy='AutoAddPolicy')
+def test_closed_connection_ssh():
+    """Test calling command on a closed connection."""
+    with pytest.raises(TransportInternalError):
+        transport = SshTransport(machine='localhost')
+        transport._exec_command_internal('ls')  # pylint: disable=protected-access
 
 
-class TestBasicConnection(unittest.TestCase):
-    """
-    Test basic connections.
-    """
+def test_closed_connection_sftp():
+    """Test calling sftp command on a closed connection."""
+    with pytest.raises(TransportInternalError):
+        transport = SshTransport(machine='localhost')
+        transport.listdir()
 
-    def test_closed_connection_ssh(self):
-        """Test calling command on a closed connection."""
-        with self.assertRaises(TransportInternalError):
-            transport = SshTransport(machine='localhost')
-            transport._exec_command_internal('ls')  # pylint: disable=protected-access
 
-    def test_closed_connection_sftp(self):
-        """Test calling sftp command on a closed connection."""
-        with self.assertRaises(TransportInternalError):
-            transport = SshTransport(machine='localhost')
-            transport.listdir()
+def test_auto_add_policy():
+    """Test the auto add policy."""
+    with SshTransport(machine='localhost', timeout=30, load_system_host_keys=True, key_policy='AutoAddPolicy'):
+        pass
 
-    @staticmethod
-    def test_auto_add_policy():
-        """Test the auto add policy."""
-        with SshTransport(machine='localhost', timeout=30, load_system_host_keys=True, key_policy='AutoAddPolicy'):
-            pass
 
-    @staticmethod
-    def test_proxy_jump():
-        """Test the connection with a proxy jump or several"""
+def test_proxy_jump():
+    """Test the connection with a proxy jump or several"""
+    with SshTransport(
+        machine='localhost', proxy_jump='localhost', timeout=30, load_system_host_keys=True, key_policy='AutoAddPolicy'
+    ):
+        pass
+
+    # kind of pointless, but should work and to check that proxy chaining works
+    with SshTransport(
+        machine='localhost',
+        proxy_jump='localhost,localhost,localhost',
+        timeout=30,
+        load_system_host_keys=True,
+        key_policy='AutoAddPolicy'
+    ):
+        pass
+
+
+def test_proxy_jump_invalid():
+    """Test proper error reporting when invalid host as a proxy"""
+
+    # import is also that when Python is running with debug warnings `-Wd`
+    # no unclosed files are reported.
+    with pytest.raises(paramiko.SSHException):
         with SshTransport(
             machine='localhost',
-            proxy_jump='localhost',
+            proxy_jump='localhost,nohost',
             timeout=30,
             load_system_host_keys=True,
             key_policy='AutoAddPolicy'
         ):
             pass
 
-        # kind of pointless, but should work and to check that proxy chaining works
-        with SshTransport(
-            machine='localhost',
-            proxy_jump='localhost,localhost,localhost',
-            timeout=30,
-            load_system_host_keys=True,
-            key_policy='AutoAddPolicy'
-        ):
+
+def test_proxy_command():
+    """Test the connection with a proxy command"""
+    with SshTransport(
+        machine='localhost',
+        proxy_command='ssh -W localhost:22 localhost',
+        timeout=30,
+        load_system_host_keys=True,
+        key_policy='AutoAddPolicy'
+    ):
+        pass
+
+
+def test_no_host_key():
+    """Test if there is no host key."""
+    # Disable logging to avoid output during test
+    logging.disable(logging.ERROR)
+
+    with pytest.raises(paramiko.SSHException):
+        with SshTransport(machine='localhost', timeout=30, load_system_host_keys=False):
             pass
 
-    def test_proxy_jump_invalid(self):
-        """Test proper error reporting when invalid host as a proxy"""
-
-        # import is also that when Python is running with debug warnings `-Wd`
-        # no unclosed files are reported.
-        with self.assertRaises(paramiko.SSHException):
-            with SshTransport(
-                machine='localhost',
-                proxy_jump='localhost,nohost',
-                timeout=30,
-                load_system_host_keys=True,
-                key_policy='AutoAddPolicy'
-            ):
-                pass
-
-    @staticmethod
-    def test_proxy_command():
-        """Test the connection with a proxy command"""
-        with SshTransport(
-            machine='localhost',
-            proxy_command='ssh -W localhost:22 localhost',
-            timeout=30,
-            load_system_host_keys=True,
-            key_policy='AutoAddPolicy'
-        ):
-            pass
-
-    def test_no_host_key(self):
-        """Test if there is no host key."""
-        # Disable logging to avoid output during test
-        logging.disable(logging.ERROR)
-
-        with self.assertRaises(paramiko.SSHException):
-            with SshTransport(machine='localhost', timeout=30, load_system_host_keys=False):
-                pass
-
-        # Reset logging level
-        logging.disable(logging.NOTSET)
+    # Reset logging level
+    logging.disable(logging.NOTSET)
 
 
 def test_gotocomputer():

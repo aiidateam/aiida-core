03050b52eabcd5482ac777e8e0513e3ce238aba2
Sebastiaan Huber <mail@sphuber.net>
2023-10-25 12:00:52 +0200

    Tests: Move ipython magic tests to main unit test suite
    The `.github/system_tests/test_ipython_magics.py` file provided tests
    for the ipython magics, however, these can simply be run in the main
    test suite invoked directly through `pytest`.

--- .github/workflows/tests.sh
+++ .github/workflows/tests.sh
@@ -4,9 +4,6 @@ set -ev
 # Make sure the folder containing the workchains is in the python path before the daemon is started
 SYSTEM_TESTS="${GITHUB_WORKSPACE}/.github/system_tests"
 
-# tests for the testing infrastructure
-pytest --cov aiida --verbose --noconftest ${SYSTEM_TESTS}/test_ipython_magics.py
-
 # Until the `${SYSTEM_TESTS}/pytest` tests are moved within `tests` we have to run them separately and pass in the path to the
 # `conftest.py` explicitly, because otherwise it won't be able to find the fixtures it provides
 AIIDA_TEST_PROFILE=test_aiida pytest --cov aiida --verbose tests/conftest.py ${SYSTEM_TESTS}/pytest

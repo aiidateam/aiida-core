46aaed3910836e20751892f72a6122b35714046e
Sebastiaan Huber <mail@sphuber.net>
2023-11-28 10:38:43 +0100

    Use warning filter instead of monkeypatching
    Note that `pytest` undoes any warning filters, so it also has to be
    added to the `pytest` configuration in `pyproject.toml`.

--- aiida/storage/psql_dos/orm/__init__.py
+++ aiida/storage/psql_dos/orm/__init__.py
@@ -8,43 +8,9 @@
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
 """Implementation of ORM backend entities."""
-import typing as t
-from typing import Any, Optional
+import warnings
 
-from sqlalchemy.orm import unitofwork
-from sqlalchemy.orm.interfaces import MapperProperty
-from sqlalchemy.orm.state import InstanceState
-
-
-def register_object(
-    self,
-    state: InstanceState[Any],
-    isdelete: bool = False,
-    listonly: bool = False,
-    cancel_delete: bool = False,
-    operation: Optional[str] = None,
-    prop: Optional[MapperProperty] = None,
-) -> bool:
-    """Monkeypatch :meth:`sqlalchemy.orm.unitofwork.UOWTransaction.register_object` to remove warning."""
-    # pylint: disable=protected-access,unused-argument
-    if not self.session._contains_state(state):
-        # The original implementation raises a warning here if ``not state.deleted and operation is not None`` which is
-        # intentionally removed.
-        return False
-
-    if state not in self.states:
-        mapper = state.manager.mapper
-
-        if mapper not in self.mappers:
-            self._per_mapper_flush_actions(mapper)
-
-        self.mappers[mapper].add(state)
-        self.states[state] = (isdelete, listonly)
-    else:
-        if not listonly and (isdelete or cancel_delete):
-            self.states[state] = (isdelete, False)
-    return True
-
+from sqlalchemy.exc import SAWarning
 
 # The :meth:`sqlalchemy.orm.unitofwork.UOWTransaction.register_object` method emits a warning whenever an object is
 # registered that is not part of the session. This can happen when the session is committed or flushed and an object
@@ -61,7 +27,7 @@ def register_object(
 # One direct solution would be to add the input nodes again to the session before committing the process node and the
 # links. However, this code is part of the backend independent :mod:`aiida.orm` module and this is a backend-specific
 # problem. This is also just one example and there are most likely other places in the code where the problem arises.
-# Therefore, as an ugly hack, the :meth:`sqlalchemy.orm.unitofwork.UOWTransaction.register_object` is monkey patched to
-# remove the warning. This will be sensitive to future changes in sqlalchemy though as this is probably not intended to
-# be public API.
-setattr(unitofwork.UOWTransaction, 'register_object', register_object)
+# Therefore, as a work around, the specific warning is ignored through a warrning filter.
+warnings.filterwarnings(
+    'ignore', category=SAWarning, message='Object of type .* not in session, .* operation along .* will not proceed'
+)

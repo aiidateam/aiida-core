27c384e3850c7799165cceacfd33c49e9328409b
Sebastiaan Huber <mail@sphuber.net>
2024-01-11 12:30:39 +0100

    ORM: Use `pydantic` to specify a schema for each ORM entity

--- src/aiida/orm/nodes/data/code/legacy.py
+++ src/aiida/orm/nodes/data/code/legacy.py
@@ -10,13 +10,13 @@
 
 import os
 import pathlib
-from typing import Optional
+import typing as t
 
 from aiida.common import exceptions
 from aiida.common.log import override_log_level
+from aiida.common.pydantic import MetadataField
 from aiida.common.warnings import warn_deprecation
 from aiida.orm import Computer
-from aiida.orm.fields import add_field
 
 from .abstract import AbstractCode
 
@@ -39,38 +39,21 @@ class Code(AbstractCode):
     for the code to be run).
     """
 
-    __qb_fields__ = [
-        add_field(
-            'prepend_text',
-            dtype=Optional[str],
-            doc='The code that will be put in the scheduler script before the execution of the code',
-        ),
-        add_field(
-            'append_text',
-            dtype=Optional[str],
-            doc='The code that will be put in the scheduler script after the execution of the code',
-        ),
-        add_field(
-            'input_plugin',
-            dtype=Optional[str],
-            doc='The name of the input plugin to be used for this code',
-        ),
-        add_field(
-            'local_executable',
-            dtype=Optional[str],
-            doc='Path to a local executable',
-        ),
-        add_field(
-            'remote_exec_path',
-            dtype=Optional[str],
-            doc='Remote path to executable',
-        ),
-        add_field(
-            'is_local',
-            dtype=Optional[bool],
-            doc='Whether the code is local or remote',
-        ),
-    ]
+    class Model(AbstractCode.Model):
+        prepend_text: str = MetadataField(
+            '',
+            description='The code that will be put in the scheduler script before the execution of the code',
+        )
+        append_text: str = MetadataField(
+            '',
+            description='The code that will be put in the scheduler script after the execution of the code',
+        )
+        input_plugin: t.Optional[str] = MetadataField(
+            description='The name of the input plugin to be used for this code'
+        )
+        local_executable: t.Optional[str] = MetadataField(description='Path to a local executable')
+        remote_exec_path: t.Optional[str] = MetadataField(description='Remote path to executable')
+        is_local: t.Optional[bool] = MetadataField(description='Whether the code is local or remote')
 
     def __init__(self, remote_computer_exec=None, local_executable=None, input_plugin_name=None, files=None, **kwargs):
         super().__init__(**kwargs)

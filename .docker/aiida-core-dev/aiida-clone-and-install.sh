246edd870ae7555898f1a29ae66a160778b6d100
Edan Bainglass <edan.bainglass@gmail.com>
2024-02-26 14:33:15 +0100

    Update `git clone` command

--- .docker/aiida-core-dev/aiida-clone-and-install.sh
+++ .docker/aiida-core-dev/aiida-clone-and-install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 cd ~ || exit
-git clone https://github.com/aiidateam/aiida-core.git
+git clone https://github.com/aiidateam/aiida-core.git --origin upstream
 cd aiida-core || exit
-git remote rename origin upstream
 pip install --user -e ."[pre-commit,atomic_tools,docs,rest,tests,tui]"
 pip install tox
 cd || exit

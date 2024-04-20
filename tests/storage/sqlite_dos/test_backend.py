fd97ac5e8cd1eec1fbbea034100ab90f06371304
Sebastiaan Huber <mail@sphuber.net>
2024-04-20 15:07:51 +0200

    `SqliteDosStorage`: Fix exception when importing archive
    When an archive was imported into an `SqliteDosStorage` backend an
    exception was raised by sqlalchemy. It was treating the `uuid` column of
    the models as a UUID type but in reality it was a string. This is
    because the storage plugin inherits the implementation largely from the
    `core.psql_dos` plugin, but it converts the models, since the UUID types
    that are used by the PostgreSQL implementation are not supported by
    SQLite.
    
    The problem was that for archive importing, the `bulk_insert` method was
    used, which calls the `_get_mapper_from_entity` method to map a given
    ORM entity to the corresponding database model. But since this method
    was inherited from `core.psql_dos`, it was returning the incorrect
    models. The problem is fixed by overriding it in `SqliteDosStorage` and
    returning the SQLite-adapted models.

--- tests/storage/sqlite_dos/test_backend.py
+++ tests/storage/sqlite_dos/test_backend.py
@@ -12,3 +12,16 @@ def test_model():
     filepath = pathlib.Path.cwd() / 'archive.aiida'
     model = SqliteDosStorage.Model(filepath=filepath.name)
     assert pathlib.Path(model.filepath).is_absolute()
+
+
+def test_archive_import(aiida_config, aiida_profile_factory):
+    """Test that archives can be imported."""
+    from aiida.orm import Node, QueryBuilder
+    from aiida.tools.archive.imports import import_archive
+
+    from tests.utils.archives import get_archive_file
+
+    with aiida_profile_factory(aiida_config, storage_backend='core.sqlite_dos'):
+        assert QueryBuilder().append(Node).count() == 0
+        import_archive(get_archive_file('calcjob/arithmetic.add.aiida'))
+        assert QueryBuilder().append(Node).count() > 0

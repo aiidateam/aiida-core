ddaf7749088da5360e7eedebad42d19002e0f346
Ali <khsrali@gmail.com>
2024-05-23 16:20:41 +0200

    use hasattr()

--- src/aiida/cmdline/commands/cmd_plugin.py
+++ src/aiida/cmdline/commands/cmd_plugin.py
@@ -48,15 +48,14 @@ def plugin_list(entry_point_group, entry_point):
             echo.echo_critical(str(exception))
         else:
             try:
-                if (inspect.isclass(plugin) and issubclass(plugin, Process)) or plugin.is_process_function:
+                if (inspect.isclass(plugin) and issubclass(plugin, Process)) or (
+                    False if not hasattr(plugin, 'is_process_function') else plugin.is_process_function
+                ):
                     print_process_info(plugin)
                 else:
                     echo.echo(str(plugin.get_description()))
             except AttributeError:
-                try:
-                    echo.echo(str(plugin.get_description()))
-                except AttributeError:
-                    echo.echo_error(f'No description available for {entry_point}')
+                echo.echo_error(f'No description available for {entry_point}')
     else:
         entry_points = get_entry_point_names(entry_point_group)
         if entry_points:

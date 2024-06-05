9fe7672f36c706c7eca87a4807012a1c8a5c0259
Alexander Goscinski <alex.goscinski@posteo.de>
2024-06-05 10:43:43 +0200

    Tests: Remove test for `make_aware` using fixed date
    The test compares the timezones between a fixed date and of the current date.
    This causes problem with time zones that use daylight saving time (DST). Fixing
    the test makes would add too much complexity for what the test is actually
    covering.  The `make_aware` function is still thoroughly tested by the remaining
    tests.

--- tests/common/test_timezone.py
+++ tests/common/test_timezone.py
@@ -40,19 +40,6 @@ def test_now():
     assert from_tz >= ref - dt
 
 
-def test_make_aware():
-    """Test the :func:`aiida.common.timezone.make_aware` function.
-
-    This should make a naive datetime object aware using the timezone of the operating system.
-    """
-    system_tzinfo = datetime.now(timezone.utc).astimezone()  # This is how to get the timezone of the OS.
-    naive = datetime(1970, 1, 1)
-    aware = make_aware(naive)
-    assert is_aware(aware)
-    assert aware.tzinfo.tzname(aware) == system_tzinfo.tzname()
-    assert aware.tzinfo.utcoffset(aware) == system_tzinfo.utcoffset()
-
-
 def test_make_aware_already_aware():
     """Test the :func:`aiida.common.timezone.make_aware` function for an already aware datetime.
 

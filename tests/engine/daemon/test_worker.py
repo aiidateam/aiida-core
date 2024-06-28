737da386b8564bc78e2f4a7fd9e2ae312accf03c
Sebastiaan Huber <mail@sphuber.net>
2024-06-28 23:43:57 +0200

    Daemon: Fix `DbLogHandler` not being configured (#6491)
    Processes run through the daemon would no longer have their log messages
    attached to the database. This would result in `verdi process report`
    returning nothing.
    
    The problem is that the `start_worker` function would call
    `configure_logging` without setting `with_orm=True` and so the
    `DbLogHandler` would essentially be undone. An integration test is added
    as a regression test.

--- tests/engine/daemon/test_worker.py
+++ tests/engine/daemon/test_worker.py
@@ -10,6 +10,8 @@
 
 import pytest
 from aiida.engine.daemon.worker import shutdown_worker
+from aiida.orm import Log
+from aiida.workflows.arithmetic.multiply_add import MultiplyAddWorkChain
 
 
 @pytest.mark.requires_rmq
@@ -24,3 +26,17 @@ async def test_shutdown_worker(manager):
     finally:
         # Reset the runner of the manager, because once closed it cannot be reused by other tests.
         manager._runner = None
+
+
+@pytest.mark.usefixtures('aiida_profile_clean', 'started_daemon_client')
+def test_logging_configuration(aiida_code_installed, submit_and_await):
+    """Integration test to verify that the daemon has the logging properly configured including the ``DbLogHandler``.
+
+    This is a regression test to make sure that the ``DbLogHandler`` is properly configured for daemon workers, which
+    ensures that log messages are written to the log table in the database for the corresponding node.
+    """
+    code = aiida_code_installed('add')
+    node = submit_and_await(MultiplyAddWorkChain, x=1, y=2, z=3, code=code)
+    logs = Log.collection.get_logs_for(node)
+    assert len(logs) == 1
+    assert 'Submitted the `ArithmeticAddCalculation`' in next(log.message for log in logs)

f9f1ca7eaeafc69aacb5164d2ed7ff2e129ca8f5
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-07-14 20:41:12 +0100

    Revert "Add types to orm.utils.node"
    This reverts commit b5fa0ce72d8a21044bd852e3b0054c69d16e55a0.

--- src/aiida/orm/utils/node.py
+++ src/aiida/orm/utils/node.py
@@ -9,16 +9,11 @@
 """Utilities to operate on `Node` classes."""
 
 import logging
-import typing as t
 import warnings
 
 from aiida.common import exceptions
 from aiida.common.utils import strip_prefix
 from aiida.orm.fields import EntityFieldMeta
-
-if t.TYPE_CHECKING:
-    from aiida.orm import Data, Node
-
 
 __all__ = (
     'load_node_class',
@@ -28,12 +23,13 @@ __all__ = (
 )
 
 
-def load_node_class(type_string: str) -> type[Node]:
+def load_node_class(type_string):
     """Return the `Node` sub class that corresponds to the given type string.
 
     :param type_string: the `type` string of the node
     :return: a sub class of `Node`
     """
+    from aiida.orm import Data, Node
     from aiida.plugins.entry_point import load_entry_point
 
     if type_string == '':
@@ -78,7 +74,7 @@ def load_node_class(type_string: str) -> type[Node]:
     return Data
 
 
-def get_type_string_from_class(class_module: str, class_name: str) -> str:
+def get_type_string_from_class(class_module, class_name):
     """Given the module and name of a class, determine the orm_class_type string, which codifies the
     orm class that is to be used. The returned string will always have a terminating period, which
     is required to query for the string in the database
@@ -114,11 +110,11 @@ def get_type_string_from_class(class_module: str, class_name: str) -> str:
     return type_string
 
 
-def is_valid_node_type_string(type_string: str, raise_on_false: bool = False) -> bool:
+def is_valid_node_type_string(type_string, raise_on_false=False):
     """Checks whether type string of a Node is valid.
 
     :param type_string: the plugin_type_string attribute of a Node
-    :return: True if type string is valid, else False
+    :return: True if type string is valid, else false
     """
     # Currently the type string for the top-level node is empty.
     # Change this when a consistent type string hierarchy is introduced.
@@ -135,7 +131,7 @@ def is_valid_node_type_string(type_string: str, raise_on_false: bool = False) ->
     return True
 
 
-def get_query_type_from_type_string(type_string: str) -> str:
+def get_query_type_from_type_string(type_string):
     """Take the type string of a Node and create the queryable type string
 
     :param type_string: the plugin_type_string attribute of a Node
@@ -159,5 +155,5 @@ class AbstractNodeMeta(EntityFieldMeta):
 
     def __new__(mcs, name, bases, namespace, **kwargs):  # noqa: N804
         newcls = super().__new__(mcs, name, bases, namespace, **kwargs)
-        newcls._logger = logging.getLogger(f"{namespace['__module__']}.{name}")  # type: ignore[attr-defined]
+        newcls._logger = logging.getLogger(f"{namespace['__module__']}.{name}")
         return newcls

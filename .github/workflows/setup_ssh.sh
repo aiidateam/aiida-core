c3c259ef62fa3991cfe6882d5cdc3f57cc696a04
Alexander Goscinski <alexander.goscinski@posteo.de>
2024-09-11 18:15:41 +0200

    Tests now use ssh key specified by env var AIIDA_PYTEST_SSH_KEY
    The tests were relying on `~/.ssh/id_rsa` to be used to allow ssh to
    localhost. This had the side effect that several tests specified a ssh
    key in their configuration that was then not working in the
    authentication but a fall back to the default ssh key still allowed a
    connection. This PR fixes this behaviour and uses also the ssh key
    specified for the connection. This also allows the user to specify a
    dedicated ssh key for aiida tests and does not enforce the usage of the
    default key.

--- .github/workflows/setup_ssh.sh
+++ .github/workflows/setup_ssh.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+# Sets up ssh keys to allow a ssh connection to localhost. This is needed
+# because localhost is used as remote address to run the tests locally.
 set -ev
 
-ssh-keygen -q -t rsa -b 4096 -N "" -f "${HOME}/.ssh/id_rsa"
-ssh-keygen -y -f "${HOME}/.ssh/id_rsa" >> "${HOME}/.ssh/authorized_keys"
+mkdir -p ${HOME}/.ssh
+ssh-keygen -q -t rsa -b 4096 -N "" -f "${HOME}/.ssh/id_rsa_aiida_pytest"
+ssh-keygen -y -f "${HOME}/.ssh/id_rsa_aiida_pytest" >> "${HOME}/.ssh/authorized_keys"
 ssh-keyscan -H localhost >> "${HOME}/.ssh/known_hosts"
+# to test core.ssh_auto transport plugin we need to append this to the config
+cat <<EOT >> ${HOME}/.ssh/config
+Host localhost
+        IdentityFile ${HOME}/.ssh/id_rsa_aiida_pytest
+EOT
+
 
 # The permissions on the GitHub runner are 777 which will cause SSH to refuse the keys and cause authentication to fail
 chmod 755 "${HOME}"

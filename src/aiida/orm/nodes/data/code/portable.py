5e086001750190550610865f28b4aa7c6dde4e19
Julian Geiger <julian.geiger@psi.ch>
2024-10-02 14:36:05 +0200

    Revert changes to `PortableCode` constructor

--- src/aiida/orm/nodes/data/code/portable.py
+++ src/aiida/orm/nodes/data/code/portable.py
@@ -90,8 +90,7 @@ class PortableCode(Code):
         super().__init__(**kwargs)
         type_check(filepath_files, pathlib.Path)
         self.filepath_executable = filepath_executable  # type: ignore[assignment]
-        # self.base.repository.put_object_from_tree(str(filepath_files))
-        self.filepath_files = filepath_files
+        self.base.repository.put_object_from_tree(str(filepath_files))
 
     def _validate(self):
         """Validate the instance by checking that an executable is defined and it is part of the repository files.
@@ -178,29 +177,3 @@ class PortableCode(Code):
             raise ValueError('The `filepath_executable` should not be absolute.')
 
         self.base.attributes.set(self._KEY_ATTRIBUTE_FILEPATH_EXECUTABLE, value)
-
-    @property
-    def filepath_files(self) -> str | pathlib.Path:
-        """Return the absolute path of the directory that contains the code files.
-
-        :return: The directory of the code files required for the `filepath_executable`.
-        """
-        return self._filepath_files
-
-    @filepath_files.setter
-    def filepath_files(self, value: t.Union[str, pathlib.Path]) -> None:
-        """Set the absolute path of the directory that contains the code files.
-
-        :param value: The absolute filepath of the directory of the uploaded code files.
-        """
-        type_check(value, (str, pathlib.Path))
-
-        if not pathlib.PurePosixPath(value).is_absolute():
-            raise ValueError('`filepath_files` must be an absolute path.')
-
-        if not pathlib.Path(value).is_dir():
-            raise ValueError('`filepath_files` must be an existing directory.')
-
-        self.base.repository.erase()
-        self.base.repository.put_object_from_tree(str(value))
-        self._filepath_files = value

8abbce5de8bf77040f60c134b5c62db4e3969dae
Julian Geiger <julian.geiger@psi.ch>
2024-10-30 16:47:47 +0100

    Add check for user x permissions to InstalledCode
    In `validate_filepath_executable` called for `verdi code test`

--- src/aiida/orm/nodes/data/code/installed.py
+++ src/aiida/orm/nodes/data/code/installed.py
@@ -137,6 +137,10 @@ class InstalledCode(Code):
             with override_log_level():  # Temporarily suppress noisy logging
                 with self.computer.get_transport() as transport:
                     file_exists = transport.isfile(str(self.filepath_executable))
+                    mode = transport.get_mode(str(self.filepath_executable))
+                    user_permissions = mode >> 6  # Shift right to get the user permission bits
+                    user_has_execute = (user_permissions & 1) != 0  # Check if the execute bit is set
+
         except Exception as exception:
             raise exceptions.ValidationError(
                 'Could not connect to the configured computer to determine whether the specified executable exists.'
@@ -146,6 +150,12 @@ class InstalledCode(Code):
             raise exceptions.ValidationError(
                 f'The provided remote absolute path `{self.filepath_executable}` does not exist on the computer.'
             )
+
+        if not user_has_execute:
+            raise exceptions.ValidationError(
+                f'The executable at the remote absolute path `{self.filepath_executable}` is not executable.'
+            )
+
 
     def can_run_on_computer(self, computer: Computer) -> bool:
         """Return whether the code can run on a given computer.

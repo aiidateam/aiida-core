861dca419e6f40ad50f6d5acf563067bdf638cbd
Alexander Goscinski <alexander.goscinski@posteo.de>
2024-11-25 17:25:42 +0100

    fix test

--- tests/cmdline/commands/test_computer.py
+++ tests/cmdline/commands/test_computer.py
@@ -796,7 +796,7 @@ class TestVerdiComputerCommands:
             filepath_executable='/remote/abs/path',
         ).store()
 
-        false_user_input = 'y'  # most common mistake
+        false_user_input = ''  # most common mistake
         user_input = 'yes'
 
         # Abort in case of wrong input

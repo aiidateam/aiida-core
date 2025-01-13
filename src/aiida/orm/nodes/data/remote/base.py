cf9b884adfe5176ebb4f912825a16130e9396e5b
Julian Geiger <julian.geiger@gmx.net>
2025-01-13 21:32:41 +0100

    Correct notes on usage of `du`.

--- src/aiida/orm/nodes/data/remote/base.py
+++ src/aiida/orm/nodes/data/remote/base.py
@@ -285,13 +285,20 @@ class RemoteData(Data):
         """
 
         try:
-            # Initially, we were using the `--bytes` option here. However, this is equivalent to `--apparent-size
-            # --block-size=1`, with the `--apparent-size` option being rather fragile (e.g. its implementation changed
-            # between the different version of `du` of Ubuntu 22.04 and 24.04, causing the tests to fail). Thus, we
-            # retain only `--block-size=1` to obtain the total size in bytes. Still, block sizes might be different for
-            # different disk formattings, OSs, etc. However, as 4096 bytes is the default for Linux and macOS, it should
-            # be fine to expect it in the corresponding tests. Lastly, the `-s` (`--summarize`) option yields only a
-            # single total file size value.
+            # Initially, we were using the `--bytes` option here. However, this is equivalent to `--apparent-size` and
+            # `--block-size=1`, with the `--apparent-size` option being rather fragile (e.g., its implementation changed
+            # between the different versions of `du` of Ubuntu 22.04 and 24.04, causing the tests to fail). Thus, we
+            # retain only `--block-size=1` to obtain the total size in **bytes** as an integer value. Note that
+            # `--block-size` here refers to the size of "blocks" for displaying the output of `du`, by default 1024
+            # bytes (1 KiB), **not** the "disk block size".
+
+            # On the other hand, for the parametrized tests of this functionality in `test_remote.py`, a **disk block
+            # size** of 4096 bytes (4 KiB) is assumed, meaning that a file with, e.g., only 1 or 10 bytes of actual
+            # content (its `apparent-size`) still occupies 4096 bytes (4 KiB) on disk. In principle, the disk block size
+            # can differ between different disk formattings, OS, etc., however, as 4096 bytes (4 KiB) is the default for
+            # Linux's ext4, as well as macOS, we assume for now that it is a reasonable assumption and default value.
+
+            # Lastly, the `-s` (`--summarize`) option yields only a single total file size value.
             retval, stdout, stderr = transport.exec_command_wait(f'du -s --block-size=1 {full_path}')
         except NotImplementedError as exc:
             raise NotImplementedError('`exec_command_wait` not implemented for the current transport plugin.') from exc

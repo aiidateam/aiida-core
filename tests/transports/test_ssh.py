60da561fa6044ea02182cfd98953b98567260335
Alexander Goscinski <alexander.goscinski@posteo.de>
2025-01-27 09:03:23 +0100

    comment out all other ssh test that might interfere

--- tests/transports/test_ssh.py
+++ tests/transports/test_ssh.py
@@ -17,57 +17,57 @@ from aiida.transports.plugins.ssh import SshTransport
 from aiida.transports.transport import TransportInternalError
 
 
-def test_closed_connection_ssh():
-    """Test calling command on a closed connection."""
-    with pytest.raises(TransportInternalError):
-        transport = SshTransport(machine='localhost')
-        transport._exec_command_internal('ls')
-
-
-def test_closed_connection_sftp():
-    """Test calling sftp command on a closed connection."""
-    with pytest.raises(TransportInternalError):
-        transport = SshTransport(machine='localhost')
-        transport.listdir()
-
-
-def test_auto_add_policy():
-    """Test the auto add policy."""
-    with SshTransport(machine='localhost', timeout=30, load_system_host_keys=True, key_policy='AutoAddPolicy'):
-        pass
-
-
-def test_proxy_jump():
-    """Test the connection with a proxy jump or several"""
-    with SshTransport(
-        machine='localhost', proxy_jump='localhost', timeout=30, load_system_host_keys=True, key_policy='AutoAddPolicy'
-    ):
-        pass
-
-    # kind of pointless, but should work and to check that proxy chaining works
-    with SshTransport(
-        machine='localhost',
-        proxy_jump='localhost,localhost,localhost',
-        timeout=30,
-        load_system_host_keys=True,
-        key_policy='AutoAddPolicy',
-    ):
-        pass
-
-
-def test_proxy_jump_invalid():
-    """Test proper error reporting when invalid host as a proxy"""
-    # import is also that when Python is running with debug warnings `-Wd`
-    # no unclosed files are reported.
-    with pytest.raises(paramiko.SSHException):
-        with SshTransport(
-            machine='localhost',
-            proxy_jump='localhost,nohost',
-            timeout=30,
-            load_system_host_keys=True,
-            key_policy='AutoAddPolicy',
-        ):
-            pass
+#def test_closed_connection_ssh():
+#    """Test calling command on a closed connection."""
+#    with pytest.raises(TransportInternalError):
+#        transport = SshTransport(machine='localhost')
+#        transport._exec_command_internal('ls')
+#
+#
+#def test_closed_connection_sftp():
+#    """Test calling sftp command on a closed connection."""
+#    with pytest.raises(TransportInternalError):
+#        transport = SshTransport(machine='localhost')
+#        transport.listdir()
+#
+#
+#def test_auto_add_policy():
+#    """Test the auto add policy."""
+#    with SshTransport(machine='localhost', timeout=30, load_system_host_keys=True, key_policy='AutoAddPolicy'):
+#        pass
+#
+#
+#def test_proxy_jump():
+#    """Test the connection with a proxy jump or several"""
+#    with SshTransport(
+#        machine='localhost', proxy_jump='localhost', timeout=30, load_system_host_keys=True, key_policy='AutoAddPolicy'
+#    ):
+#        pass
+#
+#    # kind of pointless, but should work and to check that proxy chaining works
+#    with SshTransport(
+#        machine='localhost',
+#        proxy_jump='localhost,localhost,localhost',
+#        timeout=30,
+#        load_system_host_keys=True,
+#        key_policy='AutoAddPolicy',
+#    ):
+#        pass
+#
+#
+#def test_proxy_jump_invalid():
+#    """Test proper error reporting when invalid host as a proxy"""
+#    # import is also that when Python is running with debug warnings `-Wd`
+#    # no unclosed files are reported.
+#    with pytest.raises(paramiko.SSHException):
+#        with SshTransport(
+#            machine='localhost',
+#            proxy_jump='localhost,nohost',
+#            timeout=30,
+#            load_system_host_keys=True,
+#            key_policy='AutoAddPolicy',
+#        ):
+#            pass
 
 
 def test_proxy_command():
@@ -80,54 +80,54 @@ def test_proxy_command():
         key_policy='AutoAddPolicy',
     ):
         pass
-
-
-def test_no_host_key():
-    """Test if there is no host key."""
-    # Disable logging to avoid output during test
-    logging.disable(logging.ERROR)
-
-    with pytest.raises(paramiko.SSHException):
-        with SshTransport(machine='localhost', timeout=30, load_system_host_keys=False):
-            pass
-
-    # Reset logging level
-    logging.disable(logging.NOTSET)
-
-
-def test_gotocomputer():
-    """Test gotocomputer"""
-    with SshTransport(
-        machine='localhost',
-        timeout=120,
-        use_login_shell=False,
-        key_policy='AutoAddPolicy',
-        proxy_command='ssh -W localhost:22 localhost',
-    ) as transport:
-        cmd_str = transport.gotocomputer_command('/remote_dir/')
-
-        expected_str = (
-            """ssh -t localhost -o ProxyCommand='ssh -W localhost:22 localhost'  "if [ -d '/remote_dir/' ] ;"""
-            """ then cd '/remote_dir/' ; bash  ; else echo '  ** The directory' ; """
-            """echo '  ** /remote_dir/' ; echo '  ** seems to have been deleted, I logout...' ; fi" """
-        )
-        assert cmd_str == expected_str
-
-
-def test_gotocomputer_proxyjump():
-    """Test gotocomputer"""
-    with SshTransport(
-        machine='localhost',
-        timeout=30,
-        use_login_shell=False,
-        key_policy='AutoAddPolicy',
-        proxy_jump='localhost',
-    ) as transport:
-        cmd_str = transport.gotocomputer_command('/remote_dir/')
-
-        expected_str = (
-            """ssh -t localhost -o ProxyJump='localhost'  "if [ -d '/remote_dir/' ] ;"""
-            """ then cd '/remote_dir/' ; bash  ; else echo '  ** The directory' ; """
-            """echo '  ** /remote_dir/' ; echo '  ** seems to have been deleted, I logout...' ; fi" """
-        )
-        assert cmd_str == expected_str
+#
+#
+#def test_no_host_key():
+#    """Test if there is no host key."""
+#    # Disable logging to avoid output during test
+#    logging.disable(logging.ERROR)
+#
+#    with pytest.raises(paramiko.SSHException):
+#        with SshTransport(machine='localhost', timeout=30, load_system_host_keys=False):
+#            pass
+#
+#    # Reset logging level
+#    logging.disable(logging.NOTSET)
+#
+#
+#def test_gotocomputer():
+#    """Test gotocomputer"""
+#    with SshTransport(
+#        machine='localhost',
+#        timeout=120,
+#        use_login_shell=False,
+#        key_policy='AutoAddPolicy',
+#        proxy_command='ssh -W localhost:22 localhost',
+#    ) as transport:
+#        cmd_str = transport.gotocomputer_command('/remote_dir/')
+#
+#        expected_str = (
+#            """ssh -t localhost -o ProxyCommand='ssh -W localhost:22 localhost'  "if [ -d '/remote_dir/' ] ;"""
+#            """ then cd '/remote_dir/' ; bash  ; else echo '  ** The directory' ; """
+#            """echo '  ** /remote_dir/' ; echo '  ** seems to have been deleted, I logout...' ; fi" """
+#        )
+#        assert cmd_str == expected_str
+#
+#
+#def test_gotocomputer_proxyjump():
+#    """Test gotocomputer"""
+#    with SshTransport(
+#        machine='localhost',
+#        timeout=30,
+#        use_login_shell=False,
+#        key_policy='AutoAddPolicy',
+#        proxy_jump='localhost',
+#    ) as transport:
+#        cmd_str = transport.gotocomputer_command('/remote_dir/')
+#
+#        expected_str = (
+#            """ssh -t localhost -o ProxyJump='localhost'  "if [ -d '/remote_dir/' ] ;"""
+#            """ then cd '/remote_dir/' ; bash  ; else echo '  ** The directory' ; """
+#            """echo '  ** /remote_dir/' ; echo '  ** seems to have been deleted, I logout...' ; fi" """
+#        )
+#        assert cmd_str == expected_str

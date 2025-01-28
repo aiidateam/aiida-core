b98c61adbb66618aedae1a6d7446b7b278ed73c2
Julian Geiger <julian.geiger@psi.ch>
2025-01-28 12:58:22 +0100

    Fix mypy complaints

--- tests/tools/dumping/test_process.py
+++ tests/tools/dumping/test_process.py
@@ -6,7 +6,7 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-"""Tests for the dumping of ProcessNode data to disk."""
+"""Tests for the dumping of process data to disk."""
 
 from __future__ import annotations
 

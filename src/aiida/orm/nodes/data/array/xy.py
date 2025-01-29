245e77b8ecf4ba65aabd8bbfa73f980904e4c979
ayushjariyal <ayushkumarAk3115@gmail.com>
2025-01-29 07:29:18 +0530

    Improve documation in XyData

--- src/aiida/orm/nodes/data/array/xy.py
+++ src/aiida/orm/nodes/data/array/xy.py
@@ -131,11 +131,11 @@ class XyData(ArrayData):
         except NotExistent as exc:
             raise ValueError('X array has not been set yet') from exc
         # validate each of the y_arrays
-        for num, (y_array, y_name, y_unit) in enumerate(zip(y_arrays, y_names, y_units)):
+        for y_array, y_name, y_unit in zip(y_array, y_names, y_units):
             self._arrayandname_validator(y_array, y_name, y_unit)
             if np.shape(y_array) != np.shape(x_array):
-                raise ValueError(f'y_array {y_name} did not have the same shape has the x_array!')
-            self.set_array(f'y_array_{num}', y_array)
+                raise ValueError(f'y_array {y_name} does not have the same shape as the x_array!')
+            self.set_array(y_name, y_array)
 
         # if the y_arrays pass the initial validation, sets each
         self.base.attributes.set('y_names', y_names)
@@ -173,9 +173,11 @@ class XyData(ArrayData):
         except (KeyError, AttributeError):
             raise NotExistent('No y units has been set yet!')
         y_arrays = []
-        try:
-            for i in range(len(y_names)):
-                y_arrays += [self.get_array(f'y_array_{i}')]
-        except (KeyError, AttributeError):
-            raise NotExistent(f'Could not retrieve array associated with y array {y_names[i]}')
+        for y_name in y_names:
+            try:
+                y_array = self.get_array(y_name)
+                y_arrays.append((y_name, y_arrays))
+            except (KeyError, AttributeError):
+                raise NotExistent(f'Could not retrieve array associated with y array {y_name}')
+            
         return list(zip(y_names, y_arrays, y_units))

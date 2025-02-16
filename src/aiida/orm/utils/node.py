2dee154a309463274fcd3c21b8c1cb529048a829
Muhammad Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2025-02-16 16:22:04 +0500

    Functional Change added

--- src/aiida/orm/utils/node.py
+++ src/aiida/orm/utils/node.py
@@ -60,7 +60,7 @@ def load_node_class(type_string):
             return Data
 
     if base_path.startswith('process'):
-        entry_point_name = base_path.removeprefix('nodes.')
+        entry_point_name = base_path.removeprefix('.nodes')
         return load_entry_point('aiida.node', entry_point_name)
 
     # At this point we really have an anomalous type string. At some point, storing nodes with unresolvable type strings

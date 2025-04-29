f131a1f818cfdc1e0137745e6b6f0a05562f2aef
Alexander Goscinski <alexander.goscinski@posteo.de>
2025-04-29 11:44:48 +0200

    use py3.9 compatible replacement for Path.walk

--- tests/orm/data/code/test_portable.py
+++ tests/orm/data/code/test_portable.py
@@ -132,8 +132,8 @@ filepath_files: {tmp_path}/some-label
     assert result.decode() == ref_result
     repo_dump_path = tmp_path / code.label
     # In this case portablecode takes care of the dumping
-    dumped_files = {f.relative_to(repo_dump_path) for f, _, _ in repo_dump_path.walk()}
-    inserted_files = {f.relative_to(filepath_files) for f, _, _ in filepath_files.walk()}
+    dumped_files = {f.relative_to(repo_dump_path) for f in repo_dump_path.rglob('*')}
+    inserted_files = {f.relative_to(filepath_files) for f in filepath_files.rglob('*')}
     assert dumped_files == inserted_files
 
 

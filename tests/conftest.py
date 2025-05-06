474e0fabcb4e2331253e10c1fbcd8ba6a323f6d2
Ali Khosravi <khsrali@gmail.com>
2025-05-06 16:27:46 +0200

    Transport: Three bug fixed in `test_execmanager` and `AsyncSshTransport` (#6855)
    This commit fixes, three bugs! 
    Everything is squashed to one commit as they are co-dependent and separating them will not do much benefit. 
    
    First bug fix:
    - Adopt `AsyncSshTransport` with the *COUNTER-INTUITIVE* and expected behavior of engine: 
    `transport.copy()` should raise `FileNotFoundError` if remote source, or remote destination are not found. 
    This later, will result in silent failure of a process, handled here: `execmanager.py`::_copy_remote_files
    
    Second bug fix:
    - `AsyncTransport::run_command_blocking` had to use the current event loop of the manager instead of creating a new one. This likely increases efficiency quite a lot, by removing unnecessarily dead locks. 
    - `test_execmanager.py::node_and_calc_info` was not being run for `core.ssh_async` this was the reason that let go of these bugs un-notice. Fixed via this commit.
    
    Third bug fix:
    - Converts all sync tests in `test_execmanager.py` that where `runner.loop.run_until_complete()` into async tests. This prevents attaching a `Future` to a wrong event loop via `asyncssh`
    - Improves `event_loop` fixture:
    Formally always required a profile to be loaded, therefore the event loop was not cleaned before passing to a new test. Now passes `aiida_profile_clean` to `event_loop` fixture, so that a profile is loaded for all async tests, and also the event loop is cleaned for each test. This is done because `aiida_profile_clean` also triggers `manager.reset_profile()`.
    
    ---------
    
    Co-authored-by: Alexander Goscinski <alex.goscinski@posteo.de>

--- tests/conftest.py
+++ tests/conftest.py
@@ -525,8 +525,10 @@ def runner(manager):
 
 
 @pytest.fixture
-def event_loop(manager):
-    """Get the event loop instance of the currently loaded profile.
+def event_loop(manager, aiida_profile_clean):
+    """Get the event loop instance of a cleaned profile.
+    This works, because ``aiida_profile_clean`` fixture, apart from loading a profile and cleaning it,
+    and also triggers ``manager.reset_profile()`` which clears the event loop.
 
     This is automatically called as a fixture for any test marked with ``@pytest.mark.asyncio``.
     """

27af8a84288b31702099b78a83385d353a245aa9
Lorenzo <79980269+bastonero@users.noreply.github.com>
2025-05-20 15:48:11 +0000

    Add `tools` property and entrypoint for workflows
    Fixes #6865

--- src/aiida/orm/nodes/process/workflow/workchain.py
+++ src/aiida/orm/nodes/process/workflow/workchain.py
@@ -8,11 +8,15 @@
 ###########################################################################
 """Module with `Node` sub class for workchain processes."""
 
-from typing import Optional, Tuple
+from typing import TYPE_CHECKING, Optional, Tuple
 
 from aiida.common.lang import classproperty
+from aiida.common import exceptions
 
 from .workflow import WorkflowNode
+
+if TYPE_CHECKING:
+    from aiida.tools.workflows import WorkflowTools
 
 __all__ = ('WorkChainNode',)
 
@@ -21,6 +25,41 @@ class WorkChainNode(WorkflowNode):
     """ORM class for all nodes representing the execution of a WorkChain."""
 
     STEPPER_STATE_INFO_KEY = 'stepper_state_info'
+
+    # An optional entry point for a CalculationTools instance
+    _tools = None
+
+    @property
+    def tools(self) -> 'WorkflowTools':
+        """Return the calculation tools that are registered for the process type associated with this calculation.
+
+        If the entry point name stored in the `process_type` of the CalcJobNode has an accompanying entry point in the
+        `aiida.tools.calculations` entry point category, it will attempt to load the entry point and instantiate it
+        passing the node to the constructor. If the entry point does not exist, cannot be resolved or loaded, a warning
+        will be logged and the base CalculationTools class will be instantiated and returned.
+
+        :return: CalculationTools instance
+        """
+        from aiida.plugins.entry_point import get_entry_point_from_string, is_valid_entry_point_string, load_entry_point
+        from aiida.tools.workflows import WorkflowTools
+
+        if self._tools is None:
+            entry_point_string = self.process_type
+
+            if entry_point_string and is_valid_entry_point_string(entry_point_string):
+                entry_point = get_entry_point_from_string(entry_point_string)
+
+                try:
+                    tools_class = load_entry_point('aiida.tools.workflows', entry_point.name)
+                    self._tools = tools_class(self)
+                except exceptions.EntryPointError as exception:
+                    self._tools = WorkflowTools(self)
+                    self.logger.warning(
+                        f'could not load the workflow tools entry point {entry_point.name}: {exception}'
+                    )
+
+        return self._tools
+
 
     @classproperty
     def _updatable_attributes(cls) -> Tuple[str, ...]:  # type: ignore[override]  # noqa: N805

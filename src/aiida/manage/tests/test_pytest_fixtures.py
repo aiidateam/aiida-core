874c014bf94438a1b9fbe4e3a5a0c4599e4e49d9
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-06-04 15:19:50 +0100

    Better tests for legacy fixtures

--- src/aiida/manage/tests/test_pytest_fixtures.py
+++ src/aiida/manage/tests/test_pytest_fixtures.py
@@ -1,20 +1,46 @@
 """Tests for the :mod:`aiida.manage.tests.pytest_fixtures` module."""
 
 import uuid
+from pathlib import Path
 
 import pytest
 
-from aiida.manage.configuration import get_config
-from aiida.manage.configuration.config import Config
+from aiida.manage.configuration import get_config, load_config
+from aiida.manage.configuration.settings import DEFAULT_CONFIG_FILE_NAME
 from aiida.orm import Computer
 from aiida.transports import BlockingTransport
 
 pytest_plugins = ['aiida.manage.tests.pytest_fixtures']
 
 
-def test_profile_config():
-    """Check that the config file created with the test profile passes validation."""
-    Config.from_file(get_config().filepath)
+def test_aiida_config(tmp_path_factory):
+    """Test that ``aiida_config`` fixture is loaded by default and creates a config instance in temp directory."""
+    from aiida.manage.configuration import CONFIG
+
+    config = get_config(create=False)
+    assert config is CONFIG
+    assert config.dirpath.startswith(str(tmp_path_factory.getbasetemp()))
+    assert Path(config.dirpath, DEFAULT_CONFIG_FILE_NAME).is_file()
+    assert config._default_profile
+
+
+def test_aiida_config_file(tmp_path_factory):
+    """Test that ``aiida_config`` fixture stores the configuration in a config file in a temp directory."""
+    # Unlike get_config, load_config always loads the configuration from a file
+    config = load_config(create=False)
+    assert config.dirpath.startswith(str(tmp_path_factory.getbasetemp()))
+    assert Path(config.dirpath, DEFAULT_CONFIG_FILE_NAME).is_file()
+    assert config._default_profile
+
+
+def test_aiida_profile():
+    """Test that ``aiida_profile`` fixture is loaded by default and loads a temporary test profile."""
+    from aiida.manage.configuration import get_profile
+    from aiida.manage.configuration.profile import Profile
+
+    profile = get_profile()
+    assert isinstance(profile, Profile)
+    assert profile.is_test_profile
 
 
 def test_aiida_localhost(aiida_localhost):

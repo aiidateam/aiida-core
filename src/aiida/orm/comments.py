64c9b3092292a8810de175e10fb581cdf978a315
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-07-06 19:58:27 +0100

    Try fixing Comment.set_user

--- src/aiida/orm/comments.py
+++ src/aiida/orm/comments.py
@@ -149,8 +149,7 @@ class Comment(entities.Entity['BackendComment', CommentCollection]):
         return entities.from_backend_entity(User, self._backend_entity.user)
 
     def set_user(self, value: 'User') -> None:
-        # ignoring mypy error: Property "user" defined in "BackendComment" is read-only
-        self._backend_entity.user = value.backend_entity  # type: ignore[misc]
+        self._backend_entity.set_user(value.backend_entity)
 
     @property
     def content(self) -> str:

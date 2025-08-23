91e4ad5be90854e51d10998d0579db0439028aba
Edan Bainglass <edan.bainglass@gmail.com>
2025-08-23 18:11:42 +0200

    Update `RemoteStashData` and subclasses
    Notes:
    - all submodels share the same fields. Consider lifting up to base class. Ping @khsrali

--- src/aiida/orm/nodes/data/remote/stash/custom.py
+++ src/aiida/orm/nodes/data/remote/stash/custom.py
@@ -25,27 +25,31 @@ class RemoteStashCustomData(RemoteStashData):
     _storable = True
 
     class Model(RemoteStashData.Model):
-        target_basepath: str = MetadataField(description='The the target basepath')
-        source_list: List[str] = MetadataField(description='The list of source files that were stashed')
+        target_basepath: str = MetadataField(
+            description='The target basepath',
+        )
+        source_list: List[str] = MetadataField(
+            description='The list of source files that were stashed',
+        )
 
     def __init__(
         self,
         stash_mode: StashMode,
         target_basepath: str,
-        source_list: List,
+        source_list: List[str],
         **kwargs,
     ):
         """Construct a new instance
 
         :param stash_mode: the stashing mode with which the data was stashed on the remote.
         """
+        if stash_mode != StashMode.SUBMIT_CUSTOM_CODE:
+            raise ValueError('`RemoteStashCustomData` can only be used with `stash_mode == StashMode.COPY`.')
+
         super().__init__(stash_mode, **kwargs)
 
         self.target_basepath = target_basepath
         self.source_list = source_list
-
-        if stash_mode != StashMode.SUBMIT_CUSTOM_CODE:
-            raise ValueError('`RemoteStashCustomData` can only be used with `stash_mode == StashMode.COPY`.')
 
     @property
     def target_basepath(self) -> str:

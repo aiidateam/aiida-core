01fc2794e72e86cb12b6e7fc0cd6e2252caa4f0a
Julian Geiger <julian.geiger@gmx.net>
2025-09-15 09:55:22 +0200

    same `tests/data` structure as in #6993

--- tests/storage/psql_dos/test_groups.py
+++ tests/storage/psql_dos/test_groups.py
@@ -19,9 +19,11 @@ from aiida.tools.archive import import_archive
 @pytest.mark.requires_psql
 @pytest.mark.usefixtures('aiida_profile_clean')
 def test_group_bulk_operations():
-    """Regression test for PostgreSQL parameter limit issue (6545) using pre-created archive."""
+    """
+    Regression test for the PostgreSQL parameter limit OperationalError on add_nodes using a pre-created archive.
+    """
     num_nodes = 50_000
-    archive_path = Path(__file__).parent / 'data' / f'{int(num_nodes/1000)}k-int-nodes-2.7.1.post0.aiida'
+    archive_path = Path(__file__).parents[2] / 'data' / f'{int(num_nodes/1000)}k-int-nodes-2.7.1.post0.aiida'
     import_archive(archive_path)
 
     qb = orm.QueryBuilder()

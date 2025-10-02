333882ce0dd3c19c425f9657d28050df64514a6d
Julian Geiger <julian.geiger@gmx.net>
2025-10-02 13:03:21 +0200

    add also import test to regression

--- tests/tools/archive/test_regression.py
+++ tests/tools/archive/test_regression.py
@@ -6,18 +6,17 @@
 # For further information on the license, see the LICENSE.txt file        #
 # For further information please visit http://www.aiida.net               #
 ###########################################################################
-"""Regression tests for ``OperationalError``s on archive import/export"""
+"""Regression tests for large archive import/exports, e.g., to capture ``OperationalError``s."""
 
 import pytest
 
 from aiida import orm
-from aiida.tools.archive import create_archive
+from aiida.tools.archive import create_archive, import_archive
 
 
-@pytest.mark.timeout(1000)  # Should finish in ~200s
+@pytest.mark.timeout(1000)  # Should finish in ~300s
 @pytest.mark.nightly
-@pytest.mark.usefixtures('aiida_profile_clean')
-def test_large_archive_export_operr_regression(pytestconfig, tmp_path, create_int_nodes):
+def test_large_archive_export_operr_regression(pytestconfig, tmp_path, create_int_nodes, aiida_profile_clean):
     """Regression test: ensure OperationalError occurs (not) with too large (sufficiently small) filter_size."""
 
     num_nodes = 66_000  # Minimum value that OpErr is raised
@@ -29,6 +28,9 @@ def test_large_archive_export_operr_regression(pytestconfig, tmp_path, create_in
     # doesn't apply any QB filters is called
     orm_nodes = orm.QueryBuilder().append(orm.Node).all(flat=True)
 
-    # NOTE: This passes for both backends
+    # NOTE: Both, import and export should pass with both backends
     export_file = tmp_path / 'should_work.aiida'
-    create_archive(entities=orm_nodes, filename=export_file, filter_size=1000)
+    create_archive(entities=orm_nodes, filename=export_file)
+
+    aiida_profile_clean.reset_storage()
+    import_archive(path=export_file)

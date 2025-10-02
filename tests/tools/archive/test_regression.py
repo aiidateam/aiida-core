557a68ac64477dbbcca522398013b3928df0de33
Julian Geiger <julian.geiger@gmx.net>
2025-10-02 12:40:46 +0200

    remove failing regression test for archive creation with psql

--- tests/tools/archive/test_regression.py
+++ tests/tools/archive/test_regression.py
@@ -20,35 +20,15 @@ from aiida.tools.archive import create_archive
 def test_large_archive_export_operr_regression(pytestconfig, tmp_path, create_int_nodes):
     """Regression test: ensure OperationalError occurs (not) with too large (sufficiently small) filter_size."""
 
-    from sqlalchemy.exc import OperationalError
-
-    from aiida.manage import get_manager
-
     num_nodes = 66_000  # Minimum value that OpErr is raised
 
     _ = create_int_nodes(num_nodes)
 
-    # NOTE: Need to obtain a selection of `orm.Node`s, otherwise, with `entities=None`, where the profile is exported in
-    # full, no QB filters are applied, as the `_collect_all_entities` method is called
+    # NOTE: Need to obtain a selection of `orm.Node`s, otherwise, with `entities=None`,
+    # where the profile is exported in full the `_collect_all_entities` method that
+    # doesn't apply any QB filters is called
     orm_nodes = orm.QueryBuilder().append(orm.Node).all(flat=True)
 
     # NOTE: This passes for both backends
     export_file = tmp_path / 'should_work.aiida'
     create_archive(entities=orm_nodes, filename=export_file, filter_size=1000)
-
-    # NOTE: Run the failing test at the end as the exception leaves sqlachemy in an erroneus state,
-    # so the `orm_nodes` list cannot be re-used. Trying to do so gives:
-    # sqlalchemy.orm.exc.DetachedInstanceError: Instance <DbNode at 0x75d3826f1b40> is not bound to a Session;
-    # attribute refresh operation cannot proceed (Background on this error at: https://sqlalche.me/e/20/bhk3)
-    # .venv/lib/python3.10/site-packages/sqlalchemy/orm/loading.py:1603: DetachedInstanceError
-
-    db_backend = pytestconfig.getoption('--db-backend')
-
-    if db_backend.value == 'psql':
-        export_file_fail = tmp_path / 'should_fail.aiida'
-        with pytest.raises(OperationalError, match='number of parameters must be between 0 and 65535'):
-            # Using `filter_size=num_nodes` effectively disables it
-            create_archive(entities=orm_nodes, filename=export_file_fail, filter_size=num_nodes)
-
-        # Explicitly session clean-up after the error
-        get_manager().get_profile_storage().get_session().rollback()

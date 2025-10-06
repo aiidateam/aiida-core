b9d50fd8ac21de545d220a84d3012ea0b543dff4
ayushjariyal <148481273+ayushjariyal@users.noreply.github.com>
2025-10-06 18:24:54 +0530

    Show first and last node `ctime` for `verdi storage info --detailed` (#6829)
    Co-authored-by: Julian Geiger <julian.geiger@gmx.net>
    Co-authored-by: Daniel Hollas <daniel.hollas@bristol.ac.uk>
    
    Cherry-pick: f9566a4d4ef73e33c6286945e60d5ae9a5faceff

--- src/aiida/orm/implementation/storage_backend.py
+++ src/aiida/orm/implementation/storage_backend.py
@@ -471,6 +471,22 @@ class StorageBackend(abc.ABC):
         count = QueryBuilder(self).append(Node).count()
         data['Nodes'] = {'count': count}
         if detailed:
+            first_time = (
+                QueryBuilder(self)
+                .append(Node, project=['ctime'], tag='node')
+                .order_by({'node': {'ctime': 'asc'}})
+                .first(flat=True)
+            )
+            last_time = (
+                QueryBuilder(self)
+                .append(Node, project=['ctime'], tag='node')
+                .order_by({'node': {'ctime': 'desc'}})
+                .first(flat=True)
+            )
+
+            data['Nodes']['first_created'] = str(first_time) if first_time else None
+            data['Nodes']['last_created'] = str(last_time) if last_time else None
+
             node_types = sorted(
                 {typ for (typ,) in QueryBuilder(self).append(Node, project=['node_type']).iterall() if typ is not None}
             )

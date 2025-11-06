a415b842e4a8c33e1473e592153efed7befed2f5
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-11-06 01:10:02 -0500

    Don't re-export aiida.brokers.rabbitmq stuff

--- src/aiida/brokers/__init__.py
+++ src/aiida/brokers/__init__.py
@@ -4,14 +4,7 @@ from .broker import *
 from .rabbitmq import *
 
 __all__ = (
-    'BROKER_DEFAULTS',
     'Broker',
-    'ManagementApiConnectionError',
     'RabbitmqBroker',
-    'RabbitmqManagementClient',
-    'get_launch_queue_name',
-    'get_message_exchange_name',
-    'get_rmq_url',
-    'get_task_exchange_name',
 )
 # fmt: on
--- src/aiida/brokers/rabbitmq/__init__.py
+++ src/aiida/brokers/rabbitmq/__init__.py
@@ -1,18 +1,8 @@
 # AUTO-GENERATED
 # fmt: off
 from .broker import *
-from .client import *
-from .defaults import *
-from .utils import *
 
 __all__ = (
-    'BROKER_DEFAULTS',
-    'ManagementApiConnectionError',
     'RabbitmqBroker',
-    'RabbitmqManagementClient',
-    'get_launch_queue_name',
-    'get_message_exchange_name',
-    'get_rmq_url',
-    'get_task_exchange_name',
 )
 # fmt: on

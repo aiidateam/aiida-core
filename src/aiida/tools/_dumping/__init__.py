5b979c9736d37a1ebbf4f3e759795d9737fcd605
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-11-06 02:07:57 -0500

    Fix src/aiida/tools/_dumping/__init__.py

--- src/aiida/tools/_dumping/__init__.py
+++ src/aiida/tools/_dumping/__init__.py
@@ -8,39 +8,6 @@
 ###########################################################################
 """Public API for data dumping functionality."""
 
-# AUTO-GENERATED
-# fmt: off
-from .config import *
-from .detect import *
-from .executors import *
-from .utils import *
+from aiida.tools._dumping.facades import ProcessDumper
 
-__all__ = (
-    'DUMP_PROGRESS_BAR_FORMAT',
-    'ORM_TYPE_TO_REGISTRY',
-    'REGISTRY_TO_ORM_TYPE',
-    'DeletionExecutor',
-    'DumpChangeDetector',
-    'DumpMode',
-    'DumpPaths',
-    'DumpTimes',
-    'GroupChanges',
-    'GroupDumpConfig',
-    'GroupDumpExecutor',
-    'GroupDumpScope',
-    'GroupInfo',
-    'GroupRenameInfo',
-    'NodeChanges',
-    'NodeMembershipChange',
-    'NodeMetadataWriter',
-    'NodeRepoIoDumper',
-    'ProcessDumpConfig',
-    'ProcessDumpExecutor',
-    'ProcessingQueue',
-    'ProfileDumpConfig',
-    'ProfileDumpExecutor',
-    'ReadmeGenerator',
-    'RegistryNameType',
-    'WorkflowWalker',
-)
-# fmt: on
+__all__ = ('ProcessDumper',)

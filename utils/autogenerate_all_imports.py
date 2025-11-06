d0bfc3b494915bcbf7416902e972427c41e5b198
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-11-05 22:51:34 -0500

    Fix script to autogenerate __all__
    This script was not working ever since we moved
    to the src/aiida layout. :-(
    
    This also includes:
    - fixes for Python 3.14 (deprecations from ast module)
    - preservation of `from __future__` imports
    - fixes for compatibility with ruff

--- utils/autogenerate_all_imports.py
+++ utils/autogenerate_all_imports.py
@@ -6,17 +6,21 @@ import sys
 from collections import Counter
 from pathlib import Path
 from pprint import pprint
-from typing import Dict, List, Optional, Tuple
+from typing import Optional
 
+AUTO_GENERATED = "AUTO-GENERATED"
 
-def parse_all(folder_path: str) -> Tuple[dict, dict]:
+def parse_all(folder_path: Path) -> tuple[dict, dict]:
     """Walk through all files in folder, and parse the ``__all__`` variable.
 
     :return: (all dict, dict of unparsable)
     """
-    folder_path = Path(folder_path)
     all_dict = {}
     bad_all = {}
+
+    def is_ast_str(el) -> bool:
+        """Replacement for deprecated ast.Str"""
+        return isinstance(el, ast.Constant) and isinstance(el.value, str)
 
     for path in folder_path.glob('**/*.py'):
         # skip module files
@@ -35,19 +39,19 @@ def parse_all(folder_path: str) -> Tuple[dict, dict]:
                 all_token = token
                 break
 
+        # If the module doesn't define __all__, skip it
         if all_token is None:
-            bad_all.setdefault('missing', []).append(str(path.relative_to(folder_path)))
             continue
 
         if not isinstance(all_token.value, (ast.List, ast.Tuple)):
-            bad_all.setdefault('value not list/tuple', []).append(str(path.relative_to(folder_path)))
+            bad_all.setdefault('__all__ is not list/tuple', []).append(str(path.relative_to(folder_path)))
             continue
 
-        if not all(isinstance(el, ast.Str) for el in all_token.value.elts):
-            bad_all.setdefault('child not strings', []).append(str(path.relative_to(folder_path)))
+        if not all(is_ast_str(el) for el in all_token.value.elts):
+            bad_all.setdefault('__all__ contains non-string elements', []).append(str(path.relative_to(folder_path)))
             continue
 
-        names = [n.s for n in all_token.value.elts]
+        names = [n.value for n in all_token.value.elts]
         if not names:
             continue
 
@@ -60,8 +64,8 @@ def parse_all(folder_path: str) -> Tuple[dict, dict]:
 
 
 def gather_all(
-    cur_path: List[str], cur_dict: dict, skip_children: dict, all_list: Optional[List[str]] = None
-) -> List[str]:
+    cur_path: list[str], cur_dict: dict, skip_children: dict, all_list: Optional[list[str]] = None
+) -> list[str]:
     """Recursively gather __all__ names."""
     all_list = [] if all_list is None else all_list
     for key, val in cur_dict.items():
@@ -72,12 +76,11 @@ def gather_all(
     return all_list
 
 
-def write_inits(folder_path: str, all_dict: dict, skip_children: Dict[str, List[str]]) -> Dict[str, List[str]]:
+def write_inits(folder_path: Path, all_dict: dict, skip_children: dict[str, list[str]]) -> dict[str, list[str]]:
     """Write __init__.py files for all subfolders.
 
     :return: folders with non-unique imports
     """
-    folder_path = Path(folder_path)
     non_unique = {}
     for path in folder_path.glob('**/__init__.py'):
         if path.parent == folder_path:
@@ -99,7 +102,7 @@ def write_inits(folder_path: str, all_dict: dict, skip_children: Dict[str, List[
         if '*' in skip_children.get(rel_path, []):
             path_all_dict = {}
             alls = []
-            auto_content = ['', '# AUTO-GENERATED', '', '__all__ = ()', '']
+            auto_content = ['', f'# {AUTO_GENERATED}', '', '__all__ = ()', '']
         else:
             path_all_dict = {
                 key: val for key, val in path_all_dict.items() if key not in skip_children.get(rel_path, [])
@@ -111,12 +114,12 @@ def write_inits(folder_path: str, all_dict: dict, skip_children: Dict[str, List[
                 non_unique[rel_path] = [k for k, v in Counter(alls + list(path_all_dict)).items() if v > 1]
 
             auto_content = (
-                ['', '# AUTO-GENERATED']
-                + ['', '# fmt: off', '']
+                [f'# {AUTO_GENERATED}']
+                + ['# fmt: off']
                 + [f'from .{mod} import *' for mod in sorted(path_all_dict.keys())]
                 + ['', '__all__ = (']
                 + [f'    {a!r},' for a in sorted(set(alls))]
-                + [')', '', '# fmt: on', '']
+                + [')', '# fmt: on']
             )
 
         start_content = []
@@ -124,13 +127,21 @@ def write_inits(folder_path: str, all_dict: dict, skip_children: Dict[str, List[
         in_docstring = in_end_content = False
         in_start_content = True
         for line in path.read_text(encoding='utf8').splitlines():
-            if not in_start_content and line.startswith('# END AUTO-GENERATED'):
+            if not in_start_content and line.startswith(f'# END {AUTO_GENERATED}'):
                 in_end_content = True
             if in_end_content:
                 end_content.append(line)
                 continue
-            # only use initial comments and docstring
-            if not (line.startswith('#') or line.startswith('"""') or in_docstring):
+            # only use initial comments and docstring and __future__ imports
+            # We also need to retain any empty lines, since ruff formatter automatically
+            # injects empty line between a module docstring and __future__ import
+            if not (
+                line == '' or
+                (line.startswith('#') and not line == f'# {AUTO_GENERATED}')
+                or line.startswith('"""')
+                or line.startswith('from __future__ import')
+                or in_docstring
+            ):
                 in_start_content = False
                 continue
             if line.startswith('"""'):
@@ -149,7 +160,7 @@ def write_inits(folder_path: str, all_dict: dict, skip_children: Dict[str, List[
 
 
 if __name__ == '__main__':
-    _folder = Path(__file__).parent.parent.joinpath('aiida')
+    _folder = Path(__file__).parent.parent.joinpath('src', 'aiida')
     _skip = {
         # skipped since some arguments and options share the same name
         'cmdline/params': ['arguments', 'options'],
@@ -164,13 +175,14 @@ if __name__ == '__main__':
         'tools': ['archive'],
     }
     _all_dict, _bad_all = parse_all(_folder)
+    assert _all_dict, 'Did not find aiida source folder!'
     _non_unique = write_inits(_folder, _all_dict, _skip)
-    _bad_all.pop('missing', '')  # allow missing __all__
     if _bad_all:
-        print('unparsable __all__:')
-        pprint(_bad_all)
+        print('ERROR: found unparsable __all__:')
+        for reason in _bad_all:
+            print(f'{reason} in modules:', _bad_all[reason])
     if _non_unique:
-        print('non-unique imports:')
+        print('ERROR: non-unique imports:')
         pprint(_non_unique)
     if _bad_all or _non_unique:
         sys.exit(1)

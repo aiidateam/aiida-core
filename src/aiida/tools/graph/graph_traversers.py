0db32faef3603c6679a71b36bf21a1595845660d
Julian Geiger <julian.geiger@gmx.net>
2025-11-12 09:33:01 +0100

    remove previously added batch_iter

--- src/aiida/tools/graph/graph_traversers.py
+++ src/aiida/tools/graph/graph_traversers.py
@@ -15,7 +15,6 @@ from typing import TYPE_CHECKING, Any, Callable, Dict, Iterable, List, Optional,
 from aiida import orm
 from aiida.common import exceptions
 from aiida.common.links import GraphTraversalRules, LinkType
-from aiida.common.utils import DEFAULT_FILTER_SIZE
 from aiida.orm.utils.links import LinkQuadruple
 from aiida.tools.graph.age_entities import Basket
 from aiida.tools.graph.age_rules import RuleSaveWalkers, RuleSequence, RuleSetWalkers, UpdateRule
@@ -208,8 +207,6 @@ def traverse_graph(
     """
     from numpy import inf
 
-    from aiida.common.utils import batch_iter
-
     if max_iterations is None:
         max_iterations = cast(int, inf)
     elif not (isinstance(max_iterations, int) or max_iterations is inf):  # type: ignore[unreachable]
@@ -241,12 +238,9 @@ def traverse_graph(
             return {'nodes': set(), 'links': set()}
         return {'nodes': set(), 'links': None}
 
-    existing_pks = set()
-
-    for _, batch_ids in batch_iter(operational_set, DEFAULT_FILTER_SIZE):
-        query_nodes = orm.QueryBuilder(backend=backend)
-        query_nodes.append(orm.Node, project=['id'], filters={'id': {'in': batch_ids}})
-        existing_pks.update(query_nodes.all(flat=True))
+    query_nodes = orm.QueryBuilder(backend=backend)
+    query_nodes.append(orm.Node, project=['id'], filters={'id': {'in': operational_set}})
+    existing_pks = set(query_nodes.all(flat=True))
 
     missing_pks = operational_set.difference(existing_pks)
     if missing_pks and missing_callback is None:

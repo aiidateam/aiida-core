fdb270fe47b326fcdf2ec1ced23450e1ce956585
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-11-21 15:08:20 +0000

    async_ssh: Use async semaphore instead of manual locking (#7018)
    This should improve performance of async_ssh plugin
    for lots of small files since we're no longer sleeping
    500ms when waiting for a lock.
    
    Also makes the code more safe since one doesn't need
    to manually lock and unlock, and fixes a bug since the
    number of file IO connections was not decreased
    when an OsError exception was thrown.
    
    Cherry-pick: ad5cafdb10ff3889e53c3a7473cadc566b3ecb78

---
+++ tests/transports/test_asyncssh_plugin.py
@@ -0,0 +1,43 @@
+import os
+import stat
+
+import pytest
+
+from aiida.transports.plugins.ssh_async import AsyncSshTransport
+
+
+@pytest.mark.asyncio
+async def test_locking_problem(tmp_path_factory):
+    """Regression test for issue https://github.com/aiidateam/aiida-core/issues/7106"""
+
+    local_dir = tmp_path_factory.mktemp('local')
+
+    # Create a file and remove its read permissions from ourselves,
+    # so that we cannot upload it through the transport
+    unreadable_file = local_dir / 'unreadable'
+    with open(unreadable_file, 'w') as f:
+        f.write('test file without read permissions\n')
+    # Grant write permissions (so we can remove the file after the test),
+    # but not read permissions. Corresponds to `chmod 220`
+    os.chmod(unreadable_file, stat.S_IWGRP | stat.S_IWUSR)
+
+    transport_params = {
+        'machine': 'localhost',
+        'backend': 'asyncssh',
+        'max_io_allowed': 1,
+    }
+    async_transport = AsyncSshTransport(**transport_params)
+
+    assert async_transport.max_io_allowed == 1
+
+    with async_transport as transport:
+        with pytest.raises(OSError, match='Error while downloading file'):
+            await transport.getfile_async('non_existing', local_dir)
+
+        with pytest.raises(OSError, match='Error while uploading file'):
+            await transport.puttree_async(local_dir, 'target_dir')
+
+        with pytest.raises(OSError, match='Error while downloading file'):
+            await transport.getfile_async('non_existing', local_dir)
+
+    assert transport._semaphore._value == 1

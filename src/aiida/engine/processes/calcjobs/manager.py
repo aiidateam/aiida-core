3dc5f175cc1785553e04e34c7865fe020ee3b2e5
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-12-02 15:42:51 +0000

    logger.debug -> logger.warning

--- src/aiida/engine/processes/calcjobs/manager.py
+++ src/aiida/engine/processes/calcjobs/manager.py
@@ -156,7 +156,7 @@ class JobsList:
             for job_id in self._polling_jobs:
                 future = self._job_update_requests.pop(job_id, None)  # type: ignore[arg-type]
                 if future is None:
-                    self.logger.debug(  # type: ignore[unreachable]
+                    self.logger.warning(  # type: ignore[unreachable]
                         f'This should not happen: polled job_id {job_id} '
                         f'not in _job_update_requests {self._job_update_requests}'
                     )

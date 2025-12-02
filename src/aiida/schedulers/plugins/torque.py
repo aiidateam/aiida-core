db8d874f7a02a8badbf5cf61d7b5f9205c8e4dca
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-12-02 17:30:01 +0000

    Strict typing for aiida.schedulers module
    Some scheduler plugins are still esxcluded
    from type-checking, will deal with that separately.

--- src/aiida/schedulers/plugins/torque.py
+++ src/aiida/schedulers/plugins/torque.py
@@ -42,8 +42,13 @@ class TorqueScheduler(PbsBaseClass):
     # _map_status = _map_status_pbs_common
 
     def _get_resource_lines(
-        self, num_machines, num_mpiprocs_per_machine, num_cores_per_machine, max_memory_kb, max_wallclock_seconds
-    ):
+        self,
+        num_machines: int,
+        num_mpiprocs_per_machine: int | None,
+        num_cores_per_machine: int | None,
+        max_memory_kb: int | None,
+        max_wallclock_seconds: int | None,
+    ) -> list[str]:
         """Return the lines for machines, memory and wallclock relative
         to pbspro.
         """

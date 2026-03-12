3be99a61a62b1c552e7a3e190305c053d9d643ba
Marnik Bercx <mbercx@gmail.com>
2026-03-12 11:14:40 +0100

    👌 `ProcessBuilder`: prune empty namespaces in `_repr_pretty` (#7285)
    When displaying a `ProcessBuilder`, namespaces (such as `stash`, `unstash`, ...)
    were included in the output even when empty, cluttering the representation and confusing
    users.
    
    Here we improve the representation by passing the `_inputs` to the `safe_dump` function,
    with `prune` set to `True`. Since the `_inputs` method already converts both
    `ProcessBuilder` and `ProcessBuilderNamespace` into a standard dictionary, this branch
    can be removed from the `PrettyEncoder.default` method.

--- src/aiida/engine/processes/builder.py
+++ src/aiida/engine/processes/builder.py
@@ -29,8 +29,6 @@ class PrettyEncoder(json.JSONEncoder):
     """JSON encoder for returning a pretty representation of an AiiDA ``ProcessBuilder``."""
 
     def default(self, o):
-        if isinstance(o, (ProcessBuilder, ProcessBuilderNamespace)):
-            return dict(o)
         if isinstance(o, Dict):
             return o.get_dict()
         if isinstance(o, BaseType):
@@ -245,5 +243,5 @@ class ProcessBuilder(ProcessBuilderNamespace):
 
         return p.text(
             f'Process class: {self._process_class.__name__}\n'
-            f'Inputs:\n{yaml.safe_dump(json.JSONDecoder().decode(PrettyEncoder().encode(self)))}'
+            f'Inputs:\n{yaml.safe_dump(json.JSONDecoder().decode(PrettyEncoder().encode(self._inputs(prune=True))))}'
         )

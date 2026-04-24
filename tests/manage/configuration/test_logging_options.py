bebbfe364aa0ec4b669a75e1134077ba9a1d5a7d
Alexander Goscinski <alex.go@posteo.de>
2026-04-24 10:26:55 +0200

    ✨ Add routing log options and handler-level filters (#7322)
    Add `logging.terminal_loglevel` and `logging.logfile_loglevel`
    to `ProfileOptionsSchema`. Wire `terminal_loglevel` as handler
    level on `console` and `cli` handlers. Replace hardcoded `DEBUG`
    in the daemon file handler with `logfile_loglevel`. Extend
    `CLI_LOG_LEVEL` to also override the `cli` handler level so
    `verdi --verbosity` still works.

--- tests/manage/configuration/test_logging_options.py
+++ tests/manage/configuration/test_logging_options.py
@@ -9,13 +9,57 @@
 """Tests for the logging-related configuration options added by the logging redesign.
 
 Tests cover:
-- Existing per-logger options marked as advanced
-- ``Option.advanced`` property
+- Phase 1a: New ``logging.terminal_loglevel`` and ``logging.logfile_loglevel`` options
+- Phase 1b: Existing per-logger options marked as advanced
+- Phase 1c: ``Option.advanced`` property
 """
 
 import pytest
 
 from aiida.manage.configuration.options import Option, get_option, get_option_names
+
+
+@pytest.mark.presto
+class TestNewLoggingOptions:
+    """Tests for the two new routing options."""
+
+    def test_terminal_loglevel_exists(self):
+        """The ``logging.terminal_loglevel`` option should exist."""
+        option = get_option('logging.terminal_loglevel')
+        assert isinstance(option, Option)
+
+    def test_terminal_loglevel_default(self):
+        """The ``logging.terminal_loglevel`` option should default to ``REPORT``."""
+        option = get_option('logging.terminal_loglevel')
+        assert option.default == 'REPORT'
+
+    def test_logfile_loglevel_exists(self):
+        """The ``logging.logfile_loglevel`` option should exist."""
+        option = get_option('logging.logfile_loglevel')
+        assert isinstance(option, Option)
+
+    def test_logfile_loglevel_default(self):
+        """The ``logging.logfile_loglevel`` option should default to ``INFO``."""
+        option = get_option('logging.logfile_loglevel')
+        assert option.default == 'INFO'
+
+    def test_new_options_are_not_advanced(self):
+        """The new routing options should NOT be marked as advanced."""
+        for name in ('logging.terminal_loglevel', 'logging.logfile_loglevel'):
+            option = get_option(name)
+            assert option.advanced is False, f'{name} should not be advanced'
+
+    def test_new_options_are_not_global_only(self):
+        """The new routing options should be settable per-profile."""
+        for name in ('logging.terminal_loglevel', 'logging.logfile_loglevel'):
+            option = get_option(name)
+            assert option.global_only is False, f'{name} should not be global_only'
+
+    def test_new_options_in_option_names(self):
+        """The new options should appear in the list of all option names."""
+        names = get_option_names()
+        assert 'logging.terminal_loglevel' in names
+        assert 'logging.logfile_loglevel' in names
 
 
 @pytest.mark.presto

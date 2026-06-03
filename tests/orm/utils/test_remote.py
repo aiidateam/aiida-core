02c1d752a4be1a1f94b52a000dd21c50019756be
So-coder-ai <narainshiv12@gmail.com>
2026-06-03 15:13:18 +0530

    Remove unnecessary warning assertion comma

--- tests/orm/utils/test_remote.py
+++ tests/orm/utils/test_remote.py
@@ -58,5 +58,5 @@ def test_clean_mapping_remote_paths_skips_unconfigured_computer(tmp_path, monkey
     folder_configured._clean.assert_called_once()
 
     assert warnings == [
-        f'Skipping 1 remote folders on `{unconfigured.label}`: ' f'computer is not configured for user `{user.email}`',
+        f'Skipping 1 remote folders on `{unconfigured.label}`: ' f'computer is not configured for user `{user.email}`'
     ]

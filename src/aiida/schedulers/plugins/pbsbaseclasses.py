ee80dc2193585d1b23231275c6739c0bb63b0c15
So-coder-ai <narainshiv12@gmail.com>
2026-06-17 14:49:40 +0530

    Handle multiline values in PBS qstat output

--- src/aiida/schedulers/plugins/pbsbaseclasses.py
+++ src/aiida/schedulers/plugins/pbsbaseclasses.py
@@ -386,8 +386,13 @@ class PbsBaseClass(BashCliScheduler):
                     # _LOGGER.warning("I found some text before the "
                     # "first job: {}".format(l))
                 elif line.startswith(' '):
-                    # If it starts with a space, it is a new field
-                    jobdata_raw[-1]['lines'].append(line)
+                    # If it starts with a space, it is a new field. However, multi-line values such as bash
+                    # functions in `Variable_List` may contain space-indented lines without an equals sign.
+                    if '=' in line or not jobdata_raw[-1]['lines']:
+                        jobdata_raw[-1]['lines'].append(line)
+                    else:
+                        jobdata_raw[-1]['lines'][-1] += f'\n{line}'
+                        jobdata_raw[-1]['warning_lines_idx'].append(len(jobdata_raw[-1]['lines']) - 1)
                 elif line.startswith('\t'):
                     # If a line starts with a TAB,
                     # I append to the previous string

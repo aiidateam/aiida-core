79ed4ad171cdd228da6475f763cbf24134ca85b2
Edan Bainglass <edan.bainglass@gmail.com>
2026-07-18 16:28:13 +0200

    Match `UUID` as `QbStrField`

--- src/aiida/orm/fields.py
+++ src/aiida/orm/fields.py
@@ -17,6 +17,7 @@ from copy import deepcopy
 from functools import singledispatchmethod
 from pprint import pformat
 from types import UnionType
+from uuid import UUID
 
 from aiida.common.lang import isidentifier
 from aiida.common.warnings import warn_deprecation
@@ -504,7 +505,7 @@ def add_field(
         return QbBoolField(**kwargs)
     elif root_type in (list, tuple, Sequence):
         return QbArrayField(**kwargs)
-    elif root_type in (str, t.Literal):
+    elif root_type in (str, t.Literal, UUID):
         return QbStrField(**kwargs)
     elif root_type is dict:
         return QbDictField(**kwargs)

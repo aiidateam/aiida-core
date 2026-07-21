5b6ba391eb5d846ad0c4e922b8b4212802c96145
Alexander Goscinski <alex.go@posteo.de>
2026-07-21 08:04:31 +0200

    ⬆️ Bump importlib-metadata to 9.0 (#7465)
    With the drop of Python 3.9 the package can be upgraded to solve some
    type hint errors. The upgraded package has official support for Python
    3.13.

--- src/aiida/cmdline/params/types/plugin.py
+++ src/aiida/cmdline/params/types/plugin.py
@@ -169,7 +169,7 @@ class PluginParamType(EntryPointType):
             entry_point = entry_points_by_name.get(p)
             if entry_point is not None:
                 try:
-                    plugin = entry_point.load()  # type: ignore[no-untyped-call]
+                    plugin = entry_point.load()
                     if plugin.__doc__:
                         # extract the initial/summary paragraph (before double newline)
                         doc_summary = plugin.__doc__.strip().split('\n\n')[0]
@@ -260,7 +260,7 @@ class PluginParamType(EntryPointType):
 
         if self.load:
             try:
-                return entry_point.load()  # type: ignore[no-untyped-call]
+                return entry_point.load()
             except exceptions.LoadingEntryPointError as exception:
                 raise click.BadParameter(str(exception))
         else:

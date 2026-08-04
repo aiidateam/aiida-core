3aad2e4ba46a3e5c2d8c0a91c902db6804bedbbc
Edan Bainglass <edan.bainglass@gmail.com>
2026-08-04 11:31:53 +0200

    Keep `arbitrary_types_allowed` false on `CliModel`

--- src/aiida/orm/nodes/data/code/abstract.py
+++ src/aiida/orm/nodes/data/code/abstract.py
@@ -220,7 +220,7 @@ class AbstractCode(Data, metaclass=abc.ABCMeta):
             type[OrmModel],
             pdt.create_model(
                 'CliModel',
-                __config__=deepcopy(cls.ConstructorArgsModel.model_config) | {'arbitrary_types_allowed': True},
+                __config__=deepcopy(cls.ConstructorArgsModel.model_config),
                 __base__=OrmModel,
                 __module__=cls.__module__,
                 __qualname__=f'{cls.__name__}.CliModel',

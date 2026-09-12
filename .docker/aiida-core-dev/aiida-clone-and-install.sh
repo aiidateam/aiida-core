46710cbf697cab59b2e90bdee273eb1063affed1
Alexander Goscinski <alex.go@posteo.de>
2026-09-12 07:03:22 +0200

    Remove legacy tox configuration
    Drop the unreferenced [tool.tox] legacy_tox_ini block, whose envs
    pointed at nonexistent requirements files, and update the molecule
    README, dev Docker install script, and .gitignore accordingly.

--- .docker/aiida-core-dev/aiida-clone-and-install.sh
+++ .docker/aiida-core-dev/aiida-clone-and-install.sh
@@ -9,4 +9,4 @@ if [ ! -d "$REPO_PATH" ]; then
     git clone https://github.com/aiidateam/aiida-core.git --origin upstream $REPO_PATH
 fi
 
-pip install --user -e "$REPO_PATH/[pre-commit,atomic_tools,docs,rest,tests,tui]" tox
+pip install --user -e "$REPO_PATH/[pre-commit,atomic_tools,docs,rest,tests,tui]"

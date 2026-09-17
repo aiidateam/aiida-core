1e23a9ea34227cff0fb307d146b293fb9ae840da
Alexander Goscinski <alex.go@posteo.de>
2026-09-17 06:47:21 +0200

    ♻️ Rename QueryBuilder fields module to qb_fields
    Rename the ORM QueryBuilder field module and update its internal
    imports, type annotations, and tests to use `qb_fields`. This
    distinguishes QueryBuilder field definitions from ORM model fields while
    preserving the existing field lookup behaviour.

--- src/aiida/orm/entities.py
+++ src/aiida/orm/entities.py
@@ -35,8 +35,8 @@ from aiida.common.lang import call_with_super_check, classproperty, super_check,
 from aiida.common.pydantic import get_metadata
 from aiida.common.warnings import warn_deprecation
 from aiida.manage import get_manager
-from aiida.orm.fields import QbFields, add_field
 from aiida.orm.pydantic import OrmFieldsAsModelDump, OrmMetadataField, OrmModel
+from aiida.orm.qb_fields import QbFields, add_field
 
 if TYPE_CHECKING:
     from aiida.orm.implementation import BackendEntity, StorageBackend
